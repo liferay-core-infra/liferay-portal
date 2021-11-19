7e9c911e797248f71610bdf1cb6fe93b2bfbd0e6
hai.yu <8219754@liferay.com>
2021-11-19 11:56:39 +0800

    LPS-83930 Add log output.For the current case, because its recurses deeply, the last returned the error info will be replaced with the second last. We can't see the error in UI. So add log output.

--- util-taglib/src/com/liferay/taglib/portletext/RuntimeTag.java
+++ util-taglib/src/com/liferay/taglib/portletext/RuntimeTag.java
@@ -285,6 +285,10 @@ public class RuntimeTag extends TagSupport implements DirectTag {
 
 				PortalIncludeUtil.include(pageContext, _ERROR_PAGE);
 
+				if (_log.isWarnEnabled()) {
+					_log.warn(errorMessage);
+				}
+
 				return;
 			}
 

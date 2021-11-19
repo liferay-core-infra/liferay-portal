ff6b19e9d66a80c8756d47d9957cae8924b04eab
hai.yu <8219754@liferay.com>
2021-11-19 11:48:54 +0800

    LPS-83930 Please see PortalIncludeUtil.include() method, pageContext.getRequest() is different from the param httpServletRequest in RuntimeTag.doTag(). Set the value to right request. Otherwise, html\taglib\portlet\runtime\error.jsp can't get the attribute "liferay-portlet:runtime:errorMessage" value. Please refer to LPS-33201 to do the test.

--- util-taglib/src/com/liferay/taglib/portletext/RuntimeTag.java
+++ util-taglib/src/com/liferay/taglib/portletext/RuntimeTag.java
@@ -279,8 +279,9 @@ public class RuntimeTag extends TagSupport implements DirectTag {
 			}
 
 			if (displayError) {
-				httpServletRequest.setAttribute(
-					"liferay-portlet:runtime:errorMessage", errorMessage);
+				pageContext.setAttribute(
+					"liferay-portlet:runtime:errorMessage", errorMessage,
+					PageContext.REQUEST_SCOPE);
 
 				PortalIncludeUtil.include(pageContext, _ERROR_PAGE);
 

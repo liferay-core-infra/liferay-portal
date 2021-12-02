baa55b9692a168507f75f6043ae41ac0bd594a7b
Samuel Tran <samuel.tran@liferay.com>
2021-12-02 12:53:11 -0800

    LPS-136334 Regen buildService

--- modules/apps/view-count/view-count-api/src/main/java/com/liferay/view/count/service/ViewCountEntryLocalServiceWrapper.java
+++ modules/apps/view-count/view-count-api/src/main/java/com/liferay/view/count/service/ViewCountEntryLocalServiceWrapper.java
@@ -331,6 +331,11 @@ public class ViewCountEntryLocalServiceWrapper
 	}
 
 	@Override
+	public com.liferay.petra.sql.dsl.Table<?> getViewCountEntryTable() {
+		return _viewCountEntryLocalService.getViewCountEntryTable();
+	}
+
+	@Override
 	public void incrementViewCount(
 		long companyId, long classNameId, long classPK, int increment) {
 

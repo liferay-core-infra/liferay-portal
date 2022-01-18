cce2b72a791c375d3119fda319a6b5d57610d391
Jiefeng Wu <jeffwu0724@gmail.com>
2022-01-18 13:25:08 -0800

    LPS-145600 - remove two unused properties "multi.value.map.". It was finally deleted based on commit d7c844a011c0ea2dc36149c3b486ef6f906cf73f, see LPS-26306

--- portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
@@ -1787,6 +1787,8 @@ public class VerifyProperties extends VerifyProcess {
 		"module.framework.register.liferay.services", "msn.login",
 		"msn.password", "multicast.group.address[\"hibernate\"]",
 		"multicast.group.port[\"hibernate\"]",
+		"multi.value.map.com.liferay.portal.convert.ConvertPermissionAlgorithm.convertResourcePermission",
+		"multi.value.map.com.liferay.portal.convert.ConvertPermissionAlgorithm.convertRoles",
 		"net.sf.ehcache.configurationResourceName",
 		"net.sf.ehcache.configurationResourceName.peerProviderProperties",
 		"openoffice.server.enabled", "openoffice.server.host",

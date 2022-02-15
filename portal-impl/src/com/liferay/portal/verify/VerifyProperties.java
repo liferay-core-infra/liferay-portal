35b2afcdbe835c6dbcb4347b1515dd3006447ffc
Jiefeng Wu <jeffwu0724@gmail.com>
2022-02-15 13:32:08 -0800

    LPS-145769 - removed unused properties "browser.compatibility.ie.versions". Its usage was deleted based on commit 21b3463e0d51f65cd7c9fe9aef5869db7a9dd7aa, see LPS-121725.

--- portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
@@ -1571,6 +1571,7 @@ public class VerifyProperties extends VerifyProcess {
 		"auto.deploy.websphere.wsadmin.properties.file.name",
 		"auto.deploy.wildfly.dest.dir", "auto.deploy.wildfly.prefix",
 		"blogs.image.small.max.size", "breadcrumb.display.style.options",
+		"browser.compatibility.ie.versions",
 		"buffered.increment.parallel.queue.size",
 		"buffered.increment.serial.queue.size",
 		"cache.clear.on.context.initialization", "captcha.max.challenges",

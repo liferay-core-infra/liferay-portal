83f97b555ad4576a312c2ce0e332f245325897c2
Jorge Díaz <jorge.diaz@liferay.com>
2022-03-14 16:54:24 +0100

    LPS-145396 Fix false positives in tests, replacing *LocalServiceBaseImpl delete method calls with *LocalServiceImpl ones

--- modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/util/test/CPDefinitionHelperTest.java
+++ modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/util/test/CPDefinitionHelperTest.java
@@ -213,7 +213,7 @@ public class CPDefinitionHelperTest {
 		Assert.assertTrue(
 			actualCPDefinitionIds.containsAll(cpDefinitionIdsList));
 
-		AssetCategoryLocalServiceUtil.deleteAssetCategory(
+		AssetCategoryLocalServiceUtil.deleteCategory(
 			assetCategory.getCategoryId());
 	}
 

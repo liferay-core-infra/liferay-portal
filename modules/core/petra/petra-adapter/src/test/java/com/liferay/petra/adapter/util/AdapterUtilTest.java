7dc11f384ce7531579ca2fbedd46ce8feb533a57
Julius Lee <meng-huan.lee@liferay.com>
2022-03-25 18:26:44 -0700

    LPS-149460 Rewrite unit test and add code coverage

--- modules/core/petra/petra-adapter/src/test/java/com/liferay/petra/adapter/util/AdapterUtilTest.java
+++ modules/core/petra/petra-adapter/src/test/java/com/liferay/petra/adapter/util/AdapterUtilTest.java
@@ -15,11 +15,17 @@
 package com.liferay.petra.adapter.util;
 
 import com.liferay.petra.reflect.ProxyUtil;
-import com.liferay.portal.kernel.model.ModelWrapper;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.CodeCoverageAssertor;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
 
 import org.junit.Assert;
@@ -34,104 +40,215 @@ public class AdapterUtilTest {
 
 	@ClassRule
 	@Rule
-	public static final LiferayUnitTestRule liferayUnitTestRule =
-		LiferayUnitTestRule.INSTANCE;
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			CodeCoverageAssertor.INSTANCE, LiferayUnitTestRule.INSTANCE);
 
 	@Test
-	public void testAdapt() {
-		Assert.assertEquals(
-			AdapterUtil.adapt(
-				_testInterfaceModuleToKernelProxyProviderFunction,
-				new TestClass(1)),
-			new TestClass(1));
-		Assert.assertEquals(
-			AdapterUtil.adapt(
-				_testInterfaceModuleToKernelProxyProviderFunction,
-				new TestClass(1)),
-			AdapterUtil.adapt(
-				_testInterfaceModuleToKernelProxyProviderFunction,
-				new TestClass(1)));
+	public void testAdaptWithListOfDelegateObjects() {
+		List<A> aList = new ArrayList<>();
 
-		TestInterface proxyObject = AdapterUtil.adapt(
-			_testInterfaceModuleToKernelProxyProviderFunction,
-			new TestClass(1));
+		aList.add(new AImpl(_TEST_OBJECT_ID_ONE));
+		aList.add(new AImpl(_TEST_OBJECT_ID_TWO));
 
-		Assert.assertEquals(0, proxyObject.compareTo(new TestClass(1)));
-		Assert.assertEquals(-1, proxyObject.compareTo(new TestClass(2)));
-		Assert.assertEquals(1, proxyObject.compareTo(new TestClass(0)));
+		List<B> bList = AdapterUtil.adapt(_testProxyFunction(B.class), aList);
 
-		Assert.assertEquals(
-			0,
-			proxyObject.compareTo(
-				AdapterUtil.adapt(
-					_testInterfaceModuleToKernelProxyProviderFunction,
-					new TestClass(1))));
-		Assert.assertEquals(
-			-1,
-			proxyObject.compareTo(
-				AdapterUtil.adapt(
-					_testInterfaceModuleToKernelProxyProviderFunction,
-					new TestClass(2))));
-		Assert.assertEquals(
-			1,
-			proxyObject.compareTo(
-				AdapterUtil.adapt(
-					_testInterfaceModuleToKernelProxyProviderFunction,
-					new TestClass(0))));
+		B b1 = bList.get(0);
+		B b2 = bList.get(1);
+
+		Assert.assertEquals(_TEST_OBJECT_ID_ONE, b1.getId());
+
+		Assert.assertEquals(_TEST_OBJECT_ID_TWO, b2.getId());
 	}
 
-	private static final Function<InvocationHandler, TestInterface>
-		_testInterfaceModuleToKernelProxyProviderFunction =
-			ProxyUtil.getProxyProviderFunction(
-				TestInterface.class, ModelWrapper.class);
+	@Test
+	public void testAdaptWithNullObjectAndNullList() {
 
-	private class TestClass implements TestInterface {
+		// Test for Null Object and Null List
 
-		@Override
-		public int compareTo(TestInterface testInterface) {
-			if (testInterface instanceof TestClass) {
-				TestClass testClass = (TestClass)testInterface;
+		Assert.assertNull(AdapterUtil.adapt(_testProxyFunction(A.class), null));
 
-				return _id - testClass._id;
-			}
+		List<A> aNullList = new ArrayList<>();
 
-			throw new IllegalArgumentException(
-				"Unable to compare with " + testInterface);
+		aNullList.add(null);
+		aNullList.add(null);
+
+		List<B> bList = AdapterUtil.adapt(
+			_testProxyFunction(B.class), aNullList);
+
+		for (B b : bList) {
+			Assert.assertNull(b);
+		}
+	}
+
+	@Test
+	public void testAdaptWithSingleDelegatedObject() {
+		A a1 = new AImpl(_TEST_OBJECT_ID_ONE);
+
+		A a2 = new AImpl(_TEST_OBJECT_ID_TWO);
+
+		B b2 = AdapterUtil.adapt(_testProxyFunction(B.class), a2);
+
+		B b1 = AdapterUtil.adapt(_testProxyFunction(B.class), a1);
+
+		Assert.assertEquals(_TEST_OBJECT_ID_ONE, b1.getId());
+
+		Assert.assertEquals(a1.getId(), b1.getId());
+
+		Assert.assertNotEquals(b1.getId(), b2.getId());
+	}
+
+	@Test
+	public void testConstructor() {
+		new AdapterUtil();
+	}
+
+	@Test
+	public void testGetDelegateModel() {
+		A a1 = new AImpl(_TEST_OBJECT_ID_ONE);
+
+		B b1 = AdapterUtil.adapt(
+			_testProxyFunction(B.class, TestModelWrapper.class), a1);
+
+		Assert.assertEquals(
+			ReflectionTestUtil.invoke(b1, "getWrappedModel", null), a1);
+	}
+
+	@Test
+	public void testMethodCallWithDifferentParameters() {
+		Object proxyWithTestInvocationHandler = ProxyUtil.newProxyInstance(
+			A.class.getClassLoader(), new Class<?>[] {A.class},
+			new TestInvocationHandler(new AImpl(_TEST_OBJECT_ID_ONE)));
+
+		A proxyWithDelegationHandler = AdapterUtil.adapt(
+			_testProxyFunction(A.class), new AImpl(_TEST_OBJECT_ID_ONE));
+
+		// pass in a proxy instance with TestInvocationHandler
+		// should return proxyWithTestInvocationHandler id
+
+		Assert.assertEquals(
+			_TEST_OBJECT_ID_ONE,
+			proxyWithDelegationHandler.testWithDifferentParameterType(
+				proxyWithTestInvocationHandler));
+
+		// pass in a proxy instance with AdapterUtil.DelegateInvocationHandler
+		// should return proxyWithDelegationHandler id
+
+		Assert.assertEquals(
+			_TEST_OBJECT_ID_ONE,
+			proxyWithDelegationHandler.testWithDifferentParameterType(
+				proxyWithDelegationHandler));
+
+		// _TEST_OBJECT_ID_TWO is a String, should return null
+
+		Assert.assertNull(
+			proxyWithDelegationHandler.testWithDifferentParameterType(
+				_TEST_OBJECT_ID_TWO));
+	}
+
+	private <T> Function<InvocationHandler, T> _testProxyFunction(
+		Class<?>... interfaceClasses) {
+
+		Class<?> proxyClass = ProxyUtil.getProxyClass(
+			interfaceClasses[0].getClassLoader(), interfaceClasses);
+
+		try {
+			Constructor<T> constructor =
+				(Constructor<T>)proxyClass.getConstructor(
+					InvocationHandler.class);
+
+			return invocationHandler -> {
+				try {
+					return constructor.newInstance(invocationHandler);
+				}
+				catch (ReflectiveOperationException
+							reflectiveOperationException) {
+
+					throw new InternalError(reflectiveOperationException);
+				}
+			};
+		}
+		catch (NoSuchMethodException noSuchMethodException) {
+			noSuchMethodException.printStackTrace();
 		}
 
-		@Override
-		public boolean equals(Object object) {
-			if (this == object) {
-				return true;
-			}
+		return null;
+	}
 
-			if (!(object instanceof TestClass)) {
-				return false;
-			}
+	private static final String _TEST_OBJECT_ID_ONE = "OBJECT_1";
 
-			TestClass testClass = (TestClass)object;
+	private static final String _TEST_OBJECT_ID_TWO = "OBJECT_2";
 
-			if (_id == testClass._id) {
-				return true;
-			}
-
-			return false;
-		}
+	private static class AImpl implements A {
 
 		@Override
-		public int hashCode() {
+		public String getId() {
 			return _id;
 		}
 
-		private TestClass(int id) {
+		@Override
+		public void setId(String id) {
 			_id = id;
 		}
 
-		private final int _id;
+		@Override
+		public String testWithDifferentParameterType(Object object) {
+			if (object instanceof A || object instanceof B) {
+				return (String)ReflectionTestUtil.invoke(object, "getId", null);
+			}
+
+			return null;
+		}
+
+		private AImpl(String id) {
+			_id = id;
+		}
+
+		private String _id;
 
 	}
 
-	private interface TestInterface extends Comparable<TestInterface> {
+	private static class TestInvocationHandler implements InvocationHandler {
+
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args)
+			throws ReflectiveOperationException {
+
+			return method.invoke(_delegateObject, args);
+		}
+
+		private TestInvocationHandler(Object delegateObject) {
+			_delegateObject = delegateObject;
+		}
+
+		private final Object _delegateObject;
+
+	}
+
+	private interface A {
+
+		public String getId();
+
+		public void setId(String id);
+
+		public String testWithDifferentParameterType(Object object);
+
+	}
+
+	private interface B {
+
+		public String getId();
+
+		public void setId(String id);
+
+		public String testWithDifferentParameterType(Object object);
+
+	}
+
+	private interface TestModelWrapper<T> {
+
+		public T getWrappedModel();
+
 	}
 
 }

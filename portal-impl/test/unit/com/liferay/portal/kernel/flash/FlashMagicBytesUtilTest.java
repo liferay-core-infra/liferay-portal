50ccb39efce6102aa86a24be11fc247d306505e3
Jiefeng Wu <jeffwu0724@gmail.com>
2022-04-18 14:09:43 -0700

    LPS-151629 - remove deprecated class "FlashMagicBytesUtil"

--- portal-impl/test/unit/com/liferay/portal/kernel/flash/FlashMagicBytesUtilTest.java
+++ portal-impl/test/unit/com/liferay/portal/kernel/flash/FlashMagicBytesUtilTest.java
@@ -16,16 +16,8 @@ package com.liferay.portal.kernel.flash;
 
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-import java.util.Arrays;
-
-import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
-import org.junit.Test;
 
 /**
  * @author Tomas Polesovsky
@@ -36,59 +28,5 @@ public class FlashMagicBytesUtilTest {
 	@Rule
 	public static final LiferayUnitTestRule liferayUnitTestRule =
 		LiferayUnitTestRule.INSTANCE;
-
-	@Test
-	public void testCheckGZIPFlash() throws IOException {
-		test(_CWS, true);
-	}
-
-	@Test
-	public void testCheckLZMAFlash() throws IOException {
-		test(_ZWS, true);
-	}
-
-	@Test
-	public void testCheckUncompressedFlash() throws IOException {
-		test(_FWS, true);
-	}
-
-	@Test
-	public void testNoFlash() throws IOException {
-		for (int i = 0; i < 5; i++) {
-			test(new byte[i], false);
-		}
-	}
-
-	protected void test(byte[] bytes, boolean expectFlash) throws IOException {
-		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(
-			bytes);
-
-		FlashMagicBytesUtil.Result result = FlashMagicBytesUtil.check(
-			byteArrayInputStream);
-
-		Assert.assertEquals(expectFlash, result.isFlash());
-
-		InputStream returnedInputStream = result.getInputStream();
-
-		byte[] buffer = new byte[4096];
-
-		int length = returnedInputStream.read(buffer);
-
-		if (length == _EOF) {
-			Assert.assertEquals(Arrays.toString(bytes), 0, bytes.length);
-
-			return;
-		}
-
-		Assert.assertEquals(bytes.length, length);
-	}
-
-	private static final byte[] _CWS = {0x43, 0x57, 0x53};
-
-	private static final int _EOF = -1;
-
-	private static final byte[] _FWS = {0x46, 0x57, 0x53};
-
-	private static final byte[] _ZWS = {0x5a, 0x57, 0x53};
 
 }

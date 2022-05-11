3857ad59ec9458f870a2b28d15c902d98b0967ca
lily.chi <lily.chi@liferay.com>
2022-05-11 15:19:24 +0800

    LPS-152440 Apply to ImageEditableElementParser

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/internal/parser/ImageEditableElementParser.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/internal/parser/ImageEditableElementParser.java
@@ -22,13 +22,9 @@ import com.liferay.info.type.WebImage;
 import com.liferay.layout.responsive.ViewportSize;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.json.JSONException;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Html;
 import com.liferay.portal.kernel.util.ListUtil;
@@ -177,18 +173,11 @@ public class ImageEditableElementParser implements EditableElementParser {
 
 		long fileEntryId = 0;
 
-		if (JSONUtil.isValid(value)) {
-			try {
-				JSONObject jsonObject = JSONFactoryUtil.createJSONObject(value);
+		JSONObject jsonObject = JSONUtil.getValidJSONObject(value);
 
-				fileEntryId = jsonObject.getLong("fileEntryId");
-				value = jsonObject.getString("url");
-			}
-			catch (JSONException jsonException) {
-				_log.error("Unable to parse JSON value " + value);
-
-				value = StringPool.BLANK;
-			}
+		if (jsonObject != null) {
+			fileEntryId = jsonObject.getLong("fileEntryId");
+			value = jsonObject.getString("url");
 		}
 		else {
 			fileEntryId = configJSONObject.getLong("fileEntryId");
@@ -295,9 +284,6 @@ public class ImageEditableElementParser implements EditableElementParser {
 				imageConfiguration);
 		}
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		ImageEditableElementParser.class);
 
 	private static final Pattern _pattern = Pattern.compile(
 		"\\[resources:(.+?)\\]");

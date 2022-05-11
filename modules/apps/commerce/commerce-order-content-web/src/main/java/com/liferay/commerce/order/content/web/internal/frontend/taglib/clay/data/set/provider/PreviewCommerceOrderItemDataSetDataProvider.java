8bfbc43a81d58baf302d883fe79152c87e77bba7
Julius Lee <meng-huan.lee@liferay.com>
2022-05-11 12:18:17 -0700

    LPS-143632 Remove LanguageUtil usages in modules, part 1f

--- modules/apps/commerce/commerce-order-content-web/src/main/java/com/liferay/commerce/order/content/web/internal/frontend/taglib/clay/data/set/provider/PreviewCommerceOrderItemDataSetDataProvider.java
+++ modules/apps/commerce/commerce-order-content-web/src/main/java/com/liferay/commerce/order/content/web/internal/frontend/taglib/clay/data/set/provider/PreviewCommerceOrderItemDataSetDataProvider.java
@@ -33,7 +33,7 @@ import com.liferay.frontend.taglib.clay.data.Pagination;
 import com.liferay.frontend.taglib.clay.data.set.provider.ClayDataSetDataProvider;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.search.Sort;
@@ -250,7 +250,7 @@ public class PreviewCommerceOrderItemDataSetDataProvider
 		CommerceOrderImporterItem commerceOrderImporterItem, Locale locale) {
 
 		if (commerceOrderImporterItem.isValid()) {
-			return LanguageUtil.get(locale, "ok");
+			return _language.get(locale, "ok");
 		}
 
 		String[] errorMessages = commerceOrderImporterItem.getErrorMessages();
@@ -284,6 +284,9 @@ public class PreviewCommerceOrderItemDataSetDataProvider
 
 	@Reference
 	private CPInstanceLocalService _cpInstanceLocalService;
+
+	@Reference
+	private Language _language;
 
 	@Reference
 	private Portal _portal;

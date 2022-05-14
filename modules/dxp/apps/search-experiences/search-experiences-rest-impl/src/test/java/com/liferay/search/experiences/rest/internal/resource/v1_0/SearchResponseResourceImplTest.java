fb96348b148757a1fe73ff8181eef26ef940ef43
Janis Zhang <janis.zhang@liferay.com>
2022-05-14 10:43:00 +0800

    LPS-153088 Remove deprecated mockito method, use replacement in search-experiences

--- modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/test/java/com/liferay/search/experiences/rest/internal/resource/v1_0/SearchResponseResourceImplTest.java
+++ modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/test/java/com/liferay/search/experiences/rest/internal/resource/v1_0/SearchResponseResourceImplTest.java
@@ -79,7 +79,7 @@ public class SearchResponseResourceImplTest {
 
 	@Before
 	public void setUp() throws Exception {
-		MockitoAnnotations.initMocks(this);
+		MockitoAnnotations.openMocks(this);
 
 		_searchResponseResourceImpl = new SearchResponseResourceImpl();
 

891168b5779af9411582312cd831baa2469eeae1
Janis Zhang <janis.zhang@liferay.com>
2022-05-14 14:35:50 +0800

    LPS-153088 Remove deprecated mockito method, use replacement in dynamic-data-mapping-form-field-type

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/test/java/com/liferay/dynamic/data/mapping/form/field/type/internal/search/location/SearchLocationDDMFormFieldTemplateContextContributorTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/test/java/com/liferay/dynamic/data/mapping/form/field/type/internal/search/location/SearchLocationDDMFormFieldTemplateContextContributorTest.java
@@ -52,7 +52,6 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 
-import org.mockito.Matchers;
 import org.mockito.Mockito;
 
 import org.skyscreamer.jsonassert.JSONAssert;
@@ -137,7 +136,7 @@ public class SearchLocationDDMFormFieldTemplateContextContributorTest {
 
 	private static void _mockGet(String key, String message) {
 		Mockito.when(
-			_language.get(Matchers.any(ResourceBundle.class), Matchers.eq(key))
+			_language.get(Mockito.any(ResourceBundle.class), Mockito.eq(key))
 		).thenReturn(
 			message
 		);

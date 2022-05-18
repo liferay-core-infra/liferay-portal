22cf5b605b4f959efe68e9362e020cb8244a51bc
Dante Wang <dante.wang@liferay.com>
2022-05-18 18:43:19 +0800

    LPS-153305 Resolve property reference like Apache Ant (keep reference if not resolved) and use overload delegate

--- portal-kernel/src/com/liferay/portal/kernel/util/SystemProperties.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/SystemProperties.java
@@ -59,17 +59,21 @@ public class SystemProperties {
 	}
 
 	public static String get(String key) {
-		return _parseProperty(_get(key));
+		return get(key, null);
 	}
 
 	public static String get(String key, String defaultValue) {
-		String value = _parseProperty(_get(key));
+		String value = _properties.get(key);
+
+		if (value == null) {
+			value = System.getProperty(key);
+		}
 
 		if (value == null) {
 			return defaultValue;
 		}
 
-		return value;
+		return _resolveReference(value);
 	}
 
 	public static String[] getArray(String key) {
@@ -89,7 +93,7 @@ public class SystemProperties {
 					key = key.substring(prefix.length());
 				}
 
-				properties.put(key, _parseProperty(entry.getValue()));
+				properties.put(key, _resolveReference(entry.getValue()));
 			}
 		}
 
@@ -215,16 +219,6 @@ public class SystemProperties {
 		_properties.put(key, value);
 	}
 
-	private static String _get(String key) {
-		String value = _properties.get(key);
-
-		if (value == null) {
-			value = System.getProperty(key);
-		}
-
-		return value;
-	}
-
 	private static String _getDefaultLiferayHome() {
 		String defaultLiferayHome = null;
 
@@ -259,44 +253,43 @@ public class SystemProperties {
 		return defaultLiferayHome;
 	}
 
-	private static String _parseProperty(String value) {
-		return _replacePlaceholders(value);
-	}
+	private static String _resolveReference(String value) {
+		int startIndex = 0;
 
-	private static String _replacePlaceholders(String value) {
-		if (value == null) {
-			return value;
-		}
+		StringBundler sb = new StringBundler();
 
-		int startIndex = value.indexOf(StringPool.DOLLAR_AND_OPEN_CURLY_BRACE);
+		while ((startIndex = value.indexOf(
+					StringPool.DOLLAR_AND_OPEN_CURLY_BRACE)) != -1) {
 
-		if (startIndex != -1) {
 			int endIndex = value.indexOf(
 				StringPool.CLOSE_CURLY_BRACE, startIndex);
 
-			if (endIndex != -1) {
-				String placeholderKey = value.substring(
-					startIndex +
-						StringPool.DOLLAR_AND_OPEN_CURLY_BRACE.length(),
-					endIndex);
-
-				String placeholderValue = _get(placeholderKey);
-
-				if (placeholderValue == null) {
-					placeholderValue = StringPool.BLANK;
-				}
-				else {
-					placeholderValue = _replacePlaceholders(placeholderValue);
-				}
-
-				String newValue = StringUtil.replace(
-					value,
-					StringPool.DOLLAR_AND_OPEN_CURLY_BRACE + placeholderKey +
-						StringPool.CLOSE_CURLY_BRACE,
-					placeholderValue, startIndex);
-
-				value = _replacePlaceholders(newValue);
+			if (endIndex == -1) {
+				break;
 			}
+
+			sb.append(value.substring(0, startIndex));
+
+			String placeholderKey = value.substring(
+				startIndex + StringPool.DOLLAR_AND_OPEN_CURLY_BRACE.length(),
+				endIndex);
+
+			String placeholderValue = get(placeholderKey);
+
+			if (placeholderValue == null) {
+				sb.append(value.substring(startIndex, endIndex + 1));
+			}
+			else {
+				sb.append(placeholderValue);
+			}
+
+			value = value.substring(endIndex + 1);
+		}
+
+		if (sb.index() > 0) {
+			sb.append(value);
+
+			return sb.toString();
 		}
 
 		return value;

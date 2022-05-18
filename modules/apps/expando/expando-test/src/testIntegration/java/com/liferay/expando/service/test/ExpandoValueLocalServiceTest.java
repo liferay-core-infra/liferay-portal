dd9188473e13b5330096df290c6761f258b91313
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2022-05-18 11:34:07 +0200

    LPS-134898 Expose how a concurrency can easily affect ExpandoValueLocalServiceImpl.doAddValue since we are modifying two different Model objects (ExpandoValue, ExpandoRow).

--- modules/apps/expando/expando-test/src/testIntegration/java/com/liferay/expando/service/test/ExpandoValueLocalServiceTest.java
+++ modules/apps/expando/expando-test/src/testIntegration/java/com/liferay/expando/service/test/ExpandoValueLocalServiceTest.java
@@ -20,28 +20,39 @@ import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.expando.kernel.exception.ValueDataException;
 import com.liferay.expando.kernel.model.ExpandoColumn;
 import com.liferay.expando.kernel.model.ExpandoColumnConstants;
+import com.liferay.expando.kernel.model.ExpandoRow;
 import com.liferay.expando.kernel.model.ExpandoTable;
 import com.liferay.expando.kernel.model.ExpandoValue;
 import com.liferay.expando.kernel.service.ExpandoColumnLocalServiceUtil;
 import com.liferay.expando.kernel.service.ExpandoRowLocalServiceUtil;
 import com.liferay.expando.kernel.service.ExpandoValueLocalServiceUtil;
+import com.liferay.expando.kernel.service.persistence.ExpandoValuePersistence;
+import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.transaction.TransactionConfig;
+import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portlet.expando.util.test.ExpandoTestUtil;
 
 import java.io.Serializable;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -52,6 +63,11 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
 
 /**
  * @author Marcellus Tavares
@@ -168,6 +184,27 @@ public class ExpandoValueLocalServiceTest {
 		Assert.assertEquals(Arrays.toString(data), 2, data.length);
 		Assert.assertEquals("one", data[0]);
 		Assert.assertEquals("two, three", data[1]);
+	}
+
+	@Test
+	public void testAddValueConcurrently() throws Exception {
+		ExpandoColumn column = ExpandoTestUtil.addColumn(
+			_expandoTable, "Test Column", ExpandoColumnConstants.STRING);
+
+		long classPK = CounterLocalServiceUtil.increment();
+
+		ExpandoTestUtil.addValue(_expandoTable, column, classPK, "value");
+
+		ServiceRegistration<ModelListener<ExpandoValue>> serviceRegistration =
+			_registerExpandoValueModelListener();
+
+		try {
+			ExpandoTestUtil.addValue(
+				_expandoTable, column, classPK, "another value");
+		}
+		finally {
+			serviceRegistration.unregister();
+		}
 	}
 
 	@Test
@@ -309,6 +346,48 @@ public class ExpandoValueLocalServiceTest {
 		Assert.assertEquals("Hi, Joe", enValues[1]);
 	}
 
+	private ServiceRegistration<ModelListener<ExpandoValue>>
+		_registerExpandoValueModelListener() {
+
+		Bundle bundle = FrameworkUtil.getBundle(
+			ExpandoValueLocalServiceTest.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		return bundleContext.registerService(
+			(Class<ModelListener<ExpandoValue>>)(Class<?>)ModelListener.class,
+			new BaseModelListener<ExpandoValue>() {
+
+				@Override
+				public void onAfterUpdate(
+						ExpandoValue originalModel, ExpandoValue model)
+					throws ModelListenerException {
+
+					ExpandoRow expandoRow =
+						ExpandoRowLocalServiceUtil.fetchExpandoRow(
+							model.getRowId());
+
+					expandoRow.setModifiedDate(new Date());
+
+					try {
+						TransactionInvokerUtil.invoke(
+							_REQUIRES_NEW_TRANSACTION_CONFIG,
+							() -> ExpandoRowLocalServiceUtil.updateExpandoRow(
+								expandoRow));
+					}
+					catch (Throwable throwable) {
+						throw new ModelListenerException(throwable);
+					}
+				}
+
+			},
+			new HashMapDictionary<>());
+	}
+
+	private static final TransactionConfig _REQUIRES_NEW_TRANSACTION_CONFIG =
+		TransactionConfig.Factory.create(
+			Propagation.REQUIRES_NEW, new Class<?>[] {Exception.class});
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		ExpandoValueLocalServiceTest.class);
 
@@ -317,6 +396,9 @@ public class ExpandoValueLocalServiceTest {
 
 	@DeleteAfterTestRun
 	private ExpandoTable _expandoTable;
+
+	@Inject
+	private ExpandoValuePersistence _expandoValuePersistence;
 
 	private Locale _frLocale;
 	private Locale _ptLocale;

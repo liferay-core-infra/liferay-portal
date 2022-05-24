d0b03b3e311b57f549519944dd6444ddc8752961
hai.yu <hai.yu@liferay.com>
2022-05-24 15:33:11 +0800

    LPS-154230 CompanyLogServlet only is registered if the property company.log.enabled=true

---
+++ modules/apps/portal/portal-company-log/src/main/java/com/liferay/portal/company/log/internal/servlet/CompanyLogServletGatekeeper.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.company.log.internal.servlet;
+
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
+
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Hai Yu
+ */
+@Component(immediate = true, service = {})
+public class CompanyLogServletGatekeeper {
+
+	@Activate
+	protected void activate(ComponentContext componentContext) {
+		if (GetterUtil.getBoolean(
+				PropsUtil.get(PropsKeys.COMPANY_LOG_ENABLED))) {
+
+			componentContext.enableComponent(CompanyLogServlet.class.getName());
+		}
+	}
+
+}

a5d2301e3dbe01d795c9684370faf688c9beb3ed
Julius Lee <meng-huan.lee@liferay.com>
2022-06-07 11:16:30 -0700

    LPS-152081 Move MailMessageListener to portal-mail and convert to OSGi component MailSenderMessageListener

--- modules/apps/portal/portal-mail/src/main/java/com/liferay/portal/mail/sender/internal/messaging/MailSenderMessageListener.java
+++ modules/apps/portal/portal-mail/src/main/java/com/liferay/portal/mail/sender/internal/messaging/MailSenderMessageListener.java
@@ -12,21 +12,23 @@
  * details.
  */
 
-package com.liferay.mail.messaging;
+package com.liferay.portal.mail.sender.internal.messaging;
 
 import com.liferay.mail.kernel.model.MailMessage;
 import com.liferay.mail.util.HookFactory;
 import com.liferay.petra.mail.MailEngine;
-import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.BaseMessageListener;
 import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageListener;
 import com.liferay.portal.kernel.security.auth.EmailAddressGenerator;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.MethodHandler;
 import com.liferay.portal.kernel.util.PortalRunMode;
 import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.mail.sender.internal.constants.MailSenderMessagesDestinationNames;
 import com.liferay.portal.security.auth.EmailAddressGeneratorFactory;
 import com.liferay.portal.util.PropsValues;
 
@@ -38,88 +40,32 @@ import java.util.Set;
 
 import javax.mail.internet.InternetAddress;
 
+import org.osgi.service.component.annotations.Component;
+
 /**
- * @author Brian Wing Shun Chan
- * @author Wesley Gong
- * @author Zsolt Balogh
+ * @author Adolfo Pérez
+ * @author Julius Lee
  */
-public class MailMessageListener extends BaseMessageListener {
-
-	protected void doMailMessage(MailMessage mailMessage) throws Exception {
-		InternetAddress from = filterInternetAddress(mailMessage.getFrom());
-
-		if (from == null) {
-			if (_log.isWarnEnabled()) {
-				_log.warn("Skipping email because the sender is not specified");
-			}
-
-			return;
-		}
-
-		mailMessage.setFrom(from);
-
-		InternetAddress[] to = filterInternetAddresses(mailMessage.getTo());
-
-		mailMessage.setTo(to);
-
-		InternetAddress[] cc = filterInternetAddresses(mailMessage.getCC());
-
-		mailMessage.setCC(cc);
-
-		InternetAddress[] bcc = filterInternetAddresses(mailMessage.getBCC());
-
-		InternetAddress[] auditTrail = InternetAddress.parse(
-			PropsValues.MAIL_AUDIT_TRAIL);
-
-		if (auditTrail.length > 0) {
-			if (ArrayUtil.isNotEmpty(bcc)) {
-				for (InternetAddress internetAddress : auditTrail) {
-					bcc = ArrayUtil.append(bcc, internetAddress);
-				}
-			}
-			else {
-				bcc = auditTrail;
-			}
-		}
-
-		mailMessage.setBCC(bcc);
-
-		InternetAddress[] bulkAddresses = filterInternetAddresses(
-			mailMessage.getBulkAddresses());
-
-		mailMessage.setBulkAddresses(bulkAddresses);
-
-		InternetAddress[] replyTo = filterInternetAddresses(
-			mailMessage.getReplyTo());
-
-		mailMessage.setReplyTo(replyTo);
-
-		if (ArrayUtil.isNotEmpty(to) || ArrayUtil.isNotEmpty(cc) ||
-			ArrayUtil.isNotEmpty(bcc) || ArrayUtil.isNotEmpty(bulkAddresses)) {
-
-			MailEngine.send(mailMessage);
-		}
-	}
-
-	protected void doMethodHandler(MethodHandler methodHandler)
-		throws Exception {
-
-		methodHandler.invoke(HookFactory.getInstance());
-	}
+@Component(
+	immediate = true,
+	property = "destination.name=" + MailSenderMessagesDestinationNames.MAIL_SENDER_MESSAGES_PROCESSOR,
+	service = MessageListener.class
+)
+public class MailSenderMessageListener extends BaseMessageListener {
 
 	@Override
 	protected void doReceive(Message message) throws Exception {
 		Object payload = message.getPayload();
 
 		if (payload instanceof MailMessage) {
-			doMailMessage((MailMessage)payload);
+			_processMailMessage((MailMessage)payload);
 		}
 		else if (payload instanceof MethodHandler) {
-			doMethodHandler((MethodHandler)payload);
+			_processMethodHandler((MethodHandler)payload);
 		}
 	}
 
-	protected InternetAddress filterInternetAddress(
+	private InternetAddress _filterInternetAddress(
 		InternetAddress internetAddress) {
 
 		if (PortalRunMode.isTestMode()) {
@@ -149,7 +95,7 @@ public class MailMessageListener extends BaseMessageListener {
 		return internetAddress;
 	}
 
-	protected InternetAddress[] filterInternetAddresses(
+	private InternetAddress[] _filterInternetAddresses(
 		InternetAddress[] internetAddresses) {
 
 		if (internetAddresses == null) {
@@ -160,7 +106,7 @@ public class MailMessageListener extends BaseMessageListener {
 			internetAddresses.length);
 
 		for (InternetAddress internetAddress : internetAddresses) {
-			InternetAddress filteredInternetAddress = filterInternetAddress(
+			InternetAddress filteredInternetAddress = _filterInternetAddress(
 				internetAddress);
 
 			if (filteredInternetAddress != null) {
@@ -171,8 +117,70 @@ public class MailMessageListener extends BaseMessageListener {
 		return filteredInternetAddresses.toArray(new InternetAddress[0]);
 	}
 
+	private void _processMailMessage(MailMessage mailMessage) throws Exception {
+		InternetAddress from = _filterInternetAddress(mailMessage.getFrom());
+
+		if (from == null) {
+			if (_log.isWarnEnabled()) {
+				_log.warn("Skipping email because the sender is not specified");
+			}
+
+			return;
+		}
+
+		mailMessage.setFrom(from);
+
+		InternetAddress[] to = _filterInternetAddresses(mailMessage.getTo());
+
+		mailMessage.setTo(to);
+
+		InternetAddress[] cc = _filterInternetAddresses(mailMessage.getCC());
+
+		mailMessage.setCC(cc);
+
+		InternetAddress[] bcc = _filterInternetAddresses(mailMessage.getBCC());
+
+		InternetAddress[] auditTrail = InternetAddress.parse(
+			PropsValues.MAIL_AUDIT_TRAIL);
+
+		if (auditTrail.length > 0) {
+			if (ArrayUtil.isNotEmpty(bcc)) {
+				for (InternetAddress internetAddress : auditTrail) {
+					bcc = ArrayUtil.append(bcc, internetAddress);
+				}
+			}
+			else {
+				bcc = auditTrail;
+			}
+		}
+
+		mailMessage.setBCC(bcc);
+
+		InternetAddress[] bulkAddresses = _filterInternetAddresses(
+			mailMessage.getBulkAddresses());
+
+		mailMessage.setBulkAddresses(bulkAddresses);
+
+		InternetAddress[] replyTo = _filterInternetAddresses(
+			mailMessage.getReplyTo());
+
+		mailMessage.setReplyTo(replyTo);
+
+		if (ArrayUtil.isNotEmpty(to) || ArrayUtil.isNotEmpty(cc) ||
+			ArrayUtil.isNotEmpty(bcc) || ArrayUtil.isNotEmpty(bulkAddresses)) {
+
+			MailEngine.send(mailMessage);
+		}
+	}
+
+	private void _processMethodHandler(MethodHandler methodHandler)
+		throws Exception {
+
+		methodHandler.invoke(HookFactory.getInstance());
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
-		MailMessageListener.class);
+		MailSenderMessageListener.class);
 
 	private static final Set<String> _mailSendBlacklist = new HashSet<>(
 		Arrays.asList(PropsValues.MAIL_SEND_BLACKLIST));

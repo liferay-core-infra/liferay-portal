b47432eff7dc6eeaaf4dc315d559a430faf31543
ricardocousolr <ricardo.couso@liferay.com>
2022-06-09 11:31:54 +0200

    LPS-155624 Correct test because ? in where-clause should not be transformed

--- portal-impl/test/unit/com/liferay/portal/dao/sql/transformer/DB2SQLTransformerLogicTest.java
+++ portal-impl/test/unit/com/liferay/portal/dao/sql/transformer/DB2SQLTransformerLogicTest.java
@@ -83,13 +83,16 @@ public class DB2SQLTransformerLogicTest
 	public void testReplaceQuestionMark() {
 		_testReplaceQuestionMark("select foo from Foo where foo LIKE ?");
 		_testReplaceQuestionMark("select foo, ?, bar, ? from Foo");
-		_testReplaceQuestionMark("select * from Foo where foo = ? And bar = ?");
 		_testReplaceQuestionMark(
 			"select * from Foo where case when foo = ? then ? else ? end");
 		_testReplaceQuestionMark(
 			"select bar, ?, case when foo = ? then ? else ? end as columnA " +
 				"from Foo");
 
+		Assert.assertEquals(
+			"select * from Foo where foo = ? And bar = ?",
+			sqlTransformer.transform(
+				"select * from Foo where foo = ? And bar = ?"));
 		Assert.assertEquals(
 			"select * from Foo where foo = \" ?\"",
 			sqlTransformer.transform("select * from Foo where foo = \" ?\""));

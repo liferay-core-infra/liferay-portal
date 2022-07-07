32d8a03769c298ed1eea97d9ff33c4bf22d60ec2
lily.chi <lily.chi@liferay.com>
2022-07-07 20:16:24 +0800

    LPS-125421 Rename the output folder name to sqlfiles

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/SampleSQLBuilder.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/SampleSQLBuilder.java
@@ -89,7 +89,7 @@ public class SampleSQLBuilder {
 			}
 			else {
 				File outputDir = new File(
-					BenchmarksPropsValues.OUTPUT_DIR, "output");
+					BenchmarksPropsValues.OUTPUT_DIR, "sqlfiles");
 
 				FileUtil.deltree(outputDir);
 

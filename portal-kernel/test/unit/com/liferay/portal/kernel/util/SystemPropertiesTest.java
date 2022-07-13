92adead813a7034979f7347c80d8fa1172407db7
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-07-13 20:40:14 +0800

    LPS-158153 SystemProperties: Add SystemProperties's unit test

---
+++ portal-kernel/test/unit/com/liferay/portal/kernel/util/SystemPropertiesTest.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.util;
+
+import java.util.Properties;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Jiaxu Wei
+ */
+public class SystemPropertiesTest {
+
+	@Test
+	public void testGetArray() {
+		Assert.assertTrue(
+			ArrayUtil.isEmpty(SystemProperties.getArray("test.array.key")));
+
+		SystemProperties.set(
+			"test.array.key", "test.array.value,test.array.value");
+
+		Assert.assertArrayEquals(
+			new String[] {"test.array.value", "test.array.value"},
+			SystemProperties.getArray("test.array.key"));
+	}
+
+	@Test
+	public void testGetProperties() {
+		Assert.assertNull(SystemProperties.get(_TEST_KEY));
+
+		SystemProperties.set(_TEST_KEY, _TEST_VALUE);
+
+		Properties properties = SystemProperties.getProperties();
+
+		Assert.assertEquals(_TEST_VALUE, properties.get(_TEST_KEY));
+	}
+
+	@Test
+	public void testGetSetAndClear() {
+		Assert.assertNull(SystemProperties.get(_TEST_KEY));
+
+		SystemProperties.set(_TEST_KEY, _TEST_VALUE);
+
+		Assert.assertEquals(_TEST_VALUE, SystemProperties.get(_TEST_KEY));
+
+		SystemProperties.clear(_TEST_KEY);
+
+		Assert.assertNull(SystemProperties.get(_TEST_KEY));
+	}
+
+	@Test
+	public void testGetWithDefaultValue() {
+		Assert.assertNull(SystemProperties.get(_TEST_KEY));
+
+		Assert.assertEquals(
+			"defaultValue", SystemProperties.get(_TEST_KEY, "defaultValue"));
+	}
+
+	private static final String _TEST_KEY = "test.key";
+
+	private static final String _TEST_VALUE = "test.value";
+
+}

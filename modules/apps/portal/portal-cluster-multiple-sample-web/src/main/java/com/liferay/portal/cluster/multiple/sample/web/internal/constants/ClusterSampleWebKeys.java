183331dcc54e4d2cf4137b3f301a42f2057e43f2
Janis Zhang <janis.zhang@liferay.com>
2022-07-25 20:08:53 +0800

    LPS-158705 Create ClusterSampleWebKeys and add CLUSTER_SAMPLE_CACHE_ENTRIES

---
+++ modules/apps/portal/portal-cluster-multiple-sample-web/src/main/java/com/liferay/portal/cluster/multiple/sample/web/internal/constants/ClusterSampleWebKeys.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.cluster.multiple.sample.web.internal.constants;
+
+/**
+ * @author Dante Wang
+ */
+public interface ClusterSampleWebKeys {
+
+	public static final String CLUSTER_SAMPLE_CACHE_ENTRIES =
+		"CLUSTER_SAMPLE_CACHE_ENTRIES";
+
+}

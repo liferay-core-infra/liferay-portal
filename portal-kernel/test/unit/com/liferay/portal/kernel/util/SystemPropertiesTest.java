fb80cda41a8b78580ef052719b6227d9d930b7e3
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-07-29 15:10:34 +0800

    LPS-158208 Add unit test

--- portal-kernel/test/unit/com/liferay/portal/kernel/util/SystemPropertiesTest.java
+++ portal-kernel/test/unit/com/liferay/portal/kernel/util/SystemPropertiesTest.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.kernel.util;
 
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 
 import java.util.Map;
@@ -30,7 +31,12 @@ public class SystemPropertiesTest {
 
 	@After
 	public void tearDown() {
-		SystemProperties.clear(_TEST_KEY);
+		Map<String, String> properties = SystemProperties.getProperties(
+			_PREFIX, false);
+
+		for (String propertyKey : properties.keySet()) {
+			SystemProperties.clear(propertyKey);
+		}
 	}
 
 	@Test
@@ -63,24 +69,21 @@ public class SystemPropertiesTest {
 
 	@Test
 	public void testGetPropertiesWithPrefix() {
-		String prefix =
-			SystemPropertiesTest.class.getName() + StringPool.PERIOD;
-
 		Map<String, String> propertiesWithPrefix =
-			SystemProperties.getProperties(prefix, false);
+			SystemProperties.getProperties(_PREFIX, false);
 
 		Assert.assertTrue(propertiesWithPrefix.isEmpty());
 
 		SystemProperties.set(_TEST_KEY, _TEST_VALUE);
 
 		Map<String, String> propertiesWithoutPrefix =
-			SystemProperties.getProperties(prefix, true);
+			SystemProperties.getProperties(_PREFIX, true);
 
 		Assert.assertTrue(propertiesWithoutPrefix.size() == 1);
 		Assert.assertEquals(
 			_TEST_VALUE, propertiesWithoutPrefix.get("test.key"));
 
-		propertiesWithPrefix = SystemProperties.getProperties(prefix, false);
+		propertiesWithPrefix = SystemProperties.getProperties(_PREFIX, false);
 
 		Assert.assertTrue(propertiesWithPrefix.size() == 1);
 		Assert.assertEquals(_TEST_VALUE, propertiesWithPrefix.get(_TEST_KEY));
@@ -111,6 +114,141 @@ public class SystemPropertiesTest {
 
 		Assert.assertEquals(_TEST_VALUE, SystemProperties.get(_TEST_KEY));
 		Assert.assertEquals(_TEST_VALUE, System.getProperty(_TEST_KEY));
+
+		System.clearProperty(_TEST_KEY);
+
+		// Property references other properties via SystemProperties is
+		// also parsed
+
+		SystemProperties.set(_TEST_KEY, _TEST_VALUE);
+
+		Assert.assertEquals(_TEST_VALUE, SystemProperties.get(_TEST_KEY));
+
+		// Simple reference
+
+		String testReferenceKey = _PREFIX + "test.reference.key";
+		String testReferenceValue =
+			StringPool.DOLLAR_AND_OPEN_CURLY_BRACE + _TEST_KEY +
+				StringPool.CLOSE_CURLY_BRACE;
+
+		Assert.assertNull(SystemProperties.get(testReferenceKey));
+
+		SystemProperties.set(testReferenceKey, testReferenceValue);
+
+		Assert.assertEquals(
+			_TEST_VALUE, SystemProperties.get(testReferenceKey));
+
+		// The referenced property does not exist
+
+		String testNoReferenceKey = _PREFIX + "test.no.reference.key";
+		String testNoReferenceValue = "${test.no.key}";
+
+		Assert.assertNull(SystemProperties.get(testNoReferenceKey));
+
+		SystemProperties.set(testNoReferenceKey, testNoReferenceValue);
+
+		Assert.assertEquals(
+			testNoReferenceValue, SystemProperties.get(testNoReferenceKey));
+
+		// The referenced property is to the left of the value
+
+		String rightValue = "rightValue";
+		String testLeftReferenceKey = _PREFIX + "test.left.reference.key";
+		String testLeftReferenceValue = StringBundler.concat(
+			StringPool.DOLLAR_AND_OPEN_CURLY_BRACE, testReferenceKey,
+			StringPool.CLOSE_CURLY_BRACE, rightValue);
+
+		Assert.assertNull(SystemProperties.get(testLeftReferenceKey));
+
+		SystemProperties.set(testLeftReferenceKey, testLeftReferenceValue);
+
+		Assert.assertEquals(
+			_TEST_VALUE + rightValue,
+			SystemProperties.get(testLeftReferenceKey));
+
+		// Blank reference
+
+		String testBlankReferenceKey = _PREFIX + "test.blank.reference.key";
+		String testBlankReferenceValue =
+			StringPool.DOLLAR_AND_OPEN_CURLY_BRACE +
+				StringPool.CLOSE_CURLY_BRACE;
+
+		Assert.assertNull(SystemProperties.get(testBlankReferenceKey));
+
+		SystemProperties.set(testBlankReferenceKey, testBlankReferenceValue);
+
+		Assert.assertEquals(
+			testBlankReferenceValue,
+			SystemProperties.get(testBlankReferenceKey));
+
+		// Value contains a single symbol "}"
+
+		String testRightPartReferenceKey =
+			_PREFIX + "test.right.part.reference.key";
+		String testRightPartReferenceValue = StringBundler.concat(
+			_TEST_KEY, StringPool.CLOSE_CURLY_BRACE,
+			StringPool.DOLLAR_AND_OPEN_CURLY_BRACE, _TEST_KEY,
+			StringPool.CLOSE_CURLY_BRACE);
+
+		Assert.assertNull(SystemProperties.get(testRightPartReferenceKey));
+
+		SystemProperties.set(
+			testRightPartReferenceKey, testRightPartReferenceValue);
+
+		Assert.assertEquals(
+			_TEST_KEY + StringPool.CLOSE_CURLY_BRACE + _TEST_VALUE,
+			SystemProperties.get(testRightPartReferenceKey));
+
+		// Value contains a single symbol "${"
+
+		String testLeftPartReferenceKey =
+			_PREFIX + "test.left.part.reference.key";
+		String testLeftPartReferenceValue = StringBundler.concat(
+			StringPool.DOLLAR_AND_OPEN_CURLY_BRACE, _TEST_KEY,
+			StringPool.DOLLAR_AND_OPEN_CURLY_BRACE, _TEST_KEY,
+			StringPool.CLOSE_CURLY_BRACE);
+
+		Assert.assertNull(SystemProperties.get(testLeftPartReferenceKey));
+
+		SystemProperties.set(
+			testLeftPartReferenceKey, testLeftPartReferenceValue);
+
+		Assert.assertEquals(
+			testLeftPartReferenceValue,
+			SystemProperties.get(testLeftPartReferenceKey));
+
+		// Multiple reference
+
+		String testDoubleReferenceKey = _PREFIX + "test.double.reference.key";
+		String testDoubleReferenceValue = StringBundler.concat(
+			StringPool.DOLLAR_AND_OPEN_CURLY_BRACE, _TEST_KEY,
+			StringPool.CLOSE_CURLY_BRACE,
+			StringPool.DOLLAR_AND_OPEN_CURLY_BRACE, _TEST_KEY,
+			StringPool.CLOSE_CURLY_BRACE);
+
+		Assert.assertNull(SystemProperties.get(testDoubleReferenceKey));
+
+		SystemProperties.set(testDoubleReferenceKey, testDoubleReferenceValue);
+
+		Assert.assertEquals(
+			_TEST_VALUE + _TEST_VALUE,
+			SystemProperties.get(testDoubleReferenceKey));
+
+		// Nested references
+
+		String testNestedReferenceKey = "test.nested.reference.key";
+		String testNestedReferenceValue = StringBundler.concat(
+			StringPool.DOLLAR_AND_OPEN_CURLY_BRACE, _TEST_KEY,
+			StringPool.DOLLAR_AND_OPEN_CURLY_BRACE, _TEST_KEY,
+			StringPool.DOUBLE_CLOSE_CURLY_BRACE);
+
+		Assert.assertNull(SystemProperties.get(testNestedReferenceKey));
+
+		SystemProperties.set(testNestedReferenceKey, testNestedReferenceValue);
+
+		Assert.assertEquals(
+			testNestedReferenceValue,
+			SystemProperties.get(testNestedReferenceKey));
 	}
 
 	@Test
@@ -126,8 +264,10 @@ public class SystemPropertiesTest {
 			_TEST_VALUE, SystemProperties.get(_TEST_KEY, "defaultValue"));
 	}
 
-	private static final String _TEST_KEY =
-		SystemPropertiesTest.class.getName() + ".test.key";
+	private static final String _PREFIX =
+		SystemPropertiesTest.class.getName() + StringPool.PERIOD;
+
+	private static final String _TEST_KEY = _PREFIX + "test.key";
 
 	private static final String _TEST_VALUE = "test.value";
 

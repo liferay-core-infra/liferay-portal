3604a95b0505a52e8e478bbbb5040ee9e4b00e06
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-08-03 19:08:23 +0800

    LPS-158209 SystemProperties: Fix the unit test

--- portal-kernel/test/unit/com/liferay/portal/kernel/util/SystemPropertiesTest.java
+++ portal-kernel/test/unit/com/liferay/portal/kernel/util/SystemPropertiesTest.java
@@ -26,6 +26,7 @@ import java.nio.charset.StandardCharsets;
 
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -59,22 +60,6 @@ public class SystemPropertiesTest {
 	}
 
 	@Test
-	public void testGetProperties() {
-		Properties properties1 = SystemProperties.getProperties();
-
-		Assert.assertNull(properties1.getProperty(_TEST_KEY));
-
-		SystemProperties.set(_TEST_KEY, _TEST_VALUE);
-
-		Properties properties2 = SystemProperties.getProperties();
-
-		Assert.assertEquals(properties1.size() + 1, properties2.size());
-
-		Assert.assertNull(properties1.getProperty(_TEST_KEY));
-		Assert.assertEquals(_TEST_VALUE, properties2.get(_TEST_KEY));
-	}
-
-	@Test
 	public void testGetPropertiesWithPrefix() {
 		Map<String, String> propertiesWithPrefix =
 			SystemProperties.getProperties(_PREFIX, false);
@@ -99,6 +84,19 @@ public class SystemPropertiesTest {
 				_TEST_KEY, _TEST_VALUE
 			).build(),
 			propertiesWithPrefix);
+	}
+
+	@Test
+	public void testGetPropertyNames() {
+		Set<String> propertyNames = SystemProperties.getPropertyNames();
+
+		Assert.assertFalse(propertyNames.contains(_TEST_KEY));
+
+		SystemProperties.set(_TEST_KEY, _TEST_VALUE);
+
+		propertyNames = SystemProperties.getPropertyNames();
+
+		Assert.assertTrue(propertyNames.contains(_TEST_KEY));
 	}
 
 	@Test

c44d0fc4a79c4dd8ae437a600fd5d0e65b364026
Julius Lee <meng-huan.lee@liferay.com>
2022-08-03 14:42:34 -0700

    LPS-158489 Fix integration test for MailEngine

--- modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/mail/test/MailEngineTest.java
+++ modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/mail/test/MailEngineTest.java
@@ -166,10 +166,14 @@ public class MailEngineTest {
 				PropsKeys.DATA_LIMIT_MAIL_MESSAGE_MAX_PERIOD,
 				String.valueOf(maxMailMessagePeriod));
 
+			Class<?> innerClass = MailEngine.class.getDeclaredClasses()[0];
+
 			ClassLoader classLoader = new ReloadURLClassLoader(
-				MailEngine.class);
+				MailEngine.class, innerClass);
 
 			Class<?> clazz = classLoader.loadClass(MailEngine.class.getName());
+
+			classLoader.loadClass(innerClass.getName());
 
 			_sendMethod = ReflectionUtil.getDeclaredMethod(
 				clazz, "send", MailMessage.class);

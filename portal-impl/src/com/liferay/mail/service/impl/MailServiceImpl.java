1453a1bd805d774b8614dd5b0e744557485877ee
Julius Lee <meng-huan.lee@liferay.com>
2022-08-05 11:20:24 -0700

    LPS-148669 Apply to usages

--- portal-impl/src/com/liferay/mail/service/impl/MailServiceImpl.java
+++ portal-impl/src/com/liferay/mail/service/impl/MailServiceImpl.java
@@ -349,7 +349,7 @@ public class MailServiceImpl implements IdentifiableOSGiService, MailService {
 		return session;
 	}
 
-	public static Session getSession(Account account) {
+	public Session getSession(Account account) {
 		Session session = Session.getInstance(_getProperties(account));
 
 		if (_log.isDebugEnabled()) {

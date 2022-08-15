e12acb872ddc16bb7a093630ce67c5b776317727
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-08-15 15:41:02 +0800

    LPS-160434 Apply, directly reference Portlet and return via getPortlet()

--- modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/application/list/PublicationsPanelApp.java
+++ modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/application/list/PublicationsPanelApp.java
@@ -50,6 +50,11 @@ import org.osgi.service.component.annotations.Reference;
 public class PublicationsPanelApp extends BasePanelApp {
 
 	@Override
+	public Portlet getPortlet() {
+		return _portlet;
+	}
+
+	@Override
 	public String getPortletId() {
 		return CTPortletKeys.PUBLICATIONS;
 	}
@@ -92,20 +97,16 @@ public class PublicationsPanelApp extends BasePanelApp {
 		return false;
 	}
 
-	@Override
-	@Reference(
-		target = "(javax.portlet.name=" + CTPortletKeys.PUBLICATIONS + ")",
-		unbind = "-"
-	)
-	public void setPortlet(Portlet portlet) {
-		super.setPortlet(portlet);
-	}
-
 	@Reference
 	private CTPreferencesLocalService _ctPreferencesLocalService;
 
 	@Reference
 	private CTSettingsConfigurationHelper _ctSettingsConfigurationHelper;
+
+	@Reference(
+		target = "(javax.portlet.name=" + CTPortletKeys.PUBLICATIONS + ")"
+	)
+	private Portlet _portlet;
 
 	@Reference
 	private PortletPermission _portletPermission;

c48577a5657819373820b2382d2e24907c121fbb
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-08-22 10:39:01 +0800

    LPS-160434 Apply in subclasses that can not use annotation Reference to assign values to variable portlet and assign a value to the variable _portlet via the constructor

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/entries/application/list/ObjectEntriesPanelApp.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/entries/application/list/ObjectEntriesPanelApp.java
@@ -20,6 +20,7 @@ import com.liferay.petra.portlet.url.builder.PortletURLBuilder;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 
 import java.util.Locale;
@@ -34,8 +35,11 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class ObjectEntriesPanelApp extends BasePanelApp {
 
-	public ObjectEntriesPanelApp(ObjectDefinition objectDefinition) {
+	public ObjectEntriesPanelApp(
+		ObjectDefinition objectDefinition, Portlet portlet) {
+
 		_objectDefinition = objectDefinition;
+		_portlet = portlet;
 	}
 
 	@Override
@@ -47,6 +51,11 @@ public class ObjectEntriesPanelApp extends BasePanelApp {
 	@Override
 	public String getLabel(Locale locale) {
 		return _objectDefinition.getPluralLabel(locale);
+	}
+
+	@Override
+	public Portlet getPortlet() {
+		return _portlet;
 	}
 
 	@Override
@@ -79,5 +88,6 @@ public class ObjectEntriesPanelApp extends BasePanelApp {
 	}
 
 	private final ObjectDefinition _objectDefinition;
+	private final Portlet _portlet;
 
 }

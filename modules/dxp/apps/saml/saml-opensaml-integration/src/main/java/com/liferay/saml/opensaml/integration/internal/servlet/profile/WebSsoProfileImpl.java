ca1651c62c2ee4bb096864f5ef43c4843cfe152d
Lily.Chi <lily.chi@liferay.com>
2022-09-15 17:06:00 +0800

    LPS-163054 Remove the method setPortal(Portal) from BaseProfile's subclasses since BaseProfile's protected filed portal has been added with annotation @Reference

--- modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/servlet/profile/WebSsoProfileImpl.java
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/servlet/profile/WebSsoProfileImpl.java
@@ -30,7 +30,6 @@ import com.liferay.portal.kernel.servlet.HttpHeaders;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.URLCodec;
 import com.liferay.portal.kernel.util.Validator;
@@ -693,11 +692,6 @@ public class WebSsoProfileImpl extends BaseProfile implements WebSsoProfile {
 	@Reference(unbind = "-")
 	protected void setMetadataManager(MetadataManager metadataManager) {
 		super.setMetadataManager(metadataManager);
-	}
-
-	@Reference(unbind = "-")
-	protected void setPortal(Portal portal) {
-		super.portal = portal;
 	}
 
 	@Reference(

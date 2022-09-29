3c4875c71c1ca9fd710ac141b6be2f3be7809b2f
Kristof Loneck <kristof.loneck@liferay.com>
2022-09-29 10:40:30 +0200

    LPS-163426:Allow same page and sites with same name function properly in a virtual host

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -6152,8 +6152,7 @@ public class PortalImpl implements Portal {
 		String fullURL, String groupFriendlyURL, String layoutFriendlyURL) {
 
 		if (fullURL.endsWith(groupFriendlyURL) &&
-			!fullURL.endsWith(groupFriendlyURL.concat(layoutFriendlyURL))) {
-
+			!fullURL.endsWith(layoutFriendlyURL)) {
 			return true;
 		}
 

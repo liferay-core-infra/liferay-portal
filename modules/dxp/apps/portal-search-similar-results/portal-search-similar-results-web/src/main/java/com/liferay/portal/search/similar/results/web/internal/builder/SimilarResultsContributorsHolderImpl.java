f0dd33bd65071453302c2450605e229e02aff2d6
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-10-11 14:00:03 +0800

    LPS-165167 Convert method reference to ServiceTrackerList

--- modules/dxp/apps/portal-search-similar-results/portal-search-similar-results-web/src/main/java/com/liferay/portal/search/similar/results/web/internal/builder/SimilarResultsContributorsHolderImpl.java
+++ modules/dxp/apps/portal-search-similar-results/portal-search-similar-results-web/src/main/java/com/liferay/portal/search/similar/results/web/internal/builder/SimilarResultsContributorsHolderImpl.java
@@ -14,17 +14,18 @@
 
 package com.liferay.portal.search.similar.results.web.internal.builder;
 
+import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerList;
+import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerListFactory;
 import com.liferay.portal.search.similar.results.web.spi.contributor.SimilarResultsContributor;
 
-import java.util.Collection;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.service.component.annotations.Deactivate;
 
 /**
  * @author André de Oliveira
@@ -35,27 +36,29 @@ public class SimilarResultsContributorsHolderImpl
 
 	@Override
 	public Stream<SimilarResultsContributor> stream() {
-		return _similarResultsContributors.stream();
+		List<SimilarResultsContributor> similarResultsContributors =
+			new ArrayList<>(_serviceTrackerList.size());
+
+		for (SimilarResultsContributor similarResultsContributor :
+				_serviceTrackerList) {
+
+			similarResultsContributors.add(similarResultsContributor);
+		}
+
+		return similarResultsContributors.stream();
 	}
 
-	@Reference(
-		cardinality = ReferenceCardinality.MULTIPLE,
-		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY
-	)
-	protected void addSimilarResultsContributor(
-		SimilarResultsContributor similarResultsContributor) {
-
-		_similarResultsContributors.add(similarResultsContributor);
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerList = ServiceTrackerListFactory.open(
+			bundleContext, SimilarResultsContributor.class);
 	}
 
-	protected void removeSimilarResultsContributor(
-		SimilarResultsContributor similarResultsContributor) {
-
-		_similarResultsContributors.remove(similarResultsContributor);
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerList.close();
 	}
 
-	private final Collection<SimilarResultsContributor>
-		_similarResultsContributors = new CopyOnWriteArrayList<>();
+	private ServiceTrackerList<SimilarResultsContributor> _serviceTrackerList;
 
 }

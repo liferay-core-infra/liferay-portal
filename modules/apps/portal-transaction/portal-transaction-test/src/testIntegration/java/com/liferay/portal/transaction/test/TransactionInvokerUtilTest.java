ae7728bf25ffd55ca1b2652090b3612501b78d8c
Kyle Miho <kyle.miho@liferay.com>
2022-10-11 17:04:12 -0700

    LRQA-77402 Add test to cover rollback of files in conjunction with the fileEntries within a transaction

--- modules/apps/portal-transaction/portal-transaction-test/src/testIntegration/java/com/liferay/portal/transaction/test/TransactionInvokerUtilTest.java
+++ modules/apps/portal-transaction/portal-transaction-test/src/testIntegration/java/com/liferay/portal/transaction/test/TransactionInvokerUtilTest.java
@@ -16,21 +16,35 @@ package com.liferay.portal.transaction.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.counter.kernel.service.CounterLocalService;
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.model.DLFolderConstants;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
+import com.liferay.document.library.kernel.store.Store;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.ClassName;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.persistence.ClassNamePersistence;
+import com.liferay.portal.kernel.test.constants.TestDataConstants;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.TransactionConfig;
 import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
+import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.PwdGenerator;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 import java.util.concurrent.Callable;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -121,6 +135,67 @@ public class TransactionInvokerUtilTest {
 		}
 	}
 
+	@Ignore
+	@Test
+	public void testTransactionRollbackKeepsFileInStore() throws Throwable {
+		byte[] bytes = TestDataConstants.TEST_BYTE_ARRAY;
+
+		InputStream inputStream = new ByteArrayInputStream(bytes);
+
+		DLFileEntry dlFileEntry = _dlFileEntryLocalService.addFileEntry(
+			null, TestPropsValues.getUserId(), TestPropsValues.getGroupId(),
+			TestPropsValues.getGroupId(),
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			RandomTestUtil.randomString(), ContentTypes.TEXT_PLAIN,
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			RandomTestUtil.randomString(), StringPool.BLANK, 0, null, null,
+			inputStream, bytes.length, null, null,
+			ServiceContextTestUtil.getServiceContext());
+
+		try {
+			Assert.assertTrue(
+				_fileSystemStore.hasFile(
+					TestPropsValues.getCompanyId(),
+					TestPropsValues.getGroupId(), dlFileEntry.getName(), null));
+
+			TransactionInvokerUtil.invoke(
+				_transactionConfig,
+				() -> {
+					_dlFileEntryLocalService.deleteFileEntry(
+						dlFileEntry.getFileEntryId());
+
+					Assert.assertFalse(
+						_fileSystemStore.hasFile(
+							TestPropsValues.getCompanyId(),
+							TestPropsValues.getGroupId(), dlFileEntry.getName(),
+							null));
+
+					throw new Exception();
+				});
+		}
+		catch (Exception exception) {
+			Assert.assertNotNull(
+				_dlFileEntryLocalService.fetchDLFileEntry(
+					dlFileEntry.getFileEntryId()));
+
+			Assert.assertTrue(
+				_fileSystemStore.hasFile(
+					TestPropsValues.getCompanyId(),
+					TestPropsValues.getGroupId(), dlFileEntry.getName(), null));
+		}
+		finally {
+			if (dlFileEntry != null) {
+				_dlFileEntryLocalService.deleteFileEntry(
+					dlFileEntry.getFileEntryId());
+			}
+		}
+	}
+
+	@Inject(
+		filter = "store.type=com.liferay.portal.store.file.system.FileSystemStore"
+	)
+	private static Store _fileSystemStore;
+
 	private static TransactionConfig _transactionConfig;
 
 	@Inject
@@ -131,5 +206,8 @@ public class TransactionInvokerUtilTest {
 
 	@Inject
 	private CounterLocalService _counterLocalService;
+
+	@Inject
+	private DLFileEntryLocalService _dlFileEntryLocalService;
 
 }

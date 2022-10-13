07a25eaab61322b67e61d13b837fb48683215bae
Lily.Chi <lily.chi@liferay.com>
2022-10-13 14:12:21 +0800

    LPS-163778 Add reference annotation on these existed field to remove these set methods, the fields need to be protected as it needs by subclasses

--- modules/apps/marketplace/marketplace-store-web/src/main/java/com/liferay/marketplace/store/web/internal/portlet/MarketplaceStorePortlet.java
+++ modules/apps/marketplace/marketplace-store-web/src/main/java/com/liferay/marketplace/store/web/internal/portlet/MarketplaceStorePortlet.java
@@ -130,7 +130,7 @@ public class MarketplaceStorePortlet extends MVCPortlet {
 
 			downloadApp(actionRequest, appPackageId, unlicensed, file);
 
-			App app = _appService.updateApp(file);
+			App app = appService.updateApp(file);
 
 			JSONObject jsonObject = _getAppJSONObject(app.getRemoteAppId());
 
@@ -207,7 +207,7 @@ public class MarketplaceStorePortlet extends MVCPortlet {
 			"getPrepackagedApps");
 
 		Map<String, String> prepackagedApps =
-			_appLocalService.getPrepackagedApps();
+			appLocalService.getPrepackagedApps();
 
 		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
 
@@ -235,7 +235,7 @@ public class MarketplaceStorePortlet extends MVCPortlet {
 
 		long remoteAppId = ParamUtil.getLong(actionRequest, "appId");
 
-		_appService.installApp(remoteAppId);
+		appService.installApp(remoteAppId);
 
 		JSONObject jsonObject = _getAppJSONObject(remoteAppId);
 
@@ -323,7 +323,7 @@ public class MarketplaceStorePortlet extends MVCPortlet {
 
 		long remoteAppId = ParamUtil.getLong(actionRequest, "appId");
 
-		_appService.uninstallApp(remoteAppId);
+		appService.uninstallApp(remoteAppId);
 
 		JSONObject jsonObject = _getAppJSONObject(remoteAppId);
 
@@ -353,7 +353,7 @@ public class MarketplaceStorePortlet extends MVCPortlet {
 
 			downloadApp(actionRequest, appPackageId, unlicensed, file);
 
-			App app = _appService.updateApp(file);
+			App app = appService.updateApp(file);
 
 			if (Validator.isNull(orderUuid) &&
 				Validator.isNotNull(productEntryName)) {
@@ -366,7 +366,7 @@ public class MarketplaceStorePortlet extends MVCPortlet {
 					orderUuid, productEntryName);
 			}
 
-			_appService.installApp(app.getRemoteAppId());
+			appService.installApp(app.getRemoteAppId());
 
 			JSONObject jsonObject = _getAppJSONObject(app.getRemoteAppId());
 
@@ -438,9 +438,9 @@ public class MarketplaceStorePortlet extends MVCPortlet {
 
 						downloadApp(actionRequest, appPackageId, false, file);
 
-						App app = _appService.updateApp(file);
+						App app = appService.updateApp(file);
 
-						_appService.installApp(app.getRemoteAppId());
+						appService.installApp(app.getRemoteAppId());
 
 						jsonArray.put(_getAppJSONObject(app));
 					}
@@ -596,16 +596,6 @@ public class MarketplaceStorePortlet extends MVCPortlet {
 			"supportsHotDeploy", new String[] {Boolean.TRUE.toString()});
 	}
 
-	@Reference(unbind = "-")
-	protected void setAppLocalService(AppLocalService appLocalService) {
-		_appLocalService = appLocalService;
-	}
-
-	@Reference(unbind = "-")
-	protected void setAppService(AppService appService) {
-		_appService = appService;
-	}
-
 	protected void setBaseRequestParameters(
 		PortletRequest portletRequest, OAuthRequest oAuthRequest) {
 
@@ -624,11 +614,13 @@ public class MarketplaceStorePortlet extends MVCPortlet {
 		addOAuthParameter(oAuthRequest, "p_p_id", getServerPortletId());
 	}
 
-	@Reference(unbind = "-")
-	protected void setOAuthManager(OAuthManager oAuthManager) {
-		this.oAuthManager = oAuthManager;
-	}
+	@Reference
+	protected AppLocalService appLocalService;
 
+	@Reference
+	protected AppService appService;
+
+	@Reference
 	protected OAuthManager oAuthManager;
 
 	@Reference
@@ -663,7 +655,7 @@ public class MarketplaceStorePortlet extends MVCPortlet {
 	}
 
 	private JSONObject _getAppJSONObject(long remoteAppId) throws Exception {
-		App app = _appLocalService.fetchRemoteApp(remoteAppId);
+		App app = appLocalService.fetchRemoteApp(remoteAppId);
 
 		if (app != null) {
 			return _getAppJSONObject(app);
@@ -694,7 +686,7 @@ public class MarketplaceStorePortlet extends MVCPortlet {
 	private JSONArray _getInstalledAppsJSONArray() throws Exception {
 		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
 
-		List<App> apps = _appLocalService.getInstalledApps();
+		List<App> apps = appLocalService.getInstalledApps();
 
 		for (App app : apps) {
 			if (app.getRemoteAppId() > 0) {
@@ -816,8 +808,6 @@ public class MarketplaceStorePortlet extends MVCPortlet {
 	private static final Log _log = LogFactoryUtil.getLog(
 		MarketplaceStorePortlet.class);
 
-	private AppLocalService _appLocalService;
-	private AppService _appService;
 	private final ReentrantLock _reentrantLock = new ReentrantLock();
 
 }

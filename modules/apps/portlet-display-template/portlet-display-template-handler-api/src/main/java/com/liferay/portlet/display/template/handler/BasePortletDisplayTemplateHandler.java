ea4f2e99341f7a4b80b4b717129f0b7acc2a4ec6
Julius Lee <meng-huan.lee@liferay.com>
2022-10-17 15:10:01 -0700

    LPS-165878 Move BasePortletDisplayTemplateHandler into portlet-display-template-handler-api

--- modules/apps/portlet-display-template/portlet-display-template-handler-api/src/main/java/com/liferay/portlet/display/template/handler/BasePortletDisplayTemplateHandler.java
+++ modules/apps/portlet-display-template/portlet-display-template-handler-api/src/main/java/com/liferay/portlet/display/template/handler/BasePortletDisplayTemplateHandler.java
@@ -12,8 +12,9 @@
  * details.
  */
 
-package com.liferay.portal.kernel.portletdisplaytemplate;
+package com.liferay.portlet.display.template.handler;
 
+import com.liferay.portal.kernel.portletdisplaytemplate.PortletDisplayTemplateManagerUtil;
 import com.liferay.portal.kernel.template.BaseTemplateHandler;
 import com.liferay.portal.kernel.template.TemplateVariableGroup;
 

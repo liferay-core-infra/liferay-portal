ebb2a65542ae3e07b5b9cb59408236b8b9f7b95a
Janis Zhang <janis.zhang@liferay.com>
2022-10-24 15:14:51 +0800

    LPS-166421 Remove unused reference field _corEntryService, it was added since class creation but never used.

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-order-impl/src/main/java/com/liferay/headless/commerce/admin/order/internal/dto/v1_0/converter/OrderRuleOrderTypeDTOConverter.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-order-impl/src/main/java/com/liferay/headless/commerce/admin/order/internal/dto/v1_0/converter/OrderRuleOrderTypeDTOConverter.java
@@ -18,7 +18,6 @@ import com.liferay.commerce.model.CommerceOrderType;
 import com.liferay.commerce.order.rule.model.COREntry;
 import com.liferay.commerce.order.rule.model.COREntryRel;
 import com.liferay.commerce.order.rule.service.COREntryRelService;
-import com.liferay.commerce.order.rule.service.COREntryService;
 import com.liferay.commerce.service.CommerceOrderTypeService;
 import com.liferay.headless.commerce.admin.order.dto.v1_0.OrderRuleOrderType;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
@@ -73,8 +72,5 @@ public class OrderRuleOrderTypeDTOConverter
 
 	@Reference
 	private COREntryRelService _corEntryRelService;
-
-	@Reference
-	private COREntryService _corEntryService;
 
 }

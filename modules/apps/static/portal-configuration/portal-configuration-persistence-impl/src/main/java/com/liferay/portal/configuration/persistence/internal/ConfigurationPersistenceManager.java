81b929a210d4fd8eaf34386200c7350b045b94bc
Tina Tian <tina.tian@liferay.com>
2022-10-27 15:56:29 -0700

    LPS-162706 Use file name without file path consistently. After LPS-82771(739edcfb2804c293549778897593844391d10f6c Save the file name without the path and add back the path that matches the current node in the cluster so ConfigInstaller watches the correct file), the file name we used at runtime and stored in database are not consistent. Since we have inlined fileinstall, we can now make them consistent, so that we do not need to add back the file path at runtime.

--- modules/apps/static/portal-configuration/portal-configuration-persistence-impl/src/main/java/com/liferay/portal/configuration/persistence/internal/ConfigurationPersistenceManager.java
+++ modules/apps/static/portal-configuration/portal-configuration-persistence-impl/src/main/java/com/liferay/portal/configuration/persistence/internal/ConfigurationPersistenceManager.java
@@ -277,28 +277,12 @@ public class ConfigurationPersistenceManager
 
 		Dictionary<Object, Object> newDictionary = _copyDictionary(dictionary);
 
-		String fileName = (String)newDictionary.get(
-			FileInstallConstants.FELIX_FILE_INSTALL_FILENAME);
-
-		if (fileName != null) {
-			File file = new File(URI.create(fileName));
-
-			newDictionary.put(
-				FileInstallConstants.FELIX_FILE_INSTALL_FILENAME,
-				file.getName());
-		}
-
 		Lock lock = _readWriteLock.writeLock();
 
 		lock.lock();
 
 		try {
 			_storeInDatabase(pid, newDictionary);
-
-			if (fileName != null) {
-				newDictionary.put(
-					FileInstallConstants.FELIX_FILE_INSTALL_FILENAME, fileName);
-			}
 
 			_dictionaries.put(pid, _overrideDictionary(pid, newDictionary));
 		}
@@ -404,25 +388,9 @@ public class ConfigurationPersistenceManager
 						return new HashMapDictionary<>();
 					}
 
-					Dictionary<Object, Object> dictionary =
-						ConfigurationHandler.read(
-							new UnsyncByteArrayInputStream(
-								dictionaryString.getBytes(StringPool.UTF8)));
-
-					String fileName = (String)dictionary.get(
-						FileInstallConstants.FELIX_FILE_INSTALL_FILENAME);
-
-					if (fileName != null) {
-						File file = _getCanonicalConfigFile(fileName);
-
-						URI uri = file.toURI();
-
-						dictionary.put(
-							FileInstallConstants.FELIX_FILE_INSTALL_FILENAME,
-							uri.toString());
-					}
-
-					return dictionary;
+					return ConfigurationHandler.read(
+						new UnsyncByteArrayInputStream(
+							dictionaryString.getBytes(StringPool.UTF8)));
 				}
 			}
 
@@ -604,20 +572,10 @@ public class ConfigurationPersistenceManager
 
 			_storeInDatabase(pid, dictionary);
 
-			dictionary.put(
-				FileInstallConstants.FELIX_FILE_INSTALL_FILENAME,
-				felixFileInstallFileName);
-
 			needSave = false;
 		}
 		else {
 			configFile = _getCanonicalConfigFile(felixFileInstallFileName);
-
-			URI uri = configFile.toURI();
-
-			dictionary.put(
-				FileInstallConstants.FELIX_FILE_INSTALL_FILENAME,
-				uri.toString());
 		}
 
 		if (needSave) {

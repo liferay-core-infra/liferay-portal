066e339ba9e851c8ef5cc47bd3f1a55dd0ccc3f6
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2022-10-31 15:14:49 -0300

    LPS-167148 Remove immediate from Journal module

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/layout/display/page/JournalArticleLayoutDisplayPageProvider.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/layout/display/page/JournalArticleLayoutDisplayPageProvider.java
@@ -32,7 +32,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Jürgen Kappler
  */
 @Component(
-	immediate = true, property = "service.ranking:Integer=200",
+	property = "service.ranking:Integer=200",
 	service = LayoutDisplayPageProvider.class
 )
 public class JournalArticleLayoutDisplayPageProvider

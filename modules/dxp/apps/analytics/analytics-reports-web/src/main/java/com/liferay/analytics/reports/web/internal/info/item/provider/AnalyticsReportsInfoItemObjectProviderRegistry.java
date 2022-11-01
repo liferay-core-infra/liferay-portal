dbaee24cf96cc6f5cb14044cb1ddece335cadebe
Janis Zhang <janis.zhang@liferay.com>
2022-11-01 18:00:04 +0800

    LPS-167594 Rename AnalyticsReportsInfoItemObjectProviderTracker  to AnalyticsReportsInfoItemObjectProviderRegistry

--- modules/dxp/apps/analytics/analytics-reports-web/src/main/java/com/liferay/analytics/reports/web/internal/info/item/provider/AnalyticsReportsInfoItemObjectProviderRegistry.java
+++ modules/dxp/apps/analytics/analytics-reports-web/src/main/java/com/liferay/analytics/reports/web/internal/info/item/provider/AnalyticsReportsInfoItemObjectProviderRegistry.java
@@ -26,8 +26,8 @@ import org.osgi.service.component.annotations.Component;
 /**
  * @author Cristina González
  */
-@Component(service = AnalyticsReportsInfoItemObjectProviderTracker.class)
-public class AnalyticsReportsInfoItemObjectProviderTracker {
+@Component(service = AnalyticsReportsInfoItemObjectProviderRegistryImpl.class)
+public class AnalyticsReportsInfoItemObjectProviderRegistry {
 
 	public AnalyticsReportsInfoItemObjectProvider<?>
 		getAnalyticsReportsInfoItemObjectProvider(String className) {

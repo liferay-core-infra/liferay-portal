6a7fef922477e7826fb26d9833cbb20464c3aff3
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2022-11-08 16:21:14 -0300

    LPS-167880 Remove immediate from Style Book module

--- modules/apps/style-book/style-book-web/src/main/java/com/liferay/style/book/web/internal/configuration/icon/ImportPortletConfigurationIcon.java
+++ modules/apps/style-book/style-book-web/src/main/java/com/liferay/style/book/web/internal/configuration/icon/ImportPortletConfigurationIcon.java
@@ -36,7 +36,6 @@ import org.osgi.service.component.annotations.Reference;
  * @author Eudaldo Alonso
  */
 @Component(
-	immediate = true,
 	property = {
 		"javax.portlet.name=" + StyleBookPortletKeys.STYLE_BOOK, "path=-",
 		"path=/style_books", "path=/style_books/style_books"

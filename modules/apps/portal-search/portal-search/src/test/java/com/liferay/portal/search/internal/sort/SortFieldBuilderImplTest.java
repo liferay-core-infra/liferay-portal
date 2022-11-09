f243b32b1fe0228909bb2a3952c9833d82084834
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-11-09 15:11:25 +0800

    LPS-165445 Fix the test

--- modules/apps/portal-search/portal-search/src/test/java/com/liferay/portal/search/internal/sort/SortFieldBuilderImplTest.java
+++ modules/apps/portal-search/portal-search/src/test/java/com/liferay/portal/search/internal/sort/SortFieldBuilderImplTest.java
@@ -14,17 +14,17 @@
 
 package com.liferay.portal.search.internal.sort;
 
+import com.liferay.portal.kernel.module.util.SystemBundleUtil;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistry;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.test.util.PropsTestUtil;
-import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.search.contributor.constants.ContributorConstants;
 import com.liferay.portal.search.contributor.sort.SortFieldNameTranslator;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -32,6 +32,9 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import org.mockito.Mockito;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 /**
  * @author Michael C. Han
@@ -61,7 +64,14 @@ public class SortFieldBuilderImplTest {
 				"title"
 			});
 
-		_sortFieldBuilderImpl.activate();
+		_bundleContext = SystemBundleUtil.getBundleContext();
+
+		_sortFieldBuilderImpl.activate(_bundleContext);
+	}
+
+	@After
+	public void tearDown() {
+		_sortFieldBuilderImpl.deactivate();
 	}
 
 	@Test
@@ -110,25 +120,23 @@ public class SortFieldBuilderImplTest {
 
 	@Test
 	public void testGetSortFieldWithSortFieldTranslator() {
-		SortFieldNameTranslator sortFieldNameTranslator = Mockito.mock(
-			SortFieldNameTranslator.class);
+		ServiceRegistration<SortFieldNameTranslator> serviceRegistration =
+			_bundleContext.registerService(
+				SortFieldNameTranslator.class,
+				new SortFieldNameTranslator() {
 
-		Mockito.when(
-			sortFieldNameTranslator.getSortFieldName(Mockito.anyString())
-		).then(
-			invocation -> {
-				String orderByCol = invocation.getArgument(0, String.class);
+					@Override
+					public String getEntryClassName() {
+						return "modelClassName";
+					}
 
-				return StringUtil.upperCaseFirstLetter(orderByCol);
-			}
-		);
+					@Override
+					public String getSortFieldName(String orderByCol) {
+						return StringUtil.upperCaseFirstLetter(orderByCol);
+					}
 
-		_sortFieldBuilderImpl.addSortFieldNameTranslator(
-			sortFieldNameTranslator,
-			HashMapBuilder.<String, Object>put(
-				ContributorConstants.ENTRY_CLASS_NAME_PROPERTY_KEY,
-				"modelClassName"
-			).build());
+				},
+				null);
 
 		Mockito.when(
 			_indexer.getSortField(Mockito.anyString())
@@ -169,8 +177,11 @@ public class SortFieldBuilderImplTest {
 
 		Assert.assertEquals(
 			Field.getSortableFieldName("testField"), sortFieldName);
+
+		serviceRegistration.unregister();
 	}
 
+	private BundleContext _bundleContext;
 	private final Indexer<?> _indexer = Mockito.mock(Indexer.class);
 	private final IndexerRegistry _indexerRegistry = Mockito.mock(
 		IndexerRegistry.class);

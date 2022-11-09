98371cd588f70663d7291753affd2535b7c5c11b
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-11-09 14:49:05 +0800

    LPS-165445 Add method to class SortFieldNameTranslator to get entry.class.name(avoid forgetting to add property entry.class.name when adding a new implementation)

--- modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/contributor/sort/SortFieldNameTranslator.java
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/contributor/sort/SortFieldNameTranslator.java
@@ -22,6 +22,8 @@ import org.osgi.annotation.versioning.ProviderType;
 @ProviderType
 public interface SortFieldNameTranslator {
 
+	public String getEntryClassName();
+
 	public String getSortFieldName(String orderByCol);
 
 }

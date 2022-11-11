ac8529d56e043e416e56d5fa865f933873b8c742
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-11-11 17:06:17 +0800

    LPS-164451 Convert method reference to ServiceTrackerMap

--- modules/apps/portal/portal-executor/src/main/java/com/liferay/portal/executor/internal/DefaultPortalExecutorManager.java
+++ modules/apps/portal/portal-executor/src/main/java/com/liferay/portal/executor/internal/DefaultPortalExecutorManager.java
@@ -14,6 +14,9 @@
 
 package com.liferay.portal.executor.internal;
 
+import com.liferay.osgi.service.tracker.collections.map.ServiceReferenceMapperFactory;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.petra.concurrent.NoticeableExecutorService;
 import com.liferay.petra.concurrent.NoticeableFuture;
 import com.liferay.petra.concurrent.NoticeableThreadPoolExecutor;
@@ -30,12 +33,12 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * @author Shuyang Zhou
@@ -112,28 +115,54 @@ public class DefaultPortalExecutorManager implements PortalExecutorManager {
 		}
 	}
 
-	@Reference(
-		cardinality = ReferenceCardinality.MULTIPLE,
-		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY
-	)
-	protected void addPortalExecutorConfig(
-		PortalExecutorConfig portalExecutorConfig) {
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, PortalExecutorConfig.class, null,
+			ServiceReferenceMapperFactory.create(
+				bundleContext,
+				(portalExecutorConfig, emitter) -> emitter.emit(
+					portalExecutorConfig.getName())),
+			new ServiceTrackerCustomizer
+				<PortalExecutorConfig, PortalExecutorConfig>() {
 
-		_portalExecutorConfigs.putIfAbsent(
-			portalExecutorConfig.getName(), portalExecutorConfig);
+				@Override
+				public PortalExecutorConfig addingService(
+					ServiceReference<PortalExecutorConfig> serviceReference) {
+
+					PortalExecutorConfig portalExecutorConfig =
+						bundleContext.getService(serviceReference);
+
+					_portalExecutorConfigs.putIfAbsent(
+						portalExecutorConfig.getName(), portalExecutorConfig);
+
+					return portalExecutorConfig;
+				}
+
+				@Override
+				public void modifiedService(
+					ServiceReference<PortalExecutorConfig> serviceReference,
+					PortalExecutorConfig service) {
+				}
+
+				@Override
+				public void removedService(
+					ServiceReference<PortalExecutorConfig> serviceReference,
+					PortalExecutorConfig service) {
+
+					_portalExecutorConfigs.remove(service.getName(), service);
+
+					bundleContext.ungetService(serviceReference);
+				}
+
+			});
 	}
 
 	@Deactivate
 	protected void deactivate() {
+		_serviceTrackerMap.close();
+
 		shutdown(true);
-	}
-
-	protected void removePortalExecutorConfig(
-		PortalExecutorConfig portalExecutorConfig) {
-
-		_portalExecutorConfigs.remove(
-			portalExecutorConfig.getName(), portalExecutorConfig);
 	}
 
 	private NoticeableExecutorService _createPortalExecutor(
@@ -187,5 +216,6 @@ public class DefaultPortalExecutorManager implements PortalExecutorManager {
 		_noticeableExecutorServices = new ConcurrentHashMap<>();
 	private final ConcurrentMap<String, PortalExecutorConfig>
 		_portalExecutorConfigs = new ConcurrentHashMap<>();
+	private ServiceTrackerMap<String, PortalExecutorConfig> _serviceTrackerMap;
 
 }

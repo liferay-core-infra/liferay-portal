0517c784365e77e10fafb6640713c469b380640e
Julius Lee <meng-huan.lee@liferay.com>
2022-11-21 09:57:27 -0800

    LPS-168726 Remove ClassLoaderResourceParser.normalizePath() usage, use URLResourceParserTest instead

--- portal-impl/test/unit/com/liferay/portal/template/URLResourceParserTest.java
+++ portal-impl/test/unit/com/liferay/portal/template/URLResourceParserTest.java
@@ -93,13 +93,11 @@ public class URLResourceParserTest {
 
 	@Test
 	public void testNormalizePath() {
-		Assert.assertEquals(
-			"abc", ClassLoaderResourceParser.normalizePath("abc"));
-		Assert.assertEquals(
-			"/abc", ClassLoaderResourceParser.normalizePath("/abc"));
+		Assert.assertEquals("abc", URLResourceParser.normalizePath("abc"));
+		Assert.assertEquals("/abc", URLResourceParser.normalizePath("/abc"));
 
 		try {
-			ClassLoaderResourceParser.normalizePath("//");
+			URLResourceParser.normalizePath("//");
 
 			Assert.fail();
 		}
@@ -109,13 +107,12 @@ public class URLResourceParserTest {
 				illegalArgumentException.getMessage());
 		}
 
+		Assert.assertEquals("abc", URLResourceParser.normalizePath("abc/./"));
 		Assert.assertEquals(
-			"abc", ClassLoaderResourceParser.normalizePath("abc/./"));
-		Assert.assertEquals(
-			"def", ClassLoaderResourceParser.normalizePath("abc/../def"));
+			"def", URLResourceParser.normalizePath("abc/../def"));
 
 		try {
-			ClassLoaderResourceParser.normalizePath("../");
+			URLResourceParser.normalizePath("../");
 
 			Assert.fail();
 		}
@@ -126,8 +123,7 @@ public class URLResourceParserTest {
 		}
 
 		Assert.assertEquals(
-			"/efg/hij",
-			ClassLoaderResourceParser.normalizePath("/abc/../efg/./hij/"));
+			"/efg/hij", URLResourceParser.normalizePath("/abc/../efg/./hij/"));
 	}
 
 }

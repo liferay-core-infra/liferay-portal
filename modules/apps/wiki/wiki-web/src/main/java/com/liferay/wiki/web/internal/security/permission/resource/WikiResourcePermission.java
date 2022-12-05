2cdf9f8012373687f237980ce7d20a51cb496ffe
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-12-05 15:46:48 +0800

    LPS-170377 wiki-web: Make  method setXXXPermission static

--- modules/apps/wiki/wiki-web/src/main/java/com/liferay/wiki/web/internal/security/permission/resource/WikiResourcePermission.java
+++ modules/apps/wiki/wiki-web/src/main/java/com/liferay/wiki/web/internal/security/permission/resource/WikiResourcePermission.java
@@ -38,7 +38,7 @@ public class WikiResourcePermission {
 		target = "(resource.name=" + WikiConstants.RESOURCE_NAME + ")",
 		unbind = "-"
 	)
-	protected void setPortletResourcePermission(
+	protected static void setPortletResourcePermission(
 		PortletResourcePermission portletResourcePermission) {
 
 		_portletResourcePermission = portletResourcePermission;

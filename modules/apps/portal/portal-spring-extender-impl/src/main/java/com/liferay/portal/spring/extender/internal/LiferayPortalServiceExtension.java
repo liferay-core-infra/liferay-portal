fc291748f9acd446a453948c86f31678d009f231
hai.yu <hai.yu@liferay.com>
2022-12-05 15:11:53 +0800

    LPS-170489 Create LiferayPortalServiceExtension inferface, other *Extension will implements it, prepare for next.

---
+++ modules/apps/portal/portal-spring-extender-impl/src/main/java/com/liferay/portal/spring/extender/internal/LiferayPortalServiceExtension.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.spring.extender.internal;
+
+/**
+ * @author Hai Yu
+ */
+public interface LiferayPortalServiceExtension {
+
+	public default void destroy() {
+	}
+
+	public default void start() throws Exception {
+	}
+
+}

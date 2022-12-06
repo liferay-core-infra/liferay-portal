a92bad38fc2ee71cc5a65bc989f5fd4f0240a971
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-12-06 17:25:32 +0800

    LPS-170487 Add class StyleBookPermissionHelper to assign value to static variable in classes StyleBookPermission

---
+++ modules/apps/style-book/style-book-web/src/main/java/com/liferay/style/book/web/internal/security/permissions/resource/StyleBookPermissionHelper.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.style.book.web.internal.security.permissions.resource;
+
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.style.book.constants.StyleBookConstants;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jiaxu Wei
+ */
+@Component(service = {})
+public class StyleBookPermissionHelper {
+
+	@Activate
+	protected void activate() {
+		StyleBookPermission.setPortletResourcePermission(
+			_portletResourcePermission);
+	}
+
+	@Reference(
+		target = "(resource.name=" + StyleBookConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+}

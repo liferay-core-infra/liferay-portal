3665ce0565ca5e9064f19e250f91e7df42da294e
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-12-07 11:19:19 +0800

    LPS-170595 site-navigation-admin-web: Add class PermissionHelper to assign value to static variable in classes XXXPermission

---
+++ modules/apps/site-navigation/site-navigation-admin-web/src/main/java/com/liferay/site/navigation/admin/web/internal/security/permission/resource/PermissionHelper.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.admin.web.internal.security.permission.resource;
+
+import com.liferay.dynamic.data.mapping.model.DDMTemplate;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.site.navigation.constants.SiteNavigationConstants;
+import com.liferay.site.navigation.model.SiteNavigationMenu;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jiaxu Wei
+ */
+@Component(service = {})
+public class PermissionHelper {
+
+	@Activate
+	protected void activate() {
+		DDMTemplatePermission.setModelResourcePermission(
+			_ddmTemplateModelResourcePermission);
+
+		SiteNavigationMenuPermission.setModelResourcePermission(
+			_siteNavigationMenuModelResourcePermission);
+
+		SiteNavigationPermission.setPortletResourcePermission(
+			_portletResourcePermission);
+	}
+
+	@Reference(
+		target = "(model.class.name=com.liferay.dynamic.data.mapping.model.DDMTemplate)"
+	)
+	private ModelResourcePermission<DDMTemplate>
+		_ddmTemplateModelResourcePermission;
+
+	@Reference(
+		target = "(resource.name=" + SiteNavigationConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.site.navigation.model.SiteNavigationMenu)"
+	)
+	private ModelResourcePermission<SiteNavigationMenu>
+		_siteNavigationMenuModelResourcePermission;
+
+}

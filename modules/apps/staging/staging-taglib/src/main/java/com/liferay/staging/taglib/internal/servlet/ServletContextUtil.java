4d02471329487453df3e00dfb430ffc0781d44d4
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-12-07 10:49:21 +0800

    LPS-170593 SF, remove unused method

--- modules/apps/staging/staging-taglib/src/main/java/com/liferay/staging/taglib/internal/servlet/ServletContextUtil.java
+++ modules/apps/staging/staging-taglib/src/main/java/com/liferay/staging/taglib/internal/servlet/ServletContextUtil.java
@@ -21,10 +21,6 @@ import javax.servlet.ServletContext;
  */
 public class ServletContextUtil {
 
-	public static String getContextPath() {
-		return _servletContext.getContextPath();
-	}
-
 	public static ServletContext getServletContext() {
 		return _servletContext;
 	}

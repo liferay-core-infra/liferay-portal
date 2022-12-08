7e53f29273163caacaa5e6f94b678e9b03f9e664
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-12-08 15:21:20 +0800

    LPS-170716 Add class SegmentsPermissionHelper to assign value to static variable in classes SegmentsXXXPermission

---
+++ modules/apps/segments/segments-web/src/main/java/com/liferay/segments/web/internal/security/permission/resource/SegmentsPermissionHelper.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.web.internal.security.permission.resource;
+
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.segments.constants.SegmentsConstants;
+import com.liferay.segments.model.SegmentsEntry;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jiaxu Wei
+ */
+@Component(service = {})
+public class SegmentsPermissionHelper {
+
+	@Activate
+	protected void activate() {
+		SegmentsEntryPermission.setModelResourcePermission(
+			_segmentsEntryModelResourcePermission);
+
+		SegmentsResourcePermission.setPortletResourcePermission(
+			_portletResourcePermission);
+	}
+
+	@Reference(
+		target = "(resource.name=" + SegmentsConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.segments.model.SegmentsEntry)"
+	)
+	private ModelResourcePermission<SegmentsEntry>
+		_segmentsEntryModelResourcePermission;
+
+}

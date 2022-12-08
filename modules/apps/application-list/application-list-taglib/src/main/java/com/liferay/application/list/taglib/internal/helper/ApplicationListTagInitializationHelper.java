974a2acbdbd157e5a7587ad6f94abf002eb9ca1b
Lily.Chi <lily.chi@liferay.com>
2022-12-08 15:16:28 +0800

    LPS-170495 Add a component class ApplicationListTagInitializationHelper to initialize the ServletContext for BasePanelTag and PanelAppUtil

---
+++ modules/apps/application-list/application-list-taglib/src/main/java/com/liferay/application/list/taglib/internal/helper/ApplicationListTagInitializationHelper.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.application.list.taglib.internal.helper;
+
+import com.liferay.application.list.taglib.internal.util.PanelAppUtil;
+import com.liferay.application.list.taglib.servlet.taglib.BasePanelTag;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lily Chi
+ */
+@Component(service = {})
+public class ApplicationListTagInitializationHelper {
+
+	@Activate
+	protected void activate() {
+		BasePanelTag.initServletContext(_servletContext);
+		PanelAppUtil.setServletContext(_servletContext);
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.application.list.taglib)"
+	)
+	private ServletContext _servletContext;
+
+}

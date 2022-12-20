bf6c81561b3a1d3f43901793d0105ea2e589a6c8
Janis Zhang <janis.zhang@liferay.com>
2022-12-20 14:56:42 +0800

    LPS-171453 Apply usages

--- modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/info/collection/provider/AssetEntriesWithSameAssetCategoryRelatedInfoItemCollectionProvider.java
+++ modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/info/collection/provider/AssetEntriesWithSameAssetCategoryRelatedInfoItemCollectionProvider.java
@@ -59,7 +59,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Optional;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -79,10 +78,7 @@ public class AssetEntriesWithSameAssetCategoryRelatedInfoItemCollectionProvider
 	public InfoPage<AssetEntry> getCollectionInfoPage(
 		CollectionQuery collectionQuery) {
 
-		Optional<Object> relatedItemOptional =
-			collectionQuery.getRelatedItemObjectOptional();
-
-		Object relatedItem = relatedItemOptional.orElse(null);
+		Object relatedItem = collectionQuery.getRelatedItemObject();
 
 		if (!(relatedItem instanceof AssetCategory)) {
 			return InfoPage.of(
@@ -168,11 +164,8 @@ public class AssetEntriesWithSameAssetCategoryRelatedInfoItemCollectionProvider
 	}
 
 	private long[] _getClassNameIds(CollectionQuery collectionQuery) {
-		Optional<Map<String, String[]>> configurationOptional =
-			collectionQuery.getConfigurationOptional();
-
-		Map<String, String[]> configuration = configurationOptional.orElse(
-			null);
+		Map<String, String[]> configuration =
+			collectionQuery.getConfiguration();
 
 		if (MapUtil.isNotEmpty(configuration) &&
 			ArrayUtil.isNotEmpty(configuration.get("item_types"))) {

fd9d3defad5babf67f9de616a73bc2c46601726a
hai.yu <hai.yu@liferay.com>
2022-12-26 10:40:40 +0800

    LPS-169260 In activate(), the log info "System check is enabled. You can run a system check with the command "system:check" in Gogo shell" looks useful for the end user. In addition, if setting initial.system.check.enabled for true, SystemCheckOSGiCommands should be activated during portal startup.

--- modules/apps/portal-osgi-debug/portal-osgi-debug-impl/src/main/java/com/liferay/portal/osgi/debug/internal/osgi/commands/SystemCheckOSGiCommands.java
+++ modules/apps/portal-osgi-debug/portal-osgi-debug-impl/src/main/java/com/liferay/portal/osgi/debug/internal/osgi/commands/SystemCheckOSGiCommands.java
@@ -40,6 +40,7 @@ import org.osgi.util.tracker.ServiceTracker;
  * @author Tina Tian
  */
 @Component(
+	immediate = true,
 	property = {"osgi.command.function=check", "osgi.command.scope=system"},
 	service = SystemCheckOSGiCommands.class
 )

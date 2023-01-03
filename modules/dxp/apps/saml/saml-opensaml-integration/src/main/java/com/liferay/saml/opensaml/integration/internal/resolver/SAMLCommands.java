8ed0b949ae5be24c2a60ddae5ea28a58ccb35623
Jiefeng Wu <jeffwu0724@gmail.com>
2023-01-03 11:29:54 -0800

    LPS-171928 remove usage of Stream in SAMLCommands

--- modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/resolver/SAMLCommands.java
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/resolver/SAMLCommands.java
@@ -14,6 +14,8 @@
 
 package com.liferay.saml.opensaml.integration.internal.resolver;
 
+import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.saml.opensaml.integration.internal.util.SamlUtil;
 import com.liferay.saml.opensaml.integration.resolver.AttributeResolver;
 import com.liferay.saml.opensaml.integration.resolver.Resolver;
@@ -21,13 +23,11 @@ import com.liferay.saml.opensaml.integration.resolver.UserResolver;
 
 import java.io.Serializable;
 
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.opensaml.messaging.context.InOutOperationContext;
 import org.opensaml.messaging.context.MessageContext;
@@ -35,6 +35,7 @@ import org.opensaml.saml.common.messaging.context.SAMLMetadataContext;
 import org.opensaml.saml.common.messaging.context.SAMLPeerEntityContext;
 import org.opensaml.saml.common.messaging.context.SAMLSubjectNameIdentifierContext;
 import org.opensaml.saml.saml2.core.Assertion;
+import org.opensaml.saml.saml2.core.Attribute;
 import org.opensaml.saml.saml2.core.AttributeStatement;
 import org.opensaml.saml.saml2.core.NameID;
 import org.opensaml.saml.saml2.metadata.IDPSSODescriptor;
@@ -76,22 +77,18 @@ public interface SAMLCommands {
 				}
 
 				Assertion assertion = subjectAssertionContext.getAssertion();
+				List<Attribute> attributes = new ArrayList<>();
 
-				List<AttributeStatement> attributeStatements =
-					assertion.getAttributeStatements();
+				for (List<Attribute> attributeList :
+						TransformUtil.transform(
+							assertion.getAttributeStatements(),
+							AttributeStatement::getAttributes)) {
 
-				Stream<AttributeStatement> stream =
-					attributeStatements.stream();
+					attributes.addAll(attributeList);
+				}
 
 				return SamlUtil.getAttributesMap(
-					stream.map(
-						AttributeStatement::getAttributes
-					).flatMap(
-						Collection::stream
-					).collect(
-						Collectors.toList()
-					),
-					userAttributeMappingsProperties);
+					attributes, userAttributeMappingsProperties);
 			});
 	}
 
@@ -118,16 +115,11 @@ public interface SAMLCommands {
 					return null;
 				}
 
-				Stream<SingleSignOnService> singleSignOnServicesStream =
-					singleSignOnServices.stream();
-
-				return singleSignOnServicesStream.filter(
-					ssos -> binding.equals(ssos.getBinding())
-				).map(
-					SingleSignOnService::getLocation
-				).collect(
-					Collectors.toList()
-				);
+				return TransformUtil.transform(
+					ListUtil.filter(
+						singleSignOnServices,
+						ssos -> binding.equals(ssos.getBinding())),
+					SingleSignOnService::getLocation);
 			});
 	}
 

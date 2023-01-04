b656c9f4ca1fec176d41be6f948617addd60b0fe
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-01-04 14:42:42 +0800

    LPS-166968 Remove unused property "content.type"

--- modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v2_0/test/util/content/type/TestDataDefinitionContentType.java
+++ modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v2_0/test/util/content/type/TestDataDefinitionContentType.java
@@ -35,7 +35,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Rodrigo Paulino
  */
 @Component(
-	immediate = true, property = "content.type=test",
+	immediate = true,
 	service = {
 		DataDefinitionContentType.class, TestDataDefinitionContentType.class
 	}

7c9061cb39a2977a3bf26ae8410a4e0d18d7e52b
Hayden Zheng <hayden.zheng@liferay.com>
2023-01-10 11:13:01 +0800

    LPS-172262 Apply usages of RankingIndexReader

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/test/java/com/liferay/portal/search/tuning/rankings/web/internal/results/builder/BaseRankingResultsBuilderTestCase.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/test/java/com/liferay/portal/search/tuning/rankings/web/internal/results/builder/BaseRankingResultsBuilderTestCase.java
@@ -37,7 +37,7 @@ public abstract class BaseRankingResultsBuilderTestCase
 			rankingOptional
 		).when(
 			rankingIndexReader
-		).fetchOptional(
+		).fetchRanking(
 			Mockito.any(), Mockito.nullable(String.class)
 		);
 	}

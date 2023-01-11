f9da8076d8b0919e733c7f1af232671f6f29ed6f
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-01-11 17:05:31 +0800

    LPS-165285 Apply

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/facet/CompositeFacetProcessor.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/facet/CompositeFacetProcessor.java
@@ -15,7 +15,6 @@
 package com.liferay.portal.search.elasticsearch7.internal.facet;
 
 import com.liferay.portal.kernel.search.facet.Facet;
-import com.liferay.portal.kernel.util.MapUtil;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -58,30 +57,31 @@ public class CompositeFacetProcessor
 	@Reference(
 		cardinality = ReferenceCardinality.MULTIPLE,
 		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY,
-		target = "(&(class.name=*)(!(class.name=DEFAULT)))"
+		policyOption = ReferencePolicyOption.GREEDY
 	)
 	protected void setFacetProcessor(
-		FacetProcessor<SearchRequestBuilder> facetProcessor,
-		Map<String, Object> properties) {
+		FacetProcessor<SearchRequestBuilder> facetProcessor) {
 
-		String className = MapUtil.getString(properties, "class.name");
+		String facetClassName = facetProcessor.getFacetClassName();
 
-		_facetProcessors.put(className, facetProcessor);
+		if (facetClassName != null) {
+			_facetProcessors.put(facetClassName, facetProcessor);
+		}
 	}
 
 	protected void unsetFacetProcessor(
-		FacetProcessor<SearchRequestBuilder> facetProcessor,
-		Map<String, Object> properties) {
+		FacetProcessor<SearchRequestBuilder> facetProcessor) {
 
-		String className = MapUtil.getString(properties, "class.name");
+		String facetClassName = facetProcessor.getFacetClassName();
 
-		_facetProcessors.remove(className);
+		if (facetClassName != null) {
+			_facetProcessors.remove(facetClassName, facetProcessor);
+		}
 	}
 
 	@Reference(
 		cardinality = ReferenceCardinality.MANDATORY,
-		target = "(class.name=DEFAULT)"
+		target = "(component.name=com.liferay.portal.search.elasticsearch7.internal.facet.DefaultFacetProcessor)"
 	)
 	protected FacetProcessor<SearchRequestBuilder> defaultFacetProcessor;
 

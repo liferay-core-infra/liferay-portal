aabba9a3d3f5a44dcdf8ca2594f9cb77a7e3f206
hai.yu <hai.yu@liferay.com>
2023-01-13 16:55:48 +0800

    LPS-172632 SF, remove SynchronousDestinationTestRule. Because LiferayIntegrationTestRule includes this rule.

--- modules/apps/redirect/redirect-test/src/testIntegration/java/com/liferay/redirect/internal/messaging/test/CheckRedirectNotFoundEntriesMessageListenerTest.java
+++ modules/apps/redirect/redirect-test/src/testIntegration/java/com/liferay/redirect/internal/messaging/test/CheckRedirectNotFoundEntriesMessageListenerTest.java
@@ -22,7 +22,6 @@ import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.rule.Sync;
-import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.SchedulerDispatchMessageListenerTestUtil;
 import com.liferay.portal.test.rule.Inject;
@@ -55,9 +54,7 @@ public class CheckRedirectNotFoundEntriesMessageListenerTest {
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
-		new AggregateTestRule(
-			new LiferayIntegrationTestRule(),
-			SynchronousDestinationTestRule.INSTANCE);
+		new LiferayIntegrationTestRule();
 
 	@BeforeClass
 	public static void setUpClass() {

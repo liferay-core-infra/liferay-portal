b4cb44a48e12b1179db785820b90cc49c1764f9c
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-01-13 09:43:39 +0800

    LPS-167598 Convert method reference to ServiceTrackerMap(it should not be judged in the tracker whether there is coverage and whether there is missing property, this is the developer's responsibility)

--- modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/notification/NotificationSenderFactory.java
+++ modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/notification/NotificationSenderFactory.java
@@ -15,18 +15,14 @@
 package com.liferay.calendar.internal.notification;
 
 import com.liferay.calendar.notification.NotificationSender;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.service.component.annotations.Deactivate;
 
 /**
  * @author Eduardo Lundgren
@@ -37,7 +33,7 @@ public class NotificationSenderFactory {
 	public NotificationSender getNotificationSender(String notificationType)
 		throws PortalException {
 
-		NotificationSender notificationSender = _notificationSenders.get(
+		NotificationSender notificationSender = _serviceTrackerMap.getService(
 			notificationType);
 
 		if (notificationSender == null) {
@@ -48,48 +44,17 @@ public class NotificationSenderFactory {
 		return notificationSender;
 	}
 
-	@Reference(
-		cardinality = ReferenceCardinality.MULTIPLE,
-		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY
-	)
-	protected void setNotificationSender(
-		NotificationSender notificationSender, Map<String, Object> properties) {
-
-		String notificationType = (String)properties.get("notification.type");
-
-		if (notificationType == null) {
-			throw new IllegalArgumentException(
-				"The property \"notification.type\" is null");
-		}
-
-		NotificationSender previousNotificationSender =
-			_notificationSenders.put(notificationType, notificationSender);
-
-		if (_log.isWarnEnabled() && (previousNotificationSender != null)) {
-			Class<?> clazz = previousNotificationSender.getClass();
-
-			_log.warn("Overriding notification sender " + clazz.getName());
-		}
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, NotificationSender.class, "notification.type");
 	}
 
-	protected void unsetNotificationSender(
-		NotificationSender notificationSender, Map<String, Object> properties) {
-
-		String notificationType = (String)properties.get("notification.type");
-
-		if (notificationType == null) {
-			throw new IllegalArgumentException(
-				"The property \"notification.type\" is null");
-		}
-
-		_notificationSenders.remove(notificationType);
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
 	}
 
-	private static final Log _log = LogFactoryUtil.getLog(
-		NotificationSenderFactory.class);
-
-	private final Map<String, NotificationSender> _notificationSenders =
-		new ConcurrentHashMap<>();
+	private ServiceTrackerMap<String, NotificationSender> _serviceTrackerMap;
 
 }

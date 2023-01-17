b1e6074d236e1c698685223374439678643bc193
Lily.Chi <lily.chi@liferay.com>
2023-01-17 19:19:07 +0800

    LPS-172272 Remove Stream usage in GetTrafficSourcesMVCResourceCommand's method body

--- modules/dxp/apps/analytics/analytics-reports-web/src/main/java/com/liferay/analytics/reports/web/internal/portlet/action/GetTrafficSourcesMVCResourceCommand.java
+++ modules/dxp/apps/analytics/analytics-reports-web/src/main/java/com/liferay/analytics/reports/web/internal/portlet/action/GetTrafficSourcesMVCResourceCommand.java
@@ -20,6 +20,7 @@ import com.liferay.analytics.reports.web.internal.model.TimeRange;
 import com.liferay.analytics.reports.web.internal.model.TimeSpan;
 import com.liferay.analytics.reports.web.internal.model.TrafficChannel;
 import com.liferay.analytics.settings.rest.manager.AnalyticsSettingsManager;
+import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONObject;
@@ -45,7 +46,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
-import java.util.stream.Stream;
 
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
@@ -198,19 +198,18 @@ public class GetTrafficSourcesMVCResourceCommand
 		List<TrafficChannel> trafficChannels = _getTrafficChannels(
 			analyticsReportsDataProvider, canonicalURL, companyId, timeRange);
 
-		Stream<TrafficChannel> stream = trafficChannels.stream();
-
 		Comparator<TrafficChannel> comparator = Comparator.comparing(
 			TrafficChannel::getTrafficShare);
 
+		trafficChannels.sort(comparator.reversed());
+
 		return JSONUtil.putAll(
-			stream.sorted(
-				comparator.reversed()
-			).map(
+			TransformUtil.transformToArray(
+				trafficChannels,
 				trafficChannel -> trafficChannel.toJSONObject(
 					liferayPortletRequest, liferayPortletResponse,
-					resourceBundle)
-			).toArray());
+					resourceBundle),
+				JSONObject.class));
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(

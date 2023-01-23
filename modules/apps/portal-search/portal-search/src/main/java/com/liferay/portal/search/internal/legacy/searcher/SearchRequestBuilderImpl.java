0f39e8f4066ceec87222f3d7fd6b3c9a70751fcc
Jiefeng Wu <jeffwu0724@gmail.com>
2023-01-23 14:35:31 -0800

    LPS-173503 remove the usage of optional in SearchRequestBuilderImpl

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/legacy/searcher/SearchRequestBuilderImpl.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/legacy/searcher/SearchRequestBuilderImpl.java
@@ -40,7 +40,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -577,14 +576,17 @@ public class SearchRequestBuilderImpl implements SearchRequestBuilder {
 	private SearchRequestImpl _getSearchRequestImpl(
 		SearchContext searchContext) {
 
-		return Optional.ofNullable(
+		SearchRequestImpl searchRequestImpl =
 			(SearchRequestImpl)searchContext.getAttribute(
-				_SEARCH_CONTEXT_KEY_SEARCH_REQUEST)
-		).orElseGet(
-			() -> setAttribute(
-				searchContext, _SEARCH_CONTEXT_KEY_SEARCH_REQUEST,
-				new SearchRequestImpl(searchContext))
-		);
+				_SEARCH_CONTEXT_KEY_SEARCH_REQUEST);
+
+		if (searchRequestImpl != null) {
+			return searchRequestImpl;
+		}
+
+		return setAttribute(
+			searchContext, _SEARCH_CONTEXT_KEY_SEARCH_REQUEST,
+			new SearchRequestImpl(searchContext));
 	}
 
 	private SearchRequestBuilder _newFederatedSearchRequestBuilder(

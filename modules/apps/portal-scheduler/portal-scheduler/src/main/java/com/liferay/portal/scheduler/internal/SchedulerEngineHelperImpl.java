13dee00822d3c88e60f4b5603f5df02b1d062b68
Tina Tian <tina.tian@liferay.com>
2023-01-23 16:32:21 -0800

    LPS-173546 Remove unused methods (pause and resume) and corresponding tests

--- modules/apps/portal-scheduler/portal-scheduler/src/main/java/com/liferay/portal/scheduler/internal/SchedulerEngineHelperImpl.java
+++ modules/apps/portal-scheduler/portal-scheduler/src/main/java/com/liferay/portal/scheduler/internal/SchedulerEngineHelperImpl.java
@@ -367,13 +367,6 @@ public class SchedulerEngineHelperImpl implements SchedulerEngineHelper {
 	}
 
 	@Override
-	public void pause(String groupName, StorageType storageType)
-		throws SchedulerException {
-
-		_schedulerEngine.pause(groupName, storageType);
-	}
-
-	@Override
 	public void pause(String jobName, String groupName, StorageType storageType)
 		throws SchedulerException {
 
@@ -408,13 +401,6 @@ public class SchedulerEngineHelperImpl implements SchedulerEngineHelper {
 
 		_serviceRegistrations.put(
 			messageListenerClass.getName(), serviceRegistration);
-	}
-
-	@Override
-	public void resume(String groupName, StorageType storageType)
-		throws SchedulerException {
-
-		_schedulerEngine.resume(groupName, storageType);
 	}
 
 	@Override

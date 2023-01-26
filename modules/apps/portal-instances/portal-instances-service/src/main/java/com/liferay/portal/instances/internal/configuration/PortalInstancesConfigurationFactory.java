70a01793533c0334ec8e58562a097b7052d5eee2
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2023-01-26 17:32:09 -0300

    LPS-173655 Remove service property and let PortalInstancesConfigurationFactory implied as immediate

--- modules/apps/portal-instances/portal-instances-service/src/main/java/com/liferay/portal/instances/internal/configuration/PortalInstancesConfigurationFactory.java
+++ modules/apps/portal-instances/portal-instances-service/src/main/java/com/liferay/portal/instances/internal/configuration/PortalInstancesConfigurationFactory.java
@@ -40,7 +40,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	configurationPid = "com.liferay.portal.instances.internal.configuration.PortalInstancesConfiguration",
 	configurationPolicy = ConfigurationPolicy.REQUIRE,
-	service = PortalInstancesConfigurationFactory.class
+	service = {}
 )
 public class PortalInstancesConfigurationFactory {
 

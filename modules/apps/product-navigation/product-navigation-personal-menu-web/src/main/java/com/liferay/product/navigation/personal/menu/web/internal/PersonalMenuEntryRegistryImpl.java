f36bd831846bbad8889ed50528340d24f504fdf7
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-01-27 15:37:22 +0800

    LPS-169905 Rename class PersonalMenuEntryRegistry to PersonalMenuEntryRegistryImpl and implement interface PersonalMenuEntryRegistry

--- modules/apps/product-navigation/product-navigation-personal-menu-web/src/main/java/com/liferay/product/navigation/personal/menu/web/internal/PersonalMenuEntryRegistryImpl.java
+++ modules/apps/product-navigation/product-navigation-personal-menu-web/src/main/java/com/liferay/product/navigation/personal/menu/web/internal/PersonalMenuEntryRegistryImpl.java
@@ -15,12 +15,10 @@
 package com.liferay.product.navigation.personal.menu.web.internal;
 
 import com.liferay.osgi.service.tracker.collections.map.PropertyServiceReferenceComparator;
-import com.liferay.osgi.service.tracker.collections.map.ServiceReferenceMapper;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.product.navigation.personal.menu.PersonalMenuEntry;
+import com.liferay.product.navigation.personal.menu.PersonalMenuEntryRegistry;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,13 +29,16 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 
 /**
  * @author Pei-Jung Lan
  */
 @Component(service = PersonalMenuEntryRegistry.class)
-public class PersonalMenuEntryRegistry {
+public class PersonalMenuEntryRegistryImpl
+	implements PersonalMenuEntryRegistry {
 
+	@Override
 	public List<List<PersonalMenuEntry>> getGroupedPersonalMenuEntries() {
 		SortedSet<String> personalMenuGroups = new TreeSet<>(
 			_serviceTrackerMap.keySet());
@@ -53,18 +54,37 @@ public class PersonalMenuEntryRegistry {
 		return groupedPersonalMenuEntries;
 	}
 
+	@Override
+	public List<PersonalMenuEntry> getPersonalMenuEntries() {
+		SortedSet<String> personalMenuGroups = new TreeSet<>(
+			_serviceTrackerMap.keySet());
+
+		List<PersonalMenuEntry> personalMenuEntries = new ArrayList<>();
+
+		for (String group : personalMenuGroups) {
+			personalMenuEntries.addAll(_serviceTrackerMap.getService(group));
+		}
+
+		return personalMenuEntries;
+	}
+
 	@Activate
 	protected void activate(BundleContext bundleContext) {
 		_serviceTrackerMap = ServiceTrackerMapFactory.openMultiValueMap(
 			bundleContext, PersonalMenuEntry.class,
 			"(product.navigation.personal.menu.group=*)",
-			new PersonalMenuEntryServiceReferenceMapper(),
+			(serviceReference, emitter) -> emitter.emit(
+				String.valueOf(
+					serviceReference.getProperty(
+						"product.navigation.personal.menu.group"))),
 			new PersonalMenuEntryOrderComparator(
 				"product.navigation.personal.menu.entry.order"));
 	}
 
-	private static final Log _log = LogFactoryUtil.getLog(
-		PersonalMenuEntryRegistry.class);
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
+	}
 
 	private ServiceTrackerMap<String, List<PersonalMenuEntry>>
 		_serviceTrackerMap;
@@ -82,30 +102,6 @@ public class PersonalMenuEntryRegistry {
 			ServiceReference<PersonalMenuEntry> serviceReference2) {
 
 			return -super.compare(serviceReference1, serviceReference2);
-		}
-
-	}
-
-	private class PersonalMenuEntryServiceReferenceMapper
-		implements ServiceReferenceMapper<String, PersonalMenuEntry> {
-
-		@Override
-		public void map(
-			ServiceReference<PersonalMenuEntry> serviceReference,
-			Emitter<String> emitter) {
-
-			Integer personalMenuGroup = (Integer)serviceReference.getProperty(
-				"product.navigation.personal.menu.group");
-
-			if (personalMenuGroup == null) {
-				_log.error(
-					"Unable to register personal menu entry because of " +
-						"missing service property " +
-							"\"product.navigation.personal.menu.group\"");
-			}
-			else {
-				emitter.emit(String.valueOf(personalMenuGroup));
-			}
 		}
 
 	}

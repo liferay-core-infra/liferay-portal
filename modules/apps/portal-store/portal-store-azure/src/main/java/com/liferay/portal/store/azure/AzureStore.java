8a9b7e3f14dd2b8cfa0590e052c7ab9930884907
Jiefeng Wu <jeffwu0724@gmail.com>
2023-01-31 12:41:16 -0800

    LPS-174240 remove usage of stream in AzureStore

--- modules/apps/portal-store/portal-store-azure/src/main/java/com/liferay/portal/store/azure/AzureStore.java
+++ modules/apps/portal-store/portal-store-azure/src/main/java/com/liferay/portal/store/azure/AzureStore.java
@@ -58,10 +58,10 @@ import java.io.InputStream;
 import java.io.UncheckedIOException;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Stream;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -208,14 +208,14 @@ public class AzureStore implements Store {
 		PagedIterable<BlobItem> pagedIterable = _blobContainerClient.listBlobs(
 			listBlobsOptions, null);
 
-		Stream<BlobItem> stream = pagedIterable.stream();
+		List<String> fileNames = new ArrayList<>();
 
-		return stream.map(
-			blobItem -> _getFileName(
-				companyId, repositoryId, blobItem.getName())
-		).toArray(
-			String[]::new
-		);
+		for (BlobItem blobItem : pagedIterable) {
+			fileNames.add(
+				_getFileName(companyId, repositoryId, blobItem.getName()));
+		}
+
+		return (String[])fileNames.toArray();
 	}
 
 	@Override
@@ -256,21 +256,21 @@ public class AzureStore implements Store {
 			_blobContainerClient.listBlobsByHierarchy(
 				StringPool.SLASH, listBlobsOptions, null);
 
-		Stream<BlobItem> stream = pagedIterable.stream();
+		List<String> fileVersions = new ArrayList<>();
 
-		return stream.filter(
-			blobItem -> !GetterUtil.getBoolean(blobItem.isPrefix())
-		).map(
-			blobItem -> {
-				String blobItemName = blobItem.getName();
-
-				return blobItemName.substring(prefix.length());
+		for (BlobItem blobItem : pagedIterable) {
+			if (GetterUtil.getBoolean(blobItem.isPrefix())) {
+				continue;
 			}
-		).sorted(
-			DLUtil::compareVersions
-		).toArray(
-			String[]::new
-		);
+
+			String blobItemName = blobItem.getName();
+
+			fileVersions.add(blobItemName.substring(prefix.length()));
+		}
+
+		Collections.sort(fileVersions, DLUtil::compareVersions);
+
+		return (String[])fileVersions.toArray();
 	}
 
 	@Override

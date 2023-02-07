43c168bc822359b83374cb36ccb1a608876b841a
Joao Victor <joao.victor@liferay.com>
2023-02-07 18:51:08 -0300

    LPS-173597 Create a module and move RawMetadataProcessorImpl there

--- modules/apps/document-library/document-library-preview-raw-metadata/src/main/java/com/liferay/document/library/preview/raw/metadata/internal/RawMetadataProcessorImpl.java
+++ modules/apps/document-library/document-library-preview-raw-metadata/src/main/java/com/liferay/document/library/preview/raw/metadata/internal/RawMetadataProcessorImpl.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portlet.documentlibrary.util;
+package com.liferay.document.library.preview.raw.metadata.internal;
 
 import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.document.library.kernel.model.DLFileEntryConstants;

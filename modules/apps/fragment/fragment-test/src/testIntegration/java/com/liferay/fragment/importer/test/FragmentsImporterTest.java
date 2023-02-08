447c873755add1ea163d0495b568a7ee571f3232
Janis Zhang <janis.zhang@liferay.com>
2023-02-08 21:10:26 +0800

    LPS-175105 Remove stream usages in FragmentsImporterTest

--- modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/importer/test/FragmentsImporterTest.java
+++ modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/importer/test/FragmentsImporterTest.java
@@ -44,6 +44,7 @@ import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
 import com.liferay.portal.kernel.util.Validator;
@@ -64,8 +65,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -291,17 +290,10 @@ public class FragmentsImporterTest {
 
 		FragmentCollection fragmentCollection = fragmentCollections.get(0);
 
-		List<FragmentEntry> fragmentEntries =
+		List<String> fragmentEntryNames = ListUtil.filter(
 			_fragmentEntryLocalService.getFragmentEntries(
-				fragmentCollection.getFragmentCollectionId());
-
-		Stream<FragmentEntry> stream = fragmentEntries.stream();
-
-		List<String> fragmentEntryNames = stream.map(
-			fragmentEntry -> fragmentEntry.getFragmentEntryKey()
-		).collect(
-			Collectors.toList()
-		);
+				fragmentCollection.getFragmentCollectionId()),
+			fragmentEntry -> fragmentEntry.getFragmentEntryKey());
 
 		Assert.assertTrue(fragmentEntryNames.contains("resource"));
 	}
@@ -335,18 +327,11 @@ public class FragmentsImporterTest {
 
 		FragmentCollection fragmentCollection = fragmentCollections.get(0);
 
-		List<FragmentEntry> fragmentEntries =
+		List<FragmentEntry> filteredFragmentEntries = ListUtil.filter(
 			_fragmentEntryLocalService.getFragmentEntries(
-				fragmentCollection.getFragmentCollectionId());
-
-		Stream<FragmentEntry> stream = fragmentEntries.stream();
-
-		List<FragmentEntry> filteredFragmentEntries = stream.filter(
+				fragmentCollection.getFragmentCollectionId()),
 			fragmentEntry -> Objects.equals(
-				fragmentEntry.getName(), "Fragment With Icon")
-		).collect(
-			Collectors.toList()
-		);
+				fragmentEntry.getName(), "Fragment With Icon"));
 
 		Assert.assertEquals(
 			filteredFragmentEntries.toString(), 1,
@@ -376,18 +361,12 @@ public class FragmentsImporterTest {
 
 		FragmentCollection fragmentCollection = fragmentCollections.get(0);
 
-		List<FragmentEntry> fragmentEntries =
+		List<FragmentEntry> filteredFragmentEntries = ListUtil.filter(
 			_fragmentEntryLocalService.getFragmentEntries(
-				fragmentCollection.getFragmentCollectionId());
-
-		Stream<FragmentEntry> stream = fragmentEntries.stream();
-
-		List<FragmentEntry> filteredFragmentEntries = stream.filter(
+				fragmentCollection.getFragmentCollectionId()),
 			fragmentEntry -> Objects.equals(
-				fragmentEntry.getName(), "Fragment With Invalid Configuration")
-		).collect(
-			Collectors.toList()
-		);
+				fragmentEntry.getName(),
+				"Fragment With Invalid Configuration"));
 
 		Assert.assertEquals(
 			filteredFragmentEntries.toString(), 1,
@@ -417,18 +396,11 @@ public class FragmentsImporterTest {
 
 		FragmentCollection fragmentCollection = fragmentCollections.get(0);
 
-		List<FragmentEntry> fragmentEntries =
+		List<FragmentEntry> filteredFragmentEntries = ListUtil.filter(
 			_fragmentEntryLocalService.getFragmentEntries(
-				fragmentCollection.getFragmentCollectionId());
-
-		Stream<FragmentEntry> stream = fragmentEntries.stream();
-
-		List<FragmentEntry> filteredFragmentEntries = stream.filter(
+				fragmentCollection.getFragmentCollectionId()),
 			fragmentEntry -> Objects.equals(
-				fragmentEntry.getName(), "Fragment With Invalid HTML")
-		).collect(
-			Collectors.toList()
-		);
+				fragmentEntry.getName(), "Fragment With Invalid HTML"));
 
 		Assert.assertEquals(
 			filteredFragmentEntries.toString(), 1,
@@ -468,18 +440,11 @@ public class FragmentsImporterTest {
 
 		FragmentCollection fragmentCollection = fragmentCollections.get(0);
 
-		List<FragmentEntry> fragmentEntries =
+		List<FragmentEntry> filteredFragmentEntries = ListUtil.filter(
 			_fragmentEntryLocalService.getFragmentEntries(
-				fragmentCollection.getFragmentCollectionId());
-
-		Stream<FragmentEntry> stream = fragmentEntries.stream();
-
-		List<FragmentEntry> filteredFragmentEntries = stream.filter(
+				fragmentCollection.getFragmentCollectionId()),
 			fragmentEntry -> Objects.equals(
-				fragmentEntry.getName(), "Input Fragment With Type Options")
-		).collect(
-			Collectors.toList()
-		);
+				fragmentEntry.getName(), "Input Fragment With Type Options"));
 
 		Assert.assertEquals(
 			filteredFragmentEntries.toString(), 1,
@@ -510,21 +475,14 @@ public class FragmentsImporterTest {
 			ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
 
 		try {
-			List<FragmentsImporterResultEntry> fragmentsImporterResultEntries =
-				_fragmentsImporter.importFragmentEntries(
-					_user.getUserId(), _group.getGroupId(), 0, _file, false);
-
-			Stream<FragmentsImporterResultEntry> stream =
-				fragmentsImporterResultEntries.stream();
-
 			List<FragmentsImporterResultEntry>
-				filteredFragmentsImporterResultEntries = stream.filter(
+				filteredFragmentsImporterResultEntries = ListUtil.filter(
+					_fragmentsImporter.importFragmentEntries(
+						_user.getUserId(), _group.getGroupId(), 0, _file,
+						false),
 					fragmentsImporterResultEntry -> Objects.equals(
 						fragmentsImporterResultEntry.getName(),
-						"React Fragment With Invalid Configuration")
-				).collect(
-					Collectors.toList()
-				);
+						"React Fragment With Invalid Configuration"));
 
 			Assert.assertEquals(
 				filteredFragmentsImporterResultEntries.toString(), 1,
@@ -648,19 +606,12 @@ public class FragmentsImporterTest {
 
 		FragmentCollection fragmentCollection = fragmentCollections.get(0);
 
-		List<FragmentEntry> fragmentEntries =
-			_fragmentEntryLocalService.getFragmentEntries(
-				fragmentCollection.getFragmentCollectionId());
-
-		Stream<FragmentEntry> stream = fragmentEntries.stream();
-
 		List<String> expectedFragmentsEntries = _fragmentEntryTypes.get(type);
 
-		List<FragmentEntry> actualFragmentEntries = stream.filter(
-			fragmentEntry -> fragmentEntry.getType() == type
-		).collect(
-			Collectors.toList()
-		);
+		List<FragmentEntry> actualFragmentEntries = ListUtil.filter(
+			_fragmentEntryLocalService.getFragmentEntries(
+				fragmentCollection.getFragmentCollectionId()),
+			fragmentEntry -> fragmentEntry.getType() == type);
 
 		Assert.assertEquals(
 			actualFragmentEntries.toString(), expectedFragmentsEntries.size(),

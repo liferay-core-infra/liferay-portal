5256abaa48289edd0efc1e2f0a3975dbd6840446
Janis Zhang <janis.zhang@liferay.com>
2023-02-13 14:30:47 +0800

    LPS-173791 Using TreeMap is more neat

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/info/item/renderer/ObjectEntryRowInfoItemRenderer.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/info/item/renderer/ObjectEntryRowInfoItemRenderer.java
@@ -41,7 +41,6 @@ import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.util.FastDateFormatFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
 
@@ -50,11 +49,11 @@ import java.io.Serializable;
 import java.text.Format;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+import java.util.TreeMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -166,12 +165,9 @@ public class ObjectEntryRowInfoItemRenderer
 				return null;
 			});
 
-		Map<String, Serializable> stringSerializableMap = new LinkedHashMap<>(
-			entries.size());
+		Map<String, Serializable> stringSerializableMap = new TreeMap<>();
 
-		for (Map.Entry<String, Serializable> entry :
-				ListUtil.sort(entries, Map.Entry.comparingByKey())) {
-
+		for (Map.Entry<String, Serializable> entry : entries) {
 			if (entry.getValue() == null) {
 				stringSerializableMap.put(entry.getKey(), StringPool.BLANK);
 

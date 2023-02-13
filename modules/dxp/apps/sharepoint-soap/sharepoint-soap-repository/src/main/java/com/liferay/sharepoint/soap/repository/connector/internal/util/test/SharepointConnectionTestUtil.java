a13ca6456d23ce218318d9750b675f86065c5adf
Jiefeng Wu <jeffwu0724@gmail.com>
2023-02-13 15:05:29 -0800

    LPS-175651 remove stream usage in SharepointConnectionTestUtil

--- modules/dxp/apps/sharepoint-soap/sharepoint-soap-repository/src/main/java/com/liferay/sharepoint/soap/repository/connector/internal/util/test/SharepointConnectionTestUtil.java
+++ modules/dxp/apps/sharepoint-soap/sharepoint-soap-repository/src/main/java/com/liferay/sharepoint/soap/repository/connector/internal/util/test/SharepointConnectionTestUtil.java
@@ -31,8 +31,6 @@ import java.net.URL;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * @author Adolfo Pérez
@@ -80,9 +78,19 @@ public class SharepointConnectionTestUtil {
 		try (BufferedReader bufferedReader = new BufferedReader(
 				new InputStreamReader(httpURLConnection.getInputStream()))) {
 
-			Stream<String> payloadStream = bufferedReader.lines();
+			StringBundler sb = new StringBundler();
 
-			String payload = payloadStream.collect(Collectors.joining());
+			String line = null;
+
+			while ((line = bufferedReader.readLine()) != null) {
+				sb.append(line);
+			}
+
+			if (sb.index() > 1) {
+				sb.setIndex(sb.index() - 1);
+			}
+
+			String payload = sb.toString();
 
 			Matcher matcher = _pattern.matcher(payload);
 

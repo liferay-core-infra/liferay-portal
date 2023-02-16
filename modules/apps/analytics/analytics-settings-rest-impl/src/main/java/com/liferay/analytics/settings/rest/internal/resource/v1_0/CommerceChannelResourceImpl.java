ad589740112554150c03080350a2bd2bc27f9cb3
Joao Victor <joao.victor@liferay.com>
2023-02-16 13:28:56 -0300

    LPS-175886 Change CommerceChannelDTOConverter reference to be DTOConverter

--- modules/apps/analytics/analytics-settings-rest-impl/src/main/java/com/liferay/analytics/settings/rest/internal/resource/v1_0/CommerceChannelResourceImpl.java
+++ modules/apps/analytics/analytics-settings-rest-impl/src/main/java/com/liferay/analytics/settings/rest/internal/resource/v1_0/CommerceChannelResourceImpl.java
@@ -17,14 +17,15 @@ package com.liferay.analytics.settings.rest.internal.resource.v1_0;
 import com.liferay.analytics.settings.rest.dto.v1_0.CommerceChannel;
 import com.liferay.analytics.settings.rest.internal.client.AnalyticsCloudClient;
 import com.liferay.analytics.settings.rest.internal.client.model.AnalyticsChannel;
-import com.liferay.analytics.settings.rest.internal.dto.v1_0.converter.CommerceChannelDTOConverter;
 import com.liferay.analytics.settings.rest.internal.dto.v1_0.converter.CommerceChannelDTOConverterContext;
 import com.liferay.analytics.settings.rest.internal.util.SortUtil;
 import com.liferay.analytics.settings.rest.resource.v1_0.CommerceChannelResource;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.service.GroupService;
 import com.liferay.portal.kernel.util.LinkedHashMapBuilder;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.pagination.Pagination;
 
@@ -105,8 +106,10 @@ public class CommerceChannelResourceImpl
 
 	private long[] _classNameIds;
 
-	@Reference
-	private CommerceChannelDTOConverter _commerceChannelDTOConverter;
+	@Reference(
+		target = "(dto.class.name=com.liferay.portal.kernel.model.Group)"
+	)
+	private DTOConverter<Group, CommerceChannel> _commerceChannelDTOConverter;
 
 	@Reference
 	private GroupService _groupService;

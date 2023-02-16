5b7cf17313995f78ffbdec9be148cf39d0dee10e
Joao Victor <joao.victor@liferay.com>
2023-02-16 16:53:51 -0300

    LPS-175994 Change WikiNodeDTOConverter reference to be DTOConverter

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/WikiNodeResourceImpl.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/WikiNodeResourceImpl.java
@@ -16,7 +16,7 @@ package com.liferay.headless.delivery.internal.resource.v1_0;
 
 import com.liferay.headless.common.spi.service.context.ServiceContextRequestUtil;
 import com.liferay.headless.delivery.dto.v1_0.WikiNode;
-import com.liferay.headless.delivery.internal.dto.v1_0.converter.WikiNodeDTOConverter;
+import com.liferay.headless.delivery.internal.dto.v1_0.converter.constants.DTOConverterConstants;
 import com.liferay.headless.delivery.internal.odata.entity.v1_0.WikiNodeEntityModel;
 import com.liferay.headless.delivery.resource.v1_0.WikiNodeResource;
 import com.liferay.portal.kernel.change.tracking.CTAware;
@@ -31,6 +31,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.odata.entity.EntityModel;
 import com.liferay.portal.vulcan.aggregation.Aggregation;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
 import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
 import com.liferay.portal.vulcan.pagination.Page;
@@ -268,8 +269,9 @@ public class WikiNodeResourceImpl extends BaseWikiNodeResourceImpl {
 	@Reference
 	private DTOConverterRegistry _dtoConverterRegistry;
 
-	@Reference
-	private WikiNodeDTOConverter _wikiNodeDTOConverter;
+	@Reference(target = DTOConverterConstants.WIKI_NODE_DTO_CONVERTER)
+	private DTOConverter<com.liferay.wiki.model.WikiNode, WikiNode>
+		_wikiNodeDTOConverter;
 
 	@Reference
 	private WikiNodeLocalService _wikiNodeLocalService;

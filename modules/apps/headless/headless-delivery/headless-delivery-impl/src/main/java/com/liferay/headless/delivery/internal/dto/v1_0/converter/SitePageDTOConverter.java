d7ebf4e2c775708ee634941b35a20be7948c7429
Joao Victor <joao.victor@liferay.com>
2023-02-16 17:04:47 -0300

    LPS-175994 Change PageDefinitionDTOConverter reference to be DTOConverter

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/converter/SitePageDTOConverter.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/converter/SitePageDTOConverter.java
@@ -21,6 +21,7 @@ import com.liferay.document.library.kernel.service.DLAppService;
 import com.liferay.document.library.util.DLURLHelper;
 import com.liferay.dynamic.data.mapping.kernel.StorageEngineManager;
 import com.liferay.headless.delivery.dto.v1_0.Experience;
+import com.liferay.headless.delivery.dto.v1_0.PageDefinition;
 import com.liferay.headless.delivery.dto.v1_0.SitePage;
 import com.liferay.headless.delivery.dto.v1_0.TaxonomyCategoryBrief;
 import com.liferay.headless.delivery.dto.v1_0.util.CreatorUtil;
@@ -232,8 +233,9 @@ public class SitePageDTOConverter implements DTOConverter<Layout, SitePage> {
 	@Reference
 	private LayoutSEOEntryLocalService _layoutSEOEntryLocalService;
 
-	@Reference
-	private PageDefinitionDTOConverter _pageDefinitionDTOConverter;
+	@Reference(target = DTOConverterConstants.PAGE_DEFINITION_DTO_CONVERTER)
+	private DTOConverter<LayoutStructure, PageDefinition>
+		_pageDefinitionDTOConverter;
 
 	@Reference
 	private Portal _portal;

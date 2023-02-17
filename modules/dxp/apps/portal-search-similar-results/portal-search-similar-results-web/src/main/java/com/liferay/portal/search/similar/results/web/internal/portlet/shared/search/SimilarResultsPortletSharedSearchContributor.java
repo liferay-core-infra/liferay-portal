42c4272e21e8d82d2cb3d91f57e9a4ab47c526d0
Lily.Chi <lily.chi@liferay.com>
2023-02-17 16:50:58 +0800

    LPS-172112 Apply

--- modules/dxp/apps/portal-search-similar-results/portal-search-similar-results-web/src/main/java/com/liferay/portal/search/similar/results/web/internal/portlet/shared/search/SimilarResultsPortletSharedSearchContributor.java
+++ modules/dxp/apps/portal-search-similar-results/portal-search-similar-results-web/src/main/java/com/liferay/portal/search/similar/results/web/internal/portlet/shared/search/SimilarResultsPortletSharedSearchContributor.java
@@ -173,16 +173,12 @@ public class SimilarResultsPortletSharedSearchContributor
 			return;
 		}
 
-		Optional<String> classNameOptional = criteria.getTypeOptional();
+		String className = criteria.getType();
 
-		classNameOptional.ifPresent(
-			className -> {
-				if (!Validator.isBlank(className)) {
-					searchRequestBuilder.addComplexQueryPart(
-						_getComplexQueryPart(
-							_getEntryClassNameQuery(className)));
-				}
-			});
+		if ((className != null) && !Validator.isBlank(className)) {
+			searchRequestBuilder.addComplexQueryPart(
+				_getComplexQueryPart(_getEntryClassNameQuery(className)));
+		}
 	}
 
 	private void _filterByGroupId(

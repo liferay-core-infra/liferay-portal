970a8f6faa9af39ee98668870b4b12b2ef043b58
Janis Zhang <janis.zhang@liferay.com>
2023-02-21 09:44:07 +0800

    LPS-175361 SF, as used

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/info/item/renderer/JournalArticleDDMTemplateInfoItemTemplatedRenderer.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/info/item/renderer/JournalArticleDDMTemplateInfoItemTemplatedRenderer.java
@@ -79,6 +79,9 @@ public class JournalArticleDDMTemplateInfoItemTemplatedRenderer
 	public List<InfoItemRendererTemplate> getInfoItemRendererTemplates(
 		String className, String classTypeKey, Locale locale) {
 
+		List<InfoItemRendererTemplate> infoItemRendererTemplates =
+			new ArrayList<>();
+
 		List<DDMStructure> ddmStructures =
 			_ddmStructureLocalService.getClassStructures(
 				CompanyThreadLocal.getCompanyId(),
@@ -91,9 +94,6 @@ public class JournalArticleDDMTemplateInfoItemTemplatedRenderer
 					ddmStructure.getStructureId(),
 					GetterUtil.getLong(classTypeKey)));
 		}
-
-		List<InfoItemRendererTemplate> infoItemRendererTemplates =
-			new ArrayList<>();
 
 		for (DDMStructure ddmStructure : ddmStructures) {
 			for (DDMTemplate ddmTemplate : ddmStructure.getTemplates()) {

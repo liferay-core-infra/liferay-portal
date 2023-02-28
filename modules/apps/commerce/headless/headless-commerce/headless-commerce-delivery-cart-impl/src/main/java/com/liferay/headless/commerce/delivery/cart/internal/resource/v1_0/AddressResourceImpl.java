ef5167799fd5379e67aa51fee4a118ddb50dddd4
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2023-02-28 15:03:53 -0300

    LPS-177028 Change AddressDTOConverter references to be DTOConverter

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-cart-impl/src/main/java/com/liferay/headless/commerce/delivery/cart/internal/resource/v1_0/AddressResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-cart-impl/src/main/java/com/liferay/headless/commerce/delivery/cart/internal/resource/v1_0/AddressResourceImpl.java
@@ -20,8 +20,9 @@ import com.liferay.commerce.service.CommerceAddressService;
 import com.liferay.commerce.service.CommerceOrderService;
 import com.liferay.headless.commerce.delivery.cart.dto.v1_0.Address;
 import com.liferay.headless.commerce.delivery.cart.dto.v1_0.Cart;
-import com.liferay.headless.commerce.delivery.cart.internal.dto.v1_0.AddressDTOConverter;
+import com.liferay.headless.commerce.delivery.cart.internal.dto.v1_0.constants.DTOConverterConstants;
 import com.liferay.headless.commerce.delivery.cart.resource.v1_0.AddressResource;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
 import com.liferay.portal.vulcan.fields.NestedField;
 import com.liferay.portal.vulcan.fields.NestedFieldId;
@@ -78,8 +79,8 @@ public class AddressResourceImpl
 				contextAcceptLanguage.getPreferredLocale()));
 	}
 
-	@Reference
-	private AddressDTOConverter _addressDTOConverter;
+	@Reference(target = DTOConverterConstants.ADDRESS_DTO_CONVERTER)
+	private DTOConverter<CommerceAddress, Address> _addressDTOConverter;
 
 	@Reference
 	private CommerceAddressService _commerceAddressService;

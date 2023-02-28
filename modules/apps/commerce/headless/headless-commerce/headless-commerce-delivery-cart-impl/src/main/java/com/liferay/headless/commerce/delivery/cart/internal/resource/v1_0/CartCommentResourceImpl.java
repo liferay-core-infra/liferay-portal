9eb905dec2b8f405bc0137f3a5df4a0aa4adc913
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2023-02-28 14:52:51 -0300

    LPS-177028 Change NoteDTOConverter references to be DTOConverter

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-cart-impl/src/main/java/com/liferay/headless/commerce/delivery/cart/internal/resource/v1_0/CartCommentResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-cart-impl/src/main/java/com/liferay/headless/commerce/delivery/cart/internal/resource/v1_0/CartCommentResourceImpl.java
@@ -21,9 +21,10 @@ import com.liferay.commerce.service.CommerceOrderService;
 import com.liferay.headless.commerce.core.util.ServiceContextHelper;
 import com.liferay.headless.commerce.delivery.cart.dto.v1_0.Cart;
 import com.liferay.headless.commerce.delivery.cart.dto.v1_0.CartComment;
-import com.liferay.headless.commerce.delivery.cart.internal.dto.v1_0.NoteDTOConverter;
+import com.liferay.headless.commerce.delivery.cart.internal.dto.v1_0.constants.DTOConverterConstants;
 import com.liferay.headless.commerce.delivery.cart.resource.v1_0.CartCommentResource;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
 import com.liferay.portal.vulcan.fields.NestedField;
 import com.liferay.portal.vulcan.fields.NestedFieldId;
@@ -143,8 +144,8 @@ public class CartCommentResourceImpl
 	@Reference
 	private CommerceOrderService _commerceOrderService;
 
-	@Reference
-	private NoteDTOConverter _noteDTOConverter;
+	@Reference(target = DTOConverterConstants.NOTE_DTO_CONVERTER)
+	private DTOConverter<CommerceOrderNote, CartComment> _noteDTOConverter;
 
 	@Reference
 	private ServiceContextHelper _serviceContextHelper;

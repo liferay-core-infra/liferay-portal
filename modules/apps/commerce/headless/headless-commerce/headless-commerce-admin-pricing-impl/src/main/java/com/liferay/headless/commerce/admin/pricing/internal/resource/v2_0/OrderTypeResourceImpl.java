0230934f7b0664ec65be90d527fc96f1f9398bc0
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2023-03-02 14:34:39 -0300

    LPS-172925 Change OrderTypeDTOConverter reference to be DTOConverter

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-impl/src/main/java/com/liferay/headless/commerce/admin/pricing/internal/resource/v2_0/OrderTypeResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-impl/src/main/java/com/liferay/headless/commerce/admin/pricing/internal/resource/v2_0/OrderTypeResourceImpl.java
@@ -16,13 +16,15 @@ package com.liferay.headless.commerce.admin.pricing.internal.resource.v2_0;
 
 import com.liferay.commerce.discount.model.CommerceDiscountOrderTypeRel;
 import com.liferay.commerce.discount.service.CommerceDiscountOrderTypeRelService;
+import com.liferay.commerce.model.CommerceOrderType;
 import com.liferay.commerce.price.list.model.CommercePriceListOrderTypeRel;
 import com.liferay.commerce.price.list.service.CommercePriceListOrderTypeRelService;
 import com.liferay.headless.commerce.admin.pricing.dto.v2_0.DiscountOrderType;
 import com.liferay.headless.commerce.admin.pricing.dto.v2_0.OrderType;
 import com.liferay.headless.commerce.admin.pricing.dto.v2_0.PriceListOrderType;
-import com.liferay.headless.commerce.admin.pricing.internal.dto.v2_0.converter.OrderTypeDTOConverter;
+import com.liferay.headless.commerce.admin.pricing.internal.dto.v2_0.converter.constants.DTOConverterConstants;
 import com.liferay.headless.commerce.admin.pricing.resource.v2_0.OrderTypeResource;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
 import com.liferay.portal.vulcan.fields.NestedField;
 import com.liferay.portal.vulcan.fields.NestedFieldSupport;
@@ -76,7 +78,7 @@ public class OrderTypeResourceImpl
 	private CommercePriceListOrderTypeRelService
 		_commercePriceListOrderTypeRelService;
 
-	@Reference
-	private OrderTypeDTOConverter _orderTypeDTOConverter;
+	@Reference(target = DTOConverterConstants.ORDER_TYPE_DTO_CONVERTER)
+	private DTOConverter<CommerceOrderType, OrderType> _orderTypeDTOConverter;
 
 }

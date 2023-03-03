5ab7ae02d78e7f5756949139ac01669ac353b3cd
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2023-03-03 14:42:59 -0300

    LPS-176895 Create DTO Constants file

---
+++ modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/dto/v1_0/converter/constants/DTOConverterConstants.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.search.experiences.rest.internal.dto.v1_0.converter.constants;
+
+/**
+ * @author Renan Vasconcelos
+ */
+public class DTOConverterConstants {
+
+	public static final String SXP_BLUEPRINT_DTO_CONVERTER =
+		"(component.name=com.liferay.search.experiences.rest.internal.dto." +
+			"v1_0.converter.SXPBlueprintDTOConverter)";
+
+	public static final String SXP_ELEMENT_DTO_CONVERTER =
+		"(component.name=com.liferay.search.experiences.rest.internal.dto." +
+			"v1_0.converter.SXPElementDTOConverter)";
+
+}

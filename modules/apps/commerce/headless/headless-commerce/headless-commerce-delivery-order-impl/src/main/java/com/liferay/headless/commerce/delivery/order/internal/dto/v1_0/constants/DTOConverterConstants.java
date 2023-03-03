d8c24d415f8bbef4f3024d3740def9e36e6fcd25
Joao Victor <joao.victor@liferay.com>
2023-03-03 11:50:49 -0300

    LPS-177024 Create constants file

---
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-order-impl/src/main/java/com/liferay/headless/commerce/delivery/order/internal/dto/v1_0/constants/DTOConverterConstants.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.commerce.delivery.order.internal.dto.v1_0.constants;
+
+/**
+ * @author Joao Victor
+ */
+public class DTOConverterConstants {
+
+	public static final String PLACED_ORDER_ADDRESS_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.order." +
+			"internal.dto.v1_0.PlacedOrderAddressDTOConverter)";
+
+	public static final String PLACED_ORDER_COMMENT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.order." +
+			"internal.dto.v1_0.PlacedOrderCommentDTOConverter)";
+
+	public static final String PLACED_ORDER_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.order." +
+			"internal.dto.v1_0.PlacedOrderDTOConverter)";
+
+	public static final String PLACED_ORDER_ITEM_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.order." +
+			"internal.dto.v1_0.PlacedOrderItemDTOConverter)";
+
+	public static final String PLACED_ORDER_ITEM_SHIPMENT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.order." +
+			"internal.dto.v1_0.PlacedOrderItemShipmentDTOConverter)";
+
+}

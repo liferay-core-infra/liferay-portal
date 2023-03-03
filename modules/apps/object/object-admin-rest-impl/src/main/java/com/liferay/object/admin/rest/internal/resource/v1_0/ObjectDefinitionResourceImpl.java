1849cc9e664b82a352007946360e318d93659117
Tina Tian <tina.tian@liferay.com>
2023-03-02 18:37:16 -0800

    LPS-175891 Use constants

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
@@ -27,6 +27,7 @@ import com.liferay.object.admin.rest.dto.v1_0.ObjectView;
 import com.liferay.object.admin.rest.dto.v1_0.Status;
 import com.liferay.object.admin.rest.dto.v1_0.util.ObjectActionUtil;
 import com.liferay.object.admin.rest.internal.dto.v1_0.converter.ObjectValidationRuleDTOConverter;
+import com.liferay.object.admin.rest.internal.dto.v1_0.converter.constants.DTOConverterConstants;
 import com.liferay.object.admin.rest.internal.dto.v1_0.util.ObjectFieldSettingUtil;
 import com.liferay.object.admin.rest.internal.dto.v1_0.util.ObjectFieldUtil;
 import com.liferay.object.admin.rest.internal.dto.v1_0.util.ObjectLayoutUtil;
@@ -837,9 +838,7 @@ public class ObjectDefinitionResourceImpl
 	@Reference
 	private ObjectDefinitionService _objectDefinitionService;
 
-	@Reference(
-		target = "(component.name=com.liferay.object.admin.rest.internal.dto.v1_0.converter.ObjectFieldDTOConverter)"
-	)
+	@Reference(target = DTOConverterConstants.OBJECT_FIELD_DTO_CONVERTER)
 	private DTOConverter<com.liferay.object.model.ObjectField, ObjectField>
 		_objectFieldDTOConverter;
 
@@ -858,9 +857,7 @@ public class ObjectDefinitionResourceImpl
 	@Reference
 	private ObjectLayoutResource.Factory _objectLayoutResourceFactory;
 
-	@Reference(
-		target = "(component.name=com.liferay.object.admin.rest.internal.dto.v1_0.converter.ObjectRelationshipDTOConverter)"
-	)
+	@Reference(target = DTOConverterConstants.OBJECT_RELATIONSHIP_DTO_CONVERTER)
 	private DTOConverter
 		<com.liferay.object.model.ObjectRelationship, ObjectRelationship>
 			_objectRelationshipDTOConverter;
@@ -882,9 +879,7 @@ public class ObjectDefinitionResourceImpl
 	private ObjectValidationRuleResource.Factory
 		_objectValidationRuleResourceFactory;
 
-	@Reference(
-		target = "(component.name=com.liferay.object.admin.rest.internal.dto.v1_0.converter.ObjectViewDTOConverter)"
-	)
+	@Reference(target = DTOConverterConstants.OBJECT_VIEW_DTO_CONVERTER)
 	private DTOConverter<com.liferay.object.model.ObjectView, ObjectView>
 		_objectViewDTOConverter;
 

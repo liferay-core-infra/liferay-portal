fa46416f7ef5d0c1a80b171dc6c721ea368d8daf
Joao Victor <joao.victor@liferay.com>
2023-03-03 12:36:07 -0300

    LPS-175994 Create constants files

---
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/converter/constants/DTOConverterConstants.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.delivery.internal.dto.v1_0.converter.constants;
+
+/**
+ * @author Joao Victor
+ */
+public class DTOConverterConstants {
+
+	public static final String BLOG_POSTING_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.delivery.internal.dto.v1_0." +
+			"converter.BlogPostingDTOConverter)";
+
+	public static final String DOCUMENT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.delivery.internal.dto.v1_0." +
+			"converter.DocumentDTOConverter)";
+
+	public static final String DOCUMENT_FOLDER_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.delivery.internal.dto.v1_0." +
+			"converter.DocumentFolderDTOConverter)";
+
+	public static final String EXPERIENCE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.delivery.internal.dto.v1_0." +
+			"converter.ExperienceDTOConverter)";
+
+	public static final String KNOWLEDGE_BASE_ARTICLE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.delivery.internal.dto.v1_0." +
+			"converter.KnowledgeBaseArticleDTOConverter)";
+
+	public static final String MESSAGE_BOARD_MESSAGE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.delivery.internal.dto.v1_0." +
+			"converter.MessageBoardMessageDTOConverter)";
+
+	public static final String MESSAGE_BOARD_SECTION_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.delivery.internal.dto.v1_0." +
+			"converter.MessageBoardSectionDTOConverter)";
+
+	public static final String MESSAGE_BOARD_THREAD_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.delivery.internal.dto.v1_0." +
+			"converter.MessageBoardThreadDTOConverter)";
+
+	public static final String PAGE_DEFINITION_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.delivery.internal.dto.v1_0." +
+			"converter.PageDefinitionDTOConverter)";
+
+	public static final String SITE_PAGE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.delivery.internal.dto.v1_0." +
+			"converter.SitePageDTOConverter)";
+
+	public static final String STRUCTURED_CONTENT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.delivery.internal.dto.v1_0." +
+			"converter.StructuredContentDTOConverter)";
+
+	public static final String STRUCTURED_CONTENT_FOLDER_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.delivery.internal.dto.v1_0." +
+			"converter.StructuredContentFolderDTOConverter)";
+
+	public static final String WIKI_NODE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.delivery.internal.dto.v1_0." +
+			"converter.WikiNodeDTOConverter)";
+
+	public static final String WIKI_PAGE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.delivery.internal.dto.v1_0." +
+			"converter.WikiPageDTOConverter)";
+
+}

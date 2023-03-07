3547acc6bbf245aaa4ebd10c9d83d7107782aab4
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2023-03-07 17:17:13 -0300

    LPS-172925 Create DTO Constants file

---
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-impl/src/main/java/com/liferay/headless/commerce/admin/pricing/internal/dto/v1_0/converter/constants/DTOConverterConstants.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.commerce.admin.pricing.internal.dto.v1_0.converter.constants;
+
+/**
+ * @author Renan Vasconcelos
+ */
+public class DTOConverterConstants {
+
+	public static final String DISCOUNT_ACCOUNT_GROUP_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v1_0.converter.DiscountAccountGroupDTOConverter)";
+
+	public static final String DISCOUNT_CATEGORY_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v1_0.converter.DiscountCategoryDTOConverter)";
+
+	public static final String DISCOUNT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v1_0.converter.DiscountDTOConverter)";
+
+	public static final String DISCOUNT_PRODUCT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v1_0.converter.DiscountProductDTOConverter)";
+
+	public static final String DISCOUNT_RULE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v1_0.converter.DiscountRuleDTOConverter)";
+
+	public static final String PRICE_ENTRY_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v1_0.converter.PriceEntryDTOConverter)";
+
+	public static final String PRICE_LIST_ACCOUNT_GROUP_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v1_0.converter.PriceListAccountGroupDTOConverter)";
+
+	public static final String PRICE_LIST_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v1_0.converter.PriceListDTOConverter)";
+
+	public static final String TIER_PRICE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v1_0.converter.TierPriceDTOConverter)";
+
+}
---
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-impl/src/main/java/com/liferay/headless/commerce/admin/pricing/internal/dto/v2_0/converter/constants/DTOConverterConstants.java
@@ -0,0 +1,138 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.commerce.admin.pricing.internal.dto.v2_0.converter.constants;
+
+/**
+ * @author Renan Vasconcelos
+ */
+public class DTOConverterConstants {
+
+	public static final String ACCOUNT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.AccountDTOConverter)";
+
+	public static final String ACCOUNT_GROUP_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.AccountGroupDTOConverter)";
+
+	public static final String CATEGORY_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.CategoryDTOConverter)";
+
+	public static final String DISCOUNT_ACCOUNT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.DiscountAccountDTOConverter)";
+
+	public static final String DISCOUNT_ACCOUNT_GROUP_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.DiscountAccountGroupDTOConverter)";
+
+	public static final String DISCOUNT_CATEGORY_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.DiscountCategoryDTOConverter)";
+
+	public static final String DISCOUNT_CHANNEL_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.DiscountChannelDTOConverter)";
+
+	public static final String DISCOUNT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.DiscountDTOConverter)";
+
+	public static final String DISCOUNT_ORDER_TYPE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.DiscountOrderTypeDTOConverter)";
+
+	public static final String DISCOUNT_PRODUCT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.DiscountProductDTOConverter)";
+
+	public static final String DISCOUNT_PRODUCT_GROUP_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.DiscountProductGroupDTOConverter)";
+
+	public static final String DISCOUNT_RULE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.DiscountRuleDTOConverter)";
+
+	public static final String DISCOUNT_SKU_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.DiscountSkuDTOConverter)";
+
+	public static final String ORDER_TYPE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.OrderTypeDTOConverter)";
+
+	public static final String PRICE_ENTRY_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.PriceEntryDTOConverter)";
+
+	public static final String PRICE_LIST_ACCOUNT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.PriceListAccountDTOConverter)";
+
+	public static final String PRICE_LIST_ACCOUNT_GROUP_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.PriceListAccountGroupDTOConverter)";
+
+	public static final String PRICE_LIST_CHANNEL_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.PriceListChannelDTOConverter)";
+
+	public static final String PRICE_LIST_DISCOUNT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.PriceListDiscountDTOConverter)";
+
+	public static final String PRICE_LIST_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.PriceListDTOConverter)";
+
+	public static final String PRICE_LIST_ORDER_TYPE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.PriceListOrderTypeDTOConverter)";
+
+	public static final String PRICE_MODIFIER_CATEGORY_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.PriceModifierCategoryDTOConverter)";
+
+	public static final String PRICE_MODIFIER_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.PriceModifierDTOConverter)";
+
+	public static final String PRICE_MODIFIER_PRODUCT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.PriceModifierDTOConverter)";
+
+	public static final String PRICE_MODIFIER_PRODUCT_GROUP_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.PriceModifierProductDTOConverter)";
+
+	public static final String PRODUCT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.ProductDTOConverter)";
+
+	public static final String PRODUCT_GROUP_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.ProductGroupDTOConverter)";
+
+	public static final String SKU_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.SkuDTOConverter)";
+
+	public static final String TIER_PRICE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.pricing." +
+			"internal.dto.v2_0.converter.TierPriceDTOConverter)";
+
+}

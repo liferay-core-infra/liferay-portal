13fc757570bd3e5b345e45e86df370a20e93e77b
Joao Victor <joao.victor@liferay.com>
2023-03-07 17:24:11 -0300

    LPS-177030 Create constants file

---
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-catalog-impl/src/main/java/com/liferay/headless/commerce/delivery/catalog/internal/dto/v1_0/converter/constants/DTOConverterConstants.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.commerce.delivery.catalog.internal.dto.v1_0.converter.constants;
+
+/**
+ * @author Joao Victor
+ */
+public class DTOConverterConstants {
+
+	public static final String ATTACHMENT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.catalog." +
+			"internal.dto.v1_0.converter.AttachmentDTOConverter)";
+
+	public static final String CATEGORY_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.catalog." +
+			"internal.dto.v1_0.converter.CategoryDTOConverter)";
+
+	public static final String CHANNEL_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.catalog." +
+			"internal.dto.v1_0.converter.ChannelDTOConverter)";
+
+	public static final String MAPPED_PRODUCT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.catalog." +
+			"internal.dto.v1_0.converter.MappedProductDTOConverter)";
+
+	public static final String PIN_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.catalog." +
+			"internal.dto.v1_0.converter.PinDTOConverter)";
+
+	public static final String PRODUCT_CONFIGURATION_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.catalog." +
+			"internal.dto.v1_0.converter.ProductConfigurationDTOConverter)";
+
+	public static final String PRODUCT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.catalog." +
+			"internal.dto.v1_0.converter.ProductDTOConverter)";
+
+	public static final String PRODUCT_OPTION_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.catalog." +
+			"internal.dto.v1_0.converter.ProductOptionDTOConverter)";
+
+	public static final String PRODUCT_SPECIFICATION_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.catalog." +
+			"internal.dto.v1_0.converter.ProductSpecificationDTOConverter)";
+
+	public static final String RELATED_PRODUCT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.catalog." +
+			"internal.dto.v1_0.converter.RelatedProductDTOConverter)";
+
+	public static final String SKU_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.catalog." +
+			"internal.dto.v1_0.converter.SkuDTOConverter)";
+
+	public static final String WISH_LIST_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.catalog." +
+			"internal.dto.v1_0.converter.WishListDTOConverter)";
+
+	public static final String WISH_LIST_ITEM_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.delivery.catalog." +
+			"internal.dto.v1_0.converter.WishListItemDTOConverter)";
+
+}

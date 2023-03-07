feaf2892a098436edc181c9467ac7462ed2c1c39
Joao Victor <joao.victor@liferay.com>
2023-03-07 15:48:01 -0300

    LPS-175886 Create constants file

---
+++ modules/apps/analytics/analytics-settings-rest-impl/src/main/java/com/liferay/analytics/settings/rest/internal/dto/v1_0/converter/constants/DTOConverterConstants.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.settings.rest.internal.dto.v1_0.converter.constants;
+
+/**
+ * @author Joao Victor
+ */
+public class DTOConverterConstants {
+
+	public static final String CHANNEL_DTO_CONVERTER =
+		"(component.name=com.liferay.analytics.settings.rest.internal.dto." +
+			"v1_0.converter.ChannelDTOConverter)";
+
+	public static final String COMMERCE_CHANNEL_DTO_CONVERTER =
+		"(component.name=com.liferay.analytics.settings.rest.internal.dto." +
+			"v1_0.converter.CommerceChannelDTOConverter)";
+
+	public static final String CONTACT_ACCOUNT_GROUP_DTO_CONVERTER =
+		"(component.name=com.liferay.analytics.settings.rest.internal.dto." +
+			"v1_0.converter.ContactAccountGroupDTOConverter)";
+
+	public static final String CONTACT_ORGANIZATION_DTO_CONVERTER =
+		"(component.name=com.liferay.analytics.settings.rest.internal.dto." +
+			"v1_0.converter.ContactOrganizationDTOConverter)";
+
+	public static final String CONTACT_USER_GROUP_DTO_CONVERTER =
+		"(component.name=com.liferay.analytics.settings.rest.internal.dto." +
+			"v1_0.converter.ContactUserGroupDTOConverter)";
+
+	public static final String DATA_SOURCE_DTO_CONVERTER =
+		"(component.name=com.liferay.analytics.settings.rest.internal.dto." +
+			"v1_0.converter.DataSourceDTOConverter)";
+
+	public static final String SITE_DTO_CONVERTER =
+		"(component.name=com.liferay.analytics.settings.rest.internal.dto." +
+			"v1_0.converter.SiteDTOConverter)";
+
+}

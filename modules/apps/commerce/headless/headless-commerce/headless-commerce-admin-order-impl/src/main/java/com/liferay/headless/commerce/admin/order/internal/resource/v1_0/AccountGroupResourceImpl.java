f15d3a85f5932a1fad164836e325910d459e4faa
Joao Victor <joao.victor@liferay.com>
2023-03-08 15:55:31 -0300

    LPS-177219 Change AccountGroupDTOConverter reference to be DTOConverter

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-order-impl/src/main/java/com/liferay/headless/commerce/admin/order/internal/resource/v1_0/AccountGroupResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-order-impl/src/main/java/com/liferay/headless/commerce/admin/order/internal/resource/v1_0/AccountGroupResourceImpl.java
@@ -20,8 +20,9 @@ import com.liferay.commerce.order.rule.model.COREntryRel;
 import com.liferay.commerce.order.rule.service.COREntryRelService;
 import com.liferay.headless.commerce.admin.order.dto.v1_0.AccountGroup;
 import com.liferay.headless.commerce.admin.order.dto.v1_0.OrderRuleAccountGroup;
-import com.liferay.headless.commerce.admin.order.internal.dto.v1_0.converter.AccountGroupDTOConverter;
+import com.liferay.headless.commerce.admin.order.internal.dto.v1_0.converter.constants.DTOConverterConstants;
 import com.liferay.headless.commerce.admin.order.resource.v1_0.AccountGroupResource;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
 import com.liferay.portal.vulcan.fields.NestedField;
 import com.liferay.portal.vulcan.fields.NestedFieldSupport;
@@ -67,8 +68,9 @@ public class AccountGroupResourceImpl
 				contextAcceptLanguage.getPreferredLocale()));
 	}
 
-	@Reference
-	private AccountGroupDTOConverter _accountGroupDTOConverter;
+	@Reference(target = DTOConverterConstants.ACCOUNT_GROUP_DTO_CONVERTER)
+	private DTOConverter<CommerceAccountGroup, AccountGroup>
+		_accountGroupDTOConverter;
 
 	@Reference
 	private CommerceAccountGroupService _commerceAccountGroupService;

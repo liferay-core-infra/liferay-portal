dd30b74c3261bd47faaa6ef274eaa9c223c49f5c
Joao Victor <joao.victor@liferay.com>
2023-03-08 15:43:32 -0300

    LPS-177219 Change OrderRuleOrderTypeDTOConverter reference to be DTOConverter

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-order-impl/src/main/java/com/liferay/headless/commerce/admin/order/internal/resource/v1_0/OrderRuleOrderTypeResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-order-impl/src/main/java/com/liferay/headless/commerce/admin/order/internal/resource/v1_0/OrderRuleOrderTypeResourceImpl.java
@@ -23,10 +23,11 @@ import com.liferay.commerce.order.rule.service.COREntryService;
 import com.liferay.commerce.service.CommerceOrderTypeService;
 import com.liferay.headless.commerce.admin.order.dto.v1_0.OrderRule;
 import com.liferay.headless.commerce.admin.order.dto.v1_0.OrderRuleOrderType;
-import com.liferay.headless.commerce.admin.order.internal.dto.v1_0.converter.OrderRuleOrderTypeDTOConverter;
+import com.liferay.headless.commerce.admin.order.internal.dto.v1_0.converter.constants.DTOConverterConstants;
 import com.liferay.headless.commerce.admin.order.resource.v1_0.OrderRuleOrderTypeResource;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
 import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
 import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
 import com.liferay.portal.vulcan.fields.NestedField;
@@ -208,7 +209,10 @@ public class OrderRuleOrderTypeResourceImpl
 	@Reference
 	private DTOConverterRegistry _dtoConverterRegistry;
 
-	@Reference
-	private OrderRuleOrderTypeDTOConverter _orderRuleOrderTypeDTOConverter;
+	@Reference(
+		target = DTOConverterConstants.ORDER_RULE_ORDER_TYPE_DTO_CONVERTER
+	)
+	private DTOConverter<COREntryRel, OrderRuleOrderType>
+		_orderRuleOrderTypeDTOConverter;
 
 }

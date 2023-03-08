d21a47f77717f4ca0d4438b48cb85941d574ba30
Joao Victor <joao.victor@liferay.com>
2023-03-08 15:37:21 -0300

    LPS-177219 Create constants file

---
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-order-impl/src/main/java/com/liferay/headless/commerce/admin/order/internal/dto/v1_0/converter/constants/DTOConverterConstants.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.commerce.admin.order.internal.dto.v1_0.converter.constants;
+
+/**
+ * @author Joao Victor
+ */
+public class DTOConverterConstants {
+
+	public static final String ACCOUNT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.order.internal." +
+			"dto.v1_0.converter.AccountDTOConverter)";
+
+	public static final String ACCOUNT_GROUP_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.order.internal." +
+			"dto.v1_0.converter.AccountGroupDTOConverter)";
+
+	public static final String BILLING_ADDRESS_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.order.internal." +
+			"dto.v1_0.converter.BillingAddressDTOConverter)";
+
+	public static final String CHANNEL_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.order.internal." +
+			"dto.v1_0.converter.ChannelDTOConverter)";
+
+	public static final String ORDER_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.order.internal." +
+			"dto.v1_0.converter.OrderDTOConverter)";
+
+	public static final String ORDER_ITEM_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.order.internal." +
+			"dto.v1_0.converter.OrderItemDTOConverter)";
+
+	public static final String ORDER_NOTE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.order.internal." +
+			"dto.v1_0.converter.OrderNoteDTOConverter)";
+
+	public static final String ORDER_RULE_ACCOUNT_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.order.internal." +
+			"dto.v1_0.converter.OrderRuleAccountDTOConverter)";
+
+	public static final String ORDER_RULE_ACCOUNT_GROUP_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.order.internal." +
+			"dto.v1_0.converter.OrderRuleAccountGroupDTOConverter)";
+
+	public static final String ORDER_RULE_CHANNEL_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.order.internal." +
+			"dto.v1_0.converter.OrderRuleChannelDTOConverter)";
+
+	public static final String ORDER_RULE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.order.internal." +
+			"dto.v1_0.converter.OrderRuleDTOConverter)";
+
+	public static final String ORDER_RULE_ORDER_TYPE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.order.internal." +
+			"dto.v1_0.converter.OrderRuleOrderTypeDTOConverter)";
+
+	public static final String ORDER_TYPE_CHANNEL_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.order.internal." +
+			"dto.v1_0.converter.OrderTypeChannelDTOConverter)";
+
+	public static final String ORDER_TYPE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.order.internal." +
+			"dto.v1_0.converter.OrderTypeDTOConverter)";
+
+	public static final String SHIPPING_ADDRESS_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.order.internal." +
+			"dto.v1_0.converter.ShippingAddressDTOConverter)";
+
+	public static final String TERM_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.order.internal." +
+			"dto.v1_0.converter.TermDTOConverter)";
+
+	public static final String TERM_ORDER_TYPE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.order.internal." +
+			"dto.v1_0.converter.TermOrderTypeDTOConverter)";
+
+}

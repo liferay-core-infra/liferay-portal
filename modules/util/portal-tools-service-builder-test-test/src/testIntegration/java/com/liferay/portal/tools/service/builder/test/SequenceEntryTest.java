40847f9e46f12b9bccd23848466bc908044475ed
ricardocousolr <ricardo.couso@liferay.com>
2023-03-15 15:40:28 +0100

    LPS-157843 Test that new entry can be added to database without any errors such as relation "hibernate_sequence" does not exist

---
+++ modules/util/portal-tools-service-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/service/builder/test/SequenceEntryTest.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.tools.service.builder.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.tools.service.builder.test.model.SequenceEntry;
+import com.liferay.portal.tools.service.builder.test.service.SequenceEntryLocalService;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Ricardo Couso
+ */
+@RunWith(Arquillian.class)
+public class SequenceEntryTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testAddSequenceEntry() {
+		SequenceEntry sequenceEntry =
+			_sequenceEntryLocalService.createSequenceEntry(0);
+
+		sequenceEntry = _sequenceEntryLocalService.addSequenceEntry(
+			sequenceEntry);
+
+		Assert.assertEquals(
+			sequenceEntry,
+			_sequenceEntryLocalService.fetchSequenceEntry(
+				sequenceEntry.getSequenceEntryId()));
+	}
+
+	@Inject
+	private SequenceEntryLocalService _sequenceEntryLocalService;
+
+}

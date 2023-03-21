92c65c2d10e63a9e4622b7376fe9f77d96c82946
Joao Victor <joao.victor@liferay.com>
2023-03-21 17:01:42 -0300

    LPS-178898 Extract ScreenNavigationEntry methods from ServerPortalPropertiesScreenNavigationCategory to ServerPortalPropertiesScreenNavigationEntry

--- modules/apps/server/server-admin-web/src/main/java/com/liferay/server/admin/web/internal/frontend/taglib/servlet/taglib/ServerPortalPropertiesScreenNavigationEntry.java
+++ modules/apps/server/server-admin-web/src/main/java/com/liferay/server/admin/web/internal/frontend/taglib/servlet/taglib/ServerPortalPropertiesScreenNavigationEntry.java
@@ -14,29 +14,40 @@
 
 package com.liferay.server.admin.web.internal.frontend.taglib.servlet.taglib;
 
-import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationCategory;
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.server.admin.web.internal.constants.ServerAdminNavigationEntryConstants;
 
+import java.io.IOException;
+
 import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Albert Lee
+ * @author Joao Victor Alves
  */
 @Component(
-	property = "screen.navigation.category.order:Integer=20",
-	service = ScreenNavigationCategory.class
+	property = "screen.navigation.entry.order:Integer=10",
+	service = ScreenNavigationEntry.class
 )
-public class ServerPortalPropertiesScreenNavigationCategory
-	implements ScreenNavigationCategory {
+public class ServerPortalPropertiesScreenNavigationEntry
+	implements ScreenNavigationEntry<Object> {
 
 	@Override
 	public String getCategoryKey() {
 		return ServerAdminNavigationEntryConstants.
 			CATEGORY_KEY_PORTAL_PROPERTIES;
+	}
+
+	@Override
+	public String getEntryKey() {
+		return ServerAdminNavigationEntryConstants.ENTRY_KEY_PORTAL_PROPERTIES;
 	}
 
 	@Override
@@ -49,6 +60,20 @@ public class ServerPortalPropertiesScreenNavigationCategory
 		return ServerAdminNavigationEntryConstants.
 			SCREEN_NAVIGATION_KEY_PROPERTIES;
 	}
+
+	@Override
+	public void render(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		_jspRenderer.renderJSP(
+			httpServletRequest, httpServletResponse,
+			"/view_portal_properties.jsp");
+	}
+
+	@Reference
+	private JSPRenderer _jspRenderer;
 
 	@Reference
 	private Language _language;

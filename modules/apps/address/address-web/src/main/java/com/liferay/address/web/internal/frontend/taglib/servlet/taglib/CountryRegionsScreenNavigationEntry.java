7da7ec0d64211c0ebc07e2080529b1200cfef1d5
Joao Victor <joao.victor@liferay.com>
2023-03-21 17:01:01 -0300

    LPS-178898 Extract ScreenNavigationEntry methods from CountryRegionsScreenNavigationCategory to CountryRegionsScreenNavigationEntry

--- modules/apps/address/address-web/src/main/java/com/liferay/address/web/internal/frontend/taglib/servlet/taglib/CountryRegionsScreenNavigationEntry.java
+++ modules/apps/address/address-web/src/main/java/com/liferay/address/web/internal/frontend/taglib/servlet/taglib/CountryRegionsScreenNavigationEntry.java
@@ -15,26 +15,40 @@
 package com.liferay.address.web.internal.frontend.taglib.servlet.taglib;
 
 import com.liferay.address.web.internal.constants.CountryScreenNavigationConstants;
-import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationCategory;
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
 import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.model.Country;
+import com.liferay.portal.kernel.model.User;
+
+import java.io.IOException;
 
 import java.util.Locale;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Pei-Jung Lan
+ * @author Joao Victor Alves
  */
 @Component(
-	property = "screen.navigation.category.order:Integer=20",
-	service = ScreenNavigationCategory.class
+	property = "screen.navigation.entry.order:Integer=20",
+	service = ScreenNavigationEntry.class
 )
-public class CountryRegionsScreenNavigationCategory
-	implements ScreenNavigationCategory {
+public class CountryRegionsScreenNavigationEntry
+	implements ScreenNavigationEntry<Country> {
 
 	@Override
 	public String getCategoryKey() {
+		return CountryScreenNavigationConstants.CATEGORY_KEY_REGIONS;
+	}
+
+	@Override
+	public String getEntryKey() {
 		return CountryScreenNavigationConstants.CATEGORY_KEY_REGIONS;
 	}
 
@@ -48,7 +62,33 @@ public class CountryRegionsScreenNavigationCategory
 		return CountryScreenNavigationConstants.SCREEN_NAVIGATION_KEY_COUNTRY;
 	}
 
+	@Override
+	public boolean isVisible(User user, Country country) {
+		if (country == null) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public void render(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		_jspRenderer.renderJSP(
+			_servletContext, httpServletRequest, httpServletResponse,
+			"/country/regions.jsp");
+	}
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
 	@Reference
 	private Language _language;
+
+	@Reference(target = "(osgi.web.symbolicname=com.liferay.address.web)")
+	private ServletContext _servletContext;
 
 }

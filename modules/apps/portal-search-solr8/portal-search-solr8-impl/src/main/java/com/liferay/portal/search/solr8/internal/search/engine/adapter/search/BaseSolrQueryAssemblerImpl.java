ec32886331b5f22a2b475a6a8a67e0ddd43cfc48
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-03-21 17:01:57 +0800

    LPS-176632 Make method getFacetParameters private, since it's only used in class BaseSolrQueryAssemblerImpl

--- modules/apps/portal-search-solr8/portal-search-solr8-impl/src/main/java/com/liferay/portal/search/solr8/internal/search/engine/adapter/search/BaseSolrQueryAssemblerImpl.java
+++ modules/apps/portal-search-solr8/portal-search-solr8-impl/src/main/java/com/liferay/portal/search/solr8/internal/search/engine/adapter/search/BaseSolrQueryAssemblerImpl.java
@@ -129,19 +129,6 @@ public class BaseSolrQueryAssemblerImpl implements BaseSolrQueryAssembler {
 		return excludeTagsString;
 	}
 
-	protected Map<String, JSONObject> getFacetParameters(Facet facet) {
-		Class<?> clazz = facet.getClass();
-
-		FacetProcessor<SolrQuery> facetProcessor =
-			_serviceTrackerMap.getService(clazz.getName());
-
-		if (facetProcessor == null) {
-			facetProcessor = _defaultFacetProcessor;
-		}
-
-		return facetProcessor.processFacet(facet);
-	}
-
 	protected String getFacetString(Map<String, JSONObject> jsonObjects) {
 		Set<Map.Entry<String, JSONObject>> entrySet = jsonObjects.entrySet();
 
@@ -211,7 +198,8 @@ public class BaseSolrQueryAssemblerImpl implements BaseSolrQueryAssembler {
 
 			addFilterQuery(postFilterQueries, facet, tag);
 
-			Map<String, JSONObject> facetParameters = getFacetParameters(facet);
+			Map<String, JSONObject> facetParameters = _getFacetParameters(
+				facet);
 
 			excludeTags(
 				facetParameters,
@@ -310,6 +298,19 @@ public class BaseSolrQueryAssemblerImpl implements BaseSolrQueryAssembler {
 		if (!ArrayUtil.isEmpty(facetPostFilterQueries)) {
 			Collections.addAll(filterQueries, facetPostFilterQueries);
 		}
+	}
+
+	private Map<String, JSONObject> _getFacetParameters(Facet facet) {
+		Class<?> clazz = facet.getClass();
+
+		FacetProcessor<SolrQuery> facetProcessor =
+			_serviceTrackerMap.getService(clazz.getName());
+
+		if (facetProcessor == null) {
+			facetProcessor = _defaultFacetProcessor;
+		}
+
+		return facetProcessor.processFacet(facet);
 	}
 
 	private final FacetProcessor<SolrQuery> _defaultFacetProcessor =

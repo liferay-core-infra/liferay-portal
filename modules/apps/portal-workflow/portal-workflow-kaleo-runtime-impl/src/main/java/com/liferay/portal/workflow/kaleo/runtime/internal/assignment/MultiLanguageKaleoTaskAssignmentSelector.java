d14842b3f990240a99793cc391476320b2db9e63
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-04-03 13:39:19 +0800

    LPS-171231 remove the logic to validate osgi property value, it should be the developer's responsibility to make sure the value is not empty rather than checking each time at runtime

--- modules/apps/portal-workflow/portal-workflow-kaleo-runtime-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/internal/assignment/MultiLanguageKaleoTaskAssignmentSelector.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-runtime-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/internal/assignment/MultiLanguageKaleoTaskAssignmentSelector.java
@@ -16,7 +16,6 @@ package com.liferay.portal.workflow.kaleo.runtime.internal.assignment;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.ClassUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -95,8 +94,7 @@ public class MultiLanguageKaleoTaskAssignmentSelector
 			Map<String, Object> properties)
 		throws KaleoDefinitionValidationException {
 
-		String[] scriptingLanguages = _getScriptingLanguages(
-			scriptingKaleoTaskAssignmentSelector, properties);
+		String[] scriptingLanguages = _getScriptingLanguages(properties);
 
 		for (String scriptingLanguage : scriptingLanguages) {
 			_kaleoTaskAssignmentSelectors.put(
@@ -114,8 +112,7 @@ public class MultiLanguageKaleoTaskAssignmentSelector
 			Map<String, Object> properties)
 		throws KaleoDefinitionValidationException {
 
-		String[] scriptingLanguages = _getScriptingLanguages(
-			scriptingKaleoTaskAssignmentSelector, properties);
+		String[] scriptingLanguages = _getScriptingLanguages(properties);
 
 		for (String scriptingLanguage : scriptingLanguages) {
 			_kaleoTaskAssignmentSelectors.remove(
@@ -140,22 +137,11 @@ public class MultiLanguageKaleoTaskAssignmentSelector
 		return language;
 	}
 
-	private String[] _getScriptingLanguages(
-		KaleoTaskAssignmentSelector kaleoTaskAssignmentSelector,
-		Map<String, Object> properties) {
-
+	private String[] _getScriptingLanguages(Map<String, Object> properties) {
 		Object value = properties.get("scripting.language");
 
-		String[] scriptingLanguages = GetterUtil.getStringValues(
+		return GetterUtil.getStringValues(
 			value, new String[] {String.valueOf(value)});
-
-		if (ArrayUtil.isEmpty(scriptingLanguages)) {
-			throw new IllegalArgumentException(
-				"The property \"scripting.language\" is invalid for " +
-					ClassUtil.getClassName(kaleoTaskAssignmentSelector));
-		}
-
-		return scriptingLanguages;
 	}
 
 	@Reference

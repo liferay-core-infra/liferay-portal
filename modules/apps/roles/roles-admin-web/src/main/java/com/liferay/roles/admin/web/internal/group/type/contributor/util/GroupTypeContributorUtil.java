5a1a1605facb4fb70e2935b8c79b64aa88a46d9e
Janis Zhang <janis.zhang@liferay.com>
2023-04-06 10:28:33 +0800

    LPS-179499 Convert method reference to ServiceTrackerList in GroupTypeContributorUtil

--- modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/group/type/contributor/util/GroupTypeContributorUtil.java
+++ modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/group/type/contributor/util/GroupTypeContributorUtil.java
@@ -14,6 +14,8 @@
 
 package com.liferay.roles.admin.web.internal.group.type.contributor.util;
 
+import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerList;
+import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerListFactory;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.roles.admin.group.type.contributor.GroupTypeContributor;
@@ -21,11 +23,10 @@ import com.liferay.roles.admin.group.type.contributor.GroupTypeContributor;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.service.component.annotations.Deactivate;
 
 /**
  * @author Alejandro Tardín
@@ -36,9 +37,7 @@ public class GroupTypeContributorUtil {
 	public static long[] getClassNameIds() {
 		List<Long> classNameIds = new ArrayList<>();
 
-		for (GroupTypeContributor groupTypeContributor :
-				_groupTypeContributors) {
-
+		for (GroupTypeContributor groupTypeContributor : _serviceTrackerList) {
 			if (!groupTypeContributor.isEnabled()) {
 				continue;
 			}
@@ -50,24 +49,17 @@ public class GroupTypeContributorUtil {
 		return ListUtil.toLongArray(classNameIds, Long::valueOf);
 	}
 
-	@Reference(
-		cardinality = ReferenceCardinality.MULTIPLE,
-		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY
-	)
-	protected void addGroupTypeContributor(
-		GroupTypeContributor groupTypeContributor) {
-
-		_groupTypeContributors.add(groupTypeContributor);
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerList = ServiceTrackerListFactory.open(
+			bundleContext, GroupTypeContributor.class);
 	}
 
-	protected void removeGroupTypeContributor(
-		GroupTypeContributor groupTypeContributor) {
-
-		_groupTypeContributors.remove(groupTypeContributor);
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerList.close();
 	}
 
-	private static final List<GroupTypeContributor> _groupTypeContributors =
-		new ArrayList<>();
+	private static ServiceTrackerList<GroupTypeContributor> _serviceTrackerList;
 
 }

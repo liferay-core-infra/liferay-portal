8161733d60ad630c916f21f38805ad390531e846
Julius Lee <meng-huan.lee@liferay.com>
2023-04-07 11:57:04 -0700

    LPS-178022 Remove Optional usages in PortletPreferencesHelper except for constructor We are not removing from constructor yet because it is dependent on api changes in portal-search-web-api

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/helper/PortletPreferencesHelper.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/helper/PortletPreferencesHelper.java
@@ -16,7 +16,8 @@ package com.liferay.portal.search.web.internal.helper;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.search.web.internal.util.SearchStringUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Optional;
 
@@ -33,44 +34,44 @@ public class PortletPreferencesHelper {
 		_portletPreferencesOptional = portletPreferencesOptional;
 	}
 
-	public Optional<Boolean> getBoolean(String key) {
-		Optional<String> valueOptional = _getValue(key);
-
-		return valueOptional.map(GetterUtil::getBoolean);
-	}
-
 	public boolean getBoolean(String key, boolean defaultValue) {
-		Optional<Boolean> valueOptional = getBoolean(key);
-
-		return valueOptional.orElse(defaultValue);
-	}
-
-	public Optional<Integer> getInteger(String key) {
-		Optional<String> valueOptional = _getValue(key);
-
-		return valueOptional.map(GetterUtil::getInteger);
+		return GetterUtil.getBoolean(_getValue(key), defaultValue);
 	}
 
 	public int getInteger(String key, int defaultValue) {
-		Optional<Integer> valueOptional = getInteger(key);
-
-		return valueOptional.orElse(defaultValue);
+		return GetterUtil.getInteger(_getValue(key), defaultValue);
 	}
 
-	public Optional<String> getString(String key) {
+	public String getString(String key) {
 		return _getValue(key);
 	}
 
 	public String getString(String key, String defaultValue) {
-		Optional<String> valueOptional = getString(key);
+		String value = getString(key);
 
-		return valueOptional.orElse(defaultValue);
+		if (value == null) {
+			return defaultValue;
+		}
+
+		return value;
 	}
 
-	private Optional<String> _getValue(String key) {
-		return _portletPreferencesOptional.flatMap(
-			portletPreferences -> SearchStringUtil.maybe(
-				portletPreferences.getValue(key, StringPool.BLANK)));
+	private String _getValue(String key) {
+		if (!_portletPreferencesOptional.isPresent()) {
+			return null;
+		}
+
+		PortletPreferences portletPreferences =
+			_portletPreferencesOptional.get();
+
+		String value = StringUtil.trim(
+			portletPreferences.getValue(key, StringPool.BLANK));
+
+		if (Validator.isBlank(value)) {
+			return null;
+		}
+
+		return value;
 	}
 
 	private final Optional<PortletPreferences> _portletPreferencesOptional;

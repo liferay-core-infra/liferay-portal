c013edd4a13af686e6533af69c27ed7490042820
Renan Vasconelos <renan.vasconcelos@liferay.com>
2023-04-20 18:42:24 -0300

    LPS-182376 Transform the static component NavItemClassNameIdUtil into a Util class using a Snapshot field

--- modules/apps/site-navigation/site-navigation-taglib/src/main/java/com/liferay/site/navigation/taglib/internal/servlet/NavItemClassNameIdUtil.java
+++ modules/apps/site-navigation/site-navigation-taglib/src/main/java/com/liferay/site/navigation/taglib/internal/servlet/NavItemClassNameIdUtil.java
@@ -14,35 +14,25 @@
 
 package com.liferay.site.navigation.taglib.internal.servlet;
 
-import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.osgi.util.service.Snapshot;
 import com.liferay.portal.kernel.theme.NavItem;
 import com.liferay.portal.kernel.util.Portal;
-
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Shuyang Zhou
  */
-@Component(service = {})
 public class NavItemClassNameIdUtil {
 
 	public static long getNavItemClassNameId() {
-		return _navItemClassNameId;
+		return _NAV_ITEM_CLASS_NAME_ID;
 	}
 
-	@Activate
-	protected void activate() {
-		_navItemClassNameId = _portal.getClassNameId(NavItem.class);
-	}
+	private static final long _NAV_ITEM_CLASS_NAME_ID =
+		NavItemClassNameIdUtil._portal.getClassNameId(NavItem.class);
 
-	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED)
-	protected ModuleServiceLifecycle moduleServiceLifecycle;
-
-	private static long _navItemClassNameId;
-
-	@Reference
-	private Portal _portal;
+	private static final Portal _portal =
+		NavItemClassNameIdUtil._portalSnapshot.get();
+	private static final Snapshot<Portal> _portalSnapshot = new Snapshot<>(
+		NavItemClassNameIdUtil.class, Portal.class);
 
 }

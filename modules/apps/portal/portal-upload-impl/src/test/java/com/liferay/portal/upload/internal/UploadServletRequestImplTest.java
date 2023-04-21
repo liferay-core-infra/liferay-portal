ede790a027a2a0155c834b1e6af597c30a7a14b0
Jiefeng Wu <jeffwu0724@gmail.com>
2023-04-20 17:09:34 -0700

    LPS-183594 rename portal-upload to portal-upload-impl

--- modules/apps/portal/portal-upload-impl/src/test/java/com/liferay/portal/upload/internal/UploadServletRequestImplTest.java
+++ modules/apps/portal/portal-upload-impl/src/test/java/com/liferay/portal/upload/internal/UploadServletRequestImplTest.java
@@ -11,7 +11,6 @@ import com.liferay.portal.kernel.upload.FileItem;
 import com.liferay.portal.kernel.util.ProxyFactory;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 import com.liferay.portal.tools.ToolDependencies;
-import com.liferay.portal.upload.UploadServletRequestImpl;
 
 import java.util.ArrayList;
 import java.util.List;

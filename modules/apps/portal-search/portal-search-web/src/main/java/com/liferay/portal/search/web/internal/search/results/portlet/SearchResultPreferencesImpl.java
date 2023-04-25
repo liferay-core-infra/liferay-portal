fb025016256ac094b683c654f11b54842cd27ce6
Julius Lee <meng-huan.lee@liferay.com>
2023-04-25 09:15:35 -0700

    LPS-178022 Remove Optional usage from SearchResultPreferences getFieldsToDisplay method

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/results/portlet/SearchResultPreferencesImpl.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/results/portlet/SearchResultPreferencesImpl.java
@@ -33,8 +33,8 @@ public class SearchResultPreferencesImpl implements SearchResultPreferences {
 	}
 
 	@Override
-	public Optional<String> getFieldsToDisplayOptional() {
-		return Optional.of(_searchResultsPortletPreferences.getFieldsToDisplay());
+	public String getFieldsToDisplay() {
+		return _searchResultsPortletPreferences.getFieldsToDisplay();
 	}
 
 	@Override

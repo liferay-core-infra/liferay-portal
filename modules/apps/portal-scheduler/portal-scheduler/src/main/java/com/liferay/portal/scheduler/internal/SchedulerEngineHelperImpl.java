18377738fe7018647a500957e4aab44abc0c97af
Tina Tian <tina.tian@liferay.com>
2023-04-25 16:29:27 -0700

    LPS-x Add another quartz scheduler to deal with memory clustered jobs by quartz' own cluster support, thus we can remove our own support of cluster for quartz in portal-scheduler-mulitple

--- modules/apps/portal-scheduler/portal-scheduler/src/main/java/com/liferay/portal/scheduler/internal/SchedulerEngineHelperImpl.java
+++ modules/apps/portal-scheduler/portal-scheduler/src/main/java/com/liferay/portal/scheduler/internal/SchedulerEngineHelperImpl.java
@@ -467,9 +467,8 @@ public class SchedulerEngineHelperImpl implements SchedulerEngineHelper {
 	@Reference
 	private Portal _portal;
 
-	@Reference(target = "(scheduler.engine.proxy=true)")
+	@Reference
 	private SchedulerEngine _schedulerEngine;
-
 	private volatile SchedulerEngineHelperConfiguration
 		_schedulerEngineHelperConfiguration;
 	private ServiceTracker<SchedulerJobConfiguration, SchedulerJobConfiguration>

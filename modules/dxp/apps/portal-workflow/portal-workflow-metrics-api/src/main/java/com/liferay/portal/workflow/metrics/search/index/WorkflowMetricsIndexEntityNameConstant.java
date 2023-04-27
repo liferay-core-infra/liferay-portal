dbd2acdccf74e5088b1af0b5a834c7b914997f85
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-04-27 13:28:24 +0800

    LPS-171023 Extract constant

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-api/src/main/java/com/liferay/portal/workflow/metrics/search/index/WorkflowMetricsIndexEntityNameConstant.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.workflow.metrics.search.index;
+
+/**
+ * @author Jiaxu Wei
+ */
+public interface WorkflowMetricsIndexEntityNameConstant {
+
+	public static final String INSTANCE = "instance";
+
+	public static final String NODE = "node";
+
+	public static final String PROCESS = "process";
+
+	public static final String SLA_INSTANCE_RESULT = "sla-instance-result";
+
+	public static final String SLA_TASK_RESULT = "sla-task-result";
+
+	public static final String TASK = "task";
+
+	public static final String TRANSITION = "transition";
+
+}

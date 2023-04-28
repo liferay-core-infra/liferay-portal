b59d8c77c82558f6eca6a7a9b4b317dc2cc4c062
Joao Victor <joao.victor@liferay.com>
2023-04-28 10:09:51 -0300

    LPS-183224 Extract services from CommerceInventoryPortletProvider and create CommerceInventoryManagePortletProvider and CommerceInventoryViewPortletProvider to use them

--- modules/apps/commerce/commerce-inventory-web/src/main/java/com/liferay/commerce/inventory/web/internal/portlet/CommerceInventoryViewPortletProvider.java
+++ modules/apps/commerce/commerce-inventory-web/src/main/java/com/liferay/commerce/inventory/web/internal/portlet/CommerceInventoryViewPortletProvider.java
@@ -16,19 +16,19 @@ package com.liferay.commerce.inventory.web.internal.portlet;
 
 import com.liferay.commerce.product.constants.CPPortletKeys;
 import com.liferay.portal.kernel.portlet.BasePortletProvider;
-import com.liferay.portal.kernel.portlet.EditPortletProvider;
+import com.liferay.portal.kernel.portlet.ViewPortletProvider;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
- * @author Luca Pellizzon
+ * @author Joao Victor Alves
  */
 @Component(
 	property = "model.class.name=com.liferay.commerce.inventory.model.CommerceInventoryWarehouseItem",
-	service = EditPortletProvider.class
+	service = ViewPortletProvider.class
 )
-public class CommerceInventoryPortletProvider
-	extends BasePortletProvider implements EditPortletProvider {
+public class CommerceInventoryViewPortletProvider
+	extends BasePortletProvider implements ViewPortletProvider {
 
 	@Override
 	public String getPortletName() {

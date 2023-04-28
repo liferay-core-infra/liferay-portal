ec74107c82444aa8f79525c7cf4996a2fb3cdd0c
Joao Victor <joao.victor@liferay.com>
2023-04-28 10:19:58 -0300

    LPS-183459 Rename COREntryPortletProvider to COREntryEditPortletProvider to follow the component name pattern

--- modules/apps/commerce/commerce-order-rule-web/src/main/java/com/liferay/commerce/order/rule/web/internal/portlet/COREntryEditPortletProvider.java
+++ modules/apps/commerce/commerce-order-rule-web/src/main/java/com/liferay/commerce/order/rule/web/internal/portlet/COREntryEditPortletProvider.java
@@ -27,7 +27,7 @@ import org.osgi.service.component.annotations.Component;
 	property = "model.class.name=com.liferay.commerce.order.rule.model.COREntry",
 	service = EditPortletProvider.class
 )
-public class COREntryPortletProvider
+public class COREntryEditPortletProvider
 	extends BasePortletProvider implements EditPortletProvider {
 
 	@Override

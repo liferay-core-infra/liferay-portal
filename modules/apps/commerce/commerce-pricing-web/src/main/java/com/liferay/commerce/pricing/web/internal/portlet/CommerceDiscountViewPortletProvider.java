717fef94e82f58eb404faac73bdf04c989e6efa3
Joao Victor <joao.victor@liferay.com>
2023-04-28 10:32:37 -0300

    LPS-183224 Extract services from CommerceDiscountPortletProvider and create CommerceDiscountManagePortletProvider and CommerceDiscountViewPortletProvider to use them

--- modules/apps/commerce/commerce-pricing-web/src/main/java/com/liferay/commerce/pricing/web/internal/portlet/CommerceDiscountViewPortletProvider.java
+++ modules/apps/commerce/commerce-pricing-web/src/main/java/com/liferay/commerce/pricing/web/internal/portlet/CommerceDiscountViewPortletProvider.java
@@ -16,19 +16,19 @@ package com.liferay.commerce.pricing.web.internal.portlet;
 
 import com.liferay.commerce.pricing.constants.CommercePricingPortletKeys;
 import com.liferay.portal.kernel.portlet.BasePortletProvider;
-import com.liferay.portal.kernel.portlet.EditPortletProvider;
+import com.liferay.portal.kernel.portlet.ViewPortletProvider;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
- * @author Marco Leo
+ * @author Joao Victor Alves
  */
 @Component(
 	property = "model.class.name=com.liferay.commerce.discount.model.CommerceDiscount",
-	service = EditPortletProvider.class
+	service = ViewPortletProvider.class
 )
-public class CommerceDiscountPortletProvider
-	extends BasePortletProvider implements EditPortletProvider {
+public class CommerceDiscountViewPortletProvider
+	extends BasePortletProvider implements ViewPortletProvider {
 
 	@Override
 	public String getPortletName() {

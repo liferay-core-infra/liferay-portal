724ad33d9c95988430ad3cdd1711e4b7d70b91a4
Joao Victor <joao.victor@liferay.com>
2023-04-28 10:24:55 -0300

    LPS-183224 Extract services from CommerceOrderTypePortletProvider and create CommerceOrderTypeManagePortletProvider and CommerceOrderTypeViewPortletProvider to use them

--- modules/apps/commerce/commerce-order-web/src/main/java/com/liferay/commerce/order/web/internal/portlet/CommerceOrderTypeManagePortletProvider.java
+++ modules/apps/commerce/commerce-order-web/src/main/java/com/liferay/commerce/order/web/internal/portlet/CommerceOrderTypeManagePortletProvider.java
@@ -16,19 +16,19 @@ package com.liferay.commerce.order.web.internal.portlet;
 
 import com.liferay.commerce.constants.CommercePortletKeys;
 import com.liferay.portal.kernel.portlet.BasePortletProvider;
-import com.liferay.portal.kernel.portlet.EditPortletProvider;
+import com.liferay.portal.kernel.portlet.ManagePortletProvider;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
- * @author Marco Leo
+ * @author Joao Victor Alves
  */
 @Component(
 	property = "model.class.name=com.liferay.commerce.model.CommerceOrderType",
-	service = EditPortletProvider.class
+	service = ManagePortletProvider.class
 )
-public class CommerceOrderTypePortletProvider
-	extends BasePortletProvider implements EditPortletProvider {
+public class CommerceOrderTypeManagePortletProvider
+	extends BasePortletProvider implements ManagePortletProvider {
 
 	@Override
 	public String getPortletName() {

44b96aa8d3a7d8957f3554272d6dbd08037f6676
Joao Victor <joao.victor@liferay.com>
2023-04-28 11:23:12 -0300

    LPS-183239 Extract services from CommerceSubscriptionEntryPortletProvider and create CommerceSubscriptionEntryManagePortletProvider and CommerceSubscriptionEntryViewPortletProvider to use them

--- modules/apps/commerce/commerce-subscription-web/src/main/java/com/liferay/commerce/subscription/web/internal/portlet/CommerceSubscriptionEntryManagePortletProvider.java
+++ modules/apps/commerce/commerce-subscription-web/src/main/java/com/liferay/commerce/subscription/web/internal/portlet/CommerceSubscriptionEntryManagePortletProvider.java
@@ -16,19 +16,19 @@ package com.liferay.commerce.subscription.web.internal.portlet;
 
 import com.liferay.commerce.product.constants.CPPortletKeys;
 import com.liferay.portal.kernel.portlet.BasePortletProvider;
-import com.liferay.portal.kernel.portlet.EditPortletProvider;
+import com.liferay.portal.kernel.portlet.ManagePortletProvider;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
- * @author Luca Pellizzon
+ * @author Joao Victor Alves
  */
 @Component(
 	property = "model.class.name=com.liferay.commerce.model.CommerceSubscriptionEntry",
-	service = EditPortletProvider.class
+	service = ManagePortletProvider.class
 )
-public class CommerceSubscriptionEntryPortletProvider
-	extends BasePortletProvider implements EditPortletProvider {
+public class CommerceSubscriptionEntryManagePortletProvider
+	extends BasePortletProvider implements ManagePortletProvider {
 
 	@Override
 	public String getPortletName() {
--- modules/apps/commerce/commerce-subscription-web/src/main/java/com/liferay/commerce/subscription/web/internal/portlet/CommerceSubscriptionEntryViewPortletProvider.java
+++ modules/apps/commerce/commerce-subscription-web/src/main/java/com/liferay/commerce/subscription/web/internal/portlet/CommerceSubscriptionEntryViewPortletProvider.java
@@ -16,19 +16,19 @@ package com.liferay.commerce.subscription.web.internal.portlet;
 
 import com.liferay.commerce.product.constants.CPPortletKeys;
 import com.liferay.portal.kernel.portlet.BasePortletProvider;
-import com.liferay.portal.kernel.portlet.EditPortletProvider;
+import com.liferay.portal.kernel.portlet.ViewPortletProvider;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
- * @author Luca Pellizzon
+ * @author Joao Victor Alves
  */
 @Component(
 	property = "model.class.name=com.liferay.commerce.model.CommerceSubscriptionEntry",
-	service = EditPortletProvider.class
+	service = ViewPortletProvider.class
 )
-public class CommerceSubscriptionEntryPortletProvider
-	extends BasePortletProvider implements EditPortletProvider {
+public class CommerceSubscriptionEntryViewPortletProvider
+	extends BasePortletProvider implements ViewPortletProvider {
 
 	@Override
 	public String getPortletName() {

ddb9e7c80b26418accc89f6c1adc2daf243b9424
Joao Victor <joao.victor@liferay.com>
2023-04-28 10:19:11 -0300

    LPS-183459 Extract services from COREntryPortletProvider and create COREntryManagePortletProvider and COREntryPortletViewProvider to use them

--- modules/apps/commerce/commerce-order-rule-web/src/main/java/com/liferay/commerce/order/rule/web/internal/portlet/COREntryManagePortletProvider.java
+++ modules/apps/commerce/commerce-order-rule-web/src/main/java/com/liferay/commerce/order/rule/web/internal/portlet/COREntryManagePortletProvider.java
@@ -16,19 +16,19 @@ package com.liferay.commerce.order.rule.web.internal.portlet;
 
 import com.liferay.commerce.order.rule.constants.COREntryPortletKeys;
 import com.liferay.portal.kernel.portlet.BasePortletProvider;
-import com.liferay.portal.kernel.portlet.EditPortletProvider;
+import com.liferay.portal.kernel.portlet.ManagePortletProvider;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
- * @author Alessio Antonio Rendina
+ * @author Joao Victor Alves
  */
 @Component(
 	property = "model.class.name=com.liferay.commerce.order.rule.model.COREntry",
-	service = EditPortletProvider.class
+	service = ManagePortletProvider.class
 )
-public class COREntryPortletProvider
-	extends BasePortletProvider implements EditPortletProvider {
+public class COREntryManagePortletProvider
+	extends BasePortletProvider implements ManagePortletProvider {
 
 	@Override
 	public String getPortletName() {

31f950d3fda67f2a5ff631dbbb8eb7e535fe21f2
Julius Lee <meng-huan.lee@liferay.com>
2023-05-01 10:33:31 -0700

    LPS-173369 Remove Optional from PortletSharedSearchResponse

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/portlet/shared/search/PortletSharedSearchResponseImpl.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/portlet/shared/search/PortletSharedSearchResponseImpl.java
@@ -26,7 +26,6 @@ import com.liferay.portal.search.web.portlet.shared.search.PortletSharedSearchRe
 import com.liferay.portal.search.web.search.request.SearchSettings;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.portlet.PortletPreferences;
 import javax.portlet.RenderRequest;
@@ -84,27 +83,23 @@ public class PortletSharedSearchResponseImpl
 	}
 
 	@Override
-	public Optional<String> getParameter(
-		String name, RenderRequest renderRequest) {
-
-		return Optional.ofNullable(
-			_portletSharedRequestHelper.getParameter(name, renderRequest));
+	public String getParameter(String name, RenderRequest renderRequest) {
+		return _portletSharedRequestHelper.getParameter(name, renderRequest);
 	}
 
 	@Override
-	public Optional<String[]> getParameterValues(
+	public String[] getParameterValues(
 		String name, RenderRequest renderRequest) {
 
-		return Optional.ofNullable(
-			_portletSharedRequestHelper.getParameterValues(
-				name, renderRequest));
+		return _portletSharedRequestHelper.getParameterValues(
+			name, renderRequest);
 	}
 
 	@Override
-	public Optional<PortletPreferences> getPortletPreferences(
+	public PortletPreferences getPortletPreferences(
 		RenderRequest renderRequest) {
 
-		return Optional.ofNullable(renderRequest.getPreferences());
+		return renderRequest.getPreferences();
 	}
 
 	@Override

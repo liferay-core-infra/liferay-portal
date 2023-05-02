be83b534f4f9a57fe1e9c6bb839a6fdf84afb5bf
Joao Victor <joao.victor@liferay.com>
2023-05-02 15:56:21 -0300

    LPS-183459 Create BaseCPDefinitionsPortletProvider, extract getPortletName method from CPDefinitionsEditPortletProvider to it and apply to usages

--- modules/apps/commerce/commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/portlet/BaseCPDefinitionsPortletProvider.java
+++ modules/apps/commerce/commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/portlet/BaseCPDefinitionsPortletProvider.java
@@ -14,17 +14,18 @@
 
 package com.liferay.commerce.product.definitions.web.internal.portlet;
 
-import com.liferay.portal.kernel.portlet.ViewPortletProvider;
-
-import org.osgi.service.component.annotations.Component;
+import com.liferay.commerce.product.constants.CPPortletKeys;
+import com.liferay.portal.kernel.portlet.BasePortletProvider;
 
 /**
  * @author Joao Victor Alves
  */
-@Component(
-	property = "model.class.name=com.liferay.commerce.product.model.CPDefinition",
-	service = ViewPortletProvider.class
-)
-public class CPDefinitionsViewPortletProvider
-	extends BaseCPDefinitionsPortletProvider implements ViewPortletProvider {
+public abstract class BaseCPDefinitionsPortletProvider
+	extends BasePortletProvider {
+
+	@Override
+	public String getPortletName() {
+		return CPPortletKeys.CP_DEFINITIONS;
+	}
+
 }

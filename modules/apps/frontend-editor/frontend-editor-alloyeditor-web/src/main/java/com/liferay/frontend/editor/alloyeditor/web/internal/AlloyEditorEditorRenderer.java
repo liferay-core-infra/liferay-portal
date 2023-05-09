ebc614faea78d5645e084593d4846845ec46136d
Joao Victor <joao.victor@liferay.com>
2023-05-09 15:02:34 -0300

    LPS-184060 Extract methods from AlloyEditorEditor and extends Base classes in AlloyEditorEditor and AlloyEditorEditorRenderer

--- modules/apps/frontend-editor/frontend-editor-alloyeditor-web/src/main/java/com/liferay/frontend/editor/alloyeditor/web/internal/AlloyEditorEditorRenderer.java
+++ modules/apps/frontend-editor/frontend-editor-alloyeditor-web/src/main/java/com/liferay/frontend/editor/alloyeditor/web/internal/AlloyEditorEditorRenderer.java
@@ -14,23 +14,13 @@
 
 package com.liferay.frontend.editor.alloyeditor.web.internal;
 
-import com.liferay.portal.kernel.editor.Editor;
+import com.liferay.frontend.editor.EditorRenderer;
 
-import java.util.Map;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 
 /**
- * @author Raymond Augé
- * @author Roberto Díaz
+ * @author Joao Victor Alves
  */
-@Component(property = "name=alloyeditor", service = Editor.class)
-public class AlloyEditorEditor extends BaseAlloyEditor {
-
-	@Activate
-	protected void activate(Map<String, Object> properties) {
-		setName((String)properties.get("name"));
-	}
-
+@Component(property = "name=alloyeditor", service = EditorRenderer.class)
+public class AlloyEditorEditorRenderer extends BaseAlloyEditorRenderer {
 }

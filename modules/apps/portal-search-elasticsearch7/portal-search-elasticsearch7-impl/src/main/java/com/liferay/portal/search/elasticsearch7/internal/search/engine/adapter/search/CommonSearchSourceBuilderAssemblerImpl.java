ec1793817a26fc65e5a9f03d5f55e75b1ca72020
Joao Victor <joao.victor@liferay.com>
2023-05-11 15:11:13 -0300

    LPS-184314 Change QueryToQueryBuilderTranslator reference to be a QueryTranslator

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/search/engine/adapter/search/CommonSearchSourceBuilderAssemblerImpl.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/search/engine/adapter/search/CommonSearchSourceBuilderAssemblerImpl.java
@@ -23,7 +23,7 @@ import com.liferay.portal.search.aggregation.AggregationTranslator;
 import com.liferay.portal.search.aggregation.pipeline.PipelineAggregation;
 import com.liferay.portal.search.aggregation.pipeline.PipelineAggregationTranslator;
 import com.liferay.portal.search.elasticsearch7.internal.facet.FacetTranslator;
-import com.liferay.portal.search.elasticsearch7.internal.query.QueryToQueryBuilderTranslator;
+import com.liferay.portal.search.elasticsearch7.internal.legacy.query.QueryToQueryBuilderTranslator;
 import com.liferay.portal.search.elasticsearch7.internal.stats.StatsTranslator;
 import com.liferay.portal.search.engine.adapter.search.BaseSearchRequest;
 import com.liferay.portal.search.filter.ComplexQueryBuilderFactory;
@@ -31,6 +31,7 @@ import com.liferay.portal.search.filter.ComplexQueryPart;
 import com.liferay.portal.search.pit.PointInTime;
 import com.liferay.portal.search.query.BooleanQuery;
 import com.liferay.portal.search.query.Query;
+import com.liferay.portal.search.query.QueryTranslator;
 import com.liferay.portal.search.rescore.Rescore;
 import com.liferay.portal.search.stats.StatsRequest;
 
@@ -580,15 +581,14 @@ public class CommonSearchSourceBuilderAssemblerImpl
 	private FilterTranslator<QueryBuilder> _filterToQueryBuilderTranslator;
 
 	@Reference
-	private com.liferay.portal.search.elasticsearch7.internal.legacy.query.
-		QueryToQueryBuilderTranslator _legacyQueryToQueryBuilderTranslator;
+	private QueryToQueryBuilderTranslator _legacyQueryToQueryBuilderTranslator;
 
 	@Reference(target = "(search.engine.impl=Elasticsearch)")
 	private PipelineAggregationTranslator<PipelineAggregationBuilder>
 		_pipelineAggregationTranslator;
 
-	@Reference
-	private QueryToQueryBuilderTranslator _queryToQueryBuilderTranslator;
+	@Reference(target = "(search.engine.impl=Elasticsearch)")
+	private QueryTranslator<QueryBuilder> _queryToQueryBuilderTranslator;
 
 	@Reference
 	private StatsTranslator _statsTranslator;

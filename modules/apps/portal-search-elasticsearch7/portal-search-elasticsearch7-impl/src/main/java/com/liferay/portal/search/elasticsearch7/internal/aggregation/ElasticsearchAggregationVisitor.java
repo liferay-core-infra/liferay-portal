b09222a9e3c0904adb3aa93b4c1c69f06248187f
Joao Victor <joao.victor@liferay.com>
2023-05-11 15:12:34 -0300

    LPS-184314 Remove services from components

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/aggregation/ElasticsearchAggregationVisitor.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/aggregation/ElasticsearchAggregationVisitor.java
@@ -90,7 +90,7 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(
 	property = "search.engine.impl=Elasticsearch",
-	service = {AggregationTranslator.class, AggregationVisitor.class}
+	service = AggregationTranslator.class
 )
 public class ElasticsearchAggregationVisitor
 	implements AggregationTranslator<AggregationBuilder>,

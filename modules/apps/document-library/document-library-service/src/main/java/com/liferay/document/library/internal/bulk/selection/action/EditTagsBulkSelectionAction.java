5d78a26dbded4b873b86bace75fdd4484ddf8472
Renan Vasconelos <renan.vasconcelos@liferay.com>
2023-05-11 14:06:15 -0300

    LPS-183634 Remove EditTagsBulkSelectionAction interface, rename EditTagsBulkSelectionActionImpl to EditCategoriesBulkSelectionAction, make EditTagsBulkSelectionAction implement BulkSelectionAction directly and change EditTagsBulkSelectionAction reference to be BulkSelectionAction

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/bulk/selection/action/EditTagsBulkSelectionAction.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/bulk/selection/action/EditTagsBulkSelectionAction.java
@@ -20,7 +20,6 @@ import com.liferay.asset.kernel.service.AssetEntryLocalService;
 import com.liferay.asset.util.AssetHelper;
 import com.liferay.bulk.selection.BulkSelection;
 import com.liferay.bulk.selection.BulkSelectionAction;
-import com.liferay.document.library.bulk.selection.EditTagsBulkSelectionAction;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -43,14 +42,9 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Adolfo Pérez
  */
-@Component(
-	service = {
-		BulkSelectionAction.class, EditTagsBulkSelectionAction.class,
-		EditTagsBulkSelectionActionImpl.class
-	}
-)
-public class EditTagsBulkSelectionActionImpl
-	implements EditTagsBulkSelectionAction {
+@Component(service = BulkSelectionAction.class)
+public class EditTagsBulkSelectionAction
+	implements BulkSelectionAction<AssetEntry> {
 
 	@Override
 	public void execute(
@@ -122,7 +116,7 @@ public class EditTagsBulkSelectionActionImpl
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
-		EditTagsBulkSelectionActionImpl.class);
+		EditTagsBulkSelectionAction.class);
 
 	@Reference
 	private AssetEntryLocalService _assetEntryLocalService;

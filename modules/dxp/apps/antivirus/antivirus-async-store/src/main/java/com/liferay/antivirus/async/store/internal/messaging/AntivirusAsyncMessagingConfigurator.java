c14850270ca4e834a89bbaecfb695eb81595d719
Tina Tian <tina.tian@liferay.com>
2023-05-15 10:12:03 -0700

    LPS-183809 Separate destination registration logic from message listener

---
+++ modules/dxp/apps/antivirus/antivirus-async-store/src/main/java/com/liferay/antivirus/async/store/internal/messaging/AntivirusAsyncMessagingConfigurator.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.antivirus.async.store.internal.messaging;
+
+import com.liferay.antivirus.async.store.configuration.AntivirusAsyncConfiguration;
+import com.liferay.antivirus.async.store.constants.AntivirusAsyncDestinationNames;
+import com.liferay.antivirus.async.store.retry.AntivirusAsyncRetryScheduler;
+import com.liferay.antivirus.async.store.util.AntivirusAsyncUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.Destination;
+import com.liferay.portal.kernel.messaging.DestinationConfiguration;
+import com.liferay.portal.kernel.messaging.DestinationFactory;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageRunnable;
+import com.liferay.portal.kernel.util.MapUtil;
+
+import java.util.Map;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Tina Tian
+ */
+@Component(
+	configurationPid = "com.liferay.antivirus.async.store.configuration.AntivirusAsyncConfiguration",
+	configurationPolicy = ConfigurationPolicy.REQUIRE, service = {}
+)
+public class AntivirusAsyncMessagingConfigurator {
+
+	@Activate
+	protected void activate(
+		BundleContext bundleContext, Map<String, Object> properties) {
+
+		AntivirusAsyncConfiguration antivirusAsyncConfiguration =
+			ConfigurableUtil.createConfigurable(
+				AntivirusAsyncConfiguration.class, properties);
+
+		DestinationConfiguration destinationConfiguration =
+			DestinationConfiguration.createSerialDestinationConfiguration(
+				AntivirusAsyncDestinationNames.ANTIVIRUS);
+
+		int maximumQueueSize = antivirusAsyncConfiguration.maximumQueueSize();
+
+		if (maximumQueueSize == 0) {
+			maximumQueueSize = Integer.MAX_VALUE;
+		}
+
+		destinationConfiguration.setMaximumQueueSize(maximumQueueSize);
+
+		destinationConfiguration.setRejectedExecutionHandler(
+			(runnable, threadPoolExecutor) -> {
+				MessageRunnable messageRunnable = (MessageRunnable)runnable;
+
+				Message message = messageRunnable.getMessage();
+
+				if (_log.isDebugEnabled()) {
+					_log.debug(
+						StringBundler.concat(
+							"Schedule ",
+							AntivirusAsyncUtil.getFileIdentifier(message),
+							" into persistent storage because the async ",
+							"antivirus queue is overflowing: ",
+							message.getValues()));
+				}
+
+				_antivirusAsyncRetryScheduler.schedule(message);
+			});
+
+		Destination destination = _destinationFactory.createDestination(
+			destinationConfiguration);
+
+		_destinationServiceRegistration = bundleContext.registerService(
+			Destination.class, destination,
+			MapUtil.singletonDictionary(
+				"destination.name", destination.getName()));
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		if (_destinationServiceRegistration != null) {
+			_destinationServiceRegistration.unregister();
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AntivirusAsyncMessagingConfigurator.class);
+
+	@Reference
+	private AntivirusAsyncRetryScheduler _antivirusAsyncRetryScheduler;
+
+	@Reference
+	private DestinationFactory _destinationFactory;
+
+	private ServiceRegistration<Destination> _destinationServiceRegistration;
+
+}

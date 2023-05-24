2fb4998b74319fcc9414276e87b83f682e43ed14
hai.yu <hai.yu@liferay.com>
2023-05-24 13:48:21 +0800

    LPS-183229 The root issue is java.lang.Class.forName(String, boolean, ClassLoader) will return the first loaded class(cache the first loaded value). When render template, freemarker.template.utility.ClassUtil.forName() uses this api. The fix makes FreeMarkerBundleClassloader as new created object after redeploying(addingBundle/removedBundle) the taglib module to avoid using the first/previous loaded tagClass.

--- modules/apps/portal-template/portal-template-freemarker/src/main/java/com/liferay/portal/template/freemarker/internal/FreeMarkerManager.java
+++ modules/apps/portal-template/portal-template-freemarker/src/main/java/com/liferay/portal/template/freemarker/internal/FreeMarkerManager.java
@@ -259,8 +259,7 @@ public class FreeMarkerManager extends BaseTemplateManager {
 
 		BundleWiring bundleWiring = _bundle.adapt(BundleWiring.class);
 
-		_freeMarkerBundleClassloader = new FreeMarkerBundleClassloader(
-			bundleWiring.getClassLoader());
+		_bundleClassLoader = bundleWiring.getClassLoader();
 
 		_bundleTracker = new BundleTracker<>(
 			bundleContext, Bundle.ACTIVE, new TaglibBundleTrackerCustomizer());
@@ -564,7 +563,10 @@ public class FreeMarkerManager extends BaseTemplateManager {
 			ProxyUtil.getProxyProviderFunction(ServletContext.class);
 
 	private Bundle _bundle;
+	private ClassLoader _bundleClassLoader;
 	private BundleTracker<Set<String>> _bundleTracker;
+	private final Set<ClassLoader> _classLoaders =
+		ConcurrentHashMap.newKeySet();
 	private volatile Configuration _configuration;
 	private volatile BeansWrapper _defaultBeansWrapper;
 	private FreeMarkerBundleClassloader _freeMarkerBundleClassloader;
@@ -836,10 +838,11 @@ public class FreeMarkerManager extends BaseTemplateManager {
 
 				BundleWiring bundleWiring = bundle.adapt(BundleWiring.class);
 
-				_freeMarkerBundleClassloader.addclassLoader(
-					bundleWiring.getClassLoader());
+				_classLoaders.add(bundleWiring.getClassLoader());
 
 				_taglibMappings.putAll(map);
+
+				_freeMarkerBundleClassloader = null;
 
 				return map.keySet();
 			}
@@ -867,8 +870,9 @@ public class FreeMarkerManager extends BaseTemplateManager {
 
 			BundleWiring bundleWiring = bundle.adapt(BundleWiring.class);
 
-			_freeMarkerBundleClassloader.removeClassLoader(
-				bundleWiring.getClassLoader());
+			_classLoaders.remove(bundleWiring.getClassLoader());
+
+			_freeMarkerBundleClassloader = null;
 		}
 
 	}
@@ -880,7 +884,7 @@ public class FreeMarkerManager extends BaseTemplateManager {
 
 			_taglibFactory = new TaglibFactory(
 				_getServletContextWrapper(
-					servletContext, _freeMarkerBundleClassloader));
+					servletContext, _getFreeMarkerBundleClassloader()));
 
 			_taglibFactory.setObjectWrapper(objectWrapper);
 		}
@@ -914,6 +918,19 @@ public class FreeMarkerManager extends BaseTemplateManager {
 		@Override
 		public boolean isEmpty() {
 			return false;
+		}
+
+		private FreeMarkerBundleClassloader _getFreeMarkerBundleClassloader() {
+			if (_freeMarkerBundleClassloader != null) {
+				return _freeMarkerBundleClassloader;
+			}
+
+			_classLoaders.add(_bundleClassLoader);
+
+			_freeMarkerBundleClassloader = new FreeMarkerBundleClassloader(
+				_classLoaders);
+
+			return _freeMarkerBundleClassloader;
 		}
 
 		private final TaglibFactory _taglibFactory;

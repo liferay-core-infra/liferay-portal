02b2d5583bdf489f46d231c4a59c5e653e1a1236
hai.yu <hai.yu@liferay.com>
2023-05-25 11:19:25 +0800

    LPS-181446 When CompanyWebIdConsoleAppender's attribute "enabled" for true, register CompanyWebIdLogContext. In the log4j config file, when define layout's pattern include %X{company.webId}, it will be replaced with companyWebId to output in the console.

---
+++ modules/apps/portal/portal-log4j/src/main/java/com/liferay/portal/log4j/internal/CompanyWebIdLogContext.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.log4j.internal;
+
+import com.liferay.portal.kernel.log.LogContext;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.util.PortalInstances;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * @author Hai Yu
+ */
+public class CompanyWebIdLogContext implements LogContext {
+
+	@Override
+	public Map<String, String> getContext(String logName) {
+		long companyId = CompanyThreadLocal.getCompanyId();
+
+		if (companyId == 0) {
+			return Collections.emptyMap();
+		}
+
+		return Collections.singletonMap(
+			"webId", PortalInstances.getWebId(companyId));
+	}
+
+	@Override
+	public String getName() {
+		return "company";
+	}
+
+}
--- modules/apps/portal/portal-log4j/src/main/java/com/liferay/portal/log4j/internal/Log4jConfigUtil.java
+++ modules/apps/portal/portal-log4j/src/main/java/com/liferay/portal/log4j/internal/Log4jConfigUtil.java
@@ -18,6 +18,7 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
 import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
 import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogContextRegistryUtil;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -74,6 +75,13 @@ public class Log4jConfigUtil {
 
 			for (Element element : rootElement.elements()) {
 				_removeAppender(element, removedAppenderNames);
+
+				if (_isEnabledCompanyWebIdLogContext(element)) {
+					_companyWebIdLogContext = new CompanyWebIdLogContext();
+
+					LogContextRegistryUtil.registerLogContext(
+						_companyWebIdLogContext);
+				}
 
 				for (Element childElement : element.elements("Logger")) {
 					priorities.put(
@@ -172,6 +180,25 @@ public class Log4jConfigUtil {
 		LogManager.shutdown();
 	}
 
+	private static boolean _isEnabledCompanyWebIdLogContext(
+		Element parentElement) {
+
+		if (_companyWebIdLogContext != null) {
+			return false;
+		}
+
+		for (Element element : parentElement.elements("Appender")) {
+			if (Objects.equals(
+					element.attributeValue("type"), "CompanyWebIdConsole") &&
+				Objects.equals(element.attributeValue("enabled"), "true")) {
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
 	private static void _removeAppender(
 		Element parentElement, String... removedAppenderNames) {
 
@@ -204,6 +231,7 @@ public class Log4jConfigUtil {
 		Log4jConfigUtil.class);
 
 	private static final CentralizedConfiguration _centralizedConfiguration;
+	private static CompanyWebIdLogContext _companyWebIdLogContext;
 	private static final LoggerContext _loggerContext =
 		LoggerContext.getContext();
 

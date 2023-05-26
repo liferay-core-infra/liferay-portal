329519c5b68b0ba4bee010493d728d5c94b1dbaf
Jiefeng Wu <jeffwu0724@gmail.com>
2023-05-26 16:44:01 -0700

    LPS-183594 apply usages in test that use _portal.getUploadServletRequest()

--- modules/apps/static/portal-osgi-web/portal-osgi-web-portlet-container-test/src/testIntegration/java/com/liferay/portal/osgi/web/portlet/container/upload/portlet/request/test/UploadPortletRequestWhenCreatingFromMainConstructorTest.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-portlet-container-test/src/testIntegration/java/com/liferay/portal/osgi/web/portlet/container/upload/portlet/request/test/UploadPortletRequestWhenCreatingFromMainConstructorTest.java
@@ -14,6 +14,7 @@ import com.liferay.portal.osgi.web.portlet.container.test.util.PortletContainerT
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.upload.LiferayServletRequest;
+import com.liferay.portal.upload.UploadPortal;
 import com.liferay.portal.upload.test.util.UploadTestUtil;
 
 import java.util.List;
@@ -56,8 +57,8 @@ public class UploadPortletRequestWhenCreatingFromMainConstructorTest {
 
 		UploadPortletRequest uploadPortletRequest =
 			UploadTestUtil.createUploadPortletRequest(
-				_portal.getUploadServletRequest(mockHttpServletRequest), null,
-				_portletNamespace);
+				_uploadPortal.getUploadServletRequest(mockHttpServletRequest),
+				null, _portletNamespace);
 
 		Map<String, FileItem[]> multipartParameterMap =
 			uploadPortletRequest.getMultipartParameterMap();
@@ -81,5 +82,8 @@ public class UploadPortletRequestWhenCreatingFromMainConstructorTest {
 
 	@Inject
 	private Portal _portal;
+
+	@Inject
+	private UploadPortal _uploadPortal;
 
 }

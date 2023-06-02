b8436d844bd0c4827f6a89ee35381cb870c731dd
Jiefeng Wu <jeffwu0724@gmail.com>
2023-06-02 12:48:18 -0700

    LPS-184385 set "forwardedServer" as virtual.hosts.valid.hosts while running testGetForwardedHostWithXForwardedHostEnabled and testGetForwardedHostWithCustomXForwardedHostEnabled, because we would like to check when WEB_SERVER_FORWARDED_HOST_ENABLED is enabled, we can successfully getForwardedHost, instead of serverName

--- portal-impl/test/unit/com/liferay/portal/util/PortalImplUnitTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/PortalImplUnitTest.java
@@ -148,10 +148,14 @@ public class PortalImplUnitTest {
 		String webServerForwardedHostHeader =
 			PropsValues.WEB_SERVER_FORWARDED_HOST_HEADER;
 
+		String[] virtualHostsValidHosts = PropsValues.VIRTUAL_HOSTS_VALID_HOSTS;
+
 		try {
 			setPropsValuesValue("WEB_SERVER_FORWARDED_HOST_ENABLED", true);
 			setPropsValuesValue(
 				"WEB_SERVER_FORWARDED_HOST_HEADER", "X-Forwarded-Custom-Host");
+			setPropsValuesValue(
+				"VIRTUAL_HOSTS_VALID_HOSTS", new String[] {"forwardedServer"});
 
 			MockHttpServletRequest mockHttpServletRequest =
 				new MockHttpServletRequest();
@@ -171,6 +175,8 @@ public class PortalImplUnitTest {
 			setPropsValuesValue(
 				"WEB_SERVER_FORWARDED_HOST_HEADER",
 				webServerForwardedHostHeader);
+			setPropsValuesValue(
+				"VIRTUAL_HOSTS_VALID_HOSTS", virtualHostsValidHosts);
 		}
 	}
 
@@ -209,8 +215,13 @@ public class PortalImplUnitTest {
 		boolean webServerForwardedHostEnabled =
 			PropsValues.WEB_SERVER_FORWARDED_HOST_ENABLED;
 
+		String[] virtualHostsValidHosts = PropsValues.VIRTUAL_HOSTS_VALID_HOSTS;
+
 		try {
 			setPropsValuesValue("WEB_SERVER_FORWARDED_HOST_ENABLED", true);
+
+			setPropsValuesValue(
+				"VIRTUAL_HOSTS_VALID_HOSTS", new String[] {"forwardedServer"});
 
 			MockHttpServletRequest mockHttpServletRequest =
 				new MockHttpServletRequest();
@@ -227,6 +238,9 @@ public class PortalImplUnitTest {
 			setPropsValuesValue(
 				"WEB_SERVER_FORWARDED_HOST_ENABLED",
 				webServerForwardedHostEnabled);
+
+			setPropsValuesValue(
+				"VIRTUAL_HOSTS_VALID_HOSTS", virtualHostsValidHosts);
 		}
 	}
 

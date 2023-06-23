a261ddabef499809bdaa26bfaece1715eb22ec6c
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-06-23 09:15:07 +0800

    LPS-188563 There is no need to use blocking ServiceProxyFactory, ServiceProxyFactory was added in commit 08efb9680dc3f0dcf603977e75da72708de7191c to replace serviceTracker

--- portal-kernel/src/com/liferay/portal/kernel/security/permission/UserBagFactoryUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/security/permission/UserBagFactoryUtil.java
@@ -32,6 +32,6 @@ public class UserBagFactoryUtil {
 	private static volatile UserBagFactory _userBagFactory =
 		ServiceProxyFactory.newServiceTrackedInstance(
 			UserBagFactory.class, UserBagFactoryUtil.class, "_userBagFactory",
-			true);
+			false);
 
 }

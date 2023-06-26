12c6cc26b2342690c7206d7d8b4e9a0e8a3ab3b2
Renan Vasconelos <renan.vasconcelos@liferay.com>
2023-06-26 12:25:16 -0300

    LPS-187673 Rename OpenIdConnectProviderManagedServiceFactory to OpenIdConnectProviderPortalInstanceLifecycleListener and extract ManagedServiceFactory into inner class and register/unregister it in OpenIdConnectProviderPortalInstanceLifecycleListener, prepare for next

--- modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/OpenIdConnectAuthenticationHandlerImpl.java
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/OpenIdConnectAuthenticationHandlerImpl.java
@@ -22,7 +22,7 @@ import com.liferay.portal.security.sso.openid.connect.OpenIdConnectAuthenticatio
 import com.liferay.portal.security.sso.openid.connect.OpenIdConnectServiceException;
 import com.liferay.portal.security.sso.openid.connect.constants.OpenIdConnectConstants;
 import com.liferay.portal.security.sso.openid.connect.constants.OpenIdConnectWebKeys;
-import com.liferay.portal.security.sso.openid.connect.internal.configuration.admin.service.OpenIdConnectProviderManagedServiceFactory;
+import com.liferay.portal.security.sso.openid.connect.internal.configuration.admin.service.OpenIdConnectProviderPortalInstanceLifecycleListener;
 import com.liferay.portal.security.sso.openid.connect.internal.session.manager.OfflineOpenIdConnectSessionManager;
 import com.liferay.portal.security.sso.openid.connect.internal.util.OpenIdConnectRequestParametersUtil;
 import com.liferay.portal.security.sso.openid.connect.internal.util.OpenIdConnectTokenRequestUtil;
@@ -243,7 +243,7 @@ public class OpenIdConnectAuthenticationHandlerImpl
 		throws PortalException {
 
 		requestAuthentication(
-			_openIdConnectProviderManagedServiceFactory.getOAuthClientEntryId(
+			_openIdConnectProviderPortalInstanceLifecycleListener.getOAuthClientEntryId(
 				_portal.getCompanyId(httpServletRequest),
 				openIdConnectProviderName),
 			httpServletRequest, httpServletResponse);
@@ -467,8 +467,8 @@ public class OpenIdConnectAuthenticationHandlerImpl
 	private OIDCUserInfoProcessor _oidcUserInfoProcessor;
 
 	@Reference
-	private OpenIdConnectProviderManagedServiceFactory
-		_openIdConnectProviderManagedServiceFactory;
+	private OpenIdConnectProviderPortalInstanceLifecycleListener
+		_openIdConnectProviderPortalInstanceLifecycleListener;
 
 	@Reference
 	private Portal _portal;

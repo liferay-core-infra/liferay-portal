939b58bd5e244ef3220f03af0d5d63b48dc7082f
Tina Tian <tina.tian@liferay.com>
2023-07-19 16:25:42 -0700

    LPS-185138 SF, extract logic to UpgradeStepRegistry and apply

--- modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/release/osgi/commands/ReleaseManagerOSGiCommands.java
+++ modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/release/osgi/commands/ReleaseManagerOSGiCommands.java
@@ -99,7 +99,7 @@ public class ReleaseManagerOSGiCommands {
 			() -> _upgradeExecutor.executeUpgradeInfos(
 				bundleSymbolicName,
 				releaseGraphManager.getUpgradeInfos(
-					_releaseManagerImpl.getSchemaVersionString(
+					_upgradeStepRegistry.getSchemaVersionString(
 						bundleSymbolicName),
 					toVersionString)));
 
@@ -164,7 +164,7 @@ public class ReleaseManagerOSGiCommands {
 		sb.append(bundleSymbolicName);
 		sb.append(StringPool.SPACE);
 		sb.append(
-			_releaseManagerImpl.getSchemaVersionString(bundleSymbolicName));
+			_upgradeStepRegistry.getSchemaVersionString(bundleSymbolicName));
 		sb.append(StringPool.NEW_LINE);
 
 		for (UpgradeInfo upgradeProcess : upgradeInfos) {
@@ -183,7 +183,7 @@ public class ReleaseManagerOSGiCommands {
 
 		while (true) {
 			Set<String> upgradableBundleSymbolicNames =
-				_releaseManagerImpl.getUpgradableBundleSymbolicNames();
+				_upgradeStepRegistry.getUpgradableBundleSymbolicNames();
 
 			upgradableBundleSymbolicNames.removeAll(
 				upgradeThrewExceptionBundleSymbolicNames);

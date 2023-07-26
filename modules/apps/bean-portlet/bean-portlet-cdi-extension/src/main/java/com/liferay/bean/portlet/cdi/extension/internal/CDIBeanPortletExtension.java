e31a368c0f9be3c460fd44ff54c01daca46fa3a6
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-07-26 13:29:04 +0800

    LPS-189701 Apply, register BeanPortletRegistrarBag instead of using class BeanPortletRegistrar to register bean portlet.

--- modules/apps/bean-portlet/bean-portlet-cdi-extension/src/main/java/com/liferay/bean/portlet/cdi/extension/internal/CDIBeanPortletExtension.java
+++ modules/apps/bean-portlet/bean-portlet-cdi-extension/src/main/java/com/liferay/bean/portlet/cdi/extension/internal/CDIBeanPortletExtension.java
@@ -23,14 +23,13 @@ import com.liferay.bean.portlet.extension.BeanPortletMethodInvoker;
 import com.liferay.bean.portlet.extension.BeanPortletMethodType;
 import com.liferay.bean.portlet.extension.ScopedBean;
 import com.liferay.bean.portlet.extension.ViewRenderer;
-import com.liferay.bean.portlet.registration.BeanPortletRegistrar;
+import com.liferay.bean.portlet.registration.BeanPortletRegistrarBag;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.async.PortletAsyncListenerFactory;
 import com.liferay.portal.kernel.portlet.async.PortletAsyncScopeManagerFactory;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.ServiceProxyFactory;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.io.PrintWriter;
@@ -374,12 +373,16 @@ public class CDIBeanPortletExtension implements Extension {
 
 			};
 
-		_serviceRegistrations.addAll(
-			_beanPortletRegistrar.register(
-				new CDIBeanFilterMethodFactory(beanManager),
-				beanFilterMethodInvoker,
-				new CDIBeanPortletMethodFactory(beanManager),
-				beanPortletMethodInvoker, _discoveredClasses, servletContext));
+		_serviceRegistrations.add(
+			bundleContext.registerService(
+				BeanPortletRegistrarBag.class,
+				new BeanPortletRegistrarBag(
+					new CDIBeanFilterMethodFactory(beanManager),
+					beanFilterMethodInvoker,
+					new CDIBeanPortletMethodFactory(beanManager),
+					beanPortletMethodInvoker, _discoveredClasses,
+					servletContext),
+				null));
 
 		Bean<?> bean = beanManager.resolve(
 			beanManager.getBeans(ViewRenderer.class));
@@ -444,8 +447,11 @@ public class CDIBeanPortletExtension implements Extension {
 	public void step6ApplicationScopedBeforeDestroyed(
 		@Destroyed(ApplicationScoped.class) @Observes Object contextObject) {
 
-		_beanPortletRegistrar.unregister(
-			_serviceRegistrations, (ServletContext)contextObject);
+		for (ServiceRegistration<?> serviceRegistration :
+				_serviceRegistrations) {
+
+			serviceRegistration.unregister();
+		}
 
 		_serviceRegistrations.clear();
 	}
@@ -646,11 +652,6 @@ public class CDIBeanPortletExtension implements Extension {
 			}
 
 		};
-
-	private static volatile BeanPortletRegistrar _beanPortletRegistrar =
-		ServiceProxyFactory.newServiceTrackedInstance(
-			BeanPortletRegistrar.class, CDIBeanPortletExtension.class,
-			"_beanPortletRegistrar", true);
 
 	private static final Annotation _portletRequestScoped =
 		new PortletRequestScoped() {

e75f3ea1ca1a17dbe5323721cf8c0953d12158fd
Janis Zhang <janis.zhang@liferay.com>
2023-07-28 11:05:22 +0800

    LPS-192026 Reference WorkflowTaskManager in WorkflowInstanceEditDisplayContext and its subclass.

--- modules/apps/portal-workflow/portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/display/context/MyWorkflowInstanceEditDisplayContext.java
+++ modules/apps/portal-workflow/portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/display/context/MyWorkflowInstanceEditDisplayContext.java
@@ -10,7 +10,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
 import com.liferay.portal.kernel.workflow.WorkflowTask;
-import com.liferay.portal.kernel.workflow.WorkflowTaskManagerUtil;
+import com.liferay.portal.kernel.workflow.WorkflowTaskManager;
 import com.liferay.portal.workflow.comparator.WorkflowComparatorFactory;
 
 import java.util.List;
@@ -24,18 +24,19 @@ public class MyWorkflowInstanceEditDisplayContext
 	public MyWorkflowInstanceEditDisplayContext(
 		LiferayPortletRequest liferayPortletRequest,
 		LiferayPortletResponse liferayPortletResponse,
-		WorkflowComparatorFactory workflowComparatorFactory) {
+		WorkflowComparatorFactory workflowComparatorFactory,
+		WorkflowTaskManager workflowTaskManager) {
 
 		super(
 			liferayPortletRequest, liferayPortletResponse,
-			workflowComparatorFactory);
+			workflowComparatorFactory, workflowTaskManager);
 	}
 
 	@Override
 	public List<WorkflowTask> getWorkflowTasks() throws PortalException {
 		if (workflowTasks == null) {
 			workflowTasks =
-				WorkflowTaskManagerUtil.getWorkflowTasksByWorkflowInstance(
+				workflowTaskManager.getWorkflowTasksByWorkflowInstance(
 					workflowInstanceRequestHelper.getCompanyId(),
 					workflowInstanceRequestHelper.getUserId(),
 					getWorkflowInstanceId(), false, QueryUtil.ALL_POS,

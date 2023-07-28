c993bad5ba05ac1082d4f377476fcc3f5b6c14f4
Janis Zhang <janis.zhang@liferay.com>
2023-07-28 10:33:30 +0800

    LPS-192026 Merge logic into MyWorkflowTaskControlPanelEntry and delete unused class MyWorkflowTasksControlPanelEntry. Reference WorkflowTaskManager instead of util. Prepare for next.

--- modules/apps/portal-workflow/portal-workflow-task-web/src/main/java/com/liferay/portal/workflow/task/web/internal/portlet/MyWorkflowTaskControlPanelEntry.java
+++ modules/apps/portal-workflow/portal-workflow-task-web/src/main/java/com/liferay/portal/workflow/task/web/internal/portlet/MyWorkflowTaskControlPanelEntry.java
@@ -5,11 +5,16 @@
 
 package com.liferay.portal.workflow.task.web.internal.portlet;
 
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.portlet.ControlPanelEntry;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.util.PortletKeys;
-import com.liferay.portal.workflow.MyWorkflowTasksControlPanelEntry;
+import com.liferay.portal.kernel.workflow.WorkflowTaskManager;
+import com.liferay.portal.workflow.WorkflowControlPanelEntry;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Leonardo Barros
@@ -18,6 +23,34 @@ import org.osgi.service.component.annotations.Component;
 	property = "javax.portlet.name=" + PortletKeys.MY_WORKFLOW_TASK,
 	service = ControlPanelEntry.class
 )
-public class MyWorkflowTaskControlPanelEntry
-	extends MyWorkflowTasksControlPanelEntry {
+public class MyWorkflowTaskControlPanelEntry extends WorkflowControlPanelEntry {
+
+	@Override
+	protected boolean hasPermissionImplicitlyGranted(
+			PermissionChecker permissionChecker, Group group, Portlet portlet)
+		throws Exception {
+
+		int count = _workflowTaskManager.getWorkflowTaskCountByUser(
+			permissionChecker.getCompanyId(), permissionChecker.getUserId(),
+			null);
+
+		if (count > 0) {
+			return true;
+		}
+
+		count = _workflowTaskManager.getWorkflowTaskCountByUserRoles(
+			permissionChecker.getCompanyId(), permissionChecker.getUserId(),
+			null);
+
+		if (count > 0) {
+			return true;
+		}
+
+		return super.hasPermissionImplicitlyGranted(
+			permissionChecker, group, portlet);
+	}
+
+	@Reference
+	private WorkflowTaskManager _workflowTaskManager;
+
 }

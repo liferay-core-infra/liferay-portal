37f4673e4b6adc622716877e9ee84349c185f6f9
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-08-04 16:11:38 +0800

    LPS-192590 Replace ServiceProxyFactory with Snapshot in class ReindexSingleIndexerBackgroundTaskExecutor, not using hard references is to prevent class IndexWriterHelper from being initialized too early, resulting in classes SearchEngineHelper and  SearchEngine also being initialized too early, which can cause the IllegalArgumentException: no raw transformer found for class class org.elasticsearch.core.TimeValue in class ElasticsearchSearchEngine

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/background/task/ReindexSingleIndexerBackgroundTaskExecutor.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/background/task/ReindexSingleIndexerBackgroundTaskExecutor.java
@@ -23,7 +23,6 @@ import com.liferay.portal.kernel.search.SearchEngine;
 import com.liferay.portal.kernel.search.SearchEngineHelper;
 import com.liferay.portal.kernel.search.background.task.ReindexBackgroundTaskConstants;
 import com.liferay.portal.kernel.search.background.task.ReindexStatusMessageSender;
-import com.liferay.portal.kernel.util.ServiceProxyFactory;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.search.index.SyncReindexManager;
 
@@ -139,6 +138,9 @@ public class ReindexSingleIndexerBackgroundTaskExecutor
 					Thread.sleep(1000);
 				}
 				else {
+					IndexWriterHelper indexWriterHelper =
+						_indexWriterHelperSnapshot.get();
+
 					indexWriterHelper.deleteEntityDocuments(
 						companyId, className, true);
 				}
@@ -172,12 +174,6 @@ public class ReindexSingleIndexerBackgroundTaskExecutor
 			}
 		}
 	}
-
-	protected static volatile IndexWriterHelper indexWriterHelper =
-		ServiceProxyFactory.newServiceTrackedInstance(
-			IndexWriterHelper.class,
-			ReindexSingleIndexerBackgroundTaskExecutor.class,
-			"indexWriterHelper", true);
 
 	@Reference
 	protected IndexerRegistry indexerRegistry;
@@ -215,6 +211,10 @@ public class ReindexSingleIndexerBackgroundTaskExecutor
 	private static final Log _log = LogFactoryUtil.getLog(
 		ReindexSingleIndexerBackgroundTaskExecutor.class);
 
+	private static final Snapshot<IndexWriterHelper>
+		_indexWriterHelperSnapshot = new Snapshot<>(
+			ReindexSingleIndexerBackgroundTaskExecutor.class,
+			IndexWriterHelper.class);
 	private static final Snapshot<SyncReindexManager>
 		_syncReindexManagerSnapshot = new Snapshot<>(
 			ReindexSingleIndexerBackgroundTaskExecutor.class,

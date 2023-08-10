8b48267ceb5a598d7ed75bf29f1fe0b7bafcaea8
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-08-10 11:00:35 +0800

    LPS-188270 Remove unused methods

--- modules/apps/portal-configuration/portal-configuration-module-configuration-api/src/main/java/com/liferay/portal/configuration/module/configuration/ConfigurationProviderUtil.java
+++ modules/apps/portal-configuration/portal-configuration-module-configuration-api/src/main/java/com/liferay/portal/configuration/module/configuration/ConfigurationProviderUtil.java
@@ -11,52 +11,10 @@ import com.liferay.portal.kernel.module.configuration.ConfigurationException;
 import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.settings.SettingsLocator;
 
-import java.util.Dictionary;
-
 /**
  * @author Jorge Ferrer
  */
 public class ConfigurationProviderUtil {
-
-	public static <T> void deleteCompanyConfiguration(
-			Class<T> clazz, long companyId)
-		throws ConfigurationException {
-
-		ConfigurationProvider configurationProvider =
-			_configurationProviderSnapshot.get();
-
-		configurationProvider.deleteCompanyConfiguration(clazz, companyId);
-	}
-
-	public static <T> void deleteGroupConfiguration(
-			Class<T> clazz, long groupId)
-		throws ConfigurationException {
-
-		ConfigurationProvider configurationProvider =
-			_configurationProviderSnapshot.get();
-
-		configurationProvider.deleteGroupConfiguration(clazz, groupId);
-	}
-
-	public static <T> void deletePortletInstanceConfiguration(
-			Class<T> clazz, String portletId)
-		throws ConfigurationException {
-
-		ConfigurationProvider configurationProvider =
-			_configurationProviderSnapshot.get();
-
-		configurationProvider.deletePortletInstanceConfiguration(
-			clazz, portletId);
-	}
-
-	public static <T> void deleteSystemConfiguration(Class<T> clazz)
-		throws ConfigurationException {
-
-		ConfigurationProvider configurationProvider =
-			_configurationProviderSnapshot.get();
-
-		configurationProvider.deleteSystemConfiguration(clazz);
-	}
 
 	public static <T> T getCompanyConfiguration(Class<T> clazz, long companyId)
 		throws ConfigurationException {
@@ -75,10 +33,6 @@ public class ConfigurationProviderUtil {
 			_configurationProviderSnapshot.get();
 
 		return configurationProvider.getConfiguration(clazz, settingsLocator);
-	}
-
-	public static ConfigurationProvider getConfigurationProvider() {
-		return _configurationProviderSnapshot.get();
 	}
 
 	public static <T> T getGroupConfiguration(Class<T> clazz, long groupId)
@@ -108,51 +62,6 @@ public class ConfigurationProviderUtil {
 			_configurationProviderSnapshot.get();
 
 		return configurationProvider.getSystemConfiguration(clazz);
-	}
-
-	public static <T> void saveCompanyConfiguration(
-			Class<T> clazz, long companyId,
-			Dictionary<String, Object> properties)
-		throws ConfigurationException {
-
-		ConfigurationProvider configurationProvider =
-			_configurationProviderSnapshot.get();
-
-		configurationProvider.saveCompanyConfiguration(
-			clazz, companyId, properties);
-	}
-
-	public static <T> void saveGroupConfiguration(
-			Class<T> clazz, long groupId, Dictionary<String, Object> properties)
-		throws ConfigurationException {
-
-		ConfigurationProvider configurationProvider =
-			_configurationProviderSnapshot.get();
-
-		configurationProvider.saveGroupConfiguration(
-			clazz, groupId, properties);
-	}
-
-	public static <T> void savePortletInstanceConfiguration(
-			Class<T> clazz, String portletId,
-			Dictionary<String, Object> properties)
-		throws ConfigurationException {
-
-		ConfigurationProvider configurationProvider =
-			_configurationProviderSnapshot.get();
-
-		configurationProvider.savePortletInstanceConfiguration(
-			clazz, portletId, properties);
-	}
-
-	public static <T> void saveSystemConfiguration(
-			Class<T> clazz, Dictionary<String, Object> properties)
-		throws ConfigurationException {
-
-		ConfigurationProvider configurationProvider =
-			_configurationProviderSnapshot.get();
-
-		configurationProvider.saveSystemConfiguration(clazz, properties);
 	}
 
 	private static final Snapshot<ConfigurationProvider>

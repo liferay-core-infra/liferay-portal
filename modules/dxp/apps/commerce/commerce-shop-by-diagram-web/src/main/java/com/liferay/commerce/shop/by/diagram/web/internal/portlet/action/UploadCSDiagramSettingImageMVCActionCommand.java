07b4fa313cc8a31749fbffe1835d1d5e5adb0d2c
Regisson Aguiar <regisson.aguiar@liferay.com>
2023-08-16 17:05:52 -0300

    LPS-193648 refactor Handler classes from commerce-shop-by-diagram-web

--- modules/dxp/apps/commerce/commerce-shop-by-diagram-web/src/main/java/com/liferay/commerce/shop/by/diagram/web/internal/portlet/action/UploadCSDiagramSettingImageMVCActionCommand.java
+++ modules/dxp/apps/commerce/commerce-shop-by-diagram-web/src/main/java/com/liferay/commerce/shop/by/diagram/web/internal/portlet/action/UploadCSDiagramSettingImageMVCActionCommand.java
@@ -3,14 +3,14 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.commerce.shop.by.diagram.web.internal.portlet.action;
+package com.liferay.commerce.shop.by.diagram.admin.web.internal.portlet.action;
 
 import com.liferay.commerce.product.constants.CPPortletKeys;
-import com.liferay.commerce.shop.by.diagram.web.internal.upload.CSDiagramSettingImageUploadFileEntryHandler;
-import com.liferay.commerce.shop.by.diagram.web.internal.upload.CSDiagramSettingImageUploadResponseHandler;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.upload.UploadFileEntryHandler;
 import com.liferay.upload.UploadHandler;
+import com.liferay.upload.UploadResponseHandler;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -42,13 +42,15 @@ public class UploadCSDiagramSettingImageMVCActionCommand
 			actionResponse);
 	}
 
-	@Reference
-	private CSDiagramSettingImageUploadFileEntryHandler
-		_csDiagramSettingImageUploadFileEntryHandler;
+	@Reference(
+		target = "(component.name=com.liferay.commerce.shop.by.diagram.web.internal.upload.CSDiagramSettingImageUploadFileEntryHandler)"
+	)
+	private UploadFileEntryHandler _csDiagramSettingImageUploadFileEntryHandler;
 
-	@Reference
-	private CSDiagramSettingImageUploadResponseHandler
-		_csDiagramSettingImageUploadResponseHandler;
+	@Reference(
+		target = "(component.name=com.liferay.commerce.shop.by.diagram.web.internal.upload.CSDiagramSettingImageUploadResponseHandler)"
+	)
+	private UploadResponseHandler _csDiagramSettingImageUploadResponseHandler;
 
 	@Reference
 	private UploadHandler _uploadHandler;

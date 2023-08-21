903a817ba4d2677404e6b2fdc3a564249e68d7f8
Gabriel Henrique <gabriel.santos@liferay.com>
2023-08-21 14:15:33 -0300

    LPS-193655 Transform component ResourceBundleLoaderProvider into an Util class initializing the Service tracker into a static block instead of an activate method

--- modules/apps/push-notifications/push-notifications-web/src/main/java/com/liferay/push/notifications/web/internal/util/ResourceBundleLoaderProviderUtil.java
+++ modules/apps/push-notifications/push-notifications-web/src/main/java/com/liferay/push/notifications/web/internal/util/ResourceBundleLoaderProviderUtil.java
@@ -14,19 +14,18 @@ import com.liferay.push.notifications.sender.PushNotificationsSender;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceReference;
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * @author Andrea Di Giorgi
  */
-@Component(service = ResourceBundleLoaderProvider.class)
-public class ResourceBundleLoaderProvider {
+public class ResourceBundleLoaderProviderUtil {
 
-	public ResourceBundleLoader getResourceBundleLoader(String platform) {
+	public static ResourceBundleLoader getResourceBundleLoader(
+		String platform) {
+
 		ResourceBundleLoader resourceBundleLoader =
 			_serviceTrackerMap.getService(platform);
 
@@ -39,8 +38,15 @@ public class ResourceBundleLoaderProvider {
 			ResourceBundleLoaderUtil.getPortalResourceBundleLoader());
 	}
 
-	@Activate
-	protected void activate(BundleContext bundleContext) {
+	private static final ServiceTrackerMap<String, ResourceBundleLoader>
+		_serviceTrackerMap;
+
+	static {
+		Bundle bundle = FrameworkUtil.getBundle(
+			ResourceBundleLoaderProviderUtil.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
 		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
 			bundleContext, PushNotificationsSender.class, "platform",
 			new ServiceTrackerCustomizer
@@ -72,12 +78,5 @@ public class ResourceBundleLoaderProvider {
 
 			});
 	}
-
-	@Deactivate
-	protected void deactivate() {
-		_serviceTrackerMap.close();
-	}
-
-	private ServiceTrackerMap<String, ResourceBundleLoader> _serviceTrackerMap;
 
 }

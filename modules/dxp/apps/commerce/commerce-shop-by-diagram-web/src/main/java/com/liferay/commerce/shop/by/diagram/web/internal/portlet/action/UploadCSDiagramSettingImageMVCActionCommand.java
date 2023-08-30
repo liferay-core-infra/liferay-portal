5f64657066d8f1e6304e30934c3e75946a3cce7c
Regisson Aguiar <regisson.aguiar@liferay.com>
2023-08-30 13:49:31 -0300

    LPS-193648 refactor CSDiagramSettingImageUploadResponseHandler.java
    move and create inner class in UploadCSDiagramSettingImageMVCActionCommand.java and delete handler class

--- modules/dxp/apps/commerce/commerce-shop-by-diagram-web/src/main/java/com/liferay/commerce/shop/by/diagram/web/internal/portlet/action/UploadCSDiagramSettingImageMVCActionCommand.java
+++ modules/dxp/apps/commerce/commerce-shop-by-diagram-web/src/main/java/com/liferay/commerce/shop/by/diagram/web/internal/portlet/action/UploadCSDiagramSettingImageMVCActionCommand.java
@@ -9,24 +9,29 @@ import com.liferay.commerce.product.constants.CPPortletKeys;
 import com.liferay.commerce.product.exception.CPAttachmentFileEntryNameException;
 import com.liferay.commerce.product.exception.CPAttachmentFileEntrySizeException;
 import com.liferay.commerce.shop.by.diagram.configuration.CSDiagramSettingImageConfiguration;
-import com.liferay.commerce.shop.by.diagram.web.internal.upload.CSDiagramSettingImageUploadResponseHandler;
+import com.liferay.item.selector.ItemSelectorUploadResponseHandler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.servlet.ServletResponseConstants;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.upload.UploadPortletRequest;
 import com.liferay.portal.kernel.util.File;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.TempFileEntryUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.upload.UniqueFileNameProvider;
 import com.liferay.upload.UploadFileEntryHandler;
 import com.liferay.upload.UploadHandler;
+import com.liferay.upload.UploadResponseHandler;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -35,6 +40,7 @@ import java.util.Map;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.PortletRequest;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -145,6 +151,65 @@ public class UploadCSDiagramSettingImageMVCActionCommand
 
 	}
 
+	public class CSDiagramSettingImageUploadResponseHandler
+		implements UploadResponseHandler {
+
+		@Override
+		public JSONObject onFailure(
+				PortletRequest portletRequest, PortalException portalException)
+			throws PortalException {
+
+			JSONObject jsonObject =
+				_itemSelectorUploadResponseHandler.onFailure(
+					portletRequest, portalException);
+
+			if (portalException instanceof CPAttachmentFileEntryNameException ||
+				portalException instanceof CPAttachmentFileEntrySizeException) {
+
+				String errorMessage = StringPool.BLANK;
+				int errorType = 0;
+
+				if (portalException instanceof
+						CPAttachmentFileEntryNameException) {
+
+					errorMessage = StringUtil.merge(
+						_csDiagramSettingImageConfiguration.imageExtensions());
+
+					errorType =
+						ServletResponseConstants.SC_FILE_EXTENSION_EXCEPTION;
+				}
+				else if (portalException instanceof
+							CPAttachmentFileEntrySizeException) {
+
+					errorType = ServletResponseConstants.SC_FILE_SIZE_EXCEPTION;
+				}
+
+				jsonObject.put(
+					"error",
+					JSONUtil.put(
+						"errorType", errorType
+					).put(
+						"message", errorMessage
+					));
+			}
+			else {
+				throw portalException;
+			}
+
+			return jsonObject;
+		}
+
+		@Override
+		public JSONObject onSuccess(
+				UploadPortletRequest uploadPortletRequest, FileEntry fileEntry)
+			throws PortalException {
+
+			return _itemSelectorUploadResponseHandler.onSuccess(
+				uploadPortletRequest, fileEntry);
+		}
+
+	}
+
 	@Activate
 	protected void activate(Map<String, Object> properties) {
 		_csDiagramSettingImageConfiguration =
@@ -159,7 +224,7 @@ public class UploadCSDiagramSettingImageMVCActionCommand
 
 		_uploadHandler.upload(
 			new CSDiagramSettingImageUploadFileEntryHandler(),
-			_csDiagramSettingImageUploadResponseHandler, actionRequest,
+			new CSDiagramSettingImageUploadResponseHandler(), actionRequest,
 			actionResponse);
 	}
 
@@ -177,11 +242,11 @@ public class UploadCSDiagramSettingImageMVCActionCommand
 		_csDiagramSettingImageConfiguration;
 
 	@Reference
-	private CSDiagramSettingImageUploadResponseHandler
-		_csDiagramSettingImageUploadResponseHandler;
+	private File _file;
 
 	@Reference
-	private File _file;
+	private ItemSelectorUploadResponseHandler
+		_itemSelectorUploadResponseHandler;
 
 	@Reference
 	private UniqueFileNameProvider _uniqueFileNameProvider;

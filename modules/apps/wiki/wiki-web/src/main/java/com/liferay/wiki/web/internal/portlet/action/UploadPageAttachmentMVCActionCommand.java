768a40b7a8e75dedcee9115785a4d206c38381d2
Regisson Aguiar <regisson.aguiar@liferay.com>
2023-08-31 15:02:21 -0300

    LPS-194420 refactor PageAttachmentWikiUploadResponseHandler.java
    moving class to UploadPageAttachmentMVCActionCommand.java and deleting handler class

--- modules/apps/wiki/wiki-web/src/main/java/com/liferay/wiki/web/internal/portlet/action/UploadPageAttachmentMVCActionCommand.java
+++ modules/apps/wiki/wiki-web/src/main/java/com/liferay/wiki/web/internal/portlet/action/UploadPageAttachmentMVCActionCommand.java
@@ -5,15 +5,25 @@
 
 package com.liferay.wiki.web.internal.portlet.action;
 
+import com.liferay.item.selector.ItemSelectorUploadResponseHandler;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.servlet.ServletResponseConstants;
+import com.liferay.portal.kernel.upload.UploadPortletRequest;
 import com.liferay.upload.UploadHandler;
+import com.liferay.upload.UploadResponseHandler;
 import com.liferay.wiki.constants.WikiPortletKeys;
+import com.liferay.wiki.exception.WikiAttachmentMimeTypeException;
+import com.liferay.wiki.exception.WikiAttachmentSizeException;
 import com.liferay.wiki.web.internal.upload.PageAttachmentWikiUploadFileEntryHandler;
-import com.liferay.wiki.web.internal.upload.PageAttachmentWikiUploadResponseHandler;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.PortletRequest;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -31,6 +41,47 @@ import org.osgi.service.component.annotations.Reference;
 )
 public class UploadPageAttachmentMVCActionCommand extends BaseMVCActionCommand {
 
+	public class PageAttachmentWikiUploadResponseHandler
+		implements UploadResponseHandler {
+
+		@Override
+		public JSONObject onFailure(
+				PortletRequest portletRequest, PortalException portalException)
+			throws PortalException {
+
+			JSONObject jsonObject =
+				_itemSelectorUploadResponseHandler.onFailure(
+					portletRequest, portalException);
+
+			JSONObject errorJSONObject = null;
+
+			if (portalException instanceof WikiAttachmentMimeTypeException) {
+				errorJSONObject = JSONUtil.put(
+					"errorType",
+					ServletResponseConstants.SC_FILE_EXTENSION_EXCEPTION);
+			}
+			else if (portalException instanceof WikiAttachmentSizeException) {
+				errorJSONObject = JSONUtil.put(
+					"errorType",
+					ServletResponseConstants.SC_FILE_SIZE_EXCEPTION);
+			}
+
+			jsonObject.put("error", errorJSONObject);
+
+			return jsonObject;
+		}
+
+		@Override
+		public JSONObject onSuccess(
+				UploadPortletRequest uploadPortletRequest, FileEntry fileEntry)
+			throws PortalException {
+
+			return _itemSelectorUploadResponseHandler.onSuccess(
+				uploadPortletRequest, fileEntry);
+		}
+
+	}
+
 	@Override
 	protected void doProcessAction(
 			ActionRequest actionRequest, ActionResponse actionResponse)
@@ -43,12 +94,16 @@ public class UploadPageAttachmentMVCActionCommand extends BaseMVCActionCommand {
 	}
 
 	@Reference
+	private ItemSelectorUploadResponseHandler
+		_itemSelectorUploadResponseHandler;
+
+	@Reference
 	private PageAttachmentWikiUploadFileEntryHandler
 		_pageAttachmentWikiUploadFileEntryHandler;
 
-	@Reference
-	private PageAttachmentWikiUploadResponseHandler
-		_pageAttachmentWikiUploadResponseHandler;
+	private final PageAttachmentWikiUploadResponseHandler
+		_pageAttachmentWikiUploadResponseHandler =
+			new PageAttachmentWikiUploadResponseHandler();
 
 	@Reference
 	private UploadHandler _uploadHandler;

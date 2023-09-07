9c2e1338ca44f00727afd214ba079128f7b76d1e
Julius Lee <meng-huan.lee@liferay.com>
2023-09-07 16:26:44 -0700

    LPS-195588 Inline CommonPermissionImpl into Util

--- portal-kernel/src/com/liferay/portal/kernel/service/permission/CommonPermissionUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/permission/CommonPermissionUtil.java
@@ -6,7 +6,18 @@
 package com.liferay.portal.kernel.service.permission;
 
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Contact;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 
 /**
  * @author Brian Wing Shun Chan
@@ -18,8 +29,9 @@ public class CommonPermissionUtil {
 			String actionId)
 		throws PortalException {
 
-		_commonPermission.check(
-			permissionChecker, classNameId, classPK, actionId);
+		check(
+			permissionChecker, PortalUtil.getClassName(classNameId), classPK,
+			actionId);
 	}
 
 	public static void check(
@@ -27,18 +39,45 @@ public class CommonPermissionUtil {
 			String actionId)
 		throws PortalException {
 
-		_commonPermission.check(
-			permissionChecker, className, classPK, actionId);
+		if (className.equals(Company.class.getName())) {
+			long companyId = permissionChecker.getCompanyId();
+
+			if (classPK > 0) {
+				companyId = classPK;
+			}
+
+			if (!RoleLocalServiceUtil.hasUserRole(
+					permissionChecker.getUserId(), companyId,
+					RoleConstants.ADMINISTRATOR, true)) {
+
+				throw new PrincipalException.MustBeCompanyAdmin(
+					permissionChecker);
+			}
+		}
+		else if (className.equals(Contact.class.getName())) {
+			User user = UserLocalServiceUtil.getUserByContactId(classPK);
+
+			UserPermissionUtil.check(
+				permissionChecker, user.getUserId(), actionId);
+		}
+		else if (className.equals(Organization.class.getName())) {
+			OrganizationPermissionUtil.check(
+				permissionChecker, classPK, actionId);
+		}
+		else if (className.equals(User.class.getName())) {
+			UserPermissionUtil.check(permissionChecker, classPK, actionId);
+		}
+		else {
+			if (_log.isWarnEnabled()) {
+				_log.warn("Invalid class name " + className);
+			}
+
+			throw new PrincipalException.MustHavePermission(
+				permissionChecker, className, classPK, actionId);
+		}
 	}
 
-	public static CommonPermission getCommonPermission() {
-		return _commonPermission;
-	}
-
-	public void setCommonPermission(CommonPermission commonPermission) {
-		_commonPermission = commonPermission;
-	}
-
-	private static CommonPermission _commonPermission;
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommonPermissionUtil.class);
 
 }

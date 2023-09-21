096c2cff85593617bbca22975baae1d9c502b65b
Julius Lee <meng-huan.lee@liferay.com>
2023-09-21 16:27:29 -0700

    LPS-195588 Move CommonPermissionUtil to portal-impl

--- portal-impl/src/com/liferay/portal/service/permission/CommonPermissionUtil.java
+++ portal-impl/src/com/liferay/portal/service/permission/CommonPermissionUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.service.permission;
+package com.liferay.portal.service.permission;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
@@ -17,6 +17,8 @@ import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.service.permission.OrganizationPermissionUtil;
+import com.liferay.portal.kernel.service.permission.UserPermissionUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 
 /**

316cb7d674b50a6e4ea072da88bd129794190914
Julius Lee <meng-huan.lee@liferay.com>
2023-09-22 10:50:53 -0700

    LPS-195671 Move UsersAdminUtil to portal-impl

--- portal-impl/src/com/liferay/portlet/usersadmin/util/UsersAdminUtil.java
+++ portal-impl/src/com/liferay/portlet/usersadmin/util/UsersAdminUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.users.admin.kernel.util;
+package com.liferay.portlet.usersadmin.util;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.configuration.Filter;

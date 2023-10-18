9a104dc86c384c3075fc8c5baf6e99ff3fdcbb02
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2023-10-18 17:12:51 -0300

    LPD-8526 Inline UADApplicationExportHelper methods into AddUADExportProcessesMVCRenderCommand

--- modules/apps/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/portlet/action/AddUADExportProcessesMVCRenderCommand.java
+++ modules/apps/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/portlet/action/AddUADExportProcessesMVCRenderCommand.java
@@ -5,15 +5,28 @@
 
 package com.liferay.user.associated.data.web.internal.portlet.action;
 
+import com.liferay.portal.kernel.backgroundtask.BackgroundTask;
+import com.liferay.portal.kernel.backgroundtask.constants.BackgroundTaskConstants;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.user.associated.data.constants.UserAssociatedDataPortletKeys;
+import com.liferay.user.associated.data.display.UADDisplay;
+import com.liferay.user.associated.data.exporter.UADExporter;
 import com.liferay.user.associated.data.web.internal.constants.UADWebKeys;
-import com.liferay.user.associated.data.web.internal.helper.UADApplicationExportHelper;
+import com.liferay.user.associated.data.web.internal.display.UADApplicationExportDisplay;
+import com.liferay.user.associated.data.web.internal.export.background.task.UADExportBackgroundTaskManagerUtil;
+import com.liferay.user.associated.data.web.internal.registry.UADRegistry;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
 
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
@@ -47,7 +60,7 @@ public class AddUADExportProcessesMVCRenderCommand implements MVCRenderCommand {
 
 			renderRequest.setAttribute(
 				UADWebKeys.UAD_APPLICATION_EXPORT_DISPLAY_LIST,
-				_uadApplicationExportHelper.getUADApplicationExportDisplays(
+				_getUADApplicationExportDisplays(
 					themeDisplay.getScopeGroupId(), selectedUser.getUserId()));
 		}
 		catch (PortalException portalException) {
@@ -57,10 +70,77 @@ public class AddUADExportProcessesMVCRenderCommand implements MVCRenderCommand {
 		return "/add_uad_export_processes.jsp";
 	}
 
+	private Date _getApplicationLastExportDate(
+		String applicationKey, long groupId, long userId) {
+
+		BackgroundTask backgroundTask =
+			UADExportBackgroundTaskManagerUtil.fetchLastBackgroundTask(
+				applicationKey, groupId, userId,
+				BackgroundTaskConstants.STATUS_SUCCESSFUL);
+
+		if (backgroundTask != null) {
+			return backgroundTask.getCompletionDate();
+		}
+
+		return null;
+	}
+
+	private UADApplicationExportDisplay _getUADApplicationExportDisplay(
+		String applicationKey, long groupId, long userId) {
+
+		List<UADExporter<?>> uadExporters = new ArrayList<>();
+
+		for (UADDisplay<?> uadDisplay :
+				_uadRegistry.getApplicationUADDisplays(applicationKey)) {
+
+			uadExporters.add(
+				_uadRegistry.getUADExporter(uadDisplay.getTypeKey()));
+		}
+
+		int applicationDataCount = 0;
+
+		for (UADExporter<?> uadExporter : uadExporters) {
+			try {
+				applicationDataCount += (int)uadExporter.count(userId);
+			}
+			catch (PortalException portalException) {
+				_log.error(portalException);
+			}
+		}
+
+		return new UADApplicationExportDisplay(
+			applicationKey, applicationDataCount, !uadExporters.isEmpty(),
+			_getApplicationLastExportDate(applicationKey, groupId, userId));
+	}
+
+	private List<UADApplicationExportDisplay> _getUADApplicationExportDisplays(
+		long groupId, long userId) {
+
+		List<UADApplicationExportDisplay> uadApplicationExportDisplays =
+			new ArrayList<>();
+
+		for (String applicationKey :
+				_uadRegistry.getApplicationUADDisplaysKeySet()) {
+
+			uadApplicationExportDisplays.add(
+				_getUADApplicationExportDisplay(
+					applicationKey, groupId, userId));
+		}
+
+		uadApplicationExportDisplays.sort(
+			Comparator.comparing(
+				UADApplicationExportDisplay::getApplicationKey));
+
+		return uadApplicationExportDisplays;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AddUADExportProcessesMVCRenderCommand.class);
+
 	@Reference
 	private Portal _portal;
 
 	@Reference
-	private UADApplicationExportHelper _uadApplicationExportHelper;
+	private UADRegistry _uadRegistry;
 
 }

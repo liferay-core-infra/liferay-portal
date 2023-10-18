de4e4e6df20f15fe6eac4747f9ca47f5b67acf55
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-10-18 13:55:10 +0800

    LPS-196123 Use the DLProcessor interface in references instead of AudioProcessor and VideoProcessor, since they are now registered under the DLProcessor service only

--- modules/apps/server/server-admin-web/src/main/java/com/liferay/server/admin/web/internal/portlet/action/EditServerMVCActionCommand.java
+++ modules/apps/server/server-admin-web/src/main/java/com/liferay/server/admin/web/internal/portlet/action/EditServerMVCActionCommand.java
@@ -217,7 +217,9 @@ public class EditServerMVCActionCommand
 			_deleteFiles();
 		}
 		else if (cmd.equals("dlGenerateAudioPreviews")) {
-			_audioProcessor.generatePreviews();
+			AudioProcessor audioProcessor = (AudioProcessor)_audioDLProcessor;
+
+			audioProcessor.generatePreviews();
 
 			hideDefaultSuccessMessage(actionRequest);
 
@@ -240,7 +242,9 @@ public class EditServerMVCActionCommand
 			SessionMessages.add(actionRequest, "dlGeneratePDFPreviews");
 		}
 		else if (cmd.equals("dlGenerateVideoPreviews")) {
-			_videoProcessor.generatePreviews();
+			VideoProcessor videoProcessor = (VideoProcessor)_videoDLProcessor;
+
+			videoProcessor.generatePreviews();
 
 			hideDefaultSuccessMessage(actionRequest);
 
@@ -919,8 +923,8 @@ public class EditServerMVCActionCommand
 		EditServerMVCActionCommand.class, "_updateLogLevels", Map.class,
 		String.class);
 
-	@Reference
-	private AudioProcessor _audioProcessor;
+	@Reference(target = "(type=" + DLProcessorConstants.AUDIO_PROCESSOR + ")")
+	private DLProcessor _audioDLProcessor;
 
 	@Reference
 	private ClusterExecutor _clusterExecutor;
@@ -998,7 +1002,7 @@ public class EditServerMVCActionCommand
 	@Reference
 	private UserGroupMembershipPolicyFactory _userGroupMembershipPolicyFactory;
 
-	@Reference
-	private VideoProcessor _videoProcessor;
+	@Reference(target = "(type=" + DLProcessorConstants.VIDEO_PROCESSOR + ")")
+	private DLProcessor _videoDLProcessor;
 
 }

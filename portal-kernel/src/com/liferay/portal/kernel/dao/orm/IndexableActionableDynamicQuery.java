c0a98cafa95d77765157c6c8a9544b5fe575f10e
Janis Zhang <janis.zhang@liferay.com>
2023-10-25 11:08:45 +0800

    LPS-199958 Replace non-blocking ServiceProxyFactory usages by using Snapshot in IndexableActionableDynamicQuery

--- portal-kernel/src/com/liferay/portal/kernel/dao/orm/IndexableActionableDynamicQuery.java
+++ portal-kernel/src/com/liferay/portal/kernel/dao/orm/IndexableActionableDynamicQuery.java
@@ -11,11 +11,11 @@ import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.change.tracking.sql.CTSQLModeThreadLocal;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.change.tracking.CTModel;
+import com.liferay.portal.kernel.module.service.Snapshot;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.IndexWriterHelper;
 import com.liferay.portal.kernel.search.background.task.ReindexStatusMessageSenderUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
-import com.liferay.portal.kernel.util.ServiceProxyFactory;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -151,14 +151,14 @@ public class IndexableActionableDynamicQuery
 
 	private static final long _STATUS_INTERVAL = 100;
 
-	private static volatile IndexWriterHelper _indexWriterHelperProxy =
-		ServiceProxyFactory.newServiceTrackedInstance(
-			IndexWriterHelper.class, IndexableActionableDynamicQuery.class,
-			"_indexWriterHelperProxy", false);
+	private static final Snapshot<IndexWriterHelper>
+		_indexWriterHelperProxySnapshot = new Snapshot<>(
+			IndexableActionableDynamicQuery.class, IndexWriterHelper.class);
 
 	private long _count;
 	private Collection<Document> _documents = new ArrayList<>();
-	private IndexWriterHelper _indexWriterHelper = _indexWriterHelperProxy;
+	private IndexWriterHelper _indexWriterHelper =
+		_indexWriterHelperProxySnapshot.get();
 	private long _total;
 
 }

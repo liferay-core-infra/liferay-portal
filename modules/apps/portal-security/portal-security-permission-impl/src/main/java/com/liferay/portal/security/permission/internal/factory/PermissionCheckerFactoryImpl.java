236f2799fd2680e7a2202a31e27099b2e19d49d9
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-10-27 14:16:52 +0800

    LPS-200089 Make class PermissionCheckerFactoryImpl a component instead of a spring bean

--- modules/apps/portal-security/portal-security-permission-impl/src/main/java/com/liferay/portal/security/permission/internal/factory/PermissionCheckerFactoryImpl.java
+++ modules/apps/portal-security/portal-security-permission-impl/src/main/java/com/liferay/portal/security/permission/internal/factory/PermissionCheckerFactoryImpl.java
@@ -8,7 +8,6 @@ package com.liferay.portal.security.permission.internal.factory;
 import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerList;
 import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerListFactory;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.module.util.SystemBundleUtil;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactory;
 import com.liferay.portal.kernel.security.permission.contributor.RoleContributor;
@@ -17,30 +16,17 @@ import com.liferay.portal.security.permission.StagingPermissionChecker;
 import com.liferay.portal.util.PropsValues;
 
 import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 
 /**
  * @author Charles May
  * @author Brian Wing Shun Chan
  * @author Shuyang Zhou
  */
+@Component(service = PermissionCheckerFactory.class)
 public class PermissionCheckerFactoryImpl implements PermissionCheckerFactory {
-
-	public PermissionCheckerFactoryImpl() throws Exception {
-		Class<PermissionChecker> clazz =
-			(Class<PermissionChecker>)Class.forName(
-				PropsValues.PERMISSIONS_CHECKER);
-
-		_permissionChecker = clazz.newInstance();
-	}
-
-	public void afterPropertiesSet() {
-		BundleContext bundleContext = SystemBundleUtil.getBundleContext();
-
-		_permissionCheckerWrapperFactories = ServiceTrackerListFactory.open(
-			bundleContext, PermissionCheckerWrapperFactory.class);
-		_roleContributors = ServiceTrackerListFactory.open(
-			bundleContext, RoleContributor.class);
-	}
 
 	@Override
 	public PermissionChecker create(User user) {
@@ -62,12 +48,27 @@ public class PermissionCheckerFactoryImpl implements PermissionCheckerFactory {
 		return permissionChecker;
 	}
 
-	public void destroy() {
+	@Activate
+	protected void activate(BundleContext bundleContext) throws Exception {
+		Class<PermissionChecker> clazz =
+			(Class<PermissionChecker>)Class.forName(
+				PropsValues.PERMISSIONS_CHECKER);
+
+		_permissionChecker = clazz.newInstance();
+
+		_permissionCheckerWrapperFactories = ServiceTrackerListFactory.open(
+			bundleContext, PermissionCheckerWrapperFactory.class);
+		_roleContributors = ServiceTrackerListFactory.open(
+			bundleContext, RoleContributor.class);
+	}
+
+	@Deactivate
+	protected void deactivate() {
 		_permissionCheckerWrapperFactories.close();
 		_roleContributors.close();
 	}
 
-	private final PermissionChecker _permissionChecker;
+	private PermissionChecker _permissionChecker;
 	private ServiceTrackerList<PermissionCheckerWrapperFactory>
 		_permissionCheckerWrapperFactories;
 	private ServiceTrackerList<RoleContributor> _roleContributors;

15fe74ea96d0b2a03b63ea75558335fcde400984
Joao Victor <joao.victor@liferay.com>
2023-10-30 18:08:56 -0300

    LPS-199532 Remove JavaScriptAwarePortalWebResources interface, create NPMJavaScriptLastModifiedUtil, extract methods from NPMJavaScriptAwarePortalWebResources to it and apply to usages

--- modules/apps/frontend-js/frontend-js-loader-modules-extender/src/main/java/com/liferay/frontend/js/loader/modules/extender/internal/npm/NPMRegistryImpl.java
+++ modules/apps/frontend-js/frontend-js-loader-modules-extender/src/main/java/com/liferay/frontend/js/loader/modules/extender/internal/npm/NPMRegistryImpl.java
@@ -17,8 +17,8 @@ import com.liferay.frontend.js.loader.modules.extender.npm.JSModule;
 import com.liferay.frontend.js.loader.modules.extender.npm.JSModuleAlias;
 import com.liferay.frontend.js.loader.modules.extender.npm.JSPackage;
 import com.liferay.frontend.js.loader.modules.extender.npm.JSPackageDependency;
-import com.liferay.frontend.js.loader.modules.extender.npm.JavaScriptAwarePortalWebResources;
 import com.liferay.frontend.js.loader.modules.extender.npm.ModuleNameUtil;
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMJavaScriptLastModifiedUtil;
 import com.liferay.frontend.js.loader.modules.extender.npm.NPMRegistry;
 import com.liferay.frontend.js.loader.modules.extender.npm.NPMRegistryUpdate;
 import com.liferay.frontend.js.loader.modules.extender.npm.NPMRegistryUpdatesListener;
@@ -340,9 +340,6 @@ public class NPMRegistryImpl implements NPMRegistry {
 		_applyVersioning = details.applyVersioning();
 
 		_serviceTracker = _openServiceTracker();
-
-		_javaScriptAwarePortalWebResources = ServiceTrackerListFactory.open(
-			bundleContext, JavaScriptAwarePortalWebResources.class);
 	}
 
 	@Deactivate
@@ -350,8 +347,6 @@ public class NPMRegistryImpl implements NPMRegistry {
 		if (_npmRegistryUpdatesListeners != null) {
 			_npmRegistryUpdatesListeners.close();
 		}
-
-		_javaScriptAwarePortalWebResources.close();
 
 		_serviceTracker.close();
 
@@ -592,8 +587,6 @@ public class NPMRegistryImpl implements NPMRegistry {
 		new DCLSingleton<>();
 	private volatile Supplier<DataBag> _dataBagSupplier;
 	private final Map<String, String> _globalAliases = new HashMap<>();
-	private ServiceTrackerList<JavaScriptAwarePortalWebResources>
-		_javaScriptAwarePortalWebResources;
 
 	@Reference
 	private JSBundleProcessor _jsBundleProcessor;
@@ -678,13 +671,8 @@ public class NPMRegistryImpl implements NPMRegistry {
 
 				_notifyNPMRegistryUpdatesListeners();
 
-				for (JavaScriptAwarePortalWebResources
-						javaScriptAwarePortalWebResources :
-							_javaScriptAwarePortalWebResources) {
-
-					javaScriptAwarePortalWebResources.updateLastModifed(
-						bundle.getLastModified());
-				}
+				NPMJavaScriptLastModifiedUtil.updateLastModified(
+					bundle.getLastModified());
 			}
 
 			return jsBundle;

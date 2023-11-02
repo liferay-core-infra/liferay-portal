b4c3d1229d9359ca141cc5c6c01e3fb76928abe5
Jiefeng Wu <jeffwu0724@gmail.com>
2023-11-02 09:26:03 -0700

    LPS-200689 move GroupModelListener to modules and make it a component, so that we can avoid using spring-bean

--- modules/apps/portal-security/portal-security-permission-impl/src/main/java/com/liferay/portal/security/permission/internal/model/listener/GroupModelListener.java
+++ modules/apps/portal-security/portal-security-permission-impl/src/main/java/com/liferay/portal/security/permission/internal/model/listener/GroupModelListener.java
@@ -3,15 +3,20 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.security.permission;
+package com.liferay.portal.security.permission.internal.model.listener;
 
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.security.permission.PermissionCacheUtil;
+
+import org.osgi.service.component.annotations.Component;
 
 /**
  * @author Preston Crary
  */
+@Component(service = ModelListener.class)
 public class GroupModelListener extends BaseModelListener<Group> {
 
 	@Override

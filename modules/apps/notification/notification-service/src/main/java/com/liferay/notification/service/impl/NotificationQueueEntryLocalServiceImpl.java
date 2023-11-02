3dbdfbaea19b2bc697ecf4f05dc2725041d5b428
Janis Zhang <janis.zhang@liferay.com>
2023-11-02 13:26:55 +0800

    LPS-200625 Remove unused Reference field _userLocalService as its usage was removed by 12802fb5f9f2429e7c7927700855475f669c2b7d

--- modules/apps/notification/notification-service/src/main/java/com/liferay/notification/service/impl/NotificationQueueEntryLocalServiceImpl.java
+++ modules/apps/notification/notification-service/src/main/java/com/liferay/notification/service/impl/NotificationQueueEntryLocalServiceImpl.java
@@ -24,7 +24,6 @@ import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.service.ResourceLocalService;
-import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.Portal;
 
@@ -251,8 +250,5 @@ public class NotificationQueueEntryLocalServiceImpl
 
 	@Reference
 	private ResourceLocalService _resourceLocalService;
-
-	@Reference
-	private UserLocalService _userLocalService;
 
 }

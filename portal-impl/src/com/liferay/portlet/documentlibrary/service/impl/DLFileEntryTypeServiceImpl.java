3fb344c940bde97ea7dbf913602e24d0129244ff
Hayden Zheng <hayden.zheng@liferay.com>
2023-11-07 09:58:30 +0800

    LPS-193174 Get ModelResourcePermission via ModelResourcePermissionRegistryUtil in DLFileEntryTypeServiceImpl

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryTypeServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryTypeServiceImpl.java
@@ -18,11 +18,11 @@ import com.liferay.portal.kernel.module.service.Snapshot;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionRegistryUtil;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
-import com.liferay.portal.kernel.util.ServiceProxyFactory;
 import com.liferay.portlet.documentlibrary.constants.DLConstants;
 import com.liferay.portlet.documentlibrary.service.base.DLFileEntryTypeServiceBaseImpl;
 import com.liferay.util.dao.orm.CustomSQLUtil;
@@ -63,7 +63,12 @@ public class DLFileEntryTypeServiceImpl extends DLFileEntryTypeServiceBaseImpl {
 	public void deleteFileEntryType(long fileEntryTypeId)
 		throws PortalException {
 
-		_dlFileEntryTypeModelResourcePermission.check(
+		ModelResourcePermission<DLFileEntryType>
+			dlFileEntryTypeModelResourcePermission =
+				ModelResourcePermissionRegistryUtil.getModelResourcePermission(
+					DLFileEntryType.class.getName());
+
+		dlFileEntryTypeModelResourcePermission.check(
 			getPermissionChecker(), fileEntryTypeId, ActionKeys.DELETE);
 
 		dlFileEntryTypeLocalService.deleteFileEntryType(fileEntryTypeId);
@@ -76,7 +81,13 @@ public class DLFileEntryTypeServiceImpl extends DLFileEntryTypeServiceBaseImpl {
 		if (fileEntryTypeId !=
 				DLFileEntryTypeConstants.FILE_ENTRY_TYPE_ID_BASIC_DOCUMENT) {
 
-			_dlFileEntryTypeModelResourcePermission.check(
+			ModelResourcePermission<DLFileEntryType>
+				dlFileEntryTypeModelResourcePermission =
+					ModelResourcePermissionRegistryUtil.
+						getModelResourcePermission(
+							DLFileEntryType.class.getName());
+
+			dlFileEntryTypeModelResourcePermission.check(
 				getPermissionChecker(), fileEntryTypeId, ActionKeys.VIEW);
 		}
 
@@ -201,7 +212,12 @@ public class DLFileEntryTypeServiceImpl extends DLFileEntryTypeServiceBaseImpl {
 			Map<Locale, String> descriptionMap)
 		throws PortalException {
 
-		_dlFileEntryTypeModelResourcePermission.check(
+		ModelResourcePermission<DLFileEntryType>
+			dlFileEntryTypeModelResourcePermission =
+				ModelResourcePermissionRegistryUtil.getModelResourcePermission(
+					DLFileEntryType.class.getName());
+
+		dlFileEntryTypeModelResourcePermission.check(
 			getPermissionChecker(), fileEntryTypeId, ActionKeys.UPDATE);
 
 		return dlFileEntryTypeLocalService.updateFileEntryType(
@@ -221,8 +237,14 @@ public class DLFileEntryTypeServiceImpl extends DLFileEntryTypeServiceBaseImpl {
 		while (iterator.hasNext()) {
 			DLFileEntryType fileEntryType = iterator.next();
 
+			ModelResourcePermission<DLFileEntryType>
+				dlFileEntryTypeModelResourcePermission =
+					ModelResourcePermissionRegistryUtil.
+						getModelResourcePermission(
+							DLFileEntryType.class.getName());
+
 			if ((fileEntryType.getFileEntryTypeId() > 0) &&
-				!_dlFileEntryTypeModelResourcePermission.contains(
+				!dlFileEntryTypeModelResourcePermission.contains(
 					permissionChecker, fileEntryType, ActionKeys.VIEW)) {
 
 				iterator.remove();
@@ -313,14 +335,6 @@ public class DLFileEntryTypeServiceImpl extends DLFileEntryTypeServiceBaseImpl {
 		);
 	}
 
-	private static volatile ModelResourcePermission<DLFileEntryType>
-		_dlFileEntryTypeModelResourcePermission =
-			ServiceProxyFactory.newServiceTrackedInstance(
-				ModelResourcePermission.class, DLFileEntryTypeServiceImpl.class,
-				"_dlFileEntryTypeModelResourcePermission",
-				"(model.class.name=com.liferay.document.library.kernel.model." +
-					"DLFileEntryType)",
-				true);
 	private static final Snapshot<PortletResourcePermission>
 		_portletResourcePermissionSnapshot = new Snapshot<>(
 			DLFileEntryTypeServiceImpl.class, PortletResourcePermission.class,

f8db934ebf2876792ab24260a22769eff99a42f7
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-11-08 13:52:13 +0800

    LPS-201156 Move LayoutPrototypePermissionUtil into Portal-impl
    # breaking_change_report
    
    ## What portal-kernel/src/com/liferay/portal/kernel/service/permission/LayoutPrototypePermissionUtil.java
    
    LayoutPrototypePermissionUtil is moving from portal-kernel into portal-impl.
    
    ## Why
    
    We are merging portal-kernel into portal-impl as a long term goal to simplify portal structure.
    
    ## Alternatives
    
    Add portal-impl in build dependency and use the same class.
    
    ----

--- portal-impl/src/com/liferay/portal/service/permission/LayoutPrototypePermissionUtil.java
+++ portal-impl/src/com/liferay/portal/service/permission/LayoutPrototypePermissionUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.service.permission;
+package com.liferay.portal.service.permission;
 
 import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerList;
 import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerListFactory;
@@ -11,6 +11,7 @@ import com.liferay.portal.kernel.model.LayoutPrototype;
 import com.liferay.portal.kernel.module.util.SystemBundleUtil;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.service.permission.LayoutPrototypePermission;
 
 /**
  * @author Jorge Ferrer

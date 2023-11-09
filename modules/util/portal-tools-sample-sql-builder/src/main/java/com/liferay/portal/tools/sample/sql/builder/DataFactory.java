06ae5d7dc3b6629e96a7ff9ef84b898c4d64be25
Lily.Chi <lily.chi@liferay.com>
2023-11-09 13:49:53 +0800

    LPS-203707 Apply to MBMessageModel to make the users more even

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -6739,9 +6739,14 @@ public class DataFactory {
 
 		// Audit fields
 
+		UserModel userModel = _userModels.get(
+			_mbMessageUserIndex % _userModels.size());
+
+		_mbMessageUserIndex++;
+
 		mBMessageModel.setCompanyId(_companyId);
-		mBMessageModel.setUserId(_sampleUserId);
-		mBMessageModel.setUserName(_SAMPLE_USER_NAME);
+		mBMessageModel.setUserId(userModel.getUserId());
+		mBMessageModel.setUserName(userModel.getScreenName());
 		mBMessageModel.setCreateDate(new Date());
 		mBMessageModel.setModifiedDate(new Date());
 
@@ -7598,6 +7603,7 @@ public class DataFactory {
 	private final SimpleCounter _layoutPlidCounter;
 	private final SimpleCounter _layoutSetIdCounter;
 	private int _mbCategoryUserIndex;
+	private int _mbMessageUserIndex;
 	private RoleModel _ownerRoleModel;
 	private final SimpleCounter _portletPreferenceValueIdCounter;
 	private RoleModel _powerUserRoleModel;

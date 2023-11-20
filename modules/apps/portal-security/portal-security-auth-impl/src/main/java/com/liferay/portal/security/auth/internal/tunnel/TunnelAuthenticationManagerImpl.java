932f64256b5a887fce264f5d728905799b3aad8c
Julius Lee <meng-huan.lee@liferay.com>
2023-11-20 15:04:04 -0800

    LPS-200170 Move TunnelAuthenticationManagerImpl to new module: portal-security-auth-impl

--- modules/apps/portal-security/portal-security-auth-impl/src/main/java/com/liferay/portal/security/auth/internal/tunnel/TunnelAuthenticationManagerImpl.java
+++ modules/apps/portal-security/portal-security-auth-impl/src/main/java/com/liferay/portal/security/auth/internal/tunnel/TunnelAuthenticationManagerImpl.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.security.auth.tunnel;
+package com.liferay.portal.security.auth.internal.tunnel;
 
 import com.liferay.portal.kernel.encryptor.EncryptorException;
 import com.liferay.portal.kernel.encryptor.EncryptorUtil;

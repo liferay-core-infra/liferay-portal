64d4abc293d6210a7965dde9b91358ec29394a42
Julius Lee <meng-huan.lee@liferay.com>
2023-11-21 14:18:56 -0800

    LPS-190557 Merge CustomJspRegistryImpl into Util in portal-kernel

--- portal-kernel/src/com/liferay/portal/kernel/util/CustomJspRegistryUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/CustomJspRegistryUtil.java
@@ -5,7 +5,13 @@
 
 package com.liferay.portal.kernel.util;
 
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Ryan Park
@@ -16,8 +22,16 @@ public class CustomJspRegistryUtil {
 	public static String getCustomJspFileName(
 		String servletContextName, String fileName) {
 
-		return _customJspRegistry.getCustomJspFileName(
-			servletContextName, fileName);
+		int pos = fileName.lastIndexOf(CharPool.PERIOD);
+
+		if (pos == -1) {
+			return StringBundler.concat(
+				fileName, StringPool.PERIOD, servletContextName);
+		}
+
+		return StringBundler.concat(
+			fileName.substring(0, pos), CharPool.PERIOD, servletContextName,
+			fileName.substring(pos));
 	}
 
 	public static CustomJspRegistry getCustomJspRegistry() {
@@ -25,22 +39,21 @@ public class CustomJspRegistryUtil {
 	}
 
 	public static String getDisplayName(String servletContextName) {
-		return _customJspRegistry.getDisplayName(servletContextName);
+		return _servletContextNames.get(servletContextName);
 	}
 
 	public static Set<String> getServletContextNames() {
-		return _customJspRegistry.getServletContextNames();
+		return _servletContextNames.keySet();
 	}
 
 	public static void registerServletContextName(
 		String servletContextName, String displayName) {
 
-		_customJspRegistry.registerServletContextName(
-			servletContextName, displayName);
+		_servletContextNames.put(servletContextName, displayName);
 	}
 
 	public static void unregisterServletContextName(String servletContextName) {
-		_customJspRegistry.unregisterServletContextName(servletContextName);
+		_servletContextNames.remove(servletContextName);
 	}
 
 	public void setCustomJspRegistry(CustomJspRegistry customJspRegistry) {
@@ -48,5 +61,8 @@ public class CustomJspRegistryUtil {
 	}
 
 	private static CustomJspRegistry _customJspRegistry;
+
+	private static final Map<String, String> _servletContextNames =
+		new ConcurrentHashMap<>();
 
 }

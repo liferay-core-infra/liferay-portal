cde06ba2c7590e776a75c1f25124cc879a8ed774
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-11-23 10:02:51 +0800

    LPS-202459 Make PrefsPropsUtil use Snapshot to fetch the real service, prepare for next.
    # breaking
    ## What portal-kernel/src/com/liferay/portal/kernel/util/PrefsPropsUtil.java
    Method setPrefsProps in class PrefsPropsUtil is being removed.
    ## Why
    PrefsProps is an OSGI component now,  and we can use Snapshot to fetch the real service in class PrefsPropsUtil.
    ----

--- portal-kernel/src/com/liferay/portal/kernel/util/PrefsPropsUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/PrefsPropsUtil.java
@@ -5,6 +5,8 @@
 
 package com.liferay.portal.kernel.util;
 
+import com.liferay.portal.kernel.module.service.Snapshot;
+
 import java.util.Properties;
 
 import javax.portlet.PortletPreferences;
@@ -15,305 +17,412 @@ import javax.portlet.PortletPreferences;
 public class PrefsPropsUtil {
 
 	public static boolean getBoolean(long companyId, String name) {
-		return _prefsProps.getBoolean(companyId, name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getBoolean(companyId, name);
 	}
 
 	public static boolean getBoolean(
 		long companyId, String name, boolean defaultValue) {
 
-		return _prefsProps.getBoolean(companyId, name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getBoolean(companyId, name, defaultValue);
 	}
 
 	public static boolean getBoolean(
 		PortletPreferences portletPreferences, String name) {
 
-		return _prefsProps.getBoolean(portletPreferences, name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getBoolean(portletPreferences, name);
 	}
 
 	public static boolean getBoolean(
 		PortletPreferences portletPreferences, String name,
 		boolean defaultValue) {
 
-		return _prefsProps.getBoolean(portletPreferences, name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getBoolean(portletPreferences, name, defaultValue);
 	}
 
 	public static boolean getBoolean(String name) {
-		return _prefsProps.getBoolean(name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getBoolean(name);
 	}
 
 	public static boolean getBoolean(String name, boolean defaultValue) {
-		return _prefsProps.getBoolean(name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getBoolean(name, defaultValue);
 	}
 
 	public static String getContent(long companyId, String name) {
-		return _prefsProps.getContent(companyId, name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getContent(companyId, name);
 	}
 
 	public static String getContent(
 		PortletPreferences portletPreferences, String name) {
 
-		return _prefsProps.getContent(portletPreferences, name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getContent(portletPreferences, name);
 	}
 
 	public static String getContent(String name) {
-		return _prefsProps.getContent(name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getContent(name);
 	}
 
 	public static double getDouble(long companyId, String name) {
-		return _prefsProps.getDouble(companyId, name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getDouble(companyId, name);
 	}
 
 	public static double getDouble(
 		long companyId, String name, double defaultValue) {
 
-		return _prefsProps.getDouble(companyId, name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getDouble(companyId, name, defaultValue);
 	}
 
 	public static double getDouble(
 		PortletPreferences portletPreferences, String name) {
 
-		return _prefsProps.getDouble(portletPreferences, name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getDouble(portletPreferences, name);
 	}
 
 	public static double getDouble(
 		PortletPreferences portletPreferences, String name,
 		double defaultValue) {
 
-		return _prefsProps.getDouble(portletPreferences, name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getDouble(portletPreferences, name, defaultValue);
 	}
 
 	public static double getDouble(String name) {
-		return _prefsProps.getDouble(name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getDouble(name);
 	}
 
 	public static double getDouble(String name, double defaultValue) {
-		return _prefsProps.getDouble(name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getDouble(name, defaultValue);
 	}
 
 	public static int getInteger(long companyId, String name) {
-		return _prefsProps.getInteger(companyId, name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getInteger(companyId, name);
 	}
 
 	public static int getInteger(
 		long companyId, String name, int defaultValue) {
 
-		return _prefsProps.getInteger(companyId, name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getInteger(companyId, name, defaultValue);
 	}
 
 	public static int getInteger(
 		PortletPreferences portletPreferences, String name) {
 
-		return _prefsProps.getInteger(portletPreferences, name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getInteger(portletPreferences, name);
 	}
 
 	public static int getInteger(
 		PortletPreferences portletPreferences, String name, int defaultValue) {
 
-		return _prefsProps.getInteger(portletPreferences, name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getInteger(portletPreferences, name, defaultValue);
 	}
 
 	public static int getInteger(String name) {
-		return _prefsProps.getInteger(name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getInteger(name);
 	}
 
 	public static int getInteger(String name, int defaultValue) {
-		return _prefsProps.getInteger(name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getInteger(name, defaultValue);
 	}
 
 	public static long getLong(long companyId, String name) {
-		return _prefsProps.getLong(companyId, name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getLong(companyId, name);
 	}
 
 	public static long getLong(long companyId, String name, long defaultValue) {
-		return _prefsProps.getLong(companyId, name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getLong(companyId, name, defaultValue);
 	}
 
 	public static long getLong(
 		PortletPreferences portletPreferences, String name) {
 
-		return _prefsProps.getLong(portletPreferences, name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getLong(portletPreferences, name);
 	}
 
 	public static long getLong(
 		PortletPreferences portletPreferences, String name, long defaultValue) {
 
-		return _prefsProps.getLong(portletPreferences, name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getLong(portletPreferences, name, defaultValue);
 	}
 
 	public static long getLong(String name) {
-		return _prefsProps.getLong(name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getLong(name);
 	}
 
 	public static long getLong(String name, long defaultValue) {
-		return _prefsProps.getLong(name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getLong(name, defaultValue);
 	}
 
 	public static PortletPreferences getPreferences() {
-		return _prefsProps.getPreferences();
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getPreferences();
 	}
 
 	public static PortletPreferences getPreferences(long companyId) {
-		return _prefsProps.getPreferences(companyId);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getPreferences(companyId);
 	}
 
 	public static PrefsProps getPrefsProps() {
-		return _prefsProps;
+		return _prefsPropsSnapshot.get();
 	}
 
 	public static Properties getProperties(
 		PortletPreferences portletPreferences, String prefix,
 		boolean removePrefix) {
 
-		return _prefsProps.getProperties(
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getProperties(
 			portletPreferences, prefix, removePrefix);
 	}
 
 	public static Properties getProperties(
 		String prefix, boolean removePrefix) {
 
-		return _prefsProps.getProperties(prefix, removePrefix);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getProperties(prefix, removePrefix);
 	}
 
 	public static short getShort(long companyId, String name) {
-		return _prefsProps.getShort(companyId, name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getShort(companyId, name);
 	}
 
 	public static short getShort(
 		long companyId, String name, short defaultValue) {
 
-		return _prefsProps.getShort(companyId, name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getShort(companyId, name, defaultValue);
 	}
 
 	public static short getShort(
 		PortletPreferences portletPreferences, String name) {
 
-		return _prefsProps.getShort(portletPreferences, name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getShort(portletPreferences, name);
 	}
 
 	public static short getShort(
 		PortletPreferences portletPreferences, String name,
 		short defaultValue) {
 
-		return _prefsProps.getShort(portletPreferences, name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getShort(portletPreferences, name, defaultValue);
 	}
 
 	public static short getShort(String name) {
-		return _prefsProps.getShort(name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getShort(name);
 	}
 
 	public static short getShort(String name, short defaultValue) {
-		return _prefsProps.getShort(name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getShort(name, defaultValue);
 	}
 
 	public static String getString(long companyId, String name) {
-		return _prefsProps.getString(companyId, name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getString(companyId, name);
 	}
 
 	public static String getString(
 		long companyId, String name, String defaultValue) {
 
-		return _prefsProps.getString(companyId, name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getString(companyId, name, defaultValue);
 	}
 
 	public static String getString(
 		PortletPreferences portletPreferences, String name) {
 
-		return _prefsProps.getString(portletPreferences, name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getString(portletPreferences, name);
 	}
 
 	public static String getString(
 		PortletPreferences portletPreferences, String name,
 		boolean defaultValue) {
 
-		return _prefsProps.getString(portletPreferences, name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getString(portletPreferences, name, defaultValue);
 	}
 
 	public static String getString(
 		PortletPreferences portletPreferences, String name,
 		double defaultValue) {
 
-		return _prefsProps.getString(portletPreferences, name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getString(portletPreferences, name, defaultValue);
 	}
 
 	public static String getString(
 		PortletPreferences portletPreferences, String name, int defaultValue) {
 
-		return _prefsProps.getString(portletPreferences, name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getString(portletPreferences, name, defaultValue);
 	}
 
 	public static String getString(
 		PortletPreferences portletPreferences, String name, long defaultValue) {
 
-		return _prefsProps.getString(portletPreferences, name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getString(portletPreferences, name, defaultValue);
 	}
 
 	public static String getString(
 		PortletPreferences portletPreferences, String name,
 		short defaultValue) {
 
-		return _prefsProps.getString(portletPreferences, name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getString(portletPreferences, name, defaultValue);
 	}
 
 	public static String getString(
 		PortletPreferences portletPreferences, String name,
 		String defaultValue) {
 
-		return _prefsProps.getString(portletPreferences, name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getString(portletPreferences, name, defaultValue);
 	}
 
 	public static String getString(String name) {
-		return _prefsProps.getString(name);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getString(name);
 	}
 
 	public static String getString(String name, String defaultValue) {
-		return _prefsProps.getString(name, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getString(name, defaultValue);
 	}
 
 	public static String[] getStringArray(
 		long companyId, String name, String delimiter) {
 
-		return _prefsProps.getStringArray(companyId, name, delimiter);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getStringArray(companyId, name, delimiter);
 	}
 
 	public static String[] getStringArray(
 		long companyId, String name, String delimiter, String[] defaultValue) {
 
-		return _prefsProps.getStringArray(
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getStringArray(
 			companyId, name, delimiter, defaultValue);
 	}
 
 	public static String[] getStringArray(
 		PortletPreferences portletPreferences, String name, String delimiter) {
 
-		return _prefsProps.getStringArray(portletPreferences, name, delimiter);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getStringArray(portletPreferences, name, delimiter);
 	}
 
 	public static String[] getStringArray(
 		PortletPreferences portletPreferences, String name, String delimiter,
 		String[] defaultValue) {
 
-		return _prefsProps.getStringArray(
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getStringArray(
 			portletPreferences, name, delimiter, defaultValue);
 	}
 
 	public static String[] getStringArray(String name, String delimiter) {
-		return _prefsProps.getStringArray(name, delimiter);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getStringArray(name, delimiter);
 	}
 
 	public static String[] getStringArray(
 		String name, String delimiter, String[] defaultValue) {
 
-		return _prefsProps.getStringArray(name, delimiter, defaultValue);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getStringArray(name, delimiter, defaultValue);
 	}
 
 	public static String getStringFromNames(long companyId, String... names) {
-		return _prefsProps.getStringFromNames(companyId, names);
+		PrefsProps prefsProps = _prefsPropsSnapshot.get();
+
+		return prefsProps.getStringFromNames(companyId, names);
 	}
 
-	public void setPrefsProps(PrefsProps prefsProps) {
-		_prefsProps = prefsProps;
-	}
-
-	private static PrefsProps _prefsProps;
+	private static final Snapshot<PrefsProps> _prefsPropsSnapshot =
+		new Snapshot<>(PrefsPropsUtil.class, PrefsProps.class);
 
 }

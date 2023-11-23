9a895aa87d644fcd1ddd7e5562d5921b98468b89
Janis Zhang <janis.zhang@liferay.com>
2023-11-23 15:50:47 +0800

    LPS-201364 Make PDFProcessorMessageListener as a component and register Destination in activate method.

--- modules/apps/document-library/document-library-preview-pdf/src/main/java/com/liferay/document/library/preview/pdf/internal/messaging/PDFProcessorMessageListener.java
+++ modules/apps/document-library/document-library-preview-pdf/src/main/java/com/liferay/document/library/preview/pdf/internal/messaging/PDFProcessorMessageListener.java
@@ -8,14 +8,49 @@ package com.liferay.document.library.preview.pdf.internal.messaging;
 import com.liferay.document.library.kernel.util.PDFProcessorUtil;
 import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
+import com.liferay.portal.kernel.messaging.Destination;
+import com.liferay.portal.kernel.messaging.DestinationConfiguration;
+import com.liferay.portal.kernel.messaging.DestinationFactory;
+import com.liferay.portal.kernel.messaging.DestinationNames;
+import com.liferay.portal.kernel.messaging.MessageListener;
 import com.liferay.portal.kernel.repository.model.FileVersion;
+import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.repository.liferayrepository.model.LiferayFileVersion;
 import com.liferay.portlet.documentlibrary.messaging.BaseProcessorMessageListener;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Alexander Chow
  */
+@Component(
+	property = "destination.name=" + DestinationNames.DOCUMENT_LIBRARY_PDF_PROCESSOR,
+	service = MessageListener.class
+)
 public class PDFProcessorMessageListener extends BaseProcessorMessageListener {
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		Destination destination = _destinationFactory.createDestination(
+			new DestinationConfiguration(
+				DestinationConfiguration.DESTINATION_TYPE_SERIAL,
+				DestinationNames.DOCUMENT_LIBRARY_PDF_PROCESSOR));
+
+		_serviceRegistration = bundleContext.registerService(
+			Destination.class, destination,
+			MapUtil.singletonDictionary(
+				"destination.name", destination.getName()));
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceRegistration.unregister();
+	}
 
 	@Override
 	protected void generate(
@@ -50,5 +85,10 @@ public class PDFProcessorMessageListener extends BaseProcessorMessageListener {
 			}
 		}
 	}
+
+	@Reference
+	private DestinationFactory _destinationFactory;
+
+	private ServiceRegistration<Destination> _serviceRegistration;
 
 }

olafk/liferay-portal-old

commit 98578f788c07f813ba8db42d5e0ae9c63837db21

LPS-200508 portal-vulcan-impl - use lazy initialization for LiferayMethodDataFetchingProcessor's _graphQLContributorServiceTracker service tracker (change to ServiceTrackerList)

modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/graphql/servlet/GraphQLServletExtender.java
@@ -217,7 +217,6 @@ import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.http.context.ServletContextHelper;
 import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
-import org.osgi.util.tracker.ServiceTracker;
 import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
@@ -570,20 +569,19 @@ public class GraphQLServletExtender {
 					}
 
 				});
-		_graphQLRequestContextValidators = ServiceTrackerListFactory.open(
-			bundleContext, GraphQLRequestContextValidator.class);
 
-		_graphQLContributorServiceTracker = new ServiceTracker<>(
-			bundleContext, GraphQLContributor.class,
+		_graphQLContributorServiceTrackerList = ServiceTrackerListFactory.open(
+			bundleContext, GraphQLContributor.class, null,
 			new GraphQLContributorServiceTrackerCustomizer());
 
-		_graphQLContributorServiceTracker.open();
+		_graphQLRequestContextValidators = ServiceTrackerListFactory.open(
+			bundleContext, GraphQLRequestContextValidator.class);
 
 		_liferayMethodDataFetchingProcessor =
 			new LiferayMethodDataFetchingProcessor(
 				_bundleContext, _companyLocalService, _depotEntryLocalService,
 				_expressionConvert, _filterParserProvider,
-				_graphQLContributorServiceTracker, _groupLocalService,
+				_graphQLContributorServiceTrackerList, _groupLocalService,
 				_language, _paginationProvider, _portal,
 				_resourceActionLocalService, _resourcePermissionLocalService,
 				_roleLocalService, _sortParserProvider,
@@ -718,7 +716,7 @@ public class GraphQLServletExtender {
 
 	@Deactivate
 	protected void deactivate() {
-		_graphQLContributorServiceTracker.close();
+		_graphQLContributorServiceTrackerList.close();
 
 		_graphQLRequestContextValidators.close();
 
@@ -1950,8 +1948,8 @@ public class GraphQLServletExtender {
 	@Reference
 	private FilterParserProvider _filterParserProvider;
 
-	private ServiceTracker<GraphQLContributor, GraphQLContributor>
-		_graphQLContributorServiceTracker;
+	private ServiceTrackerList<GraphQLContributor>
+		_graphQLContributorServiceTrackerList;
 	private GraphQLDTOContributorDataFetchingProcessor
 		_graphQLDTOContributorDataFetchingProcessor;
 	private ServiceTrackerMap<String, GraphQLDTOContributor>

modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/graphql/data/processor/LiferayMethodDataFetchingProcessor.java
@@ -8,6 +8,7 @@ package com.liferay.portal.vulcan.internal.graphql.data.processor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.liferay.depot.service.DepotEntryLocalService;
+import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerList;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -85,7 +86,6 @@ import org.apache.cxf.message.MessageImpl;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
-import org.osgi.util.tracker.ServiceTracker;
 
 /**
  * @author Carlos Correa
@@ -97,8 +97,8 @@ public class LiferayMethodDataFetchingProcessor {
 		DepotEntryLocalService depotEntryLocalService,
 		ExpressionConvert<Filter> expressionConvert,
 		FilterParserProvider filterParserProvider,
-		ServiceTracker<GraphQLContributor, GraphQLContributor>
-			graphQLContributorServiceTracker,
+		ServiceTrackerList<GraphQLContributor>
+			graphQLContributorServiceTrackerList,
 		GroupLocalService groupLocalService, Language language,
 		PaginationProvider paginationProvider, Portal portal,
 		ResourceActionLocalService resourceActionLocalService,
@@ -113,7 +113,8 @@ public class LiferayMethodDataFetchingProcessor {
 		_depotEntryLocalService = depotEntryLocalService;
 		_expressionConvert = expressionConvert;
 		_filterParserProvider = filterParserProvider;
-		_graphQLContributorServiceTracker = graphQLContributorServiceTracker;
+		_graphQLContributorServiceTrackerList =
+			graphQLContributorServiceTrackerList;
 		_groupLocalService = groupLocalService;
 		_language = language;
 		_paginationProvider = paginationProvider;
@@ -668,7 +669,7 @@ public class LiferayMethodDataFetchingProcessor {
 	}
 
 	private Object _getService(Class<?> clazz) {
-		for (Object service : _graphQLContributorServiceTracker.getServices()) {
+		for (Object service : _graphQLContributorServiceTrackerList) {
 			if (clazz.isAssignableFrom(service.getClass())) {
 				return service;
 			}
@@ -698,8 +699,8 @@ public class LiferayMethodDataFetchingProcessor {
 	private final DepotEntryLocalService _depotEntryLocalService;
 	private final ExpressionConvert<Filter> _expressionConvert;
 	private final FilterParserProvider _filterParserProvider;
-	private final ServiceTracker<GraphQLContributor, GraphQLContributor>
-		_graphQLContributorServiceTracker;
+	private final ServiceTrackerList<GraphQLContributor>
+		_graphQLContributorServiceTrackerList;
 	private final GroupLocalService _groupLocalService;
 	private final Language _language;
 	private final PaginationProvider _paginationProvider;

liferay-core-infra/liferay-portal

commit 3de635ef382604ab5e7dc1b7ca4ba0e9a03cd191

LPS-203250 _graphQLContributorServiceTrackerList from GraphSQLServletExtender can't have lazy initialization because other services depends of this trackers' registrations; this commit fixes the bug which was introduced in 98578f788c07f813ba8db42d5e0ae9c63837db21

modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/graphql/servlet/GraphQLServletExtender.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 import com.liferay.depot.service.DepotEntryLocalService;
+import com.liferay.osgi.service.tracker.collections.EagerServiceTrackerCustomizer;
 import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerList;
 import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerListFactory;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
@@ -2496,7 +2497,7 @@ public class GraphQLServletExtender {
 	}
 
 	private class GraphQLContributorServiceTrackerCustomizer
-		implements ServiceTrackerCustomizer
+		implements EagerServiceTrackerCustomizer
 			<GraphQLContributor, GraphQLContributor> {
 
 		@Override
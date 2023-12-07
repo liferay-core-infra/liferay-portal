e7c89ac7f0027c46dfecc42f0ed06482ca841c3b
Hayden Zheng <hayden.zheng@liferay.com>
2023-12-07 09:41:13 +0800

    LPS-193174 Remove methods return blocking ServiceProxyFactory in ModelResourcePermissionFactory and PortletResourcePermissionFactory
    # breaking
    
    ## What portal-kernel/src/com/liferay/portal/kernel/security/permission/resource/ModelResourcePermissionFactory.java
    
    Method getInstance() is removed.
    
    ## Why
    
    We are removing blocking ServiceProxyFactory usages. Service ModelResourcePermission is get via ModelResourcePermissionRegistryUtil now.
    
    ----
    
    # breaking
    
    ## What portal-kernel/src/com/liferay/portal/kernel/security/permission/resource/PortletResourcePermissionFactory.java
    
    Method getInstance() is removed.
    
    ## Why
    
    We are removing blocking ServiceProxyFactory usages. Usages are replaced by Snapshot.

--- portal-kernel/src/com/liferay/portal/kernel/security/permission/resource/ModelResourcePermissionFactory.java
+++ portal-kernel/src/com/liferay/portal/kernel/security/permission/resource/ModelResourcePermissionFactory.java
@@ -12,8 +12,6 @@ import com.liferay.portal.kernel.model.ClassedModel;
 import com.liferay.portal.kernel.model.GroupedModel;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
-import com.liferay.portal.kernel.service.BaseService;
-import com.liferay.portal.kernel.util.ServiceProxyFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -65,17 +63,6 @@ public class ModelResourcePermissionFactory {
 				modelResourcePermission, modelResourcePermissionLogics::add);
 
 		return modelResourcePermission;
-	}
-
-	@SuppressWarnings("unchecked")
-	public static <T extends ClassedModel> ModelResourcePermission<T>
-		getInstance(
-			Class<? extends BaseService> declaringServiceClass,
-			String fieldName, Class<T> modelClass) {
-
-		return ServiceProxyFactory.newServiceTrackedInstance(
-			ModelResourcePermission.class, declaringServiceClass, fieldName,
-			"(model.class.name=" + modelClass.getName() + ")", true);
 	}
 
 	public static <T extends ClassedModel> ModelResourcePermission<T>

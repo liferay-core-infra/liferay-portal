6abf13dbdfb200ed68f7367bcbf3aa92cd3c7d02
Lily.Chi <lily.chi@liferay.com>
2023-12-11 17:27:49 +0800

    LPS-203707 Use the constant _USER_PASSWORD as all these generated users are using the same password

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -4768,7 +4768,7 @@ public class DataFactory {
 		mbMailingListModel.setInProtocol("pop3");
 		mbMailingListModel.setInServerPort(110);
 		mbMailingListModel.setInUserName(sampleUserModel.getEmailAddress());
-		mbMailingListModel.setInPassword(sampleUserModel.getPassword());
+		mbMailingListModel.setInPassword(_USER_PASSWORD);
 		mbMailingListModel.setInReadInterval(5);
 		mbMailingListModel.setOutServerPort(25);
 

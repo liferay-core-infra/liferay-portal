fd7ffb0ad2cfa48c30c7b2558d1e516c648f43fa
Jiefeng Wu <jeffwu0724@gmail.com>
2023-12-11 11:49:11 -0800

    LPS-201589 add a property LiferayActionResponse.SKIP_ESCAPE_REDIRECT to make sure only escape the unauthorized external site

--- modules/apps/marketplace/marketplace-store-web/src/main/java/com/liferay/marketplace/store/web/internal/portlet/MarketplaceStorePortlet.java
+++ modules/apps/marketplace/marketplace-store-web/src/main/java/com/liferay/marketplace/store/web/internal/portlet/MarketplaceStorePortlet.java
@@ -25,6 +25,7 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.patcher.PatcherValues;
+import com.liferay.portal.kernel.portlet.LiferayActionResponse;
 import com.liferay.portal.kernel.portlet.PortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
@@ -134,6 +135,9 @@ public class MarketplaceStorePortlet extends MVCPortlet {
 			redirect, OAuthConstants.CALLBACK, callbackURL);
 
 		actionResponse.sendRedirect(redirect);
+
+		actionResponse.addProperty(
+			LiferayActionResponse.SKIP_ESCAPE_REDIRECT, "true");
 	}
 
 	public void deauthorize(

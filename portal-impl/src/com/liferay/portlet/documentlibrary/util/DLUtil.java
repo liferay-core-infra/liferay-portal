ddc9961e3be884c53951175ce7181a9afb92d224
Janis Zhang <janis.zhang@liferay.com>
2023-12-15 15:00:22 +0800

    LPS-203810 Move DLUtil from portal-kernel to portal-impl, prepare for next.
    # breaking
    
    ## What portal-kernel/src/com/liferay/document/library/kernel/util/DLUtil.java
    
    We move DLUtil from portal-kernel to portal-impl
    
    ## Why
    
    We are merging portal-kernel into portal-impl as a long term goal to simplify portal structure.
    
    ## Alternatives
    
    Add portal-impl in build dependency and use the same class.
    
    ----

--- portal-impl/src/com/liferay/portlet/documentlibrary/util/DLUtil.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/util/DLUtil.java
@@ -1,12 +1,13 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.document.library.kernel.util;
+package com.liferay.portlet.documentlibrary.util;
 
 import com.liferay.document.library.kernel.model.DLFileEntryType;
 import com.liferay.document.library.kernel.model.DLFileVersion;
+import com.liferay.document.library.kernel.util.DL;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.FileVersion;

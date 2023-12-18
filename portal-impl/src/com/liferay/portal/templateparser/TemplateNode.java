74ec9967234fc96006b9cda9d072824594e0b321
Janis Zhang <janis.zhang@liferay.com>
2023-12-18 11:18:10 +0800

    LPS-203810 Move TemplateNode from portal-kernel to portal-impl, prepare for next.
    # breaking
    
    ## What portal-kernel/src/com/liferay/portal/kernel/templateparser/TemplateNode.java
    
    We move TemplateNode from portal-kernel to portal-impl
    
    ## Why
    
    We are merging portal-kernel into portal-impl as a long term goal to simplify portal structure.
    
    ## Alternatives
    
    Add portal-impl in build dependency and use the same class.
    
    ----

--- portal-impl/src/com/liferay/portal/templateparser/TemplateNode.java
+++ portal-impl/src/com/liferay/portal/templateparser/TemplateNode.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.templateparser;
+package com.liferay.portal.templateparser;
 
 import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
 import com.liferay.asset.kernel.model.AssetRenderer;

124eb6dedf89846741f13e7f373442f14420f873
Hayden Zheng <hayden.zheng@liferay.com>
2023-12-22 14:37:08 +0800

    LPS-203946 Move unit test

--- modules/apps/portal/portal-repository/src/test/java/com/liferay/portal/repository/internal/registry/RepositoryClassDefinitionCatalogImplTest.java
+++ modules/apps/portal/portal-repository/src/test/java/com/liferay/portal/repository/internal/registry/RepositoryClassDefinitionCatalogImplTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.repository.registry;
+package com.liferay.portal.repository.internal.registry;
 
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.CompanyConstants;
@@ -12,6 +12,7 @@ import com.liferay.portal.kernel.repository.RepositoryConfigurationBuilder;
 import com.liferay.portal.kernel.repository.registry.RepositoryDefiner;
 import com.liferay.portal.kernel.util.ProxyUtil;
 import com.liferay.portal.language.LanguageImpl;
+import com.liferay.portal.repository.registry.RepositoryClassDefinition;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import java.util.Collection;

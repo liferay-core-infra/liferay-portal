8d773c39a506ad70890bdab3f8f7e97f92790c35
Julius Lee <meng-huan.lee@liferay.com>
2023-12-27 16:49:18 -0800

    LPS-204349 Move DefaultEmailAddressGenerator to new module: portal-secuirty-auth-impl
    # breaking
    ## What portal-kernel/src/com/liferay/portal/kernel/security/auth/DefaultEmailAddressGenerator.java
    DefaultEmailAddressGenerator is moving to modules.
    ## Why
    We are eliminating all the Spring beans to be able to merge portal-kernel and portal-impl
    ----

--- modules/apps/portal-security/portal-security-auth-impl/src/main/java/com/liferay/portal/security/auth/internal/DefaultEmailAddressGenerator.java
+++ modules/apps/portal-security/portal-security-auth-impl/src/main/java/com/liferay/portal/security/auth/internal/DefaultEmailAddressGenerator.java
@@ -3,9 +3,10 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.security.auth;
+package com.liferay.portal.security.auth.internal;
 
 import com.liferay.portal.kernel.model.UserConstants;
+import com.liferay.portal.kernel.security.auth.EmailAddressGenerator;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 

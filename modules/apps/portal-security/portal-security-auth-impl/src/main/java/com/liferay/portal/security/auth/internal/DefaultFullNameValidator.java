a6e25b81b9687d9e82b541f4cc208afa2e4db9c2
Julius Lee <meng-huan.lee@liferay.com>
2024-01-09 10:57:19 -0800

    LPS-204349 Move DefaultFullNameValidator to modules and use OSGi to start instead of Spring
    # breaking
    ## What portal-kernel/src/com/liferay/portal/kernel/security/auth/DefaultFullNameValidator.java
    DefaultFullNameValidator is moving to modules.
    ## Why
    We are eliminating all the Spring beans to be able to merge portal-kernel and portal-impl
    ----

--- modules/apps/portal-security/portal-security-auth-impl/src/main/java/com/liferay/portal/security/auth/internal/DefaultFullNameValidator.java
+++ modules/apps/portal-security/portal-security-auth-impl/src/main/java/com/liferay/portal/security/auth/internal/DefaultFullNameValidator.java
@@ -3,11 +3,16 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.security.auth;
+package com.liferay.portal.security.auth.internal;
+
+import com.liferay.portal.kernel.security.auth.FullNameValidator;
+
+import org.osgi.service.component.annotations.Component;
 
 /**
  * @author Amos Fong
  */
+@Component(service = FullNameValidator.class)
 public class DefaultFullNameValidator implements FullNameValidator {
 
 	@Override

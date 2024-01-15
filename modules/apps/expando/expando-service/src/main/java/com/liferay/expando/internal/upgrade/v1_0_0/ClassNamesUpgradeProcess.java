a0456d823476219267341361e616945c83f877db
Lily.Chi <lily.chi@liferay.com>
2024-01-15 17:43:02 +0800

    LPS-190836 Add UpgradeStepRegistrator and register upgrade process to expando-service module which aims to update package names for ClassName, ResourceAtion and ResourcePermission and trigger the updateIndexes process in UpgradeExecutor and  update the schema version from 0.0.1 to 1.0.0 as ReleaseDAO.addRelease() will add an item with schema version 0.0.1 in Release_ table, however, 0.0.1 means the reserved schema version for non upgrade modules, and the upgrade process will be stopped with incorrect status even all the upgrades have been executed successfully

---
+++ modules/apps/expando/expando-service/src/main/java/com/liferay/expando/internal/upgrade/v1_0_0/ClassNamesUpgradeProcess.java
@@ -0,0 +1,47 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.expando.internal.upgrade.v1_0_0;
+
+import com.liferay.portal.kernel.dao.orm.WildcardMode;
+import com.liferay.portal.kernel.upgrade.UpgradeException;
+import com.liferay.portal.upgrade.v7_0_0.UpgradeKernelPackage;
+
+/**
+ * @author Lily Chi
+ */
+public class ClassNamesUpgradeProcess extends UpgradeKernelPackage {
+
+	@Override
+	protected void doUpgrade() throws UpgradeException {
+		super.doUpgrade();
+
+		try {
+			upgradeTable(
+				"ResourcePermission", "primKey", _CLASS_NAMES,
+				WildcardMode.SURROUND, true);
+		}
+		catch (Exception exception) {
+			throw new UpgradeException(exception);
+		}
+	}
+
+	@Override
+	protected String[][] getClassNames() {
+		return _CLASS_NAMES;
+	}
+
+	@Override
+	protected String[][] getResourceNames() {
+		return _RESOURCE_NAMES;
+	}
+
+	private static final String[][] _CLASS_NAMES = {
+		{"com.liferay.expando.kernel.model.", "com.liferay.expando.model."}
+	};
+
+	private static final String[][] _RESOURCE_NAMES = new String[0][0];
+
+}

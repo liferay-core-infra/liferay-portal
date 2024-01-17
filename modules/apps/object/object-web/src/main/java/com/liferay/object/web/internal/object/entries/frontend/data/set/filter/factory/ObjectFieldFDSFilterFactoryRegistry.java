e07408aec1d1ed0f2963a65cc9ad523d86b61692
Valmir Junior <valmir.junior@liferay.com>
2024-01-17 17:53:56 -0300

    LPS-199657 Remove annotacion component from ObjectFieldFDSFilterFactoryRegistry

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/entries/frontend/data/set/filter/factory/ObjectFieldFDSFilterFactoryRegistry.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/entries/frontend/data/set/filter/factory/ObjectFieldFDSFilterFactoryRegistry.java
@@ -19,12 +19,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-import org.osgi.service.component.annotations.Component;
-
 /**
  * @author Feliphe Marinho
  */
-@Component(service = ObjectFieldFDSFilterFactoryRegistry.class)
 public class ObjectFieldFDSFilterFactoryRegistry {
 
 	public ObjectFieldFDSFilterFactoryRegistry(

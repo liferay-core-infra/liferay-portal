646886157e08e26a18f55b17412e5cd06dd2eca8
Julius Lee <meng-huan.lee@liferay.com>
2024-03-14 16:12:40 -0700

    LPD-19869 Add new petra module petra-time and new class StopWatch

---
+++ modules/core/petra/petra-time/src/main/java/com/liferay/petra/time/StopWatch.java
@@ -0,0 +1,48 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.petra.time;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * @author Julius Lee
+ */
+public class StopWatch {
+
+	public long getStartTime() {
+		return _startTime;
+	}
+
+	public long getTime() {
+		if (_startTime == -1) {
+			return 0;
+		}
+		else if (_stopTime == -1) {
+			return System.currentTimeMillis() - _startTime;
+		}
+
+		return _stopTime - _startTime;
+	}
+
+	public void start() {
+		_startTime = System.currentTimeMillis();
+	}
+
+	public void stop() {
+		_stopTime = System.currentTimeMillis();
+	}
+
+	@Override
+	public String toString() {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("H:mm:ss.SSS");
+
+		return simpleDateFormat.format(getTime());
+	}
+
+	private long _startTime = -1;
+	private long _stopTime = -1;
+
+}

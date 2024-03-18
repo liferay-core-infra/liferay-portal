4159c2d4bd1128a8fe94cb92fa586896222c94b8
Tina Tian <tina.tian@liferay.com>
2024-03-18 16:09:24 -0700

    LPD-20566 Fix the issue described in LPS-91534 by delaying the deserialization of cache key and cache value. This is going to fix the issue for both key and value deserialization, while the original fix is only for value.

--- modules/apps/portal-cache/portal-cache-multiple/src/main/java/com/liferay/portal/cache/multiple/internal/cluster/link/ClusterLinkMessageUtil.java
+++ modules/apps/portal-cache/portal-cache-multiple/src/main/java/com/liferay/portal/cache/multiple/internal/cluster/link/ClusterLinkMessageUtil.java
@@ -5,28 +5,119 @@
 
 package com.liferay.portal.cache.multiple.internal.cluster.link;
 
+import com.liferay.petra.io.Deserializer;
+import com.liferay.petra.io.Serializer;
 import com.liferay.portal.cache.multiple.internal.PortalCacheClusterEvent;
-import com.liferay.portal.cache.multiple.internal.cluster.link.messaging.ClusterLinkPortalCacheClusterListener;
+import com.liferay.portal.cache.multiple.internal.PortalCacheClusterEventType;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.Message;
-import com.liferay.util.SerializableUtil;
+
+import java.io.Serializable;
+
+import java.nio.ByteBuffer;
 
 /**
  * @author Tina Tian
  */
 public class ClusterLinkMessageUtil {
 
-	public static PortalCacheClusterEvent
-		fetchPortalCacheClusterEventFromMessage(Message message) {
+	public static long getCompanyId(Message message) {
+		return message.getLong(_COMPANY_ID);
+	}
 
-		return (PortalCacheClusterEvent)SerializableUtil.deserialize(
-			(byte[])message.getPayload(),
-			ClusterLinkPortalCacheClusterListener.class.getClassLoader());
+	public static Serializable getKey(Message message) {
+		return _deserialize(message.get(_KEY));
+	}
+
+	public static PortalCacheClusterEventType getPortalCacheClusterEventType(
+		Message message) {
+
+		return PortalCacheClusterEventType.valueOf(
+			message.getString(_EVENT_TYPE));
+	}
+
+	public static String getPortalCacheManagerName(Message message) {
+		return message.getString(_CACHE_MANAGER_NAME);
+	}
+
+	public static String getPortalCacheName(Message message) {
+		return message.getString(_CACHE_NAME);
+	}
+
+	public static int getTimeToLive(Message message) {
+		return message.getInteger(_TIME_TO_LIVE);
+	}
+
+	public static Serializable getValue(Message message) {
+		return _deserialize(message.get(_VALUE));
 	}
 
 	public static void populateMessageFromPortalCacheClusterEvent(
 		Message message, PortalCacheClusterEvent portalCacheClusterEvent) {
 
-		message.setPayload(SerializableUtil.serialize(portalCacheClusterEvent));
+		message.put(
+			_CACHE_MANAGER_NAME,
+			portalCacheClusterEvent.getPortalCacheManagerName());
+		message.put(_CACHE_NAME, portalCacheClusterEvent.getPortalCacheName());
+		message.put(_COMPANY_ID, portalCacheClusterEvent.getCompanyId());
+		message.put(
+			_EVENT_TYPE,
+			String.valueOf(portalCacheClusterEvent.getEventType()));
+		message.put(_KEY, _serialize(portalCacheClusterEvent.getElementKey()));
+		message.put(_TIME_TO_LIVE, portalCacheClusterEvent.getTimeToLive());
+		message.put(
+			_VALUE, _serialize(portalCacheClusterEvent.getElementValue()));
 	}
+
+	private static Serializable _deserialize(Object object) {
+		if (object instanceof byte[]) {
+			byte[] bytes = (byte[])object;
+
+			Deserializer deserializer = new Deserializer(
+				ByteBuffer.wrap(bytes));
+
+			try {
+				return deserializer.readObject();
+			}
+			catch (ClassNotFoundException classNotFoundException) {
+				_log.error(
+					"Unable to deserialize object", classNotFoundException);
+			}
+		}
+
+		return (Serializable)object;
+	}
+
+	private static byte[] _serialize(Serializable serializable) {
+		if (serializable == null) {
+			return null;
+		}
+
+		Serializer serializer = new Serializer();
+
+		serializer.writeObject(serializable);
+
+		ByteBuffer byteBuffer = serializer.toByteBuffer();
+
+		return byteBuffer.array();
+	}
+
+	private static final String _CACHE_MANAGER_NAME = "cache.manager.name";
+
+	private static final String _CACHE_NAME = "cache.name";
+
+	private static final String _COMPANY_ID = "company.id";
+
+	private static final String _EVENT_TYPE = "event.type";
+
+	private static final String _KEY = "key";
+
+	private static final String _TIME_TO_LIVE = "time.to.live";
+
+	private static final String _VALUE = "value";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ClusterLinkMessageUtil.class);
 
 }

4b9afac4ad7502d5c96da0807c020ab0366d26cd
Jeff Wu <jeffwu0724@gmail.com>
2024-03-27 13:43:36 -0700

    LPD-1558 Add captcha to the Gogo Shell page and always perform a captcha check for every request by setting maxchallenges to 0

--- modules/apps/gogo-shell/gogo-shell-web/src/main/java/com/liferay/gogo/shell/web/internal/portlet/GogoShellPortlet.java
+++ modules/apps/gogo-shell/gogo-shell-web/src/main/java/com/liferay/gogo/shell/web/internal/portlet/GogoShellPortlet.java
@@ -5,8 +5,10 @@
 
 package com.liferay.gogo.shell.web.internal.portlet;
 
+import com.liferay.captcha.util.CaptchaUtil;
 import com.liferay.gogo.shell.web.internal.constants.GogoShellPortletKeys;
 import com.liferay.gogo.shell.web.internal.constants.GogoShellWebKeys;
+import com.liferay.portal.kernel.captcha.CaptchaException;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayOutputStream;
 import com.liferay.portal.kernel.language.Language;
@@ -151,6 +153,13 @@ public class GogoShellPortlet extends MVCPortlet {
 	public void processAction(
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws IOException, PortletException {
+
+		try {
+			CaptchaUtil.check(actionRequest);
+		}
+		catch (CaptchaException captchaException) {
+			throw new PortletException(captchaException);
+		}
 
 		_checkOmniadmin();
 

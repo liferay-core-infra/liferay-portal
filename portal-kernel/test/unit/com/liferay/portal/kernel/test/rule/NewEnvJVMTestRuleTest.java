5bf12d617fe8a93ffc5cbb5f58f05a3adc2d98ee
Julius Lee <meng-huan.lee@liferay.com>
2024-04-15 13:12:30 -0700

    LPD-23320 serialize null as String "null" so that we can deserialize "null" to null on the other end. Currently, null and blank would both be deserialized to be null. This is causing this test to fail comparison when environment contains blank property because it becomes null on the other end.

--- portal-kernel/test/unit/com/liferay/portal/kernel/test/rule/NewEnvJVMTestRuleTest.java
+++ portal-kernel/test/unit/com/liferay/portal/kernel/test/rule/NewEnvJVMTestRuleTest.java
@@ -7,6 +7,7 @@ package com.liferay.portal.kernel.test.rule;
 
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 
@@ -228,7 +229,9 @@ public class NewEnvJVMTestRuleTest {
 		for (Map.Entry<String, String> entry : map.entrySet()) {
 			sb.append(entry.getKey());
 			sb.append(_SEPARATOR_KEY_VALUE);
-			sb.append(entry.getValue());
+			sb.append(
+				(entry.getValue() == null) ? StringPool.NULL :
+					entry.getValue());
 			sb.append(_SEPARATOR_VARIABLE);
 		}
 
@@ -244,10 +247,12 @@ public class NewEnvJVMTestRuleTest {
 			String[] parts = StringUtil.split(entry, _SEPARATOR_KEY_VALUE);
 
 			if (parts.length == 1) {
-				map.put(parts[0], null);
+				map.put(parts[0], StringPool.BLANK);
 			}
 			else {
-				map.put(parts[0], parts[1]);
+				map.put(
+					parts[0],
+					parts[1].equals(StringPool.NULL) ? null : parts[1]);
 			}
 		}
 

f9c33dbee995c6d3c27b5b32cbdce087e91d87f9
Julius Lee <meng-huan.lee@liferay.com>
2024-04-17 12:24:56 -0700

    LPD-22758 Add testJavaHome to make sure that JAVA_HOME environment variable and java.home property are the same between junit environment and NewEnv environment

--- portal-kernel/test/unit/com/liferay/portal/kernel/test/rule/NewEnvJVMTestRuleTest.java
+++ portal-kernel/test/unit/com/liferay/portal/kernel/test/rule/NewEnvJVMTestRuleTest.java
@@ -37,6 +37,12 @@ public class NewEnvJVMTestRuleTest {
 	public static void setUpClass() {
 		System.setProperty(
 			_SYSTEM_PROPERTY_KEY_ENVIRONMENT, _toString(_getEnvironment()));
+
+		System.setProperty(
+			_PARENT_RUNTIME_JAVA_HOME_KEY, System.getProperty("java.home"));
+
+		System.setProperty(
+			_PARENT_ENVIRONMENT_JAVA_HOME_KEY, System.getenv("JAVA_HOME"));
 	}
 
 	@Before
@@ -55,6 +61,34 @@ public class NewEnvJVMTestRuleTest {
 		Assert.assertEquals(2, _counter.getAndIncrement());
 
 		assertProcessId();
+	}
+
+	@NewEnv.JVMArgsLine(
+		"-D" + _PARENT_ENVIRONMENT_JAVA_HOME_KEY + "=${" +
+			_PARENT_ENVIRONMENT_JAVA_HOME_KEY + "} -D" +
+				_PARENT_RUNTIME_JAVA_HOME_KEY + "=${" +
+					_PARENT_RUNTIME_JAVA_HOME_KEY + "}"
+	)
+	@Test
+	public void testJavaHome() {
+		_counter.getAndIncrement();
+
+		String parentJavaHome = System.getProperty(
+			_PARENT_ENVIRONMENT_JAVA_HOME_KEY);
+
+		String parentRunTimeJavaHome = _stripJREFromJavaHomePath(
+			System.getProperty(_PARENT_RUNTIME_JAVA_HOME_KEY));
+
+		String currentJavaHome = System.getenv("JAVA_HOME");
+
+		String currentRunTimeJavaHome = _stripJREFromJavaHomePath(
+			System.getProperty("java.home"));
+
+		Assert.assertEquals(parentJavaHome, parentRunTimeJavaHome);
+
+		Assert.assertEquals(parentJavaHome, currentJavaHome);
+
+		Assert.assertEquals(parentRunTimeJavaHome, currentRunTimeJavaHome);
 	}
 
 	@Test
@@ -257,9 +291,35 @@ public class NewEnvJVMTestRuleTest {
 		return map;
 	}
 
+	@NewEnv.JVMArgsLine(
+		"-D" + _PARENT_ENVIRONMENT_JAVA_HOME_KEY + "=${" +
+			_PARENT_ENVIRONMENT_JAVA_HOME_KEY + "} -D" +
+				_PARENT_RUNTIME_JAVA_HOME_KEY + "=${" +
+					_PARENT_RUNTIME_JAVA_HOME_KEY + "}"
+	)
+	private String _stripJREFromJavaHomePath(String path) {
+		int jrePos = path.lastIndexOf("/jre");
+
+		if (jrePos == -1) {
+			jrePos = path.lastIndexOf("\\jre");
+		}
+
+		if (jrePos != -1) {
+			path = path.substring(0, jrePos);
+		}
+
+		return path;
+	}
+
 	private static final String _ENVIRONMENT_KEY_USER = "USER";
 
 	private static final String _NULL_HOLDER = "_NULL_HOLDER_";
+
+	private static final String _PARENT_ENVIRONMENT_JAVA_HOME_KEY =
+		"_PARENT_ENVIRONMENT_JAVA_HOME_KEY_";
+
+	private static final String _PARENT_RUNTIME_JAVA_HOME_KEY =
+		"_PARENT_RUNTIME_JAVA_HOME_KEY_";
 
 	private static final String _SEPARATOR_KEY_VALUE = "_SEPARATOR_KEY_VALUE_";
 

56c1bce22d0db47b5cddae97f692ab34787eece5
Joshua Cords <joshua.cords@liferay.com>
2024-04-24 15:13:02 -0700

    LPD-5409 LPD-5526 portal-search-similar-results-web-spi: Add javadocs
    https://liferay.atlassian.net/browse/LPD-5526

--- modules/dxp/apps/portal-search-similar-results/portal-search-similar-results-web-spi/src/main/java/com/liferay/portal/search/similar/results/web/spi/contributor/helper/DestinationBuilder.java
+++ modules/dxp/apps/portal-search-similar-results/portal-search-similar-results-web-spi/src/main/java/com/liferay/portal/search/similar/results/web/spi/contributor/helper/DestinationBuilder.java
@@ -10,11 +10,32 @@ package com.liferay.portal.search.similar.results.web.spi.contributor.helper;
  */
 public interface DestinationBuilder {
 
+	/**
+	 * Replaces all occurrences of a substring within the destination URL with a
+	 * new substring.
+	 *
+	 * @param oldSub The substring to be replaced.
+	 * @param newSub The new substring to replace the old substring with.
+	 * @return This {@link DestinationBuilder} instance.
+	 */
 	public DestinationBuilder replace(String oldSub, String newSub);
 
+	/**
+	 * Replaces the value of a specific parameter within the destination URL.
+	 *
+	 * @param parameter The name of the parameter to replace.
+	 * @param newValue The new value for the parameter.
+	 * @return This {@link DestinationBuilder} instance.
+	 */
 	public DestinationBuilder replaceParameter(
 		String parameter, String newValue);
 
+	/**
+	 * Replaces the entire destination URL string with a new URL string.
+	 *
+	 * @param urlString The new URL string to replace the existing destination URL.
+	 * @return This {@link DestinationBuilder} instance.
+	 */
 	public DestinationBuilder replaceURLString(String urlString);
 
 }

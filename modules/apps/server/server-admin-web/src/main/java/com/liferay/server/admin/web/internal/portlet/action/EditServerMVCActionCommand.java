266bfe99af049f5ad4d14b1022414e6c8308abdf
Jeff Wu <jeffwu0724@gmail.com>
2024-05-10 13:08:10 -0700

    LPD-25555 narrow down the Captcha range to make sure only cmd from server admin will enforce the Captcha check

--- modules/apps/server/server-admin-web/src/main/java/com/liferay/server/admin/web/internal/portlet/action/EditServerMVCActionCommand.java
+++ modules/apps/server/server-admin-web/src/main/java/com/liferay/server/admin/web/internal/portlet/action/EditServerMVCActionCommand.java
@@ -85,6 +85,7 @@ import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LoggingTimer;
 import com.liferay.portal.kernel.util.MethodHandler;
@@ -183,7 +184,10 @@ public class EditServerMVCActionCommand extends BaseMVCActionCommand {
 
 		String redirect = ParamUtil.getString(actionRequest, "redirect");
 
-		if (!cmd.equals("addLogLevel") &&
+		String portletId = GetterUtil.getString(
+			actionRequest.getAttribute(WebKeys.PORTLET_ID));
+
+		if (portletId.equals(PortletKeys.SERVER_ADMIN) &&
 			!cmd.equals("dlGenerateAudioPreviews") &&
 			!cmd.equals("dlGenerateOpenOfficePreviews") &&
 			!cmd.equals("dlGenerateVideoPreviews") &&

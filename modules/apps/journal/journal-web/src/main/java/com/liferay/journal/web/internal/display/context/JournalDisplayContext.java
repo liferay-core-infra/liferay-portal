7c5896bd893389f9cdb8ceffb13d88b66aff69a1
Joao Victor <joao.victor@liferay.com>
2024-05-22 19:18:28 -0300

    LPD-26443 Transform FolderArticleDisplayDateComparator into a singleton, make the constructor private and apply to usage
    # breaking
    
    ## What modules/apps/journal/journal-api/src/main/java/com/liferay/journal/util/comparator/FolderArticleDisplayDateComparator.java
    
    the class is now a singleton
    
    ## Why
    
    this change was made to improve the performance of the querys that uses this comparator
    
    ----

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -1775,7 +1775,7 @@ public class JournalDisplayContext {
 			return FolderArticleCreateDateComparator.getInstance(orderByAsc);
 		}
 		else if (Objects.equals(getOrderByCol(), "display-date")) {
-			return new FolderArticleDisplayDateComparator(orderByAsc);
+			return FolderArticleDisplayDateComparator.getInstance(orderByAsc);
 		}
 		else if (Objects.equals(getOrderByCol(), "id")) {
 			return FolderArticleArticleIdComparator.getInstance(orderByAsc);

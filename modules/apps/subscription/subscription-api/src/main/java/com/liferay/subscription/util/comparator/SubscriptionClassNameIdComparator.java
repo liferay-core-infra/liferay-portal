ef97750b22c629b1a5d25590a1cf9cc8b0a3342f
Regisson Aguiar <regisson.aguiar@liferay.com>
2024-05-22 17:18:19 -0300

    LPD-26496 Add singletons to SubscriptionClassNameIdComparator, one for ascending, one for descending, make the constructor and atributes private, and apply

--- modules/apps/subscription/subscription-api/src/main/java/com/liferay/subscription/util/comparator/SubscriptionClassNameIdComparator.java
+++ modules/apps/subscription/subscription-api/src/main/java/com/liferay/subscription/util/comparator/SubscriptionClassNameIdComparator.java
@@ -14,14 +14,14 @@ import com.liferay.subscription.model.Subscription;
 public class SubscriptionClassNameIdComparator
 	extends OrderByComparator<Subscription> {
 
-	public static final String ORDER_BY_ASC = "Subscription.classNameId ASC";
+	public static SubscriptionClassNameIdComparator getInstance(
+		boolean ascending) {
 
-	public static final String ORDER_BY_DESC = "Subscription.classNameId DESC";
+		if (ascending) {
+			return _ASCENDING;
+		}
 
-	public static final String[] ORDER_BY_FIELDS = {"classNameId"};
-
-	public SubscriptionClassNameIdComparator(boolean ascending) {
-		_ascending = ascending;
+		return _DESCENDING;
 	}
 
 	@Override
@@ -46,21 +46,38 @@ public class SubscriptionClassNameIdComparator
 	@Override
 	public String getOrderBy() {
 		if (_ascending) {
-			return ORDER_BY_ASC;
+			return _ORDER_BY_ASC;
 		}
 
-		return ORDER_BY_DESC;
+		return _ORDER_BY_DESC;
 	}
 
 	@Override
 	public String[] getOrderByFields() {
-		return ORDER_BY_FIELDS;
+		return _ORDER_BY_FIELDS;
 	}
 
 	@Override
 	public boolean isAscending() {
 		return _ascending;
 	}
+
+	private SubscriptionClassNameIdComparator(boolean ascending) {
+		_ascending = ascending;
+	}
+
+	private static final SubscriptionClassNameIdComparator _ASCENDING =
+		new SubscriptionClassNameIdComparator(true);
+
+	private static final SubscriptionClassNameIdComparator _DESCENDING =
+		new SubscriptionClassNameIdComparator(false);
+
+	private static final String _ORDER_BY_ASC = "Subscription.classNameId ASC";
+
+	private static final String _ORDER_BY_DESC =
+		"Subscription.classNameId DESC";
+
+	private static final String[] _ORDER_BY_FIELDS = {"classNameId"};
 
 	private final boolean _ascending;
 

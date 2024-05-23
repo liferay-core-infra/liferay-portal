d64ea984dfdcd5139bf66fb1e2c0c4a47db4ca20
Joao Victor <joao.victor@liferay.com>
2024-05-23 14:20:43 -0300

    LPD-26703 Transform PortletDisplayNameComparator into a singleton, make the constructor private and apply to usages

--- modules/apps/layout/layout-portlets-web/src/main/java/com/liferay/layout/portlets/web/internal/search/PortletSearch.java
+++ modules/apps/layout/layout-portlets-web/src/main/java/com/liferay/layout/portlets/web/internal/search/PortletSearch.java
@@ -72,7 +72,8 @@ public class PortletSearch extends SearchContainer<Portlet> {
 		OrderByComparator<Portlet> orderByComparator = null;
 
 		if (orderByCol.equals("name")) {
-			orderByComparator = new PortletDisplayNameComparator(orderByAsc);
+			orderByComparator = PortletDisplayNameComparator.getInstance(
+				orderByAsc);
 		}
 
 		return orderByComparator;

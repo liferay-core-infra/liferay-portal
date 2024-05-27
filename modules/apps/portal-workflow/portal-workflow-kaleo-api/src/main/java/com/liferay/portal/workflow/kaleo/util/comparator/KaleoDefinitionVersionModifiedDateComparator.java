eef4b25297722346ded85ed0fa92bcb61582c091
jpcchaves <joao.chaves@liferay.com>
2024-05-27 16:34:28 -0300

    LPD-26917 SF, Make constant fields private
    # breaking
    
    ## What modules/apps/portal-workflow/portal-workflow-kaleo-api/src/main/java/com/liferay/portal/workflow/kaleo/util/comparator/KaleoDefinitionVersionModifiedDateComparator.java
    
    the visibility of the atributes of this class changed from public to private
    
    ## Why
    
    this change was made to not expose the attibutes of the class
    
    ----

--- modules/apps/portal-workflow/portal-workflow-kaleo-api/src/main/java/com/liferay/portal/workflow/kaleo/util/comparator/KaleoDefinitionVersionModifiedDateComparator.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-api/src/main/java/com/liferay/portal/workflow/kaleo/util/comparator/KaleoDefinitionVersionModifiedDateComparator.java
@@ -17,12 +17,6 @@ import java.util.Date;
 public class KaleoDefinitionVersionModifiedDateComparator
 	extends OrderByComparator<KaleoDefinitionVersion> {
 
-	public static final String ORDER_BY_ASC = "modifiedDate ASC";
-
-	public static final String ORDER_BY_DESC = "modifiedDate DESC";
-
-	public static final String[] ORDER_BY_FIELDS = {"modifiedDate"};
-
 	public KaleoDefinitionVersionModifiedDateComparator(boolean ascending) {
 		_ascending = ascending;
 	}
@@ -47,21 +41,27 @@ public class KaleoDefinitionVersionModifiedDateComparator
 	@Override
 	public String getOrderBy() {
 		if (_ascending) {
-			return ORDER_BY_ASC;
+			return _ORDER_BY_ASC;
 		}
 
-		return ORDER_BY_DESC;
+		return _ORDER_BY_DESC;
 	}
 
 	@Override
 	public String[] getOrderByFields() {
-		return ORDER_BY_FIELDS;
+		return _ORDER_BY_FIELDS;
 	}
 
 	@Override
 	public boolean isAscending() {
 		return _ascending;
 	}
+
+	private static final String _ORDER_BY_ASC = "modifiedDate ASC";
+
+	private static final String _ORDER_BY_DESC = "modifiedDate DESC";
+
+	private static final String[] _ORDER_BY_FIELDS = {"modifiedDate"};
 
 	private final boolean _ascending;
 

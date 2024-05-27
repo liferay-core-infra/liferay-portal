dbfb15b6bd9d39f7380db09657746d5aa02bf200
Valmir Junior <valmir.junior@liferay.com>
2024-05-27 18:54:26 -0300

    LPD-26560 Apply Singleton in class FriendlyURLEntryCreateDateComparator from module friendly-url, apply to usages
    # breaking
    
    ## What modules/apps/friendly-url/friendly-url-api/src/main/java/com/liferay/friendly/url/util/comparator/FriendlyURLEntryCreateDateComparator.java
    
    the class is now a singleton
    
    ## Why
    
    this change was made to improve the performance of the querys that uses this comparator
    
    ----

--- modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/service/impl/FriendlyURLEntryLocalServiceImpl.java
+++ modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/service/impl/FriendlyURLEntryLocalServiceImpl.java
@@ -197,7 +197,7 @@ public class FriendlyURLEntryLocalServiceImpl
 				friendlyURLEntry.getGroupId(),
 				friendlyURLEntry.getClassNameId(),
 				friendlyURLEntry.getClassPK(),
-				new FriendlyURLEntryCreateDateComparator(true));
+				FriendlyURLEntryCreateDateComparator.getInstance(true));
 
 			if (friendlyURLEntry == null) {
 				_friendlyURLEntryMappingPersistence.remove(

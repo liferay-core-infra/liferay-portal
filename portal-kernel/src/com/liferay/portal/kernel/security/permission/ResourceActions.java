5159aaccc1bc6aac9a3992a8999a80763ba4d22c
Dante Wang <dante.wang@liferay.com>
2024-06-11 15:09:36 +0800

    LPD-20946 Change method to allow populating an array of documents to do bulk check and permission population

--- portal-kernel/src/com/liferay/portal/kernel/security/permission/ResourceActions.java
+++ portal-kernel/src/com/liferay/portal/kernel/security/permission/ResourceActions.java
@@ -101,7 +101,7 @@ public interface ResourceActions {
 			boolean checkResourceActions)
 		throws ResourceActionsException;
 
-	public void populateModelResources(Document document)
+	public void populateModelResources(Document... document)
 		throws ResourceActionsException;
 
 	public void populatePortletResource(

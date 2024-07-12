41d966ff5ba74b65f86650c1ed8f0cc963c53920
Tina Tian <tina.tian@liferay.com>
2024-07-11 17:02:41 -0700

    LPD-x Add debug logic and fix

--- modules/third-party/org-apache-felix-configadmin/src/main/java/org/apache/felix/cm/impl/UpdateThread.java
+++ modules/third-party/org-apache-felix-configadmin/src/main/java/org/apache/felix/cm/impl/UpdateThread.java
@@ -22,6 +22,7 @@ import java.security.AccessControlContext;
 import java.security.AccessController;
 import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
+import java.util.Map;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
 
@@ -156,24 +157,40 @@ public class UpdateThread implements Runnable
             Thread workerThread = this.worker;
             this.worker = null;
 
+            StackTraceElement[] stackTraceElements = workerThread.getStackTrace();
+
+            String stackTraceString = null;
+
+            if (stackTraceElements != null) {
+                stackTraceString = "#######Start of stack trace for thread " + workerThread.getName();
+                for (StackTraceElement stackTraceElement : stackTraceElements) {
+                    stackTraceString +="\tat " + stackTraceElement;
+                }
+                stackTraceString += "#######End of stack trace for thread " + workerThread.getName();
+            }
+            else {
+                stackTraceString = "#######Thread " + workerThread.getName() + " not found.";
+            }
+
             updateTasks.offerFirst( this );
 
             // wait for all updates to terminate (<= 10 seconds !)
             try
             {
                 workerThread.join( 5000 );
+                
+                if ( workerThread.isAlive() )
+                {
+                    Log.logger.log( LogService.LOG_ERROR,
+                        "Worker thread {0} did not terminate within 5 seconds; trying to kill\n " + stackTraceString, new Object[]
+                            { workerBaseName } );
+
+                    workerThread.interrupt();
+                }
             }
             catch ( InterruptedException ie )
             {
                 // don't really care
-            }
-
-            if ( workerThread.isAlive() )
-            {
-                Log.logger.log( LogService.LOG_ERROR,
-                    "Worker thread {0} did not terminate within 5 seconds; trying to kill", new Object[]
-                        { workerBaseName } );
-                workerThread.interrupt();
             }
         }
     }

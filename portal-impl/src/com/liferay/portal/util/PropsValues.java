d9d0dcfc8fa01b24fa0915c85175bf14cca265e1
Anna Suszter <anna.suszter@liferay.com>
2024-07-26 18:31:27 +0200

    LPD-29513 portal-impl: read module.framework.client.extensions.dir portal property

--- portal-impl/src/com/liferay/portal/util/PropsValues.java
+++ portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -1355,6 +1355,9 @@ public class PropsValues {
 	public static final String MODULE_FRAMEWORK_BASE_DIR = PropsUtil.get(
 		PropsKeys.MODULE_FRAMEWORK_BASE_DIR);
 
+	public static final String MODULE_FRAMEWORK_CLIENT_EXTENSIONS_DIR =
+		PropsUtil.get(PropsKeys.MODULE_FRAMEWORK_CLIENT_EXTENSIONS_DIR);
+
 	public static final String MODULE_FRAMEWORK_CONFIGS_DIR = PropsUtil.get(
 		PropsKeys.MODULE_FRAMEWORK_CONFIGS_DIR);
 

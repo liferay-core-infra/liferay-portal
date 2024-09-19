59ac7302c95923f322854ca7605f849af60a5850
Alberto Chaparro <alberto.chaparro@liferay.com>
2024-09-19 17:06:55 +0200

    LPD-36689 Test in the same method

--- modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/report/UpgradeReport.java
+++ modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/report/UpgradeReport.java
@@ -302,7 +302,7 @@ public class UpgradeReport {
 		).put(
 			"document.library",
 			LinkedHashMapBuilder.put(
-				"root.dir",
+				"root.directory",
 				() -> {
 					if (StringUtil.equals(
 							PropsValues.DL_STORE_IMPL,
@@ -334,7 +334,7 @@ public class UpgradeReport {
 					return "Undefined";
 				}
 			).put(
-				"storage.impl", PropsValues.DL_STORE_IMPL
+				"storage.implementation", PropsValues.DL_STORE_IMPL
 			).put(
 				"storage.size",
 				() -> {

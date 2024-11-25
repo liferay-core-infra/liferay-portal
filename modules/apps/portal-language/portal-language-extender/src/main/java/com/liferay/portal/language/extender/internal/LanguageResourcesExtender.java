40828956da34d69050e8c2b7d44ee482910e77fc
Kevin Lee <kevin.lee@liferay.com>
2024-11-24 21:25:59 -0800

    LPD-41335 For now, publish a ResourceBundleLoader service when keys are meant to be private
    This is temporary as we plan on deprecating the ResourceBundleLoader
    approach (i.e. LanguageExtender). The LanguageResources API would need
    to be modified to allow fetching a resource bundle for a specific module
    and would require updating many usages.

--- modules/apps/portal-language/portal-language-extender/src/main/java/com/liferay/portal/language/extender/internal/LanguageResourcesExtender.java
+++ modules/apps/portal-language/portal-language-extender/src/main/java/com/liferay/portal/language/extender/internal/LanguageResourcesExtender.java
@@ -7,16 +7,24 @@ package com.liferay.portal.language.extender.internal;
 
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dependency.manager.DependencyManagerSyncUtil;
 import com.liferay.portal.kernel.language.UTF8Control;
+import com.liferay.portal.kernel.resource.bundle.AggregateResourceBundleLoader;
+import com.liferay.portal.kernel.resource.bundle.CacheResourceBundleLoader;
+import com.liferay.portal.kernel.resource.bundle.ClassResourceBundleLoader;
+import com.liferay.portal.kernel.resource.bundle.ResourceBundleLoader;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.language.LanguageResources;
 
 import java.net.URL;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Locale;
@@ -27,7 +35,9 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleEvent;
 import org.osgi.framework.Constants;
+import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceFactory;
+import org.osgi.framework.ServiceListener;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.framework.wiring.BundleCapability;
 import org.osgi.framework.wiring.BundleWiring;
@@ -66,6 +76,10 @@ public class LanguageResourcesExtender
 
 			if (baseName instanceof String) {
 				if (GetterUtil.getBoolean(attributes.get("module.only"))) {
+					_registerResourceBundleLoader(
+						bundle, bundleCapability, (String)baseName,
+						serviceRegistrations);
+
 					continue;
 				}
 
@@ -106,11 +120,75 @@ public class LanguageResourcesExtender
 			bundleContext, Bundle.ACTIVE, this);
 
 		_bundleTracker.open();
+
+		DependencyManagerSyncUtil.registerSyncCallable(
+			() -> {
+				bundleContext.addServiceListener(
+					_serviceListener,
+					"(&(!(javax.portlet.name=*))(language.id=*)(objectClass=" +
+						ResourceBundle.class.getName() + "))");
+
+				return null;
+			});
 	}
 
 	@Deactivate
 	protected void deactivate() {
+		_bundleContext.removeServiceListener(_serviceListener);
+
 		_bundleTracker.close();
+	}
+
+	private void _registerResourceBundleLoader(
+		Bundle bundle, BundleCapability bundleCapability, String baseName,
+		List<ServiceRegistration<?>> serviceRegistrations) {
+
+		Dictionary<String, Object> attributes = HashMapDictionaryBuilder.create(
+			bundleCapability.getAttributes()
+		).build();
+
+		Object bundleSymbolicName = attributes.get("bundle.symbolic.name");
+		Object serviceRanking = attributes.get(Constants.SERVICE_RANKING);
+		Object servletContextName = attributes.get("servlet.context.name");
+
+		if (bundleSymbolicName == null) {
+			attributes.put("bundle.symbolic.name", bundle.getSymbolicName());
+		}
+
+		attributes.put(
+			Constants.SERVICE_RANKING, GetterUtil.getInteger(serviceRanking));
+
+		if (servletContextName == null) {
+			Dictionary<String, String> headers = bundle.getHeaders(
+				StringPool.BLANK);
+
+			String webContextName = headers.get("Web-ContextName");
+
+			if (Validator.isNotNull(webContextName)) {
+				attributes.put("servlet.context.name", webContextName);
+			}
+			else {
+				String webContextPath = headers.get("Web-ContextPath");
+
+				if (Validator.isNotNull(webContextPath)) {
+					attributes.put(
+						"servlet.context.name", webContextPath.substring(1));
+				}
+			}
+		}
+
+		BundleWiring bundleWiring = bundle.adapt(BundleWiring.class);
+
+		ResourceBundleLoader resourceBundleLoader =
+			new CacheResourceBundleLoader(
+				new AggregateResourceBundleLoader(
+					new ClassResourceBundleLoader(
+						baseName, bundleWiring.getClassLoader()),
+					LanguageResources.PORTAL_RESOURCE_BUNDLE_LOADER));
+
+		serviceRegistrations.add(
+			_bundleContext.registerService(
+				ResourceBundleLoader.class, resourceBundleLoader, attributes));
 	}
 
 	private void _registerResourceBundles(
@@ -194,5 +272,7 @@ public class LanguageResourcesExtender
 
 	private BundleContext _bundleContext;
 	private BundleTracker<?> _bundleTracker;
+	private final ServiceListener _serviceListener =
+		serviceEvent -> CacheResourceBundleLoader.clearCache();
 
 }

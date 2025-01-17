0cb1c29d892aa43693e6131543562abe38b68c16
Kevin Lee <kevin.lee@liferay.com>
2025-01-17 14:15:52 -0800

    LPD-46383 Only the default company creation (default_company.ftl) should be creating a PortalPreferences model with an ownerId of 0

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -8150,7 +8150,7 @@ public class DataFactory {
 		return objectStateTransitionModel;
 	}
 
-	protected PortalPreferencesModel newPortalPreferencesModel(long ownerId) {
+	public PortalPreferencesModel newPortalPreferencesModel(long ownerId) {
 		PortalPreferencesModel portalPreferencesModel =
 			new PortalPreferencesModelImpl();
 

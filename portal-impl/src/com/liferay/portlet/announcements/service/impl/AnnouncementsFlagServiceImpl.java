6c70e7c7c6e2cda5a9956c96ebe33b2241727aae
Kevin Lee <kevin.lee@liferay.com>
2025-02-27 13:29:56 -0800

    LPD-49972 Remove unused AnnouncementsFlagService methods
    # breaking
    
    ## What portal-impl/src/com/liferay/portlet/announcements/service/impl/AnnouncementsFlagServiceImpl.java
    
    Removed the following methods from AnnouncementsFlagService:
    
    - public void addFlag(long entryId, int value) throws PortalException
    - public void deleteFlag(long flagId) throws PortalException
    - public AnnouncementsFlag getFlag(long entryId, int value) throws PortalException
    
    ## Why
    
    They no longer have any usages.
    
    ----

--- portal-impl/src/com/liferay/portlet/announcements/service/impl/AnnouncementsFlagServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/announcements/service/impl/AnnouncementsFlagServiceImpl.java
@@ -5,9 +5,6 @@
 
 package com.liferay.portlet.announcements.service.impl;
 
-import com.liferay.announcements.kernel.model.AnnouncementsFlag;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portlet.announcements.service.base.AnnouncementsFlagServiceBaseImpl;
 
 /**
@@ -16,30 +13,4 @@ import com.liferay.portlet.announcements.service.base.AnnouncementsFlagServiceBa
  */
 public class AnnouncementsFlagServiceImpl
 	extends AnnouncementsFlagServiceBaseImpl {
-
-	@Override
-	public void addFlag(long entryId, int value) throws PortalException {
-		announcementsFlagLocalService.addFlag(getUserId(), entryId, value);
-	}
-
-	@Override
-	public void deleteFlag(long flagId) throws PortalException {
-		AnnouncementsFlag flag = announcementsFlagPersistence.findByPrimaryKey(
-			flagId);
-
-		if (flag.getUserId() != getUserId()) {
-			throw new PrincipalException();
-		}
-
-		announcementsFlagLocalService.deleteFlag(flagId);
-	}
-
-	@Override
-	public AnnouncementsFlag getFlag(long entryId, int value)
-		throws PortalException {
-
-		return announcementsFlagLocalService.getFlag(
-			getUserId(), entryId, value);
-	}
-
 }

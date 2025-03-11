liferay-core-infra/liferay-portal

commit b92411f701d68168eef4bfb4b1e467e24043c549

LPD-48909 Patch away multi-catch usages around ServiceLoader, this breaks spifly's bytecode manipulation and leads to runtime java.lang.VerifyError

modules/third-party/jakarta-ws-rs/src/main/java/javax/ws/rs/ext/FactoryFinder.java
@@ -23,7 +23,6 @@ import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.Iterator;
 import java.util.Properties;
-import java.util.ServiceConfigurationError;
 import java.util.ServiceLoader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -125,7 +124,7 @@ final class FactoryFinder {
             if(iterator.hasNext()) {
                 return iterator.next();
             }
-        } catch (Exception | ServiceConfigurationError ex) {
+        } catch (Exception ex) {
             LOGGER.log(Level.FINER, "Failed to load service " + factoryId + ".", ex);
         }
 
@@ -135,7 +134,7 @@ final class FactoryFinder {
             if(iterator.hasNext()) {
                 return iterator.next();
             }
-        } catch (Exception | ServiceConfigurationError ex) {
+        } catch (Exception ex) {
             LOGGER.log(Level.FINER, "Failed to load service " + factoryId + ".", ex);
         }
 
@@ -185,3 +184,4 @@ final class FactoryFinder {
         return newInstance(fallbackClassName, classLoader);
     }
 }
+/* @generated */

modules/third-party/jakarta-ws-rs/patches/01-LPD-48909.patch
@@ -1,0 +1,36 @@
+diff --git a/javax/ws/rs/ext/FactoryFinder.java b/javax/ws/rs/ext/FactoryFinder.java
+index 88384f6449fa8..c396078e2a80b 100644
+--- a/javax/ws/rs/ext/FactoryFinder.java
++++ b/javax/ws/rs/ext/FactoryFinder.java
+@@ -23,7 +23,6 @@ import java.security.AccessController;
+ import java.security.PrivilegedAction;
+ import java.util.Iterator;
+ import java.util.Properties;
+-import java.util.ServiceConfigurationError;
+ import java.util.ServiceLoader;
+ import java.util.logging.Level;
+ import java.util.logging.Logger;
+@@ -125,7 +124,7 @@ final class FactoryFinder {
+             if(iterator.hasNext()) {
+                 return iterator.next();
+             }
+-        } catch (Exception | ServiceConfigurationError ex) {
++        } catch (Exception ex) {
+             LOGGER.log(Level.FINER, "Failed to load service " + factoryId + ".", ex);
+         }
+ 
+@@ -135,7 +134,7 @@ final class FactoryFinder {
+             if(iterator.hasNext()) {
+                 return iterator.next();
+             }
+-        } catch (Exception | ServiceConfigurationError ex) {
++        } catch (Exception ex) {
+             LOGGER.log(Level.FINER, "Failed to load service " + factoryId + ".", ex);
+         }
+ 
+@@ -185,3 +184,4 @@ final class FactoryFinder {
+         return newInstance(fallbackClassName, classLoader);
+     }
+ }
++/* @generated */
+\ No newline at end of file

commit 647a54c5461bd243027515c0b10e811c86ad1462

LPD-50992 Patch to catch Throwable. The original code catches Exception, in additional with ServiceConfigurationError, which is not an Exception but an Error. The first patch removes the error from the catch list, causing it to be thrown out. This is caused by b92411f701d68168eef4bfb4b1e467e24043c549.

modules/third-party/jakarta-ws-rs/patches/02-LPD-50992.patch
@@ -1,0 +1,22 @@
+diff --git a/javax/ws/rs/ext/FactoryFinder.java b/javax/ws/rs/ext/FactoryFinder.java
+index c396078e2a80b..a8010311c6290 100644
+--- a/javax/ws/rs/ext/FactoryFinder.java
++++ b/javax/ws/rs/ext/FactoryFinder.java
+@@ -124,7 +124,7 @@ final class FactoryFinder {
+             if(iterator.hasNext()) {
+                 return iterator.next();
+             }
+-        } catch (Exception ex) {
++        } catch (Throwable ex) {
+             LOGGER.log(Level.FINER, "Failed to load service " + factoryId + ".", ex);
+         }
+ 
+@@ -134,7 +134,7 @@ final class FactoryFinder {
+             if(iterator.hasNext()) {
+                 return iterator.next();
+             }
+-        } catch (Exception ex) {
++        } catch (Throwable ex) {
+             LOGGER.log(Level.FINER, "Failed to load service " + factoryId + ".", ex);
+         }
+ 

modules/third-party/jakarta-ws-rs/src/main/java/javax/ws/rs/ext/FactoryFinder.java
@@ -124,7 +124,7 @@ final class FactoryFinder {
             if(iterator.hasNext()) {
                 return iterator.next();
             }
-        } catch (Exception ex) {
+        } catch (Throwable ex) {
             LOGGER.log(Level.FINER, "Failed to load service " + factoryId + ".", ex);
         }
 
@@ -134,7 +134,7 @@ final class FactoryFinder {
             if(iterator.hasNext()) {
                 return iterator.next();
             }
-        } catch (Exception ex) {
+        } catch (Throwable ex) {
             LOGGER.log(Level.FINER, "Failed to load service " + factoryId + ".", ex);
         }
 
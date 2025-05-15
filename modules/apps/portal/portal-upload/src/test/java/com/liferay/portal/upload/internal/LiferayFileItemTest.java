46278509c4c348927bfbc6e5221559321afeae7e
Kevin Lee <kevin.lee@liferay.com>
2025-05-15 13:39:19 -0700

    LPD-55484 Just use the file name
    Files.probeContentType() uses the file extension to determine the
    content type. However, because a temporary file, which has the file
    extension '.tmp', is being used, the method is unable to handle the file
    and returns null.

--- modules/apps/portal/portal-upload/src/test/java/com/liferay/portal/upload/internal/LiferayFileItemTest.java
+++ modules/apps/portal/portal-upload/src/test/java/com/liferay/portal/upload/internal/LiferayFileItemTest.java
@@ -20,8 +20,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.Paths;
 
 import java.util.Set;
 
@@ -83,19 +82,12 @@ public class LiferayFileItemTest {
 					InputStream inputStream, String fileName) {
 
 					try {
-						Path path = Files.createTempFile(null, null);
-
-						Files.copy(
-							inputStream, path,
-							StandardCopyOption.REPLACE_EXISTING);
-
-						String contentType = Files.probeContentType(path);
+						String contentType = Files.probeContentType(
+							Paths.get(fileName));
 
 						if (contentType == null) {
 							contentType = ContentTypes.APPLICATION_OCTET_STREAM;
 						}
-
-						Files.delete(path);
 
 						return contentType;
 					}

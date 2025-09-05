d63117ad9a20c5f642768e71378aed4a1de2b437
Tina Tian <tina.tian@liferay.com>
2025-09-05 10:28:13 -0700

    LPD-59097 SF, simplify

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/Sidecar.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/Sidecar.java
@@ -368,7 +368,7 @@ public class Sidecar {
 						"BootstrapChecks",
 					"logger.bootstrapchecks.level=error",
 					"logger.deprecation.name=org.elasticsearch.deprecation",
-					"logger.deprecation.level=error", _getLogProperties(),
+					"logger.deprecation.level=error",
 					ResourceUtil.getResourceAsString(
 						Sidecar.class, "/log4j2.properties")));
 		}
@@ -437,10 +437,6 @@ public class Sidecar {
 		arguments.add("-javaagent:" + agentPath);
 
 		return arguments;
-	}
-
-	private String _getLogProperties() {
-		return StringPool.BLANK;
 	}
 
 	private String _getNodeName() {

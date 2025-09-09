9cd843f14ea505f94a418d2c3d47702d13a442e7
Dante Wang <dante.wang@liferay.com>
2025-09-09 15:09:45 +0800

    LPD-59097 Fix MaxExpansionsTest

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/mappings/MaxExpansionsTest.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/mappings/MaxExpansionsTest.java
@@ -82,7 +82,10 @@ public class MaxExpansionsTest extends BaseMaxExpansionsTestCase {
 						getElasticsearchConfigurationProperties();
 
 				elasticsearchConfigurationProperties.put(
-					"sidecarJVMOptions", "-Xms512m|-Xmx512m");
+					"sidecarJVMOptions",
+					"-Xms512m|-Xmx512m|--add-opens=java.base/java.lang=ALL-" +
+						"UNNAMED|--add-opens=java.base/java.lang.invoke=ALL-" +
+							"UNNAMED");
 
 				setElasticsearchFixture(elasticsearchFixture);
 

3646c6df6515412f42a7e8e828cb243f371dac20
Tina Tian <tina.tian@liferay.com>
2025-09-09 08:53:47 -0700

    LPD-59097 Move sidecar process logic into portal-impl and export it to be used by modules

--- portal-impl/src/com/liferay/portal/search/sidecar/process/StartSidecarProcessCallable.java
+++ portal-impl/src/com/liferay/portal/search/sidecar/process/StartSidecarProcessCallable.java
@@ -1,9 +1,9 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.search.elasticsearch7.internal.sidecar;
+package com.liferay.portal.search.sidecar.process;
 
 import com.liferay.petra.process.ProcessCallable;
 import com.liferay.petra.process.ProcessException;

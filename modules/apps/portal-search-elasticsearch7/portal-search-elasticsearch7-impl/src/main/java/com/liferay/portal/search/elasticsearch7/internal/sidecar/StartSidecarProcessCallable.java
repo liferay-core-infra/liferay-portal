495f4045a528d6961d5b9694762bc64d8f358f58
Tina Tian <tina.tian@liferay.com>
2025-09-12 11:05:24 -0700

    LPD-59097 Move logic in SidecarServerArgs to Sidecar and remove it

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/StartSidecarProcessCallable.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/StartSidecarProcessCallable.java
@@ -20,13 +20,13 @@ import org.elasticsearch.http.HttpServerTransport;
  */
 public class StartSidecarProcessCallable implements ProcessCallable<String> {
 
-	public StartSidecarProcessCallable(SidecarServerArgs sidecarServerArgs) {
-		_sidecarServerArgs = sidecarServerArgs;
+	public StartSidecarProcessCallable(byte[] bytes) {
+		_bytes = bytes;
 	}
 
 	@Override
 	public String call() throws ProcessException {
-		Object nodeObject = ElasticsearchServerUtil.start(_sidecarServerArgs);
+		Object nodeObject = ElasticsearchServerUtil.start(_bytes);
 
 		try {
 			ClassLoader classLoader =
@@ -62,6 +62,6 @@ public class StartSidecarProcessCallable implements ProcessCallable<String> {
 
 	private static final long serialVersionUID = 1L;
 
-	private final SidecarServerArgs _sidecarServerArgs;
+	private final byte[] _bytes;
 
 }

5855039c5519a2ba1c26845de852374b448dca12
Christian Moura <christian.moura@liferay.com>
2025-11-05 19:32:37 -0300

    LPD-65201 set timeouts to validate well know uri

--- modules/apps/oauth-client/oauth-client-persistence-service/src/main/java/com/liferay/oauth/client/persistence/service/impl/OAuthClientEntryLocalServiceImpl.java
+++ modules/apps/oauth-client/oauth-client-persistence-service/src/main/java/com/liferay/oauth/client/persistence/service/impl/OAuthClientEntryLocalServiceImpl.java
@@ -23,6 +23,9 @@ import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ResourceLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
@@ -31,13 +34,11 @@ import com.nimbusds.oauth2.sdk.ResponseType;
 import com.nimbusds.oauth2.sdk.Scope;
 import com.nimbusds.oauth2.sdk.client.ClientInformation;
 import com.nimbusds.oauth2.sdk.client.ClientMetadata;
-import com.nimbusds.oauth2.sdk.http.HTTPRequest;
-import com.nimbusds.oauth2.sdk.http.HTTPResponse;
 import com.nimbusds.oauth2.sdk.util.JSONObjectUtils;
 import com.nimbusds.openid.connect.sdk.rp.OIDCClientInformation;
 
+import java.net.HttpURLConnection;
 import java.net.URI;
-import java.net.URL;
 
 import java.util.List;
 
@@ -331,14 +332,21 @@ public class OAuthClientEntryLocalServiceImpl
 				return;
 			}
 
-			HTTPRequest httpRequest = new HTTPRequest(
-				HTTPRequest.Method.GET, new URL(authServerWellKnownURI));
+			int timeout = GetterUtil.getInteger(
+				PropsUtil.get(Http.class.getName() + ".timeout"));
 
-			HTTPResponse httpResponse = httpRequest.send();
+			Http.Options httpOptions = new Http.Options();
 
-			if (httpResponse.getStatusCode() != HTTPResponse.SC_OK) {
+			httpOptions.setLocation(authServerWellKnownURI);
+			httpOptions.setTimeout(timeout);
+
+			_http.URLtoString(httpOptions);
+
+			Http.Response httpResponse = httpOptions.getResponse();
+
+			if (httpResponse.getResponseCode() != HttpURLConnection.HTTP_OK) {
 				throw new OAuthClientEntryAuthServerWellKnownURIException(
-					httpResponse.getStatusMessage());
+					"Response code: " + httpResponse.getResponseCode());
 			}
 		}
 		catch (Exception exception) {
@@ -516,6 +524,9 @@ public class OAuthClientEntryLocalServiceImpl
 				exception.getMessage(), exception);
 		}
 	}
+
+	@Reference
+	private Http _http;
 
 	@Reference
 	private OAuthClientASLocalMetadataLocalService

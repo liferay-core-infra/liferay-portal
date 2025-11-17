51eb14551d8f77dcb879bd98bf9cce081a6a9195
Debora Buriti <debora.buriti@liferay.com>
2025-11-17 10:15:54 -0300

    LPD-71203 patch XMLReaderUtils class to avoid improper handling of XML Forms by disabling DTD support and external entity resolution, based on apache/tika@a048362

--- modules/third-party/org-apache-tika-core/src/main/java/org/apache/tika/utils/XMLReaderUtils.java
+++ modules/third-party/org-apache-tika-core/src/main/java/org/apache/tika/utils/XMLReaderUtils.java
@@ -327,6 +327,8 @@ public class XMLReaderUtils implements Serializable {
 
         tryToSetStaxProperty(factory, XMLInputFactory.IS_NAMESPACE_AWARE, true);
         tryToSetStaxProperty(factory, XMLInputFactory.IS_VALIDATING, false);
+        tryToSetStaxProperty(factory, XMLInputFactory.SUPPORT_DTD, false);
+        tryToSetStaxProperty(factory, XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
 
         factory.setXMLResolver(IGNORING_STAX_ENTITY_RESOLVER);
         trySetStaxSecurityManager(factory);
@@ -1011,3 +1013,4 @@ public class XMLReaderUtils implements Serializable {
         reader.setErrorHandler(IGNORING_ERROR_HANDLER);
     }
 }
+/* @generated */

9f8c37bf3c22236f4bec36e25dfb5fe875da9d02
Lily.Chi <lily.chi@liferay.com>
2026-02-10 22:22:52 +0800

    LPD-65928 (ExportImportContentProcessorRegistryUtil) Remove the usage of ServiceTracker and unused methods and fields

--- modules/apps/export-import/export-import-api/src/main/java/com/liferay/exportimport/content/processor/ExportImportContentProcessorRegistryUtil.java
+++ modules/apps/export-import/export-import-api/src/main/java/com/liferay/exportimport/content/processor/ExportImportContentProcessorRegistryUtil.java
@@ -8,21 +8,14 @@ package com.liferay.exportimport.content.processor;
 import com.liferay.osgi.service.tracker.collections.map.ServiceReferenceMapper;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
-import com.liferay.osgi.util.ServiceTrackerFactory;
 import com.liferay.osgi.util.StringPlus;
 import com.liferay.portal.kernel.util.ListUtil;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
-import org.osgi.framework.ServiceReference;
-import org.osgi.util.tracker.ServiceTracker;
-import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * @author Gergely Mathe
@@ -67,12 +60,6 @@ public class ExportImportContentProcessorRegistryUtil {
 
 		_bundleContext = bundle.getBundleContext();
 
-		_serviceTracker = ServiceTrackerFactory.open(
-			_bundleContext,
-			(Class<ExportImportContentProcessor<String>>)
-				(Class<?>)ExportImportContentProcessor.class,
-			new ExportImportContentProcessorServiceTrackerCustomizer());
-
 		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
 			_bundleContext,
 			(Class<ExportImportContentProcessor<String>>)
@@ -80,41 +67,11 @@ public class ExportImportContentProcessorRegistryUtil {
 			null, _serviceReferenceMapper);
 	}
 
-	private ExportImportContentProcessor<String>
-		_getExportImportContentProcessor(String className) {
-
-		return _modelClassNameExportImportContentProcessors.get(className);
-	}
-
-	private ExportImportContentProcessor<String>
-		_getExportImportContentProcessorByContentProcessorType(
-			String contentProcessorType) {
-
-		return _contentProcessorTypeExportImportContentProcessors.get(
-			contentProcessorType);
-	}
-
-	private List<ExportImportContentProcessor<String>>
-		_getExportImportContentProcessors() {
-
-		Collection<ExportImportContentProcessor<String>>
-			exportImportContentProcessors =
-				_modelClassNameExportImportContentProcessors.values();
-
-		return ListUtil.fromCollection(exportImportContentProcessors);
-	}
-
 	private static final ExportImportContentProcessorRegistryUtil
 		_exportImportContentProcessorRegistryUtil =
 			new ExportImportContentProcessorRegistryUtil();
 
 	private final BundleContext _bundleContext;
-	private final Map<String, ExportImportContentProcessor<String>>
-		_contentProcessorTypeExportImportContentProcessors =
-			new ConcurrentHashMap<>();
-	private final Map<String, ExportImportContentProcessor<String>>
-		_modelClassNameExportImportContentProcessors =
-			new ConcurrentHashMap<>();
 
 	private final ServiceReferenceMapper
 		<String, ExportImportContentProcessor<String>> _serviceReferenceMapper =
@@ -134,80 +91,7 @@ public class ExportImportContentProcessorRegistryUtil {
 				}
 			};
 
-	private final ServiceTracker
-		<ExportImportContentProcessor<String>,
-		 ExportImportContentProcessor<String>> _serviceTracker;
 	private final ServiceTrackerMap
 		<String, ExportImportContentProcessor<String>> _serviceTrackerMap;
-
-	private class ExportImportContentProcessorServiceTrackerCustomizer
-		implements ServiceTrackerCustomizer
-			<ExportImportContentProcessor<String>,
-			 ExportImportContentProcessor<String>> {
-
-		@Override
-		public ExportImportContentProcessor<String> addingService(
-			ServiceReference<ExportImportContentProcessor<String>>
-				serviceReference) {
-
-			ExportImportContentProcessor<String> exportImportContentProcessor =
-				_bundleContext.getService(serviceReference);
-
-			List<String> contentProcessorTypes = StringPlus.asList(
-				serviceReference.getProperty("content.processor.type"));
-
-			for (String processorType : contentProcessorTypes) {
-				_contentProcessorTypeExportImportContentProcessors.put(
-					processorType, exportImportContentProcessor);
-			}
-
-			List<String> modelClassNames = StringPlus.asList(
-				serviceReference.getProperty("model.class.name"));
-
-			for (String modelClassName : modelClassNames) {
-				_modelClassNameExportImportContentProcessors.put(
-					modelClassName, exportImportContentProcessor);
-			}
-
-			return exportImportContentProcessor;
-		}
-
-		@Override
-		public void modifiedService(
-			ServiceReference<ExportImportContentProcessor<String>>
-				serviceReference,
-			ExportImportContentProcessor<String> exportImportContentProcessor) {
-
-			removedService(serviceReference, exportImportContentProcessor);
-
-			addingService(serviceReference);
-		}
-
-		@Override
-		public void removedService(
-			ServiceReference<ExportImportContentProcessor<String>>
-				serviceReference,
-			ExportImportContentProcessor<String> exportImportContentProcessor) {
-
-			_bundleContext.ungetService(serviceReference);
-
-			List<String> contentProcessorTypes = StringPlus.asList(
-				serviceReference.getProperty("content.processor.type"));
-
-			for (String contentProcessorType : contentProcessorTypes) {
-				_contentProcessorTypeExportImportContentProcessors.remove(
-					contentProcessorType);
-			}
-
-			List<String> modelClassNames = StringPlus.asList(
-				serviceReference.getProperty("model.class.name"));
-
-			for (String modelClassName : modelClassNames) {
-				_modelClassNameExportImportContentProcessors.remove(
-					modelClassName);
-			}
-		}
-
-	}
 
 }

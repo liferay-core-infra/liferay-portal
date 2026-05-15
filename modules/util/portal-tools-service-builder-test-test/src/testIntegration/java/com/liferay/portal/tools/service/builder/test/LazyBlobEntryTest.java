34211e31a9828cb4bf4a50f5eca7ff2bd76e49ba
Tina Tian <tina.tian@liferay.com>
2026-05-15 16:04:28 -0700

    LPD-90667 Merge lazy blob entry tests into LazyBlobEntryTest

--- modules/util/portal-tools-service-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/service/builder/test/LazyBlobEntryTest.java
+++ modules/util/portal-tools-service-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/service/builder/test/LazyBlobEntryTest.java
@@ -6,12 +6,15 @@
 package com.liferay.portal.tools.service.builder.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.io.Deserializer;
+import com.liferay.petra.io.Serializer;
 import com.liferay.portal.kernel.dao.orm.Session;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.test.rule.Inject;
@@ -20,6 +23,8 @@ import com.liferay.portal.test.rule.TransactionalTestRule;
 import com.liferay.portal.tools.service.builder.test.model.LazyBlobEntry;
 import com.liferay.portal.tools.service.builder.test.service.LazyBlobEntryLocalService;
 import com.liferay.portal.tools.service.builder.test.service.persistence.LazyBlobEntryPersistence;
+
+import java.sql.Blob;
 
 import org.hibernate.engine.spi.EntityEntry;
 import org.hibernate.engine.spi.PersistenceContext;
@@ -33,9 +38,10 @@ import org.junit.runner.RunWith;
 
 /**
  * @author Dante Wang
+ * @author Tina Tian
  */
 @RunWith(Arquillian.class)
-public class LazyBlobEntryLoadedStateTest {
+public class LazyBlobEntryTest {
 
 	@ClassRule
 	@Rule
@@ -47,7 +53,7 @@ public class LazyBlobEntryLoadedStateTest {
 				"com.liferay.portal.tools.service.builder.test.service"));
 
 	@Test
-	public void test() throws Exception {
+	public void testLazyBlobLoadedValueIsNull() throws Exception {
 		_group = GroupTestUtil.addGroup();
 
 		LazyBlobEntry addedLazyBlobEntry =
@@ -92,6 +98,64 @@ public class LazyBlobEntryLoadedStateTest {
 
 		_lazyBlobEntryLocalService.deleteLazyBlobEntry(
 			addedLazyBlobEntry.getLazyBlobEntryId());
+	}
+
+	@Test
+	public void testSerialize() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		String blobContent = RandomTestUtil.randomString();
+
+		LazyBlobEntry lazyBlobEntry =
+			_lazyBlobEntryLocalService.addLazyBlobEntry(
+				_group.getGroupId(), blobContent.getBytes(),
+				ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
+
+		_assertLazyBlobEntry(blobContent, lazyBlobEntry);
+
+		Serializer serializer = new Serializer();
+
+		serializer.writeObject(lazyBlobEntry);
+
+		Deserializer deserializer = new Deserializer(serializer.toByteBuffer());
+
+		LazyBlobEntry serializedLazyBlobEntry = deserializer.readObject();
+
+		Assert.assertEquals(lazyBlobEntry, serializedLazyBlobEntry);
+
+		_assertLazyBlobEntry(blobContent, serializedLazyBlobEntry);
+
+		_lazyBlobEntryLocalService.deleteLazyBlobEntry(
+			lazyBlobEntry.getLazyBlobEntryId());
+	}
+
+	private void _assertLazyBlobEntry(
+			String expectedContent, LazyBlobEntry lazyBlobEntry)
+		throws Exception {
+
+		Assert.assertNull(
+			ReflectionTestUtil.getFieldValue(lazyBlobEntry, "_blob1BlobModel"));
+
+		Blob blob1 = lazyBlobEntry.getBlob1();
+
+		Assert.assertEquals(
+			expectedContent,
+			new String(blob1.getBytes(1, (int)blob1.length())));
+
+		Assert.assertNotNull(
+			ReflectionTestUtil.getFieldValue(lazyBlobEntry, "_blob1BlobModel"));
+
+		Assert.assertNull(
+			ReflectionTestUtil.getFieldValue(lazyBlobEntry, "_blob2BlobModel"));
+
+		Blob blob2 = lazyBlobEntry.getBlob2();
+
+		Assert.assertEquals(
+			expectedContent,
+			new String(blob2.getBytes(1, (int)blob2.length())));
+
+		Assert.assertNotNull(
+			ReflectionTestUtil.getFieldValue(lazyBlobEntry, "_blob2BlobModel"));
 	}
 
 	private SessionImplementor _unwrapSession(Session session) {

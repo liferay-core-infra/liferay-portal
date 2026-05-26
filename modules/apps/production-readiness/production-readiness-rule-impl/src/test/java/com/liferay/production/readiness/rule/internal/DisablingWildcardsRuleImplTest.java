7d0eea3cd82a6ca046a975a189d3b2cc96f0bb2a
Lily.Chi <lily.chi@liferay.com>
2026-05-26 17:28:24 +0800

    LPD-87224 Add unit and integration tests for DisablingWildcardsRuleImpl

---
+++ modules/apps/production-readiness/production-readiness-rule-impl/src/test/java/com/liferay/production/readiness/rule/internal/DisablingWildcardsRuleImplTest.java
@@ -0,0 +1,58 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.production.readiness.rule.internal;
+
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+import com.liferay.production.readiness.Result;
+
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Lily Chi
+ */
+public class DisablingWildcardsRuleImplTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testCheckReturnsFail() {
+		Collection<Result> results = _disablingWildcardsRuleImpl.check(0L);
+
+		Assert.assertEquals(results.toString(), 1, results.size());
+
+		Result result = results.iterator(
+		).next();
+
+		Assert.assertEquals(Result.Status.FAIL, result.getStatus());
+		Assert.assertEquals(Result.Severity.HIGH, result.getSeverity());
+		Assert.assertEquals(
+			"search-engine-settings-validation", result.getCategory());
+		Assert.assertEquals(
+			"production-readiness-rule-disable-wildcards-message",
+			result.getMessageKey());
+		Assert.assertNull(result.getCurrentValue());
+		Assert.assertEquals(
+			"action.destructive_requires_name: true",
+			result.getRecommendedValue());
+		Assert.assertEquals(0, result.getMessageParameters().length);
+		Assert.assertEquals(
+			"https://www.elastic.co/docs/api/doc/elasticsearch-serverless" +
+				"/operation/operation-indices-delete",
+			result.getDocsLink());
+	}
+
+	private final DisablingWildcardsRuleImpl _disablingWildcardsRuleImpl =
+		new DisablingWildcardsRuleImpl();
+
+}

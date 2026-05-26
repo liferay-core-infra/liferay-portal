7b173084e72f8b6a65eb6e3b3c88a3c4383d23e3
Lily.Chi <lily.chi@liferay.com>
2026-05-26 19:39:34 +0800

    LPD-87224 Add implementations for ProductionReadinessRule:OpenFileDescriptorLimitRuleImpl

---
+++ modules/apps/production-readiness/production-readiness-rule-impl/src/main/java/com/liferay/production/readiness/rule/internal/OpenFileDescriptorLimitRuleImpl.java
@@ -0,0 +1,45 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.production.readiness.rule.internal;
+
+import com.liferay.production.readiness.ProductionReadinessRule;
+import com.liferay.production.readiness.Result;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Lily Chi
+ */
+@Component(service = ProductionReadinessRule.class)
+public class OpenFileDescriptorLimitRuleImpl
+	implements ProductionReadinessRule {
+
+	@Override
+	public Collection<Result> check(long companyId) {
+		return Collections.singletonList(
+			new Result(
+				Result.Status.FAIL, Result.Severity.HIGH, getCategory(), null,
+				"ulimit -n >= 65536",
+				"production-readiness-rule-open-file-descriptor-limit-message",
+				new Object[0],
+				"https://www.elastic.co/docs/deploy-manage/deploy/self-" +
+					"managed/file-descriptors"));
+	}
+
+	@Override
+	public String getCategory() {
+		return "search-engine-settings-validation";
+	}
+
+	@Override
+	public String getKey() {
+		return "open-file-descriptor-limit";
+	}
+
+}

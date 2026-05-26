a4e1e9240b798c6d10d72c8410257185555ee8c5
Lily.Chi <lily.chi@liferay.com>
2026-05-26 19:00:29 +0800

    LPD-87224 Add implementations for ProductionReadinessRule:JMXConfigurationDisabledRuleImpl

---
+++ modules/apps/production-readiness/production-readiness-rule-impl/src/main/java/com/liferay/production/readiness/rule/internal/JMXConfigurationDisabledRuleImpl.java
@@ -0,0 +1,73 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.production.readiness.rule.internal;
+
+import com.liferay.production.readiness.ProductionReadinessRule;
+import com.liferay.production.readiness.Result;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Lily Chi
+ */
+@Component(service = ProductionReadinessRule.class)
+public class JMXConfigurationDisabledRuleImpl
+	implements ProductionReadinessRule {
+
+	@Override
+	public Collection<Result> check(long companyId) {
+		RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
+
+		List<String> inputArguments = runtimeMXBean.getInputArguments();
+
+		boolean jmxEnabled = false;
+		String enabledArg = null;
+
+		for (String arg : inputArguments) {
+			if (arg.startsWith("-Dcom.sun.management.jmxremote")) {
+				jmxEnabled = true;
+				enabledArg = arg;
+
+				break;
+			}
+		}
+
+		if (jmxEnabled) {
+			return Collections.singletonList(
+				new Result(
+					Result.Status.FAIL, Result.Severity.LOW, getCategory(),
+					"JMX Configuration has been enabled (" + enabledArg + ")",
+					null,
+					"production-readiness-rule-jmx-configuration-disabled-fail",
+					new Object[0], null));
+		}
+
+		return Collections.singletonList(
+			new Result(
+				Result.Status.PASS, Result.Severity.LOW, getCategory(), null,
+				null,
+				"production-readiness-rule-jmx-configuration-disabled-pass",
+				new Object[0], null));
+	}
+
+	@Override
+	public String getCategory() {
+		return "jvm-and-infrastructure-validation";
+	}
+
+	@Override
+	public String getKey() {
+		return "jmx-configuration-disabled";
+	}
+
+}

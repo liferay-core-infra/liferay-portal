db5e58420736d88c50cbc9ea24d0c3896af08ab3
Lily.Chi <lily.chi@liferay.com>
2026-05-26 19:28:43 +0800

    LPD-87224 Add implementations for ProductionReadinessRule:AnalyzerPluginsRuleImpl

---
+++ modules/apps/production-readiness/production-readiness-rule-impl/src/main/java/com/liferay/production/readiness/rule/internal/AnalyzerPluginsRuleImpl.java
@@ -0,0 +1,43 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.production.readiness.rule.internal;
+
+import com.liferay.production.readiness.ProductionReadinessRule;
+import com.liferay.production.readiness.Result;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Lily Chi
+ */
+@Component(service = ProductionReadinessRule.class)
+public class AnalyzerPluginsRuleImpl implements ProductionReadinessRule {
+
+	@Override
+	public Collection<Result> check(long companyId) {
+		return Collections.singletonList(
+			new Result(
+				Result.Status.FAIL, Result.Severity.HIGH, getCategory(), null,
+				"analysis-icu, analysis-kuromoji, analysis-smartcn, " +
+					"analysis-stempel",
+				"production-readiness-rule-analyzer-plugins-message",
+				new Object[0], null));
+	}
+
+	@Override
+	public String getCategory() {
+		return "search-engine-settings-validation";
+	}
+
+	@Override
+	public String getKey() {
+		return "analyzer-plugins";
+	}
+
+}

70847be1cba075c593b41bb1bdaef5ec82395f0a
Lily.Chi <lily.chi@liferay.com>
2026-05-26 19:37:16 +0800

    LPD-87224 Add implementations for ProductionReadinessRule:DisablingSwapRuleImpl

---
+++ modules/apps/production-readiness/production-readiness-rule-impl/src/main/java/com/liferay/production/readiness/rule/internal/DisablingSwapRuleImpl.java
@@ -0,0 +1,43 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.production.readiness.rule.internal;
+
+import com.liferay.production.readiness.ProductionReadinessRule;
+import com.liferay.production.readiness.Result;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Lily Chi
+ */
+@Component(service = ProductionReadinessRule.class)
+public class DisablingSwapRuleImpl implements ProductionReadinessRule {
+
+	@Override
+	public Collection<Result> check(long companyId) {
+		return Collections.singletonList(
+			new Result(
+				Result.Status.FAIL, Result.Severity.HIGH, getCategory(), null,
+				"bootstrap.memory_lock: true and LimitMEMLOCK=infinity",
+				"production-readiness-rule-disable-swap-message", new Object[0],
+				"https://www.elastic.co/docs/deploy-manage/deploy/self-" +
+					"managed/setup-configuration-memory"));
+	}
+
+	@Override
+	public String getCategory() {
+		return "search-engine-settings-validation";
+	}
+
+	@Override
+	public String getKey() {
+		return "disable-swap";
+	}
+
+}

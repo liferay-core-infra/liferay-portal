05d729d709cd43973ea875ca28184b2e9d77579b
Kevin Lee <kevin.lee@liferay.com>
2026-06-05 14:01:38 -0700

    LPD-90472 Add method to restart Tomcat node, prepare for next

--- modules/apps/portal/portal-license-test/src/testIntegration/java/com/liferay/portal/license/test/ClusterLicenseTest.java
+++ modules/apps/portal/portal-license-test/src/testIntegration/java/com/liferay/portal/license/test/ClusterLicenseTest.java
@@ -391,22 +391,11 @@ public class ClusterLicenseTest extends BaseLicenseTestCase {
 	}
 
 	@SafeVarargs
-	private static TomcatNode _startTomcatNode(
-			boolean overloadNodeAutoShutDown,
+	private static void _restartTomcatNode(
+			TomcatNode tomcatNode,
 			TomcatNode.ClusterExecutable<Serializable>...
 				additionalClusterExecutables)
 		throws Exception {
-
-		TomcatCluster.Builder builder = tomcatClusterTestRule.buildTomcatNode();
-
-		TomcatNode tomcatNode = builder.build();
-
-		Files.write(
-			tomcatNode.getPortalExtPropertiesPath(),
-			List.of(
-				"license.cluster.overload.node.auto.shut.down=" +
-					overloadNodeAutoShutDown),
-			StandardOpenOption.APPEND);
 
 		tomcatNode.start(true);
 
@@ -431,6 +420,27 @@ public class ClusterLicenseTest extends BaseLicenseTestCase {
 
 			tomcatNode.syncExecute(clusterExecutable);
 		}
+	}
+
+	@SafeVarargs
+	private static TomcatNode _startTomcatNode(
+			boolean overloadNodeAutoShutDown,
+			TomcatNode.ClusterExecutable<Serializable>...
+				additionalClusterExecutables)
+		throws Exception {
+
+		TomcatCluster.Builder builder = tomcatClusterTestRule.buildTomcatNode();
+
+		TomcatNode tomcatNode = builder.build();
+
+		Files.write(
+			tomcatNode.getPortalExtPropertiesPath(),
+			List.of(
+				"license.cluster.overload.node.auto.shut.down=" +
+					overloadNodeAutoShutDown),
+			StandardOpenOption.APPEND);
+
+		_restartTomcatNode(tomcatNode, additionalClusterExecutables);
 
 		return tomcatNode;
 	}

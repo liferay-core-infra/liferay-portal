cd4ffd4494439ded32f93be10e3f9290c2851022
Jeff Wu <jeffwu0724@gmail.com>
2026-06-10 11:43:56 -0700

    LPD-92188 Migrate StartupJDBCPING into JGroupsClusterChannelTest
    testConnectWithJDBCPing recreates the JGROUPSPING table, starts two TomcatNodes whose cluster link runs over a JDBC_PING channel, injects the test data source into the JDBC_PING protocol, and asserts both nodes discover each other and that a user can authenticate on the second node.
    
    The channel sets initialize_sql="" so JDBC_PING skips its built-in CREATE TABLE: the test owns the JGROUPSPING table, created with Liferay's DB types, which avoids a dialect mismatch and keeps full control of its rows.

---
+++ modules/apps/portal-cache/portal-cache-test/src/testIntegration/java/com/liferay/portal/cache/internal/test/JGroupsClusterChannelTest.java
@@ -0,0 +1,288 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.cache.internal.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.cluster.ClusterExecutor;
+import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
+import com.liferay.portal.kernel.cluster.ClusterNode;
+import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBInspector;
+import com.liferay.portal.kernel.dao.db.DBManagerUtil;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.module.util.SystemBundleUtil;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.rule.TomcatClusterTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.portal.security.auth.session.AuthenticatedSessionManagerUtil;
+import com.liferay.portal.test.cluster.tomcat.TomcatCluster;
+import com.liferay.portal.test.cluster.tomcat.TomcatNode;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.Serializable;
+
+import java.lang.reflect.Constructor;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+
+/**
+ * @author Jiefeng Wu
+ */
+@RunWith(Arquillian.class)
+public class JGroupsClusterChannelTest implements Serializable {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
+
+	@ClassRule
+	public static final TomcatClusterTestRule tomcatClusterTestRule =
+		new TomcatClusterTestRule();
+
+	@Test
+	public void testConnectWithJDBCPing() throws Exception {
+		Path jdbcPingXMLPath = _createJDBCPingXMLPath(
+			"clustering_jdbc_ping.xml");
+
+		_recreateJGroupsPingTable();
+
+		TomcatCluster.Builder builder1 = tomcatClusterTestRule.buildTomcatNode();
+
+		TomcatNode tomcatNode1 = builder1.build();
+
+		Files.write(
+			tomcatNode1.getPortalExtPropertiesPath(),
+			List.of(
+				PropsKeys.CLUSTER_LINK_CHANNEL_PROPERTIES_CONTROL + "=" +
+					jdbcPingXMLPath.toAbsolutePath()),
+			StandardOpenOption.APPEND);
+
+		tomcatNode1.start(true);
+
+		_injectDataSourceIntoJDBCPing(tomcatNode1);
+
+		TomcatCluster.Builder builder2 = tomcatClusterTestRule.buildTomcatNode();
+
+		TomcatNode tomcatNode2 = builder2.build();
+
+		Files.write(
+			tomcatNode2.getPortalExtPropertiesPath(),
+			List.of(
+				PropsKeys.CLUSTER_LINK_CHANNEL_PROPERTIES_CONTROL + "=" +
+					jdbcPingXMLPath.toAbsolutePath()),
+			StandardOpenOption.APPEND);
+
+		tomcatNode2.start(true);
+
+		_injectDataSourceIntoJDBCPing(tomcatNode2);
+
+		_assertJGroupsPingCount(2);
+
+		ClusterNode clusterNode1 = tomcatNode1.syncExecute(
+			ClusterExecutorUtil::getLocalClusterNode);
+
+		Assert.assertNotNull(clusterNode1);
+
+		ClusterNode clusterNode2 = tomcatNode2.syncExecute(
+			ClusterExecutorUtil::getLocalClusterNode);
+
+		Assert.assertNotNull(clusterNode2);
+
+		Assert.assertTrue(
+			tomcatNode1.syncExecute(
+				() -> {
+					List<ClusterNode> clusterNodes =
+						ClusterExecutorUtil.getClusterNodes();
+
+					return clusterNodes.contains(clusterNode2);
+				}));
+
+		Assert.assertTrue(
+			tomcatNode2.syncExecute(
+				() -> {
+					List<ClusterNode> clusterNodes =
+						ClusterExecutorUtil.getClusterNodes();
+
+					return clusterNodes.contains(clusterNode1);
+				}));
+
+		_assertAuthenticatedUserId(tomcatNode2);
+	}
+
+	private void _assertAuthenticatedUserId(TomcatNode tomcatNode)
+		throws Exception {
+
+		User user = TestPropsValues.getUser();
+
+		String emailAddress = user.getEmailAddress();
+
+		Assert.assertEquals(
+			user.getUserId(),
+			(long)tomcatNode.syncExecute(
+				() -> AuthenticatedSessionManagerUtil.getAuthenticatedUserId(
+					new MockHttpServletRequest(), emailAddress,
+					TestPropsValues.USER_PASSWORD, null)));
+	}
+
+	private void _assertJGroupsPingCount(int expectedCount) throws Exception {
+		try (Connection connection = DataAccess.getConnection();
+
+			PreparedStatement preparedStatement = connection.prepareStatement(
+				"select count(*) as countValue from JGROUPSPING");
+
+			ResultSet resultSet = preparedStatement.executeQuery()) {
+
+			Assert.assertTrue(resultSet.next());
+			Assert.assertEquals(expectedCount, resultSet.getInt("countValue"));
+		}
+	}
+
+	private Path _createJDBCPingXMLPath(String fileName) throws Exception {
+		Path jdbcPingXMLPath = Files.createTempFile(
+			"clustering_jdbc_ping_", ".xml");
+
+		try (InputStream inputStream =
+				JGroupsClusterChannelTest.class.getResourceAsStream(
+					"dependencies/" + fileName)) {
+
+			Files.copy(
+				inputStream, jdbcPingXMLPath,
+				StandardCopyOption.REPLACE_EXISTING);
+		}
+
+		File jdbcPingXMLFile = jdbcPingXMLPath.toFile();
+
+		jdbcPingXMLFile.deleteOnExit();
+
+		return jdbcPingXMLPath;
+	}
+
+	private void _injectDataSourceIntoJDBCPing(TomcatNode tomcatNode)
+		throws Exception {
+
+		Assert.assertTrue(
+			tomcatNode.syncExecute(
+				() -> {
+					Class<?> driverClass = Class.forName(
+						PropsUtil.get("jdbc.default.driverClassName"), true,
+						PortalClassLoaderUtil.getClassLoader());
+
+					Constructor<?> constructor = driverClass.getConstructor();
+
+					Driver driver = (Driver)constructor.newInstance();
+
+					Properties properties = new Properties();
+
+					properties.setProperty(
+						"password", PropsUtil.get("jdbc.default.password"));
+					properties.setProperty(
+						"user", PropsUtil.get("jdbc.default.username"));
+
+					String url = PropsUtil.get("jdbc.default.url");
+
+					DataSource dataSource =
+						(DataSource)ProxyUtil.newProxyInstance(
+							DataSource.class.getClassLoader(),
+							new Class<?>[] {DataSource.class},
+							(proxy, method, arguments) -> {
+								if (Objects.equals(
+										method.getName(), "getConnection")) {
+
+									return driver.connect(url, properties);
+								}
+
+								throw new UnsupportedOperationException(
+									method.getName());
+							});
+
+					return SystemBundleUtil.callService(
+						ClusterExecutor.class,
+						clusterExecutor -> {
+							Object clusterChannel =
+								ReflectionTestUtil.getFieldValue(
+									clusterExecutor, "_clusterChannel");
+
+							Object jChannel = ReflectionTestUtil.getFieldValue(
+								clusterChannel, "_jChannel");
+
+							Object protocolStack = ReflectionTestUtil.invoke(
+								jChannel, "getProtocolStack", new Class<?>[0]);
+
+							List<?> protocols = ReflectionTestUtil.invoke(
+								protocolStack, "getProtocols", new Class<?>[0]);
+
+							for (Object protocol : protocols) {
+								Class<?> clazz = protocol.getClass();
+
+								if (!Objects.equals(
+										clazz.getSimpleName(), "JDBC_PING")) {
+
+									continue;
+								}
+
+								ReflectionTestUtil.invoke(
+									protocol, "setDataSource",
+									new Class<?>[] {DataSource.class},
+									dataSource);
+
+								return true;
+							}
+
+							return false;
+						});
+				}));
+	}
+
+	private void _recreateJGroupsPingTable() throws Exception {
+		DB db = DBManagerUtil.getDB();
+
+		try (Connection connection = DataAccess.getConnection()) {
+			DBInspector dbInspector = new DBInspector(connection);
+
+			if (dbInspector.hasTable("JGROUPSPING")) {
+				db.runSQL("drop table JGROUPSPING");
+			}
+		}
+
+		db.runSQL(
+			StringBundler.concat(
+				"create table JGROUPSPING (own_addr VARCHAR(200) not null, ",
+				"cluster_name VARCHAR(200) not null, ping_data SBLOB null, ",
+				"primary key (own_addr, cluster_name))"));
+	}
+
+}

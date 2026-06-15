6d75e7a8af0a09d7d82469f8fdcf2491a09c1245
Tina Tian <tina.tian@liferay.com>
2026-06-15 10:23:15 -0700

    Reapply "LPD-74656 Add test for VirtualHostFilter redirect to home URL"
    This reverts commit bda47cf86b7c341683049725c38bafa62d405dc3.

--- modules/apps/portal/portal-servlet-test/src/testIntegration/java/com/liferay/portal/servlet/filters/virtualhost/test/VirtualHostFilterTest.java
+++ modules/apps/portal/portal-servlet-test/src/testIntegration/java/com/liferay/portal/servlet/filters/virtualhost/test/VirtualHostFilterTest.java
@@ -208,6 +208,35 @@ public class VirtualHostFilterTest {
 	}
 
 	@Test
+	public void testProcessFilterRedirectToHomeURL() throws Exception {
+		try (SafeCloseable safeCloseable =
+				PropsValuesTestUtil.swapWithSafeCloseable(
+					"VIRTUAL_HOSTS_DEFAULT_SITE_NAME", StringPool.BLANK)) {
+
+			MockHttpServletRequest mockHttpServletRequest =
+				_getMockHttpServletRequest(null, StringPool.SLASH);
+
+			MockHttpServletResponse mockHttpServletResponse =
+				new MockHttpServletResponse();
+
+			_virtualHostFilter.init(new MockFilterConfig());
+
+			ReflectionTestUtil.invoke(
+				_virtualHostFilter, "processFilter",
+				new Class<?>[] {
+					HttpServletRequest.class, HttpServletResponse.class,
+					FilterChain.class
+				},
+				mockHttpServletRequest, mockHttpServletResponse,
+				new MockFilterChain());
+
+			Assert.assertEquals(
+				_portalUtil.getHomeURL(mockHttpServletRequest),
+				mockHttpServletResponse.getRedirectedUrl());
+		}
+	}
+
+	@Test
 	public void testProcessFilterSetsGroupOnRequestWhenLayoutSetMatches()
 		throws Exception {
 

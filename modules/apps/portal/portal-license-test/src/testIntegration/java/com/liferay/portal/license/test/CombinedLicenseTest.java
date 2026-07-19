5072f4883472339085787504c268738482ee97c5
Kevin Lee <kevin.lee@liferay.com>
2026-07-19 13:45:07 -0700

    LPD-98408 Add tests to check deploying a portal license and app licenses together

---
+++ modules/apps/portal/portal-license-test/src/testIntegration/java/com/liferay/portal/license/test/CombinedLicenseTest.java
@@ -0,0 +1,141 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.license.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.license.util.App;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Time;
+import com.liferay.portal.util.LicenseUtil;
+
+import java.io.File;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Kevin Lee
+ */
+@RunWith(Arquillian.class)
+public class CombinedLicenseTest extends BaseLicenseTestCase {
+
+	@BeforeClass
+	public static void setUpClass() {
+		_disableKeyValidatorSafeCloseable = disableValidateWithSafeCloseable();
+		_setVersionSafeCloseable = setVersionWithSafeCloseable("2026.Q1.0 LTS");
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		_disableKeyValidatorSafeCloseable.close();
+		_setVersionSafeCloseable.close();
+	}
+
+	@Test
+	public void testAppLicensesWithPortalLicenseEnterprise() throws Exception {
+		_testAppLicensesWithPortalLicense(false);
+	}
+
+	@Test
+	public void testAppLicensesWithPortalLicenseFreeTier() throws Exception {
+		_testAppLicensesWithPortalLicense(true);
+	}
+
+	private File[] _deployCombinedLicense(Collection<String> licenseXMLs)
+		throws Exception {
+
+		registerLicense(
+			StringBundler.concat(
+				"<licenses>", StringUtil.merge(licenseXMLs, StringPool.BLANK),
+				"</licenses>"));
+
+		return new File(
+			LicenseUtil.LICENSE_REPOSITORY_DIR
+		).listFiles(
+			(dirFile, name) -> name.endsWith(".li")
+		);
+	}
+
+	private void _testAppLicensesWithPortalLicense(boolean freeTier)
+		throws Exception {
+
+		Map<App, String[]> appSymbolicNamesMap = new HashMap<>();
+
+		try (SafeCloseable safeCloseable = resetLicenseDataWithSafeCloseble()) {
+			assertLicensePropertiesNotExisted(getPortalProductId());
+
+			for (App app : App.values()) {
+				String[] appSymbolicNames = getAppSymbolicNames(app);
+
+				Assert.assertFalse(
+					Arrays.toString(appSymbolicNames),
+					ArrayUtil.isEmpty(appSymbolicNames));
+
+				assertLicensePropertiesNotExisted(getProductId(app));
+
+				assertBundlesExisted(appSymbolicNames);
+
+				appSymbolicNamesMap.put(app, appSymbolicNames);
+			}
+
+			assertPortalLicenseNotRegistered();
+
+			List<String> licenseXMLs = new ArrayList<>();
+
+			if (freeTier) {
+				licenseXMLs.add(buildFreeTierPortalLicenseXML(Time.HOUR));
+			}
+			else {
+				licenseXMLs.add(buildEnterprisePortalLicenseXML(Time.HOUR));
+			}
+
+			long startTime = System.currentTimeMillis();
+
+			for (App app : App.values()) {
+				licenseXMLs.add(buildAppLicenseXML(app, startTime, Time.HOUR));
+			}
+
+			File[] binaryFiles = _deployCombinedLicense(licenseXMLs);
+
+			assertPortalLicenseRegistered();
+
+			assertLicensePropertiesExisted(getPortalProductId());
+
+			for (App app : App.values()) {
+				assertLicensePropertiesExisted(getProductId(app));
+
+				if (freeTier) {
+					assertBundlesNotExisted(appSymbolicNamesMap.get(app));
+				}
+				else {
+					assertBundlesExisted(appSymbolicNamesMap.get(app));
+				}
+			}
+
+			Assert.assertEquals(
+				Arrays.toString(binaryFiles), App.values().length + 1,
+				binaryFiles.length);
+		}
+	}
+
+	private static SafeCloseable _disableKeyValidatorSafeCloseable;
+	private static SafeCloseable _setVersionSafeCloseable;
+
+}

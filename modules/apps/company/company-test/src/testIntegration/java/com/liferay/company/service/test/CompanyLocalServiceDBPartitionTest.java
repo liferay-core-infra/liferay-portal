4cfaad9e99caeba7e08a2e42ce629e1ef7e33323
IstvanD <istvan.dezsi@liferay.com>
2026-07-30 13:29:23 +0200

    LPD-96721 Test the per partition VirtualHost data

--- modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceDBPartitionTest.java
+++ modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceDBPartitionTest.java
@@ -259,25 +259,6 @@ public class CompanyLocalServiceDBPartitionTest
 	}
 
 	@Test
-	public void testAddCompanyUsesVirtualHostCounter() throws Exception {
-		long counter = _counterLocalService.increment();
-
-		_company1 = CompanyTestUtil.addCompany();
-
-		VirtualHost virtualHost = _virtualHostLocalService.getVirtualHost(
-			_company1.getVirtualHostname());
-
-		Assert.assertEquals(counter + 1, virtualHost.getVirtualHostId());
-
-		_company2 = CompanyTestUtil.addCompany();
-
-		virtualHost = _virtualHostLocalService.getVirtualHost(
-			_company2.getVirtualHostname());
-
-		Assert.assertEquals(counter + 2, virtualHost.getVirtualHostId());
-	}
-
-	@Test
 	public void testAddCompanyWhenCompanyLocalServiceFails() throws Exception {
 		long[] companyIds = CompanyLocalServiceTestUtil.getCompanyIdsBySQL();
 		int dbPartitionsCount = _getDBPartitionsCount();
@@ -500,7 +481,7 @@ public class CompanyLocalServiceDBPartitionTest
 								if (Objects.equals(
 										method.getName(), "getCreateViewSQL") &&
 									StringUtil.equalsIgnoreCase(
-										(String)args[2], "VirtualHost")) {
+										(String)args[2], "ServiceComponent")) {
 
 									throw new Exception();
 								}
@@ -726,9 +707,9 @@ public class CompanyLocalServiceDBPartitionTest
 						new Class<?>[] {DBPartitionDB.class},
 						(proxy, method, args) -> {
 							if (Objects.equals(
-									method.getName(), "getCreateViewSQL") &&
+									method.getName(), "getCreateTableSQL") &&
 								StringUtil.equalsIgnoreCase(
-									(String)args[2], "VirtualHost")) {
+									(String)args[3], "VirtualHost")) {
 
 								throw new Exception();
 							}

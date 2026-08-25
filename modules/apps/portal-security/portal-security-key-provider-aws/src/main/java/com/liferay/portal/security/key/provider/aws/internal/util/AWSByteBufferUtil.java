3d7e2db6170dbd666feef841f4e19f208a666e10
Victor Cunha <victor.cunha@liferay.com>
2026-08-25 16:51:03 -0300

    Revert "LPD-92156 SF"
    This reverts commit 8e082aa57fe1df7ce78eceddab1d2080fb14e511.

--- modules/apps/portal-security/portal-security-key-provider-aws/src/main/java/com/liferay/portal/security/key/provider/aws/internal/util/AWSByteBufferUtil.java
+++ modules/apps/portal-security/portal-security-key-provider-aws/src/main/java/com/liferay/portal/security/key/provider/aws/internal/util/AWSByteBufferUtil.java
@@ -35,8 +35,8 @@ public class AWSByteBufferUtil {
 		}
 		else if (_log.isWarnEnabled()) {
 			_log.warn(
-				"Sensitive bytes remain until the JVM reclaims them because " +
-					"the AWS response buffer is direct or read only");
+				"Unable to zero a direct or read only AWS response buffer so " +
+					"sensitive bytes remain until the JVM reclaims them");
 		}
 
 		return bytes;

3f5baa16f63fa7f20ce4af169a07ddcb0041dc24
Victor Cunha <victor.cunha@liferay.com>
2026-08-25 16:51:00 -0300

    Revert "LPD-103084 SF"
    This reverts commit f5b22dff513e152abbd87ed126c3de2f94fd2329.

--- modules/apps/portal-security/portal-security-key-provider-aws/src/test/java/com/liferay/portal/security/key/provider/aws/internal/util/AWSByteBufferUtilTest.java
+++ modules/apps/portal-security/portal-security-key-provider-aws/src/test/java/com/liferay/portal/security/key/provider/aws/internal/util/AWSByteBufferUtilTest.java
@@ -36,7 +36,6 @@ public class AWSByteBufferUtilTest {
 		byte[] byteBufferBytes = AWSByteBufferUtil.getBytes(byteBuffer);
 
 		Assert.assertArrayEquals(bytes, byteBufferBytes);
-
 		Assert.assertArrayEquals(new byte[bytes.length], clonedBytes);
 	}
 
@@ -52,7 +51,6 @@ public class AWSByteBufferUtilTest {
 			byteBuffer.asReadOnlyBuffer());
 
 		Assert.assertArrayEquals(bytes, byteBufferBytes);
-
 		Assert.assertArrayEquals(bytes, clonedBytes);
 	}
 

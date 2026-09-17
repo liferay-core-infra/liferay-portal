dfc5b29d15e27a388f2df09c4ce4fe6045d336dc
IstvanD <istvan.dezsi@liferay.com>
2026-09-17 14:03:02 +0200

    LPD-96721 buildService

--- portal-kernel/src/com/liferay/portal/kernel/service/VirtualHostLocalServiceUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/VirtualHostLocalServiceUtil.java
@@ -299,6 +299,18 @@ public class VirtualHostLocalServiceUtil {
 			excludedLayoutSetId, virtualHostNames);
 	}
 
+	public static void registerVirtualHost(long companyId, String hostname) {
+		getService().registerVirtualHost(companyId, hostname);
+	}
+
+	public static void unregisterVirtualHost(String hostname) {
+		getService().unregisterVirtualHost(hostname);
+	}
+
+	public static void unregisterVirtualHosts(long companyId) {
+		getService().unregisterVirtualHosts(companyId);
+	}
+
 	/**
 	 * Updates the virtual host in the database or adds it if it does not yet exist. Also notifies the appropriate model listeners.
 	 *
@@ -332,4 +344,4 @@ public class VirtualHostLocalServiceUtil {
 	private static volatile VirtualHostLocalService _service;
 
 }
-// LIFERAY-SERVICE-BUILDER-HASH:-219097511
+// LIFERAY-SERVICE-BUILDER-HASH:776579618
